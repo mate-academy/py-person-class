84b570132662f05c887baabb1bc43b53d6b3857b
Alex Zhidkov <sashazudkov.@gmail.com>
2022-01-19 18:09:15 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    pupil_array = [Person(person["name"], person["age"]) for person in people]
+
+    for persons in people:
+        if persons.get("wife"):
+            Person.people[persons["name"]].wife = \
+                Person.people[persons["wife"]]
+
+        elif persons.get("husband"):
+            Person.people[persons["name"]].husband = \
+                Person.people[persons["husband"]]
+
+    return pupil_array
