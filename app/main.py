499a46fee55d6424464a488985d22c429ee5ba3d
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-01-20 16:01:09 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -12,17 +12,18 @@ def create_person_list(people: list) -> list:
         for key in i:
             Person(i["name"], i["age"])
     for j in people:
-        for key in j:
-            try:
-                if j["wife"] is not None:
-                    Person.people[j["name"]].wife = Person.people[j["wife"]]
-            except KeyError:
-                pass
-            try:
-                if j["husband"] is not None:
-                    Person.people[j["name"]].husband = \
-                        Person.people[j["husband"]]
-            except KeyError:
-                pass
-
-    return [Person.people[i] for i in Person.people]
+        try:
+            if j["wife"] is not None:
+                Person.people[j["name"]].wife = Person.people[j["wife"]]
+        except KeyError:
+            pass
+        try:
+            if j["husband"] is not None:
+                Person.people[j["name"]].husband = \
+                    Person.people[j["husband"]]
+        except KeyError:
+            pass
+    result = []
+    for i in range(len(people)):
+        result.append(Person.people[people[i]["name"]])
+    return result
