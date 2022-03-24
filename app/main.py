3c32e47e8109dce8de96b7674df714bb27bd2410
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-03-24 09:53:23 +0200

    Create in class the ability to store in memory all created instances of the class

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 class Person:
     # write your code here
+    peoples = {}
+
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
+        Person.peoples[name] = self
 
 def create_person_list(peoples: list) -> list:
     return [Person(people["name"], people["age"]) for people in peoples]
@@ -22,4 +25,8 @@ person_list = create_person_list(peoples)
 
 # for person in person_list:
 #     print(f'{person.name} - {person.age} year')
+#
+# person_4 = Person("Sasha", 26)
+# for key, value in person_4.peoples.items():
+#     print(f"{key} - {value.age}")
 
