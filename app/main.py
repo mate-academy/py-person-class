05fb58c640c53e0f5a371d7fce6894fcd9dd2380
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-03-24 11:02:07 +0200

    Solution Person Class . Katiukha

--- app/main.py
+++ app/main.py
@@ -1,32 +1,20 @@
 class Person:
     # write your code here
-    peoples = {}
+    people = {}
 
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
-        Person.peoples[name] = self
+        Person.people[name] = self
+
 
 def create_person_list(peoples: list) -> list:
-    return [Person(people["name"], people["age"]) for people in peoples]
-
-
-
-peoples = [
-    {'name': 'Ross', 'age': 30, 'wife': 'Rachel'},
-    {'name': 'Joey', 'age': 29, 'wife': None},
-    {'name': 'Rachel', 'age': 28, 'husband': 'Ross'}
-]
-
-person_list = create_person_list(peoples)
-
-
-
-
-# for person in person_list:
-#     print(f'{person.name} - {person.age} year')
-#
-# person_4 = Person("Sasha", 26)
-# for key, value in person_4.peoples.items():
-#     print(f"{key} - {value.age}")
-
+    person_list = [Person(person["name"], person["age"]) for person in peoples]
+    for person in peoples:
+        if "wife" in person and person["wife"]:
+            Person.people[person["name"]].wife = Person.people[person["wife"]]
+        elif "husband" in person and \
+                person["husband"]:
+            Person.people[person["name"]].husband = \
+                Person.people[person["husband"]]
+    return person_list
