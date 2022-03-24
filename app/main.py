4206007271241ca92f101eba1724cc96b65e3cfb
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-03-24 09:39:14 +0200

    Create class Person(but only with name, age atributs) and function create_person_list which create list instance Person)

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
 class Person:
     # write your code here
-    pass
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+
+def create_person_list(peoples: list) -> list:
+    return [Person(people["name"], people["age"]) for people in peoples]
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+
+peoples = [
+    {'name': 'Ross', 'age': 30, 'wife': 'Rachel'},
+    {'name': 'Joey', 'age': 29, 'wife': None},
+    {'name': 'Rachel', 'age': 28, 'husband': 'Ross'}
+]
+
+person_list = create_person_list(peoples)
+
+
+
+
+# for person in person_list:
+#     print(f'{person.name} - {person.age} year')
+
