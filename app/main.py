a43f1a08d5484ef4d932eedbb81d52be3d375489
Denis <sairustv228@gmail.com>
2022-05-03 14:34:38 +0300

    class_person v2

--- app/main.py
+++ app/main.py
@@ -8,15 +8,6 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    people = {}
-
-    def __init__(self, name: str, age: int) -> None:
-        self.name = name
-        self.age = age
-        self.people[name] = self
-
-
-def create_person_list(people: list) -> list:
     result = [Person(person['name'], person['age']) for person in people]
     for i, person in enumerate(people):
         if person.get('wife'):
