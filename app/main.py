fc9eebdc4f8119c73011e782b9beeae41148381b
Yurii Kindrat <yurkinkek@gmail.com>
2022-05-18 16:35:11 +0300

    ашÑfixed quotes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 class Person:
     people = {}
 
-    def __init__(self, name, age):
+    def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
         Person.people[self.name] = self
@@ -9,11 +9,11 @@ class Person:
 
 def create_person_list(people: list) -> list:
     for person in people:
-        Person(person['name'], person['age'])
+        Person(person["name"], person["age"])
     for person in people:
-        if 'wife' in person and person['wife'] is not None:
-            Person.people[person['name']].wife = Person.people[person['wife']]
-        if 'husband' in person and person['husband'] is not None:
-            Person.people[person['name']].husband = \
-                Person.people[person['husband']]
+        if "wife" in person and person["wife"] is not None:
+            Person.people[person["name"]].wife = Person.people[person["wife"]]
+        if "husband" in person and person["husband"] is not None:
+            Person.people[person["name"]].husband = \
+                Person.people[person["husband"]]
     return [Person.people[x] for x in Person.people]
