60174a8938a5c34f9276989109778a7aa2c0640e
Taras Nester <tarasnester414@gmail.com>
2022-05-18 15:49:36 +0300

    changed quotes to double

--- app/main.py
+++ app/main.py
@@ -9,19 +9,19 @@ class Person:
 
 def create_person_list(people: list) -> list:
     persons_list = [
-        Person(individual['name'], individual['age'])
+        Person(individual["name"], individual["age"])
         for individual in people
     ]
 
     for person in people:
-        name = person['name']
-        if 'wife' in person and person['wife']:
-            wife_name = person['wife']
+        name = person["name"]
+        if "wife" in person and person["wife"]:
+            wife_name = person["wife"]
             actual_person = Person.people[name]
             wife_person = Person.people[wife_name]
             actual_person.wife = wife_person
-        elif 'husband' in person and person['husband']:
-            husband_name = person['husband']
+        elif "husband" in person and person["husband"]:
+            husband_name = person["husband"]
             actual_person = Person.people[name]
             husband_person = Person.people[husband_name]
             actual_person.husband = husband_person
