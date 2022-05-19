5fa10dd505d1b63ce2c88b35577c363adac81fc1
kamilla-boiko <kamilla.boyko@gmail.com>
2022-05-19 20:27:05 +0300

    Delete unnecessary loop and if-statement

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.__class__.people[name] = self
+        self.__class__.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
@@ -13,16 +13,14 @@ def create_person_list(people: list) -> list:
         for person in people
     ]
 
-    for human in people:
-        for person in person_list:
-            if person.name == human['name']:
-                if 'wife' in human and human['wife'] is not None:
-                    for wife in person_list:
-                        if wife.name == human['wife']:
-                            person.wife = wife
-                if 'husband' in human and human['husband'] is not None:
-                    for husband in person_list:
-                        if husband.name == human['husband']:
-                            person.husband = husband
+    for i, human in enumerate(people):
+        if 'wife' in human and human['wife'] is not None:
+            for wife in person_list:
+                if wife.name == human['wife']:
+                    person_list[i].wife = wife
+        if 'husband' in human and human['husband'] is not None:
+            for husband in person_list:
+                if husband.name == human['husband']:
+                    person_list[i].husband = husband
 
     return person_list
