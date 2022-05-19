10081ae5789b4b5ce8b94d9414f7cb99ddc40c4e
Олег Белый <belochard@gmail.com>
2022-05-19 16:04:41 +0300

    Solution fixed hu_man

--- app/main.py
+++ app/main.py
@@ -12,9 +12,9 @@ def create_person_list(people: list) -> list:
     for hu_man in people:
         friend = Person(hu_man["name"], hu_man["age"])
         friends.append(friend)
-    for pos, hu_man in enumerate(people):
-        if "wife" in hu_man and hu_man["wife"] is not None:
-            friends[pos].wife = Person.people[hu_man["wife"]]
-        if "husband" in hu_man and hu_man["husband"] is not None:
-            friends[pos].husband = Person.people[hu_man["husband"]]
+    for pos, human in enumerate(people):
+        if "wife" in human and human["wife"] is not None:
+            friends[pos].wife = Person.people[human["wife"]]
+        if "husband" in human and human["husband"] is not None:
+            friends[pos].husband = Person.people[human["husband"]]
     return friends
