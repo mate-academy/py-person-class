51531898ee1bded9f974f09c6b12e10467b3db15
ILIAS <nossudak@gmail.com>
2022-05-19 18:41:39 +0300

    Used double quotes everywhere. Removed redundant check

--- app/main.py
+++ app/main.py
@@ -15,10 +15,8 @@ def create_person_list(people: list) -> list:
         people_list.append(Person(name, age))
     for person in people:
         name = person["name"]
-        if person.get('wife') is None and person.get('husband') is None:
-            continue
-        elif person.get('wife') is not None:
+        if person.get("wife") is not None:
             Person.people[name].wife = Person.people[person["wife"]]
-        else:
+        if person.get("husband") is not None:
             Person.people[name].husband = Person.people[person["husband"]]
     return people_list
