46e25bdc406b7b11a6cdbe91073edaf7f8f24cc0
Elena <lenasharabura@gmail.com>
2022-05-19 17:39:47 +0200

    changed quotes

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ class Person:
 
 def create_person_list(people: list) -> list:
     list_of_people = [
-        Person(person['name'], person['age'])
+        Person(person["name"], person["age"])
         for person in people
     ]
 
