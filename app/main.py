5903ae4608f8ab8133bd282001fce598566868bc
Katya Vasylenko <kettyblack20@gmail.com>
2022-05-19 12:58:33 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -11,10 +11,10 @@ class Person:
 def create_person_list(people: list) -> list:
     result = [Person(person["name"], person["age"]) for person in people]
 
-    for i in range(len(result)):
-        if "wife" in people[i] and people[i]["wife"] is not None:
-            result[i].wife = Person.people[people[i]["wife"]]
-        if "husband" in people[i] and people[i]["husband"] is not None:
-            result[i].husband = Person.people[people[i]["husband"]]
+    for person in range(len(people)):
+        if "wife" in people[person] and people[person]["wife"]:
+            result[person].wife = Person.people[people[person]["wife"]]
+        if "husband" in people[person] and people[person]["husband"]:
+            result[person].husband = Person.people[people[person]["husband"]]
 
     return result
