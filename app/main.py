1ff888dc43999b99a1962f36336d38bf9cb929dc
Dima Svirsa <august4smile@gmail.com>
2022-05-19 12:46:45 +0300

    add husbands/wives inside create_person_list function

--- app/main.py
+++ app/main.py
@@ -4,26 +4,16 @@ class Person:
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
-        self.people[self.name] = self
-
-    @classmethod
-    def is_wife(cls, person):
-        if "wife" in person and person["wife"] is not None:
-            if person["wife"] in cls.people:
-                cls.people[person["name"]].wife = cls.people[person["wife"]]
-                cls.people[person["wife"]].husband = cls.people[person["name"]]
-
-    @classmethod
-    def is_husband(cls, pers):
-        if "husband" in pers and pers["husband"] is not None:
-            if pers["husband"] in cls.people:
-                cls.people[pers["name"]].husband = cls.people[pers["husband"]]
-                cls.people[pers["husband"]].wife = cls.people[pers["name"]]
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
+    person_list = [Person(person["name"], person["age"]) for person in people]
+
     for person in people:
-        Person(person["name"], person["age"])
-        Person.is_wife(person)
-        Person.is_husband(person)
-    return [Person.people[name] for name in Person.people]
+        if "wife" in person and person["wife"]:
+            Person.people[person["name"]].wife = Person.people[person["wife"]]
+        if "husband" in person and person["husband"]:
+            Person.people[person["name"]].husband = \
+                Person.people[person["husband"]]
+    return person_list
