8cf988fb75e7d319625a5007ea7804fab18a8f20
Vasyl Hnatiuk <vasylhnatiuk1@gmail.com>
2022-05-24 19:08:33 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,25 +4,25 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.people[name] = self
+
+    def add_person(self):
+        self.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
     new_people = []
     for person_dict in people:
-        person = Person(person_dict['name'], person_dict['age'])
+        person = Person(person_dict["name"], person_dict["age"])
+        person.add_person()
         new_people.append(person)
 
-    for index in range(len(new_people)):
-        if 'wife' in people[index].keys():
-            if people[index]['wife']:
-                name_wife = people[index]['wife']
-                new_people[index].wife = new_people[index].people[name_wife]
+    for i, new_person in enumerate(new_people):
+        if "wife" in people[i].keys() and people[i]["wife"]:
+            name_wife = people[i]["wife"]
+            new_person.wife = new_person.people[name_wife]
 
-        elif 'husband' in people[index].keys():
-            if people[index]['husband']:
-                name_husband = people[index]['husband']
-                new_people[index].husband = \
-                    new_people[index].people[name_husband]
-
+        elif "husband" in people[i].keys() and people[i]["husband"]:
+            name_husband = people[i]["husband"]
+            new_person.husband = \
+                new_person.people[name_husband]
     return new_people
