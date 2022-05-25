04c3dbd46a5b63d46ff19faf846ff4d9b03032ea
Vasyl Hnatiuk <vasylhnatiuk1@gmail.com>
2022-05-25 14:04:26 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,16 +4,13 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-
-    def add_person(self):
-        self.people[self.name] = self
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
     new_people = []
     for person_dict in people:
         person = Person(person_dict["name"], person_dict["age"])
-        person.add_person()
         new_people.append(person)
 
     for i, new_person in enumerate(new_people):
