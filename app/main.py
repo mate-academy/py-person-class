a7886d755f7f22b7813273fd548b51768b2ed4b1
SenchaPencha <SenchaPencha@mail.ru>
2022-06-17 12:41:59 +0300

    Final

--- app/main.py
+++ app/main.py
@@ -8,16 +8,16 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    ls = []
-    for i in people:
-        name = i["name"]
-        age = i["age"]
-        ls.append(Person(name, age))
-    for pos, i in enumerate(people):
-        if "wife" in i:
-            if i["wife"] is not None:
-                ls[pos].wife = Person.people[i["wife"]]
-        if "husband" in i:
-            if i["husband"] is not None:
-                ls[pos].husband = Person.people[i["husband"]]
-    return ls
+    person_list = []
+    for person in people:
+        name = person["name"]
+        age = person["age"]
+        person_list.append(Person(name, age))
+    for position, person in enumerate(people):
+        if "wife" in person:
+            if person["wife"] is not None:
+                person_list[position].wife = Person.people[person["wife"]]
+        if "husband" in person:
+            if person["husband"] is not None:
+                person_list[position].husband = Person.people[person["husband"]]
+    return person_list
