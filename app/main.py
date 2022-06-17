a885cb589d2a44ad7f103cf27c141fedfd7d1f62
SenchaPencha <SenchaPencha@mail.ru>
2022-06-17 12:44:42 +0300

    Final

--- app/main.py
+++ app/main.py
@@ -14,10 +14,11 @@ def create_person_list(people: list) -> list:
         age = person["age"]
         person_list.append(Person(name, age))
     for position, person in enumerate(people):
+        people_dict = Person.people
         if "wife" in person:
             if person["wife"] is not None:
-                person_list[position].wife = Person.people[person["wife"]]
+                person_list[position].wife = people_dict[person["wife"]]
         if "husband" in person:
             if person["husband"] is not None:
-                person_list[position].husband = Person.people[person["husband"]]
+                person_list[position].husband = people_dict[person["husband"]]
     return person_list
