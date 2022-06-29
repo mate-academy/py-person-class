631e3d5871ae95e1260265ae85026f8efde80bf0
MIlan <milan.manoylov@gmail.com>
2022-06-29 10:11:36 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -14,9 +14,11 @@ def create_person_list(people: list) -> list:
     for person in people:
 
         if person.get("wife"):
-            Person.people[person["name"]].wife = Person.people[person["wife"]]
+            Person.people[person["name"]].wife = \
+                Person.people[person["wife"]]
 
         elif person.get("husband"):
-            Person.people[person["name"]].husband = Person.people[person["husband"]]
+            Person.people[person["name"]].husband = \
+                Person.people[person["husband"]]
 
     return people_list
