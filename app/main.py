1bd7d9f1c6566052b1f870504eb17d55a5c142f8
stanislavbublinsky <bublinsky.s.m@gmail.com>
2022-07-02 15:55:16 +0300

    cosmetics

--- app/main.py
+++ app/main.py
@@ -12,10 +12,13 @@ class Person:
 
 def create_person_list(people: list) -> list:
     person_list = [Person(i["name"], i["age"]) for i in people]
+
     for i in range(len(people)):
+
         if list(people[i].values())[2] is not None:
             if list(people[i].keys())[2] == "wife":
                 person_list[i].wife = Person.people[people[i]["wife"]]
             else:
                 person_list[i].husband = Person.people[people[i]["husband"]]
+
     return person_list
