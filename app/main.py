fb8069c2ce875e3f7c3ed31536f6865b39441b49
stanislavbublinsky <bublinsky.s.m@gmail.com>
2022-07-02 13:21:49 +0300

    start

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+
+        Person.people.update({
+            name: self
+        })
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = [Person(i["name"], i["age"]) for i in people]
+    return person_list
+
+
+
