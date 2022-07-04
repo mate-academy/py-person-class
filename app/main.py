091e64fb77068b3dfbe2f6cdfa29e5473c98751c
Anastasiia Shevchenko <anastasiia.sshevchenko@gmail.com>
2022-07-04 18:45:05 +0300

    MY_Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 class Person:
-    # write your code here
-    pass
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
 
 
 def create_person_list(people: list) -> list:
     # write your code here
     pass
+
