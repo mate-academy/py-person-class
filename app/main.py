a4c3faafc864c30befe06e11b5a8891ca5a45c30
Roman <coderc770@gmail.com>
2022-07-06 15:56:35 +0300

    new Solution

--- app/main.py
+++ app/main.py
@@ -8,15 +8,15 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_instances = []
-    for i in people:
-        instance = Person(i['name'], i['age'])
-        person_instances.append(instance)
+    list_instances = []
+    for dict_people in people:
+        instance = Person(dict_people['name'], dict_people['age'])
+        list_instances.append(instance)
     for position, dict in enumerate(people):
         if 'wife' in dict and (dict['wife'] is not None):
-            person_instances[position].wife = Person.people[dict['wife']]
+            list_instances[position].wife = Person.people[dict['wife']]
 
         elif 'husband' in dict and dict['husband'] is not None:
-            person_instances[position].husband = Person.people[dict['husband']]
+            list_instances[position].husband = Person.people[dict['husband']]
 
-    return person_instances
+    return list_instances
