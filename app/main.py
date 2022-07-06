0c88d5392906875fbf1140d7579f71829937cb72
Dmytro Hushchyk <D.hushchyk@gmail.com>
2022-07-06 17:55:27 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -13,8 +13,7 @@ def create_person_list(people: list) -> list:
     info_list = []
 
     for person in people:
-        Person(person["name"], person["age"])
-        info_list.append(Person.people[person["name"]])
+        info_list.append(Person(person["name"], person["age"]))
     for person in people:
         if "wife" in person.keys():
             if person["wife"] is not None:
