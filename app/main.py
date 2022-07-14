e0c06d31f37a49539c348413c1c9b281370294c6
Vlad Chernoborodiuk <45739207+Marsianin007@users.noreply.github.com>
2022-07-14 21:52:38 +0300

    Solution_1

--- app/main.py
+++ app/main.py
@@ -13,22 +13,21 @@ def create_person_list(people: list) -> list:
         name = people_one["name"]
         age = people_one["age"]
         peoples_dict[f"{name}"] = Person(name, age)
+
         if "wife" in list(people_one.keys()):
             wife = people_one["wife"]
             peoples_dict[f"{name}"].wife = wife
-        elif "husband" in list(people_one.keys()):
+        else:
             husband = people_one["husband"]
             peoples_dict[f"{name}"].husband = husband
 
     for key in list(peoples_dict.keys()):
         if hasattr(peoples_dict[key], "wife"):
-            if peoples_dict[key].wife in peoples_dict.keys() \
-                    and peoples_dict[key].wife is not None:
+            if peoples_dict[key].wife in peoples_dict.keys():
                 peoples_dict[key].wife = peoples_dict[peoples_dict[key].wife]
 
         if hasattr(peoples_dict[key], "husband"):
-            if peoples_dict[key].husband in peoples_dict.keys() \
-                    and peoples_dict[key].husband is not None:
+            if peoples_dict[key].husband in peoples_dict.keys():
                 peoples_dict[key].husband = \
                     peoples_dict[peoples_dict[key].husband]
 
@@ -38,4 +37,5 @@ def create_person_list(people: list) -> list:
             delattr(people, 'wife')
         elif hasattr(people, "husband") and people.husband is None:
             delattr(people, 'husband')
+
     return res_list
