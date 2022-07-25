10c01c7c2c94bfeb7d11284f0317b6ba8c4241b4
AnDy <ansmbox@gmail.com>
2022-07-25 15:39:28 +0300

    variable 'item' changed to 'person'

--- app/main.py
+++ app/main.py
@@ -14,17 +14,17 @@ class Person:
 def create_person_list(people: list) -> list:
     person_list = []
 
-    for item in people:
-        person_list.append(Person(item["name"], item["age"]))
+    for person in people:
+        person_list.append(Person(person["name"], person["age"]))
 
-    for item in people:
-        name = item["name"]
+    for person in people:
+        name = person["name"]
 
-        wife = item["wife"] if "wife" in item else None
+        wife = person["wife"] if "wife" in person else None
         if wife:
             Person.people[name].wife = Person.people[wife]
 
-        husband = item["husband"] if "husband" in item else None
+        husband = person["husband"] if "husband" in person else None
         if husband:
             Person.people[name].husband = Person.people[husband]
 
