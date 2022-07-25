199746f10cf76ca26962d5d24737ec6044148c2e
Vlad Chernoborodiuk <45739207+Marsianin007@users.noreply.github.com>
2022-07-25 13:56:07 +0300

    Simple version

--- app/main.py
+++ app/main.py
@@ -12,32 +12,26 @@ def create_person_list(people: list) -> list:
     for people_one in people:
         name = people_one["name"]
         age = people_one["age"]
-        peoples_dict[f"{name}"] = Person(name, age)
+        peoples_dict[name] = Person(name, age)
 
-        try:
-            wife = people_one["wife"]
-            peoples_dict[f"{name}"].wife = wife
-        except Exception:
-            husband = people_one["husband"]
-            peoples_dict[f"{name}"].husband = husband
+    for people_one in people:
+        print(people_one)
 
-    for key in list(peoples_dict.keys()):
-        if hasattr(peoples_dict[key], "wife"):
-            if peoples_dict[key].wife in peoples_dict.keys() \
-                    and peoples_dict[key].wife is not None:
-                peoples_dict[key].wife = peoples_dict[peoples_dict[key].wife]
+        if "wife" in people_one.keys() and people_one["wife"] is not None:
+            peoples_dict[people_one["name"]].wife = people_one["wife"]
+            try:
+                peoples_dict[people_one["name"]].wife = \
+                    peoples_dict[people_one["wife"]]
+            except Exception:
+                pass
 
-        if hasattr(peoples_dict[key], "husband"):
-            if peoples_dict[key].husband in peoples_dict.keys() \
-                    and peoples_dict[key].husband is not None:
-                peoples_dict[key].husband = \
-                    peoples_dict[peoples_dict[key].husband]
+        if "husband" in people_one.keys() and \
+                people_one["husband"] is not None:
+            peoples_dict[people_one["name"]].husband = people_one["husband"]
+            try:
+                peoples_dict[people_one["name"]].husband = \
+                    peoples_dict[people_one["husband"]]
+            except Exception:
+                pass
 
-    res_list = list(peoples_dict.values())
-    for people in res_list:
-        if hasattr(people, "wife") and people.wife is None:
-            delattr(people, 'wife')
-        elif hasattr(people, "husband") and people.husband is None:
-            delattr(people, 'husband')
-
-    return res_list
+    return [res_people for res_people in peoples_dict.values()]
