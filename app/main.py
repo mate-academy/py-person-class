9e3ab4119c696f0bd1ce12b90902cc7c1472384a
Volodymyr Panov <vpanov25@gmail.com>
2022-07-26 19:48:43 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 class Person:
     people = {}
 
-    def __int__(self, name: str, age: int):
+    def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
         Person.people[name] = self
