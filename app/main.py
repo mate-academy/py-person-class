ed8e50e0efb8363ad0a8163146163616ccf9bbd6
Vladimir Byshevets <texnadzor.info@gmail.com>
2022-07-28 09:41:59 +0300

    fixes

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ def create_person_list(people: list) -> list:
         new_list.append(person)
 
     for number, individ in enumerate(people):
-        if 'wife' in individ.keys() and individ["wife"] is not None:
+        if "wife" in individ.keys() and individ["wife"] is not None:
             setattr(new_list[number], "wife", Person.people[individ["wife"]])
         elif "husband" in individ.keys() and individ["husband"] is not None:
             setattr(
