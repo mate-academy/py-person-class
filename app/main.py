d5f8d871445a93419c93f22c14d1838d80f1a945
Katya Vasylieva <katevvasylyeva@gmail.com>
2022-08-05 17:50:55 +0300

    Solution1

--- app/main.py
+++ app/main.py
@@ -17,14 +17,19 @@ class Person:
         for key_person in cls.people:
 
             if hasattr(cls.people[key_person], "husband"):
-                if cls.people[key_person].husband in cls.people:
-                    cls.people[key_person].husband = cls.people[cls.people[key_person].husband]
+                person_husband = cls.people[key_person].husband
+                if person_husband in cls.people:
+                    cls.people[key_person].husband = cls.people[person_husband]
             elif hasattr(cls.people[key_person], "wife"):
-                if cls.people[key_person].wife in cls.people:
-                    cls.people[key_person].wife = cls.people[cls.people[key_person].wife]
+                person_wife = cls.people[key_person].wife
+                if person_wife in cls.people:
+                    cls.people[key_person].wife = cls.people[person_wife]
 
 
 def create_person_list(people: list) -> list:
-    list_of_person_objects = [Person(**person_from_list) for person_from_list in people]
+    list_of_person_objects = [
+        Person(**person_from_list)
+        for person_from_list in people
+    ]
     Person.wife_husband()
     return list_of_person_objects
