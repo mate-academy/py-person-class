352a05b0cd966d3ae47a395e791ef24460ac7b2a
Katya Vasylieva <katevvasylyeva@gmail.com>
2022-08-05 17:34:00 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,30 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name, age, **kwargs):
+        self.name = name
+        self.age = age
+
+        for key, value in kwargs.items():
+            if value is not None:
+                setattr(self, key, value)
+
+        self.__class__.people[self.name] = self
+
+    @classmethod
+    def wife_husband(cls):
+        for key_person in cls.people:
+
+            if hasattr(cls.people[key_person], "husband"):
+                if cls.people[key_person].husband in cls.people:
+                    cls.people[key_person].husband = cls.people[cls.people[key_person].husband]
+            elif hasattr(cls.people[key_person], "wife"):
+                if cls.people[key_person].wife in cls.people:
+                    cls.people[key_person].wife = cls.people[cls.people[key_person].wife]
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    list_of_person_objects = [Person(**person_from_list) for person_from_list in people]
+    Person.wife_husband()
+    return list_of_person_objects
