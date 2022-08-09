b5850ffbf84d430dcd603a1075f1f084e462ae51
Katya Vasylieva <katevvasylyeva@gmail.com>
2022-08-09 18:17:40 +0300

    Solution2

--- app/main.py
+++ app/main.py
@@ -2,34 +2,28 @@ class Person:
 
     people = {}
 
-    def __init__(self, name, age, **kwargs):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-
-        for key, value in kwargs.items():
-            if value is not None:
-                setattr(self, key, value)
-
         self.__class__.people[self.name] = self
-
-    @classmethod
-    def wife_husband(cls):
-        for key_person in cls.people:
-
-            if hasattr(cls.people[key_person], "husband"):
-                person_husband = cls.people[key_person].husband
-                if person_husband in cls.people:
-                    cls.people[key_person].husband = cls.people[person_husband]
-            elif hasattr(cls.people[key_person], "wife"):
-                person_wife = cls.people[key_person].wife
-                if person_wife in cls.people:
-                    cls.people[key_person].wife = cls.people[person_wife]
 
 
 def create_person_list(people: list) -> list:
+
     list_of_person_objects = [
-        Person(**person_from_list)
-        for person_from_list in people
+        Person(person["name"], person["age"])
+        for person in people
     ]
-    Person.wife_husband()
+
+    for ind, person in enumerate(people):
+        if list(person.values())[-1] is not None:
+            spouse_status = list(person.keys())[-1]
+            spouse_name = list(person.values())[-1]
+
+            if spouse_name in list(Person.people.keys()):
+                ind_spouse = list(Person.people.keys()).index(spouse_name)
+                setattr(list_of_person_objects[ind],
+                        spouse_status,
+                        list_of_person_objects[ind_spouse])
+
     return list_of_person_objects
