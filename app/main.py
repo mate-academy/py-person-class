5e457397879eb324671acbc5f4d9b963b33b73fc
Paul Starodub <amorallex@gmail.com>
2022-08-11 13:24:42 +0300

    Solution_fixed

--- app/main.py
+++ app/main.py
@@ -10,10 +10,10 @@ class Person:
 def create_person_list(people: list) -> list:
     people_new = []
     for man in people:
-        people_new.append((Person(man['name'], man['age'])))
+        people_new.append((Person(man["name"], man["age"])))
     for i, man in enumerate(people):
         if "wife" in man and man["wife"] is not None:
             people_new[i].wife = Person.people[man["wife"]]
         if "husband" in man and man["husband"] is not None:
-            people_new[i].husband = Person.people[man['husband']]
+            people_new[i].husband = Person.people[man["husband"]]
     return people_new
