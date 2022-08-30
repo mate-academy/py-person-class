3a6e6cf4581254f7cc75400208ed25fd652116c7
Foggy-Cat <rosky.yaroslav@gmail.com>
2022-08-30 13:44:58 +0300

    Delete F-string

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ def create_person_list(people: list) -> list:
         person_list.append(Person(one_person["name"], one_person["age"]))
     for index, first_person in enumerate(people):
         if "wife" in first_person and first_person["wife"] is not None:
-            person_list[index].wife = Person.people[f"{first_person['wife']}"]
+            person_list[index].wife = Person.people[first_person['wife']]
         if "husband" in first_person and first_person["husband"] is not None:
             person_list[index].husband =\
                 Person.people[f"{first_person['husband']}"]
