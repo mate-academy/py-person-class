bed65151c99422cde4186ea83d9c8d1d551e275f
Vlad Hrytsai <sunrises2929@gmail.com>
2022-09-09 20:07:04 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person = []
+    for key in people:
+        person.append(Person(
+            name=key["name"],
+            age=key["age"],
+        ))
+    for key in people:
+        if "wife" in key and key["wife"] is not None:
+            Person.people[key["name"]].wife = Person.people[key["wife"]]
+        elif "husband" in key and key["husband"] is not None:
+            Person.people[key["name"]].husband = Person.people[key["husband"]]
+    return person
