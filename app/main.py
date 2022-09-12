bcda488bdff08f8dcb99774c954ef4ecae73809c
Vlad Hrytsai <sunrises2929@gmail.com>
2022-09-12 16:42:18 +0300

    Solution4

--- app/main.py
+++ app/main.py
@@ -14,9 +14,10 @@ def create_person_list(people: list) -> list:
             name=info_pers["name"],
             age=info_pers["age"],
         ))
-    for info in people:
-        if "wife" in info_pers and info["wife"] is not None:
-            Person.people[info["name"]].wife = Person.people[info_pers["wife"]]
+    for info_pers in people:
+        if "wife" in info_pers and info_pers["wife"] is not None:
+            Person.people[info_pers["name"]].wife = \
+                Person.people[info_pers["wife"]]
         elif "husband" in info_pers and info_pers["husband"] is not None:
             Person.people[info_pers["name"]].husband = \
                 Person.people[info_pers["husband"]]
