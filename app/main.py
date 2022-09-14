5f47edabcd3e43ea394452c74958fbb5f414a01b
stanislavbublinsky <bublinsky.s.m@gmail.com>
2022-09-14 12:00:58 +0300

    fixed loop variable names

--- app/main.py
+++ app/main.py
@@ -8,13 +8,13 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = [Person(i["name"], i["age"]) for i in people]
+    person_list = [Person(human["name"], human["age"]) for human in people]
 
-    for i in people:
-        if "wife" in i and i["wife"] is not None:
-            Person.people[i["name"]].wife = Person.people[i["wife"]]
+    for human in people:
+        if "wife" in human and human["wife"] is not None:
+            Person.people[human["name"]].wife = Person.people[human["wife"]]
 
-        elif "husband" in i and i["husband"] is not None:
-            Person.people[i["name"]].husband = Person.people[i["husband"]]
+        elif "husband" in human and human["husband"] is not None:
+            Person.people[human["name"]].husband = Person.people[human["husband"]]
 
     return person_list
