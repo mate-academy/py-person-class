dcbba6552c40282cc2c0126d2df1218e1b4ab449
Nailia Mamiedova <nailia.mamiedova.work@gmail.com>
2022-09-15 11:10:55 +0300

    Solution_2

--- app/main.py
+++ app/main.py
@@ -10,11 +10,11 @@ class Person:
 def create_person_list(people: list) -> list:
     person_list = []
     for person in people:
-        person_list.append(Person(person['name'], person['age']))
+        person_list.append(Person(person["name"], person["age"]))
     for i, person in enumerate(people):
-        if 'wife' in dict.keys(person) and person['wife'] is not None:
-            person_list[i].wife = Person.people[person['wife']]
-        if 'husband' in dict.keys(person) and person['husband'] is not None:
-            person_list[i].husband = Person.people[person['husband']]
+        if "wife" in dict.keys(person) and person["wife"] is not None:
+            person_list[i].wife = Person.people[person["wife"]]
+        if "husband" in dict.keys(person) and person["husband"] is not None:
+            person_list[i].husband = Person.people[person["husband"]]
 
     return person_list
