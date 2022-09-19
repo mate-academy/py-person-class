fdb8aa6b84acf0070fabf6e2c01ca2514a4dc8f7
Edik Marushchak <e.moscow138@gmail.com>
2022-09-19 18:19:49 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -16,9 +16,7 @@ def create_person_list(people: list) -> list:
         if person.get("wife"):
             Person.people[person["name"]].wife =\
                 Person.people[person["wife"]]
-
         elif person.get("husband"):
             Person.people[person["name"]].husband =\
                 Person.people[person["husband"]]
-
     return classes_people
