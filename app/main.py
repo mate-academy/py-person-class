4f85e0b5cbe9fc428f3d340d843755450a49b7a9
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-09-19 13:31:56 +0300

    Solution after student's check

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ class Person:
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
-        self.people[name] = self
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
@@ -14,14 +14,12 @@ def create_person_list(people: list) -> list:
         new_person = Person(person["name"], person["age"])
         list_with_people.append(new_person)
 
-    for index, hum in enumerate(people):
-        if "wife" in hum:
-            name = hum["wife"]
-            if name is not None:
-                list_with_people[index].wife = Person.people[name]
-        if "husband" in hum:
-            name = hum["husband"]
-            if name is not None:
-                list_with_people[index].husband = Person.people[name]
+    for index, human in enumerate(people):
+        if human.get("wife") is not None:
+            name = human["wife"]
+            list_with_people[index].wife = Person.people[name]
+        if human.get("husband") is not None:
+            name = human["husband"]
+            list_with_people[index].husband = Person.people[name]
 
     return list_with_people
