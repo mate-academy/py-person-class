d222e31281ca4089c2b6d2675c50ac48d7b83368
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-09-19 18:30:16 +0300

    Change name of list

--- app/main.py
+++ app/main.py
@@ -8,13 +8,13 @@ class Person:
 
 
 def create_person_list(people: list):
-    new_list = []
+    person_list = []
     for person in people:
-        new_list.append(Person(person["name"], person["age"]))
+        person_list.append(Person(person["name"], person["age"]))
     for person in people:
         if person.get("wife"):
             Person.people[person["name"]].wife = Person.people[person["wife"]]
         if person.get("husband"):
             Person.people[person["name"]].husband = \
                 Person.people[person["husband"]]
-    return new_list
+    return person_list
