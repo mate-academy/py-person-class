0757fdebee60afb201a29b04dd0c87cf30f45bfc
Zakhar Podporinov <72568844+ketstap162@users.noreply.github.com>
2022-09-20 18:26:28 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,14 +11,12 @@ def create_person_list(people: list) -> list:
     for human in people:
         Person(human["name"], human["age"])
     for human in people:
-        if "wife" in human:
-            if human["wife"] in Person.people:
-                Person.people[human["name"]].wife = \
-                    Person.people[human["wife"]]
+        if "wife" in human and human["wife"] in Person.people:
+            Person.people[human["name"]].wife = \
+                Person.people[human["wife"]]
 
-        if "husband" in human:
-            if human["husband"] in Person.people:
-                Person.people[human["name"]].husband = \
-                    Person.people[human["husband"]]
+        if "husband" in human and human["husband"] in Person.people:
+            Person.people[human["name"]].husband = \
+                Person.people[human["husband"]]
 
     return list(Person.people.values())
