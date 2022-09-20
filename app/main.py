f3c2f04372b74a6a1475cb142383b5277a06cb2a
igor kosianenko <funkcionalkos@gmail.com>
2022-09-20 22:15:09 +0300

    chnged 'per' -> 'human'

--- app/main.py
+++ app/main.py
@@ -13,13 +13,8 @@ def create_person_list(people: list) -> list:
     person_list = [Person(person["name"], person["age"]) for person in people]
     for human in people:
         if "wife" in human and human["wife"] in Person.people:
-            # husband_wife = Person.people[person["wife"]]
-            # name = Person.people[person["name"]]
-            # husband_wife.husband = name
             Person.people[human["wife"]].husband = Person.people[human["name"]]
         elif "husband" in human and human["husband"] in Person.people:
-            # husband_wife = Person.people[person["husband"]]
-            # husband_wife.wife = name
             Person.people[human["husband"]].wife = Person.people[human["name"]]
 
     return person_list
