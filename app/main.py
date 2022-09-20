0e4afa698cfc730f0d6328d2df5c82aac9b914ee
Mowka7 <98344661+Mowka7@users.noreply.github.com>
2022-09-20 16:29:05 +0100

    Solution (〜￣▽￣)〜

--- app/main.py
+++ app/main.py
@@ -11,15 +11,13 @@ def create_person_list(people: list) -> list:
     list_of_humans = []
 
     for person in people:
-        new_person = Person(person['name'], person['age'])
+        new_person = Person(person["name"], person["age"])
         list_of_humans.append(new_person)
 
     for index, human in enumerate(people):
-        if 'wife' in human:
-            if human['wife'] is not None:
-                list_of_humans[index].wife = Person.people[human['wife']]
-        if 'husband' in human:
-            if human['husband'] is not None:
-                list_of_humans[index].husband = Person.people[human['husband']]
+        if "wife" in human and human["wife"] is not None:
+            list_of_humans[index].wife = Person.people[human["wife"]]
+        if "husband" in human and human["husband"] is not None:
+            list_of_humans[index].husband = Person.people[human["husband"]]
 
     return list_of_humans
