21562402a6e1537c9f1c22f20e68df7998bfa21b
Zakhar Podporinov <72568844+ketstap162@users.noreply.github.com>
2022-09-20 18:34:51 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,12 +11,13 @@ def create_person_list(people: list) -> list:
     for human in people:
         Person(human["name"], human["age"])
     for human in people:
-        if "wife" in human and human["wife"] in Person.people:
-            Person.people[human["name"]].wife = \
-                Person.people[human["wife"]]
+        if "wife" in human and human["wife"] is not None:
+            wife = Person.people[human["wife"]]
+            Person.people[human["name"]].wife = wife
+                
 
-        if "husband" in human and human["husband"] in Person.people:
-            Person.people[human["name"]].husband = \
-                Person.people[human["husband"]]
+        if "husband" in human and human["husband"] is not None:
+            husband = Person.people[human["husband"]]
+            Person.people[human["name"]].husband = husband
 
     return list(Person.people.values())
