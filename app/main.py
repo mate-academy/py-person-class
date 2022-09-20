1b33c89617e7c75b8132b293539715d7921b6b92
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-09-20 20:21:02 +0300

    changed block if and added variable(human)

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 class Person:
     people = {}
 
-    def __init__(self, name: str, age: int):
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[name] = self
@@ -11,14 +11,10 @@ def create_person_list(people: list) -> list:
     for person in people:
         Person(person["name"], person["age"])
     for person in people:
-        if "husband" in person:
-            if person["husband"] in Person.people:
-                Person.people[person["name"]].husband \
-                    = Person.people[person["husband"]]
-
-        if "wife" in person:
-            if person["wife"] in Person.people:
-                Person.people[person["name"]].wife\
-                    = Person.people[person["wife"]]
+        human = Person.people[person["name"]]
+        if "husband" in person and person["husband"] in Person.people:
+            human.husband = Person.people[person["husband"]]
+        elif "wife" in person and person["wife"] in Person.people:
+            human.wife = Person.people[person["wife"]]
 
     return list(Person.people.values())
