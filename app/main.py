3456d0e2fa16970730f4204793c2f1174f96993d
Oleksandr Murynskyi <95870462+Aleksandro777@users.noreply.github.com>
2022-09-20 20:28:57 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,15 +8,15 @@ class Person:
 
 
     def create_person_list(people: list) -> list:
-    person_list = []
-    for human in people:
-        person_list.append(Person(human["name"], human["age"]))
+        person_list = []
+        for human in people:
+            person_list.append(Person(human["name"], human["age"]))
 
-    for human in people:
-        if human.get("wife"):
-            husband = Person.people[human["name"]]
-            husband.wife = Person.people[human["wife"]]
-        elif human.get("husband"):
-            wife = Person.people[human["name"]]
-            wife.husband = Person.people[human["husband"]]
-    return person_list
+        for human in people:
+            if human.get("wife"):
+                husband = Person.people[human["name"]]
+                husband.wife = Person.people[human["wife"]]
+            elif human.get("husband"):
+                wife = Person.people[human["name"]]
+                wife.husband = Person.people[human["husband"]]
+        return person_list
