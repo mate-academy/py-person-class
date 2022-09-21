7666a5358b73caed16ab7c2bc6c7f60a8df3ef8e
Pavlo Kapitanets <113306400+pavlo-kapitanets@users.noreply.github.com>
2022-09-21 22:00:17 +0300

    Update main.py
    Add type annotation to __init__

--- app/main.py
+++ app/main.py
@@ -1,20 +1,18 @@
 class Person:
     people = {}
 
-    def __init__(self, name, age):
+    def __init__(self, name, age) -> None:
         self.name = name
         self.age = age
-        Person.people[self.name] = self
+        self.__class__.people[name] = self
 
 
 def create_person_list(people: list) -> list:
     new_list = [Person(human["name"], human["age"]) for human in people]
-
     for human in people:
+        human_name = Person.people[human["name"]]
         if "wife" in human and human["wife"] is not None:
-            Person.people[human["name"]].wife = Person.people[human["wife"]]
-        elif "husband" in human and \
-                human["husband"] is not None:
-            Person.people[human["name"]].husband \
-                = Person.people[human["husband"]]
+            human_name.wife = Person.people[human["wife"]]
+        elif "husband" in human and human["husband"] is not None:
+            human_name.husband = Person.people[human["husband"]]
     return new_list
