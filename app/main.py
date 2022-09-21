56f168554f07521ec2ab9a52d11e442460f53857
Alexander <alexzubkoff123@gmail.com>
2022-09-21 17:14:10 +0300

    Added some changes against code  duplicating and for a good code styles

--- app/main.py
+++ app/main.py
@@ -10,18 +10,12 @@ class Person:
 def create_person_list(people: list) -> list:
     person_instances_list = []
     for person in people:
-        if "wife" in person.keys() and person["wife"] is not None:
-            pers = Person(person["name"], person["age"])
-            pers.wife = person["wife"]
-            person_instances_list.append(pers)
-        elif "husband" in person.keys() and person["husband"] is not None:
-            pers = Person(person["name"], person["age"])
-            pers.husband = person["husband"]
-            person_instances_list.append(pers)
-        else:
-            pers = Person(person["name"], person["age"])
-            person_instances_list.append(pers)
-
+        person_new = Person(person["name"], person["age"])
+        if "wife" in person and person["wife"] is not None:
+            person_new.wife = person["wife"]
+        if "husband" in person and person["husband"] is not None:
+            person_new.husband = person["husband"]
+        person_instances_list.append(person_new)
     for person in person_instances_list:
         if hasattr(person, "wife"):
             person.wife = Person.people[person.wife]
