b95abb8584436e27f9bd57378c81042f195a6705
Vitalii Bychevyi <bychevoyvitaliy@gmail.com>
2022-09-22 01:44:27 +0300

    Person class first attempt

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 class Person:
-    # write your code here
-    pass
+    people: dict = {}
+
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+    for human in people:
+        person_list.append(Person(human["name"], human["age"]))
+
+    for human in people:
+        if human.get("husband") is not None:
+            Person.people[human["name"]].husband = \
+                Person.people[human["husband"]]
+        if human.get("wife") is not None:
+            Person.people[human["name"]].wife = \
+                Person.people[human["wife"]]
+    return person_list
