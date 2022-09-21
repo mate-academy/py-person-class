38dce9c6eaea530a02d33251d77cbbb24588d0f2
Adrian Kuzhel <adrian.kujel@gmail.com>
2022-09-21 14:53:13 +0300

    Solution fixed: fix flake8

--- app/main.py
+++ app/main.py
@@ -1,23 +1,21 @@
 class Person:
     people = {}
 
-    def __init__(self, name: str, age: int):
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people.update({self.name: self})
 
 
-def create_person_list(peoples: list) -> list:
-    person_list = [
-        Person(name=pers["name"], age=pers["age"])
-        for pers in peoples
-    ]
-    for pers in peoples:
+def create_person_list(people: list) -> list:
+    person_list = [Person(pers["name"], pers["age"]) for pers in people]
+    for pers in people:
         if pers.get("wife"):
-            Person.people[pers["name"]].wife = Person.people[pers["wife"]]
+            husband = Person.people[pers["name"]]
+            husband.wife = Person.people[pers["wife"]]
 
         if pers.get("husband"):
-            Person.people[pers["name"]]\
-                .husband = Person.people[pers["husband"]]
+            wife = Person.people[pers["name"]]
+            wife.husband = Person.people[pers["husband"]]
 
     return person_list
