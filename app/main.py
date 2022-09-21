d9ce8e672bd1a2edfaf2298f74062bf5904f47a5
meloman-13 <vlad.shovik@gmail.com>
2022-09-21 10:56:17 +0200

    Solution #2 - changed variable name

--- app/main.py
+++ app/main.py
@@ -9,9 +9,9 @@ class Person:
 
 def create_person_list(people: list) -> list:
     person_list = [Person(person["name"], person["age"]) for person in people]
-    for ind, person in enumerate(people):
+    for index, person in enumerate(people):
         if person.get("wife") is not None:
-            person_list[ind].wife = Person.people[person["wife"]]
+            person_list[index].wife = Person.people[person["wife"]]
         elif person.get("husband") is not None:
-            person_list[ind].husband = Person.people[person["husband"]]
+            person_list[index].husband = Person.people[person["husband"]]
     return person_list
