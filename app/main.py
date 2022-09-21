98e03c38741b16e5eb70d9dca47df0ee34da6893
Nikyxa <113314502+Nikyxa@users.noreply.github.com>
2022-09-21 19:23:03 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -13,8 +13,9 @@ def create_person_list(people: list) -> list:
     list_people = [Person(person["name"], person["age"]) for person in people]
     husband_name = Person.people[person["name"]].husband
     for person in people:
+        person_name = Person.people[person["name"]]
         if person.get("wife"):
-            Person.people[person["name"]].wife = Person.people[person["wife"]]
+            person_name.wife = Person.people[person["wife"]]
         if person.get("husband"):
-            husband_name = Person.people[person["husband"]]
+             person_name.husband = Person.people[person["husband"]]
     return list_people
