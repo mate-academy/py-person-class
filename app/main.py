1ad280235a94a6bddf4c18c9325959169677058b
Adrian Kuzhel <adrian.kujel@gmail.com>
2022-09-21 14:25:31 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        Person.people.update({self.name: self})
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(peoples: list) -> list:
+    person_list = [Person(name=pers["name"], age=pers["age"]) for pers in peoples]
+    for pers in peoples:
+        if pers.get("wife"):
+            Person.people[pers["name"]].wife = Person.people[pers["wife"]]
+
+        if pers.get("husband"):
+            Person.people[pers["name"]].husband = Person.people[pers["husband"]]
+
+    return person_list
