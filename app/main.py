6f92472b6fe24d20f9b63783175a7a91ac696552
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-09-22 21:33:50 +0300

    Removing line breaks

--- app/main.py
+++ app/main.py
@@ -17,9 +17,17 @@ def create_person_list(people: list) -> list:
         person_list.append(Person(human["name"], human["age"]))
     for human in people:
         if human.get("wife") is not None:
+<<<<<<< HEAD
+            husband = Person.people[human["name"]]
+            husband.wife = Person.people[human["wife"]]
+        elif human.get("husband") is not None:
+            wife = Person.people[human["name"]]
+            wife.husband = Person.people[human["husband"]]
+=======
             Person.people[human["name"]].wife = \
                 Person.people[human["wife"]]
         elif human.get("husband") is not None:
             Person.people[human["name"]].husband = \
                 Person.people[human["husband"]]
+>>>>>>> 451bc70daec1055bf9846a28fbd78e754b6c2cb1
     return person_list
