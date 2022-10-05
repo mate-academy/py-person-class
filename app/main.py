c2d0a685094cbc24d5b8caf69130aa671ee650e0
Iurii Lobunets <iurii@lobunets.com.ua>
2022-10-05 22:23:13 +0300

    Solution of py-person-class

--- app/main.py
+++ app/main.py
@@ -1,8 +1,35 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.set_person(self)
+
+    @classmethod
+    def set_person(cls, other: object) -> None:
+        cls.people[other.name] = other
+
+    @classmethod
+    def get_person(cls, name: str) -> object:
+        return cls.people[name]
+
+    def set_bedfellow(self, bedfellow: str, name: str) -> None:
+        if bedfellow == "wife":
+            self.wife = self.get_person(name)
+        elif bedfellow == "husband":
+            self.husband = self.get_person(name)
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+    for human in people:
+        person_list.append(Person(human["name"], human["age"]))
+    for index in range(len(person_list)):
+        if people[index].get("wife", None) is not None:
+            person_list[index].set_bedfellow(
+                "wife", people[index].get("wife"))
+        if people[index].get("husband", None) is not None:
+            person_list[index].set_bedfellow(
+                "husband", people[index].get("husband"))
+    return person_list
