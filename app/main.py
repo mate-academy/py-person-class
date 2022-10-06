f72c8be3109ede2c25e6972b81aea27dc2df4130
Artem Motrunich <motrunich2013@gmail.com>
2022-10-06 18:40:16 +0200

    pre_solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 class Person:
-    def __init__(self, test):
-        self.test = test # Артём топ питонист
-    return Non
+    people = {}
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        self.__class__.people[name] = self
+
+
+def create_person_list(list_of_dict: list) -> list:
+    list_of_people = []
+    for person in list_of_dict:
+        new_person = Person(person["name"], person["age"])
+        if person.get("wife") is not None:
+            new_person.wife = person["wife"]
+        if person.get("husband") is not None:
+            new_person.husband = person["husband"]
+        list_of_people.append(new_person)
+    for temp in list_of_people:
+        if hasattr(temp, "wife"):
+            if temp.wife in Person.people:
+                temp.wife = Person.people[temp.wife]
+        if hasattr(temp, "husband"):
+            if temp.husband in Person.people:
+                temp.husband = Person.people[temp.husband]
+    return list_of_people
