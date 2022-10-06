17b874f2f6c2c2d2aa95268a073f10cc53aafdef
Artem Motrunich <motrunich2013@gmail.com>
2022-10-06 16:28:27 +0200

    first

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 class Person:
-    # write your code here
-    pass
-
+    def __init__(self, test):
+        self.test = test # Артём топ питонист
+    return Non
 
 def create_person_list(people: list) -> list:
     # write your code here
