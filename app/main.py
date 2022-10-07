bbd0a4a2d92e2a4045578559cf3befc8ce416411
Roman Pisarskiy <darkvader.ukr.net@gmail.com>
2022-10-07 16:36:42 +0300

    Solution with fix

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ def create_person_list(list_of_dict: list) -> list:
         if person.get("husband") is not None:
             new_person.husband = person["husband"]
         list_of_people.append(new_person)
-    for married in list_of_people:  # changed variable 'temp' to 'married'
+    for married in list_of_people:
         if hasattr(married, "wife"):
             if married.wife in Person.people:
                 married.wife = Person.people[married.wife]
