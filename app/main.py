469c9ee21835a36ed766f7cc2fe2c3b777efa40d
Dede <specialized8393@gmail.com>
2022-10-24 20:29:53 +0300

    Done sinfle IF

--- app/main.py
+++ app/main.py
@@ -13,11 +13,10 @@ def create_person_list(people: list) -> list:
         person_list.append(Person(person["name"], person["age"]))
     for i in range(len(people)):
         for key, value in people[i].items():
-            if key == "wife":
-                if value is not None:
-                    Person.people[
-                        people[i]["name"]].wife = Person.people[value]
-                    Person.people[
-                        people[i]["name"]].wife.husband = Person.people[
-                        people[i]["name"]]
+            if key == "wife" and value is not None:
+                Person.people[
+                    people[i]["name"]].wife = Person.people[value]
+                Person.people[
+                    people[i]["name"]].wife.husband = Person.people[
+                    people[i]["name"]]
     return person_list
