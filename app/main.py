465adcad6188554cda782edd9ef3dc0b9cce93c2
Peter Abramov <sebshe3@gmail.com>
2022-10-26 20:48:01 +0300

    'Rework_cycle'

--- app/main.py
+++ app/main.py
@@ -14,14 +14,16 @@ def create_person_list(people: list) -> list:
         person = Person(one_person["name"], one_person["age"])
         person_list.append(person)
 
-    for person in Person.people.values():
-        for this_person in people:
-            if person.name == this_person["name"]:
-                if "husband" in this_person:
-                    if this_person["husband"] is not None:
-                        person.husband = Person.people[this_person["husband"]]
-                elif "wife" in this_person:
-                    if this_person["wife"] is not None:
-                        person.wife = Person.people[this_person["wife"]]
+    for person in people:
+        if "wife" in person:
+            if person["wife"] is not None:
+                Person.people[person["name"]].wife = Person.people[
+                    person["wife"]
+                ]
+        elif "husband" in person:
+            if person["husband"] is not None:
+                Person.people[person["name"]].husband = Person.people[
+                    person["husband"]
+                ]
 
     return person_list
