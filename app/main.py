a8e164918339a1b21ac9d2effe9d6dcdb8c42455
Ihor Ivashov <dm-i@i.ua>
2022-10-27 11:35:18 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,31 @@
+from __future__ import annotations
+from typing import List
+
+
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.add_people(self)
+
+    @classmethod
+    def add_people(cls, person: Person) -> None:
+        cls.people[person.name] = person
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list) -> List[Person]:
+    persons = [Person(person["name"], person["age"]) for person in people]
+    for person in people:
+        wife = person.get("wife")
+        if wife in Person.people:
+            cur_person = Person.people[person["name"]]
+            cur_person.wife = Person.people[wife]
+
+        husband = person.get("husband")
+        if husband in Person.people:
+            cur_person = Person.people[person["name"]]
+            cur_person.husband = Person.people[husband]
+
+    return persons
