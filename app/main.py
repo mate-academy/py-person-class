92969b39b586595eb9f66566d816ee7042f66847
Tatasha <NatashaLeonidovna319@gmail.com>
2022-11-06 12:33:37 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    if len(people) == 0:
+        return []
+    list_of_object = []
+    for dictionary in people:
+        person_new = Person(name=dictionary["name"], age=dictionary["age"])
+        list_of_object.append(person_new)
+    for dictionary in people:
+        if "wife" in dictionary.keys():
+            for i in range(len(list_of_object)):
+                if dictionary["wife"] == list_of_object[i].name:
+                    index = people.index(dictionary)
+                    list_of_object[index].wife = list_of_object[i]
+        if "husband" in dictionary.keys():
+            for i in range(len(list_of_object)):
+                if dictionary["husband"] == list_of_object[i].name:
+                    index = people.index(dictionary)
+                    list_of_object[index].husband = list_of_object[i]
+    return list_of_object
