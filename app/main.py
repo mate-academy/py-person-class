821e76cd4d50c2f1720e08401980f4272177f8e8
Tatasha <NatashaLeonidovna319@gmail.com>
2022-11-08 17:06:15 +0200

    Changes. Use and statement & rename i -> index

--- app/main.py
+++ app/main.py
@@ -8,15 +8,15 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    list_of_object = []
+    objects = []
     for dictionary in people:
         person_new = Person(name=dictionary["name"], age=dictionary["age"])
-        list_of_object.append(person_new)
-    for i in range(len(people)):
-        if "wife" in people[i].keys():
-            if people[i]["wife"] in Person.people:
-                list_of_object[i].wife = Person.people[people[i]["wife"]]
-        if "husband" in people[i].keys():
-            if people[i]["husband"] in Person.people:
-                list_of_object[i].husband = Person.people[people[i]["husband"]]
-    return list_of_object
+        objects.append(person_new)
+    for index in range(len(people)):
+        if "wife" in people[index].keys() and \
+                people[index]["wife"] in Person.people:
+            objects[index].wife = Person.people[people[index]["wife"]]
+        if "husband" in people[index].keys() and \
+                people[index]["husband"] in Person.people:
+            objects[index].husband = Person.people[people[index]["husband"]]
+    return objects
