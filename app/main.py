34ca37dd7ce8cf4c54e7da1c94d5a4558743c050
Andriy <an_mydel@gmail.com>
2022-11-16 12:52:21 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
+from __future__ import annotations
+
+
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
+
+    @classmethod
+    def connect_marriages(cls, person: dict) -> None:
+        if "wife" in person and person["wife"]:
+            cls.people[person["name"]].wife = cls.people[person["wife"]]
+        elif "husband" in person and person["husband"]:
+            cls.people[person["name"]].husband = cls.people[person["husband"]]
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    # iterate through list of people and add each to Person.people
+    [Person(person["name"], person["age"]) for person in people]
+    # iterate through Person.people and find marriage connections
+    [Person.connect_marriages(person) for person in people]
+    return [person for person in Person.people.values()]
