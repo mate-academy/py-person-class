de6ec2a51e8011b13b8ac6d154a65e7190fe1f05
Maksym Maksimov <maksymmaksimov88@gmail.com>
2022-11-17 18:25:06 +0800

    Soluton Maksym Maksimov

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: str) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    res_list = []
+    for person in people:
+        res_list.append(
+            Person(
+                person["name"],
+                person["age"]
+            )
+        )
+    for person in people:
+        if "wife" in person and person["wife"] is not None:
+            Person.people[person["name"]].wife = Person.people[person["wife"]]
+
+        if "husband" in person and person["husband"] is not None:
+            Person.people[person["name"]].husband = \
+                Person.people[person["husband"]]
+    return res_list
