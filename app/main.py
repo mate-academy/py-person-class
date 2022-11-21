73dbfcab2a571646f340e18e4bc69203cc2abcfb
Anastasia Len <anastasiia.len11@gmail.com>
2022-11-21 23:21:25 +0200

    Revert "implemented hw"
    This reverts commit dee0100bead421f4507a7cb41e3dc6ea31fdf46c.

--- app/main.py
+++ app/main.py
@@ -1,31 +1,8 @@
 class Person:
-    people = {}
-
-    def __init__(self,
-                 name: str,
-                 age: int
-                 ) -> None:
-        self.name = name
-        self.age = age
-        Person.people[name] = self
+    # write your code here
+    pass
 
 
 def create_person_list(people: list) -> list:
-    people_list = [
-        Person(
-            person_dict["name"],
-            person_dict["age"]
-        )
-        for person_dict in people
-    ]
-
-    for person_dict in people:
-        if "wife" in person_dict and person_dict["wife"] is not None:
-            Person.people[person_dict["name"]].wife = \
-                Person.people[person_dict["wife"]]
-
-        if "husband" in person_dict and person_dict["husband"] is not None:
-            Person.people[person_dict["name"]].husband = \
-                Person.people[person_dict["husband"]]
-
-    return people_list
+    # write your code here
+    pass
