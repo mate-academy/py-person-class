7177076c9589fc300adf5eb0d66127ced2b65c34
Kostyantyn Chernyk <chernykko@gmail.com>
2022-11-30 14:50:24 +0000

    Solution

--- app/main.py
+++ app/main.py
@@ -9,18 +9,15 @@ class Person:
 
 def create_person_list(people: list) -> list:
     final = []
-    # By adding person to a list "final",
-    # we add Person instances to Person dict attribute "people"
+
     for person in people:
         final.append(Person(person["name"], person["age"]))
 
-    # Iterating over Person dict attribute "people" and
-    # given dict "people" to find respective wife/husband pairs
     for person in people:
         if person.get("wife") is not None:
             Person.people[person["name"]].wife = \
                 Person.people[person["wife"]]
-        elif person.get("husband") is not None:
+        if person.get("husband") is not None:
             Person.people[person["name"]].husband = \
                 Person.people[person["husband"]]
     return final
