28956c412f6682b87e4eec1d56000a39e29df899
Andrii Kozlovskyi <0503424511@ukr.net>
2022-12-06 14:22:39 +0000

    Make_list_comprehension

--- app/main.py
+++ app/main.py
@@ -9,9 +9,8 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    new_list = []
-    for person in people:
-        new_list.append(Person(name=person["name"], age=person["age"]))
+    new_list = [Person(name=person["name"],
+                age=person["age"]) for person in people]
     for person in people:
         if "wife" in person and person["wife"] is not None:
             Person.people[person["name"]].wife = Person.people[person["wife"]]
