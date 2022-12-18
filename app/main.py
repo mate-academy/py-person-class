6e021aef30220d3db588d2493f63ae4553abdbe4
Tymur Semelinskyi <t.semelinsky@gmail.com>
2022-12-18 14:01:13 +0100

    made changes

--- app/main.py
+++ app/main.py
@@ -12,17 +12,16 @@ def create_person_list(people: list) -> list:
     for person in people:
         person_list.append(Person(person["name"], person["age"]))
     for person in people:
-        for key in person:
-            if key == "wife" and person["wife"] is not None:
-                for husband in person_list:
-                    if husband.name == person["name"]:
-                        for wife in person_list:
-                            if wife.name == person["wife"]:
-                                husband.wife = wife
-            if key == "husband" and person["husband"] is not None:
-                for wife in person_list:
-                    if wife.name == person["name"]:
-                        for husband in person_list:
-                            if husband.name == person["husband"]:
-                                wife.husband = husband
+        if "wife" in person:
+            for husband in person_list:
+                if husband.name == person["name"]:
+                    for wife in person_list:
+                        if wife.name == person["wife"]:
+                            husband.wife = wife
+        if "husband" in person:
+            for wife in person_list:
+                if wife.name == person["name"]:
+                    for husband in person_list:
+                        if husband.name == person["husband"]:
+                            wife.husband = husband
     return person_list
