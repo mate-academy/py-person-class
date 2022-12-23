25a7148b87a01edf4669ee25c1adeefe972e8404
Mykola Kokoiev <veokoknik@gmail.com>
2022-12-23 09:58:31 +0100

    My_Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+
+    for human in people:
+        person_list.append(Person(human["name"], human["age"]))
+
+    for i, human in enumerate(people):
+
+        if "wife" in human:
+            for pair_person in person_list:
+                if human["wife"] == pair_person.name:
+                    person_list[i].wife = pair_person
+
+        elif "husband" in human:
+            for pair_person in person_list:
+                if human["husband"] == pair_person.name:
+                    person_list[i].husband = pair_person
+
+    return person_list
