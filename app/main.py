0150176c0cab74e06a68c579bb81fa58611e3114
Dmytro Martyshevskiy <codingeo@gmail.com>
2023-01-06 20:48:13 +0200

    head-on solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    for raw_person in people:
+        person = Person(raw_person["name"], raw_person["age"])
+        partner = list(raw_person.keys())[2]
+        if raw_person[partner]:
+            setattr(person, partner, raw_person[partner])
+            setattr(person, "partner", partner)
+    for person in Person.people.values():
+        if hasattr(person, "partner"):
+            setattr(person, person.partner,
+                    Person.people[getattr(person, person.partner)])
+            del person.partner
+    return list(Person.people.values())
