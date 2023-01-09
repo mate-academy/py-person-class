0932e2f7ef4127c57d40032c00ad0adfcd86ecef
yura.vataschuk <yura.vataschuk@gmail.com>
2023-01-09 11:12:51 +0200

    Requested changes are implemented

--- app/main.py
+++ app/main.py
@@ -4,20 +4,20 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.age = age
         self.name = name
-        Person.people[f"{self.name}"] = self
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    result_list = []
+    people_list = []
     for person in people:
-        result_list.append(Person(name=person["name"], age=person["age"]))
+        people_list.append(Person(name=person["name"], age=person["age"]))
     for person in people:
-        if "wife" in person.keys():
+        if "wife" in person:
             if person["wife"] is not None:
                 person_wife = Person.people[person["wife"]]
                 Person.people[person["name"]].wife = person_wife
-        if "husband" in person.keys():
+        if "husband" in person:
             if person["husband"] is not None:
                 person_husband = Person.people[person["husband"]]
                 Person.people[person["name"]].husband = person_husband
-    return result_list
+    return people_list
