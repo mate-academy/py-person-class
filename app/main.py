c42a8d7cc221bd03f909f84ca94739224a0b1083
Vitalii <fahivchik1@gmail.com>
2023-01-10 13:03:17 +0200

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -4,26 +4,25 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.__class__.people[self.name] = self
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    new_list = list()
+    person_list = list()
     for person in people:
         if None in person.values():
-            new_list.append(Person(person["name"], person["age"]))
+            person_list.append(Person(person["name"], person["age"]))
         else:
-            keys = list(person.keys())
             new_person = Person(person["name"], person["age"])
-            if keys[2] == "wife":
+            if "wife" in person:
                 new_person.wife = person["wife"]
             else:
                 new_person.husband = person["husband"]
-            new_list.append(new_person)
+            person_list.append(new_person)
 
-    for person in new_list:
+    for person in person_list:
         if "wife" in dir(person):
             person.wife = Person.people[person.wife]
         elif "husband" in dir(person):
             person.husband = Person.people[person.husband]
-    return new_list
+    return person_list
