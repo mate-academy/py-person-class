6ddb0ff8b3829a2088e7a5ca00abb4e9a460fc0d
Andrew <6594071@gmail.com>
2023-01-13 15:52:45 +0200

    Solution_3

--- app/main.py
+++ app/main.py
@@ -12,9 +12,11 @@ def create_person_list(people: list) -> list:
     for person in people:
         person_list.append(Person(person["name"], person["age"]))
 
-    for index, person in enumerate(people):
+    for person in people:
         if "husband" in person and person["husband"]:
-            person_list[index].husband = Person.people[person["husband"]]
+            Person.people[person["name"]].husband = \
+                Person.people[person["husband"]]
         elif "wife" in person and person["wife"]:
-            person_list[index].wife = Person.people[person["wife"]]
+            Person.people[person["name"]].wife = \
+                Person.people[person["wife"]]
     return person_list
