9be4fd4f1fe9df40d9dd6686a334c9ea53b3debb
Zoriana Dvulit <zdvulit@ukr.net>
2023-01-19 14:18:05 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,55 @@
+from __future__ import annotations
+
+
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.people[self.name] = self
+
+    @property
+    def wife(self) -> Person:
+        if self._wife is None:
+            return None
+        return self.people[self._wife]
+
+    @wife.setter
+    def wife(self, name: str) -> None:
+        self._wife = name
+
+    @property
+    def husband(self) -> Person:
+        if self._husband is None:
+            return None
+        return self.people[self._husband]
+
+    @husband.setter
+    def husband(self, name: str) -> None:
+        self._husband = name
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    humans = []
+    for dictionary in people:
+        name = dictionary["name"]
+        age = dictionary["age"]
+        human = Person(name, age)
+        if "wife" in dictionary:
+            if dictionary["wife"] is not None:
+                human.wife = dictionary["wife"]
+        else:
+            if dictionary["husband"] is not None:
+                human.husband = dictionary["husband"]
+
+        humans.append(human)
+    return humans
+
+
+people = [
+    {"name": "Ross", "age": 30, "wife": "Rachel"},
+    {"name": "Joey", "age": 29, "wife": None},
+    {"name": "Rachel", "age": 28, "husband": "Ross"}
+]
