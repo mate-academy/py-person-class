071f40fbffd80c01c93874d7c2e30d37b16e1e70
Zoriana Dvulit <zdvulit@ukr.net>
2023-01-22 10:04:43 +0200

    New Solution

--- app/main.py
+++ app/main.py
@@ -13,16 +13,16 @@ class Person:
 
 def create_person_list(persons: list) -> list:
     humans = []
-    for dictionary in persons:
-        name = dictionary["name"]
-        age = dictionary["age"]
+    for person in persons:
+        name = person["name"]
+        age = person["age"]
         human = Person(name, age)
         humans.append(human)
-    for dictionary, human in zip(persons, humans):
-        if "wife" in dictionary:
-            if dictionary["wife"] is not None:
-                human.wife = Person.people[dictionary["wife"]]
+    for person, human in zip(persons, humans):
+        if "wife" in person:
+            if person["wife"] is not None:
+                human.wife = Person.people[person["wife"]]
         else:
-            if dictionary["husband"] is not None:
-                human.husband = Person.people[dictionary["husband"]]
+            if person["husband"] is not None:
+                human.husband = Person.people[person["husband"]]
     return humans
