2277944bef9737cb449fb4a4239dca04d223af74
Roman28101 <r.kovalenko28101@gmail.com>
2023-01-25 21:22:44 +0800

    Solution second fix

--- app/main.py
+++ app/main.py
@@ -12,11 +12,11 @@ def create_person_list(people: list[dict]) -> list:
     for person in people:
         person_list.append(Person(person["name"], person["age"]))
     for person in people:
-        if "wife" in person and person.get("wife") is not None:
+        if person.get("wife") is not None:
             Person.people[person["name"]].wife = (
                 Person.people[person["wife"]]
             )
-        if "husband" in person and person.get("husband") is not None:
+        if person.get("husband") is not None:
             Person.people[person["name"]].husband = (
                 Person.people[person["husband"]]
             )
