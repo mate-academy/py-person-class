cd51cc7b6feb42241f15167698496d74f27ff58b
Iryna Ishchenko <irynaira@gmail.com>
2023-01-25 17:16:31 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,45 +1,33 @@
 class Person:
 
-    people = {}
+    people: dict[str] = {}
 
-    def __init__(
-            self,
-            name: str,
-            age: int
-    ) -> None:
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.people.update({self.name: self})
+        self.people[name] = self
 
-    def add_wife(self, wife: object) -> None:
-        if wife:
-            self.wife = wife
+    def add_wife(self, wife: str) -> None:
+        if wife is not None:
+            self.wife = self.people[wife]
 
-    def add_husband(self, husband: object) -> None:
-        if husband:
-            self.husband = husband
+    def add_husband(self, husband: str) -> None:
+        if husband is not None:
+            self.husband = self.people[husband]
 
 
-def create_person_list(people1: list) -> list:
-    person_list1 = []
-    for person in people1:
-        one_person = Person(person["name"], person["age"])
-        one_person.add_wife(person.get("wife", None))
-        one_person.add_husband(person.get("husband", None))
-        person_list1.append(one_person)
-    for person in person_list1:
-        try:
-            if person.wife:
-                for object1 in person_list1:
-                    if object1.name == person.wife:
-                        person.wife = object1
-        except AttributeError:
-            pass
-        try:
-            if person.husband:
-                for object1 in person_list1:
-                    if object1.name == person.husband:
-                        person.husband = object1
-        except AttributeError:
-            pass
-    return person_list1
+def create_person_list(
+        people: list[dict[str, str | int | None]]
+) -> list[Person]:
+    person_list = [
+        Person(person["name"], person["age"])
+        for person in people
+    ]
+
+    for person, properties in zip(person_list, people):
+        if ("wife" in properties) and (properties["wife"] is not None):
+            person.add_wife(properties["wife"])
+        elif ("husband" in properties) and (properties["husband"] is not None):
+            person.add_husband(properties["husband"])
+
+    return person_list
