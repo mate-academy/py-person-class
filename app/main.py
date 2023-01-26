39e64fa568e029cfd862814a9512d0d4b8cdf920
Iryna Ishchenko <irynaira@gmail.com>
2023-01-26 16:07:31 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,17 +1,21 @@
+from __future__ import annotations
+from typing import Optional
+
+
 class Person:
 
-    people: dict[str] = {}
+    people: dict[str, Person] = {}
 
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         self.people[name] = self
 
-    def add_wife(self, wife: str) -> None:
+    def add_wife(self, wife: Optional[str]) -> None:
         if wife is not None:
             self.wife = self.people[wife]
 
-    def add_husband(self, husband: str) -> None:
+    def add_husband(self, husband: Optional[str]) -> None:
         if husband is not None:
             self.husband = self.people[husband]
 
@@ -25,9 +29,9 @@ def create_person_list(
     ]
 
     for person, properties in zip(person_list, people):
-        if ("wife" in properties) and (properties["wife"] is not None):
+        if properties.get("wife") is not None:
             person.add_wife(properties["wife"])
-        elif ("husband" in properties) and (properties["husband"] is not None):
+        elif properties.get("husband") is not None:
             person.add_husband(properties["husband"])
 
     return person_list
