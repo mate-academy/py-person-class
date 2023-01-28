210b086c45c42ec38465e3d3e6ce068e98828cee
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-01-28 14:02:43 +0200

    Changed Person.people in __init__ & create variable in func

--- app/main.py
+++ app/main.py
@@ -4,20 +4,21 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people.update({self.name: self})
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    person_list = [Person(
-        name=person["name"],
-        age=person["age"]
-    ) for person in people]
+    person_list = [
+        Person(
+            name=person["name"],
+            age=person["age"]
+        )
+        for person in people]
 
     for person in people:
+        per_obj = Person.people[person["name"]]
         if person.get("wife") is not None:
-            Person.people[person["name"]].wife = \
-                Person.people[person["wife"]]
+            per_obj.wife = Person.people[person["wife"]]
         if person.get("husband") is not None:
-            Person.people[person["name"]].husband = \
-                Person.people[person["husband"]]
+            per_obj.husband = Person.people[person["husband"]]
     return person_list
