21c2ecd940f064faa89274ac48e919db99b6025d
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-01-28 19:21:16 +0200

    Fixed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 class Person:
 
-    people: [dict[str]] = {}
+    people = {}
 
     def __init__(self, name: str, age: str) -> None:
         self.name = name
@@ -9,18 +9,18 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    result_list = [
+    person_list = [
         Person(name=person["name"], age=person["age"])
         for person in people
     ]
 
     for person in people:
-        if "wife" in person and person["wife"] is not None:
-            Person.people[person["name"]].wife = \
-                Person.people[person["wife"]]
+        if person.get("wife", None):
+            wife = Person.people[person["wife"]]
+            Person.people[person["name"]].wife = wife
 
-        if "husband" in person and person["husband"] is not None:
-            Person.people[person["name"]].husband = \
-                Person.people[person["husband"]]
+        if person.get("husband", None):
+            husband = Person.people[person["husband"]]
+            Person.people[person["name"]].husband = husband
 
-    return result_list
+    return person_list
