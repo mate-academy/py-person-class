7056a1d93df60ef5c24431256aee10840eb75f8b
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-01-28 05:44:56 +0200

    Added person to the people dict inside __init__ & change names

--- app/main.py
+++ app/main.py
@@ -4,15 +4,20 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
+        Person.people.update({self.name: self})
 
 
 def create_person_list(people: list) -> list:
-    person_list = [Person(name=i["name"], age=i["age"]) for i in people]
-    Person.people = {exemplar.name: exemplar for exemplar in person_list}
+    person_list = [Person(
+        name=person["name"],
+        age=person["age"]
+    ) for person in people]
 
-    for man in people:
-        if man.get("wife") is not None:
-            Person.people[man["name"]].wife = Person.people[man["wife"]]
-        if man.get("husband") is not None:
-            Person.people[man["name"]].husband = Person.people[man["husband"]]
+    for person in people:
+        if person.get("wife") is not None:
+            Person.people[person["name"]].wife = \
+                Person.people[person["wife"]]
+        if person.get("husband") is not None:
+            Person.people[person["name"]].husband = \
+                Person.people[person["husband"]]
     return person_list
