1df53d22530ccd9d78af9c763247a88ee22df087
Daria Zayka <dariazayka@MacBook-Air-Daria.local>
2023-02-02 20:51:01 +0200

    Add solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
+from __future__ import annotations
+
+
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    people_list = [
+        Person(person["name"], person["age"]) for person in people
+    ]
+
+    for p in people:
+
+        if "wife" in p and p["wife"] is not None:
+            Person.people[p["name"]].wife = Person.people[p["wife"]]
+
+        if "husband" in p and p["husband"] is not None:
+            Person.people[p["name"]].husband = Person.people[p["husband"]]
+
+    return people_list
