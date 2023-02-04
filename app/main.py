646cb386b0c063a621dbdf9fa6d4939b0dfe9027
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-04 11:56:57 +0200

    a little refactor change naming

--- app/main.py
+++ app/main.py
@@ -9,12 +9,13 @@ class Person:
     @classmethod
     def add_partner(cls, people: list) -> None:
         for person in people:
-            wife = person.get("wife")
-            husband = person.get("husband")
-            if wife:
-                cls.people[person.get("name")].wife = cls.people[wife]
-            if husband:
-                cls.people[person.get("name")].husband = cls.people[husband]
+            person_wife = person.get("wife")
+            person_husband = person.get("husband")
+            person_name = person.get("name")
+            if person_wife:
+                cls.people[person_name].wife = cls.people[person_wife]
+            if person_husband:
+                cls.people[person_name].husband = cls.people[person_husband]
 
 
 def create_person_list(people: list) -> list:
