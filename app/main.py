af186b1dba77c9affbc23348b919de4e30517de8
Maksym Nikityuk <maksym71417@gmail.com>
2023-02-06 15:48:23 +0200

    new Solution

--- app/main.py
+++ app/main.py
@@ -11,15 +11,12 @@ class Person:
 
 def create_person_list(people: list) -> list:
     ls_of_friends = [Person(person["name"], person["age"])for person in people]
-    for key, value in Person.people.items():
-        for person in people:
-            if key == person["name"]\
-                    and "wife" in person.keys()\
-                    and person["wife"]:
-                value.wife = Person.people[person["wife"]]
-            if key == person["name"]\
-                    and "husband" in person.keys()\
-                    and person["husband"]:
-                value.husband = Person.people[person["husband"]]
+    for person in people:
+        wife = person.get("wife")
+        husband = person.get("husband")
+        if wife:
+            Person.people[person.get("name")].wife = Person.people[wife]
+        if husband:
+            Person.people[person.get("name")].husband = Person.people[husband]
 
     return ls_of_friends
