c7ab865b837574c69a68a82a7511c7eea715866f
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-02-06 20:17:04 +0200

    fixed init and added get method

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.__class__.people[self.name] = self
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
@@ -14,7 +14,7 @@ def create_person_list(people: list) -> list:
         Person(person["name"], person["age"])
 
     for person in people:
-        class_people_person_name = Person.people[person["name"]]
+        class_people_person_name = Person.people.get(person["name"])
         if "wife" in person and person["wife"] is not None:
             class_people_person_name.wife = Person.people[person["wife"]]
         elif "husband" in person and person["husband"] is not None:
