e1cdbe1288e3a8d27f03c9cb4bcda897822d7d20
Ada Kravchenko <ada.t.krav@gmail.com>
2023-02-07 21:23:01 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -11,18 +11,16 @@ def create_person_list(people: list) -> list:
     result = []
     for person in people:
         person_in_progress = Person(person["name"], person["age"])
-        if "wife" in person.keys():
-            if person["wife"] is not None:
-                if person["wife"] in Person.people:
-                    wife_link = Person.people[person["wife"]]
-                    person_in_progress.wife = wife_link
-                    wife_link.husband = person_in_progress
+        if person.get("wife"):
+            if person["wife"] in Person.people:
+                wife_link = Person.people[person["wife"]]
+                person_in_progress.wife = wife_link
+                wife_link.husband = person_in_progress
 
-        if "husband" in person.keys():
-            if person["husband"] is not None:
-                if person["husband"] in Person.people:
-                    husband_link = Person.people[person["husband"]]
-                    person_in_progress.husband = husband_link
-                    husband_link.wife = person_in_progress
+        if person.get("husband"):
+            if person["husband"] in Person.people:
+                husband_link = Person.people[person["husband"]]
+                person_in_progress.husband = husband_link
+                husband_link.wife = person_in_progress
         result.append(person_in_progress)
     return result
