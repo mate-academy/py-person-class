aea3e70efbafca616306b71de2fcd439c80758ba
Yevhen Kaskov <kaskov.e@gmail.com>
2023-02-08 00:00:09 +0200

    Shorter solution

--- app/main.py
+++ app/main.py
@@ -11,14 +11,12 @@ def create_person_list(people_list: list) -> list:
     person_list = []
     for human in people_list:
         person = Person(human["name"], human["age"])
-        spouse = human.get("wife") or human.get("husband")
-        if spouse in Person.people:
-            spouse_link = Person.people[spouse]
-            if "wife" in human:
-                person.wife = spouse_link
-                spouse_link.husband = person
-            else:
-                person.husband = spouse_link
-                spouse_link.wife = person
         person_list.append(person)
+
+    for human in people_list:
+        if human.get("wife"):
+            Person.people[human["name"]].wife = Person.people[human["wife"]]
+        if human.get("husband"):
+            husband_link = Person.people[human["husband"]]
+            Person.people[human["name"]].husband = husband_link
     return person_list
