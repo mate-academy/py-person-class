231f53da49d3f9bb4a769993c9a8c36747d38d74
Yulia <Mala>
2023-02-07 20:51:29 +0200

    I killed the classmethod, just realised that I have straight access to the instances through the dict Name: Instance link. Thanks)

--- app/main.py
+++ app/main.py
@@ -7,12 +7,6 @@ class Person:
         self.age = age
         Person.people[self.name] = self
 
-    @classmethod
-    def pick_by_name(cls, in_name: str) -> object:
-        for person in Person.people:
-            if person == in_name:
-                return Person.people[person]
-
 
 def create_person_list(people: list) -> list:
     instances_ls = [Person(person["name"], person["age"]) for person in people]
@@ -20,7 +14,7 @@ def create_person_list(people: list) -> list:
         pair = person.get("wife") or person.get("husband")
         name = person["name"]
         if pair and "wife" in person:
-            Person.pick_by_name(name).wife = Person.pick_by_name(pair)
+            Person.people[name].wife = Person.people[pair]
         elif pair and "husband" in person:
-            Person.pick_by_name(name).husband = Person.pick_by_name(pair)
+            Person.people[name].husband = Person.people[pair]
     return instances_ls
