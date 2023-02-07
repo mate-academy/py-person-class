9d87fcc2c9221067a21227f2a6ef2ab9d64ebff6
VladyslavCherkashyn <Sparrowq2@gmail.com>
2023-02-08 00:07:32 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people.setdefault(self.name, self)
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    links_of_instances = [
+        Person(people[person]["name"], people[person]["age"])
+        for person in range(len(people))]
+
+    for waifu in range(len(people)):
+        if "wife" in people[waifu].keys():
+            for crash in range(len(people)):
+                if people[waifu]["wife"] == \
+                        people[crash]["name"]:
+                    links_of_instances[waifu].wife = \
+                        links_of_instances[crash]
+        if "husband" in people[waifu].keys():
+            for crash in range(len(people)):
+                if people[waifu]["husband"] == \
+                        people[crash]["name"]:
+                    links_of_instances[waifu].husband = \
+                        links_of_instances[crash]
+
+    return links_of_instances
