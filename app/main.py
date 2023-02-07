f6bc4a5d387614db1bcf34aa79587d531a114eb3
Yulia <Mala>
2023-02-07 17:44:14 +0200

    solution. It is not precise annotation when I return object in pick_by_name method, but if I use from future import annotations - I have 2 tests failed and strange errors with "ast.parse". For now, it's too much, so I've cheated. sorry.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
+
+    @classmethod
+    def pick_by_name(cls, in_name: str) -> object:
+        for person in Person.people:
+            if person == in_name:
+                return Person.people[person]
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    instances_ls = [Person(person["name"], person["age"]) for person in people]
+    for person in people:
+        pair = person.get("wife") or person.get("husband")
+        name = person["name"]
+        if pair and "wife" in person:
+            Person.pick_by_name(name).wife = Person.pick_by_name(pair)
+        elif pair and "husband" in person:
+            Person.pick_by_name(name).husband = Person.pick_by_name(pair)
+    return instances_ls
