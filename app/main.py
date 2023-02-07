96eb2863d97e1b9f790cb3d975dc858981f59dff
kuznetsb <kuznetsbog@gmail.com>
2023-02-07 15:24:28 +0200

    Fixed line breaking

--- app/main.py
+++ app/main.py
@@ -14,6 +14,6 @@ def create_person_list(people: list) -> list:
         if human.get("wife"):
             Person.people[human["name"]].wife = Person.people[human["wife"]]
         elif human.get("husband"):
-            Person.people[human["name"]].husband = \
-                Person.people[human["husband"]]
+            person_husband = Person.people[human["husband"]]
+            Person.people[human["name"]].husband = person_husband
     return persons
