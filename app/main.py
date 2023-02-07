3ec6174642b286b261bf4bc6c63fa433b4ca6db8
Dmytry <donchykd@gmail.com>
2023-02-07 15:56:19 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -14,16 +14,16 @@ def create_person_list(people: list) -> list:
         adding_person = Person(human_being["name"], human_being["age"])
         result_list.append(adding_person)
 
-    print(f"Return list: {result_list}")
+    for human_being in people:
 
-    for i in range(len(people)):
+        getting_wife = human_being.get("wife")
+        getting_husband = human_being.get("husband")
 
-        ind_wife = people[i].get("wife")
-        ind_husband = people[i].get("husband")
-
-        if ind_wife is not None and (people[i]["wife"]) is not None:
-            result_list[i].wife = Person.people[people[i]["wife"]]
-        if ind_husband is not None and (people[i]["husband"]) is not None:
-            result_list[i].husband = Person.people[people[i]["husband"]]
+        if getting_wife is not None:
+            Person.people[human_being["name"]].wife = \
+                Person.people[human_being["wife"]]
+        if getting_husband is not None:
+            Person.people[human_being["name"]].husband = \
+                Person.people[human_being["husband"]]
 
     return result_list
