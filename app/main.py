eb956d5c1ac717fb0cc08dacb9d4caddca6c5020
Max Bilokur <bilmaxim@gmail.com>
2023-02-07 19:12:49 +0200

    revised code structure

--- app/main.py
+++ app/main.py
@@ -9,24 +9,16 @@ class Person:
 
 def create_person_list(people: list) -> list:
     people_list = list()
+
     for person_info in people:
         person_entry = Person(name=person_info["name"], age=person_info["age"])
-        partner_status, partner_name = None, None
-        if "wife" in person_info:
-            partner_name = person_info["wife"]
-            partner_status = "wife"
-        elif "husband" in person_info:
-            partner_name = person_info["husband"]
-            partner_status = "husband"
-        if partner_status is not None and partner_name in Person.people:
-            partner_entry = Person.people.get(partner_name)
-            if partner_status == "wife":
-                person_entry.wife = partner_entry
-                partner_entry.husband = person_entry
-            else:
-                person_entry.husband = partner_entry
-                partner_entry.wife = person_entry
-
         people_list.append(person_entry)
+
+    for person in people:
+        if person.get("husband") is not None:
+            Person.people[person["name"]].husband \
+                = Person.people[person["husband"]]
+        elif person.get("wife") is not None:
+            Person.people[person["name"]].wife = Person.people[person["wife"]]
 
     return people_list
