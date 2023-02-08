44dc34037244c392488b9e86bedb04ca6c37108d
MikhailLyvak <michael.lyvak@gmail.com>
2023-02-08 16:44:20 +0200

    Improved .get <- func usage, created few variables for better readability

--- app/main.py
+++ app/main.py
@@ -12,11 +12,11 @@ def create_person_list(people: list) -> list:
     peoples_lst = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
-        if "wife" in person and person.get("wife") is not None:
-            Person.people[person.get("name")].wife = \
-                Person.people[person.get("wife")]
-        if "husband" in person and person.get("husband") is not None:
-            Person.people[person.get("name")].husband = \
-                Person.people[person.get("husband")]
+        if person.get("wife") is not None:
+            name_give_w = Person.people[person.get("wife")]
+            Person.people[person.get("name")].wife = name_give_w
+        if person.get("husband") is not None:
+            name_give_h = Person.people[person.get("husband")]
+            Person.people[person.get("name")].husband = name_give_h
 
     return peoples_lst
