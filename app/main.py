ad4999d332fc42fbd6deaa2cabca383930c3d359
Yevhen Kaskov <kaskov.e@gmail.com>
2023-02-08 15:54:07 +0200

    Solution - separate varaible

--- app/main.py
+++ app/main.py
@@ -14,9 +14,11 @@ def create_person_list(people_list: list) -> list:
         person_list.append(person)
 
     for human in people_list:
+        current_person = Person.people[human["name"]]
         if human.get("wife"):
-            Person.people[human["name"]].wife = Person.people[human["wife"]]
+            wife_link = Person.people[human["wife"]]
+            current_person.wife = wife_link
         if human.get("husband"):
             husband_link = Person.people[human["husband"]]
-            Person.people[human["name"]].husband = husband_link
+            current_person.husband = husband_link
     return person_list
