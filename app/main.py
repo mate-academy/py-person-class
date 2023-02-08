8a9cede5d40cf22639db737d70351b860d7b0dec
MikhailLyvak <michael.lyvak@gmail.com>
2023-02-08 09:44:47 +0200

    Used .get() method

--- app/main.py
+++ app/main.py
@@ -13,7 +13,8 @@ def create_person_list(people: list) -> list:
 
     for person in people:
         if "wife" in person and person.get("wife") is not None:
-            Person.people[person.get("name")].wife = Person.people[person.get("wife")]
+            Person.people[person.get("name")].wife = \
+                Person.people[person.get("wife")]
         if "husband" in person and person.get("husband") is not None:
             Person.people[person.get("name")].husband = \
                 Person.people[person.get("husband")]
