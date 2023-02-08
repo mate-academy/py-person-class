1e9c22854b8957f16fdc8edc14b48c789e387402
Eugene <eugene.lot77@gmail.com>
2023-02-08 16:38:18 +0200

    Made separate variable

--- app/main.py
+++ app/main.py
@@ -14,9 +14,9 @@ def create_person_list(people: list) -> list:
 
     for person in people:
         if person.get("wife"):
-            Person.people[person["name"]].wife\
-                = Person.people[person["wife"]]
+            wife = Person.people[person["wife"]]
+            Person.people[person["name"]].wife = wife
         if person.get("husband"):
-            Person.people[person["name"]].husband\
-                = Person.people[person["husband"]]
+            husband = Person.people[person["husband"]]
+            Person.people[person["name"]].husband = husband
     return friends
