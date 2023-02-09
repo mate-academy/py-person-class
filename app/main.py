e03f0d8fb8fe2158d40fcd02863533303333053c
Yegor Turenko <ytferma@gmail.com>
2023-02-09 12:15:14 +0200

    "Solution"

--- app/main.py
+++ app/main.py
@@ -12,10 +12,11 @@ def create_person_list(people: list) -> list:
     for person in people:
         Person(person["name"], person["age"])
         person_list.append(Person.people[person["name"]])
+    pers_dict = Person.people
     for person in people:
         if "wife" in person and person["wife"] is not None:
-            Person.people[person["name"]].wife = Person.people[person["wife"]]
+            pers_dict[person["name"]].wife = pers_dict[person["wife"]]
         elif "husband" in person and person["husband"] is not None:
-            Person.people[person["name"]].husband = Person.people[person["husband"]]
+            pers_dict[person["name"]].husband = pers_dict[person["husband"]]
 
     return person_list
