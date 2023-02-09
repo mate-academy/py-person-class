0cc57a33dec93cec2e6e460b53d337e144fa2e1f
usrofgh <mailforgimail@gmail.com>
2023-02-09 18:23:16 +0200

    person_el was renamed to person. self.__class__ was changed to Person

--- app/main.py
+++ app/main.py
@@ -4,20 +4,20 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.__class__.people[name] = self
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
     person_objects = [Person(person["name"], person["age"])
                       for person in people]
 
-    for person_el in people:
-        if person_el.get("wife"):
-            person_with_wife = Person.people[person_el["name"]]
-            person_with_wife.wife = Person.people[person_el["wife"]]
+    for person in people:
+        if person.get("wife"):
+            person_with_wife = Person.people[person["name"]]
+            person_with_wife.wife = Person.people[person["wife"]]
 
-        elif person_el.get("husband"):
-            person_with_husband = Person.people[person_el["name"]]
-            person_with_husband.husband = Person.people[person_el["husband"]]
+        elif person.get("husband"):
+            person_with_husband = Person.people[person["name"]]
+            person_with_husband.husband = Person.people[person["husband"]]
 
     return person_objects
