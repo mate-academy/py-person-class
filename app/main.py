81baaf88c82239ee02e947a6b8766f29cc380a60
usrofgh <mailforgimail@gmail.com>
2023-02-09 16:49:46 +0200

    change access to variable via Class name. '.get()' was added. '= \' was deleted.

--- app/main.py
+++ app/main.py
@@ -4,20 +4,20 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.people[name] = self
+        self.__class__.people[name] = self
 
 
 def create_person_list(people: list) -> list:
     person_objects = [Person(person["name"], person["age"])
                       for person in people]
 
-    for person in people:
-        if "wife" in person and person["wife"]:
-            Person.people[person["name"]].wife = \
-                Person.people[person["wife"]]
+    for person_el in people:
+        if person_el.get("wife"):
+            person_with_wife = Person.people[person_el["name"]]
+            person_with_wife.wife = Person.people[person_el["wife"]]
 
-        elif "husband" in person and person["husband"]:
-            Person.people[person["name"]].husband = \
-                Person.people[person["husband"]]
+        elif person_el.get("husband"):
+            person_with_husband = Person.people[person_el["name"]]
+            person_with_husband.husband = Person.people[person_el["husband"]]
 
     return person_objects
