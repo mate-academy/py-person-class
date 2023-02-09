d3ffc2f0bcba5daac24975b4d9f595fbe415f486
Ruslan <noezyboy1@gmail.com>
2023-02-09 15:33:39 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -12,25 +12,18 @@ def create_person_list(people: list) -> list:
     for person in people:
         name = person["name"]
         age = person["age"]
-        if name not in Person.people:
-            person_obj = Person(name, age)
-        else:
-            person_obj = Person.people[name]
-        if "wife" in person:
-            wife_name = person["wife"]
-            if wife_name is not None:
-                if wife_name not in Person.people:
-                    wife = Person(wife_name, None)
-                else:
-                    wife = Person.people[wife_name]
-                person_obj.wife = wife
-        if "husband" in person:
-            husband_name = person["husband"]
-            if husband_name is not None:
-                if husband_name not in Person.people:
-                    husband = Person(husband_name, None)
-                else:
-                    husband = Person.people[husband_name]
-                person_obj.husband = husband
+        person_obj = Person.people.get(name) or Person(name, age)
+
+        wife_name = person.get("wife")
+        if wife_name is not None:
+            wife = Person.people.get(wife_name) or Person(wife_name, None)
+            person_obj.wife = wife
+
+        husband_name = person.get("husband")
+        if husband_name is not None:
+            husband = \
+                Person.people.get(husband_name) or Person(husband_name, None)
+            person_obj.husband = husband
+
         person_list.append(person_obj)
     return person_list
