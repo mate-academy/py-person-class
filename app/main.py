ac14399374e7b92f2ba2ef439577e0429a1c7680
Oleksandr Leha <legus96@gmail.com>
2023-02-10 09:43:35 +0200

    'Solution_with_changes'

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people.setdefault(self.name, self)
+        Person.people[self.name] = self
 
     def __str__(self) -> str:
         return f"{self.name}, {self.age}"
@@ -12,19 +12,20 @@ class Person:
 
 def create_person_list(people: list) -> list:
     list_instances = []
-    for i in range(len(people)):
-        list_instances.append(Person(people[i]["name"], people[i]["age"]))
-    for first_person in range(len(people)):
-        if "wife" in people[first_person].keys():
-            for second_person in range(len(people)):
-                if people[first_person]["wife"] == \
-                        people[second_person]["name"]:
-                    list_instances[first_person].wife = \
-                        list_instances[second_person]
-        if "husband" in people[first_person].keys():
-            for second_person in range(len(people)):
-                if people[first_person]["husband"] == \
-                        people[second_person]["name"]:
-                    list_instances[first_person].husband = \
-                        list_instances[second_person]
+    for person_dict in people:
+        person_instance = Person(person_dict["name"], person_dict["age"])
+
+        if person_dict.get("wife"):
+            wife = Person.people.get(person_dict.get("wife"))
+
+            if wife:
+                person_instance.wife = wife
+                wife.husband = person_instance
+        elif person_dict.get("husband"):
+            husband = Person.people.get(person_dict.get("husband"))
+
+            if husband:
+                person_instance.husband = husband
+                husband.wife = person_instance
+        list_instances.append(person_instance)
     return list_instances
