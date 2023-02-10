5127efe88f20ad80216675a7c18aa92b440ec5eb
Ruslan <noezyboy1@gmail.com>
2023-02-10 16:35:28 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.__class__.people[self.name] = self
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
@@ -12,18 +12,18 @@ def create_person_list(people: list) -> list:
     for person in people:
         name = person["name"]
         age = person["age"]
-        person_obj = Person.people.get(name) or Person(name, age)
+        person_object = Person.people.get(name) or Person(name, age)
 
         wife_name = person.get("wife")
         if wife_name is not None:
             wife = Person.people.get(wife_name) or Person(wife_name, None)
-            person_obj.wife = wife
+            person_object.wife = wife
 
         husband_name = person.get("husband")
         if husband_name is not None:
-            husband = \
-                Person.people.get(husband_name) or Person(husband_name, None)
-            person_obj.husband = husband
+            husband = Person.people.get(husband_name) or Person(
+                husband_name, None)
+            person_object.husband = husband
 
-        person_list.append(person_obj)
+        person_list.append(person_object)
     return person_list
