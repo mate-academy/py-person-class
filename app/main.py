6eaea187ceca2109e8e12b66b01d795077a4c9b0
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-02-11 15:41:22 +0200

    removed nested loop and implement list comprehension for instance list

--- app/main.py
+++ app/main.py
@@ -9,19 +9,13 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    instance_list = []
-    for human in people:
-        instance = Person(human["name"], human["age"])
-        instance_list.append(instance)
+    instance_list = [Person(human["name"], human["age"]) for human in people]
 
-    for instance in instance_list:
-        for human in people:
-            if human["name"] == instance.name:
-                if "wife" not in instance.__dict__ and \
-                        human.get("wife") is not None:
-                    instance.wife = Person.people[human["wife"]]
-                if "husband" not in instance.__dict__ \
-                        and human.get("husband") is not None:
-                    instance.husband = Person.people[human["husband"]]
+    for human in people:
+        human_name = human["name"]
+        if human.get("wife") is not None:
+            Person.people[human_name].wife = Person.people[human["wife"]]
+        if human.get("husband") is not None:
+            Person.people[human_name].husband = Person.people[human["husband"]]
 
     return instance_list
