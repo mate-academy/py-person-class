628d783cc4bd67412d59a512af261828605c6a18
myroslavatrschk <tereshchukm.forstudy@gmail.com>
2023-02-12 21:28:16 +0200

    Edited create_person_list

--- app/main.py
+++ app/main.py
@@ -8,15 +8,11 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    list_of_people = [
-        Person(person["name"], person["age"]) for person in people
-    ]
-
+    [Person(person["name"], person["age"]) for person in people]
     for person in people:
         if person.get("wife"):
-            Person.people[person["name"]].wife = Person.people[person["wife"]]
-        if person.get("husband"):
-            Person.people[person["name"]].husband = Person.people[
-                person["husband"]
-            ]
-    return list_of_people
+            wife = Person.people[person.get("wife")]
+            husband = Person.people[person.get("name")]
+            husband.wife = wife
+            wife.husband = husband
+    return list(Person.people.values())
