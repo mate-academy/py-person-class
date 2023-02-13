e7fee246bf2d46c452aa6123268f3116f3d91d98
myroslavatrschk <tereshchukm.forstudy@gmail.com>
2023-02-13 15:39:58 +0200

    Added variable list_of_people

--- app/main.py
+++ app/main.py
@@ -8,11 +8,13 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    [Person(person["name"], person["age"]) for person in people]
+    list_of_people = [
+        Person(person["name"], person["age"]) for person in people
+    ]
     for person in people:
         if person.get("wife"):
             wife = Person.people[person.get("wife")]
             husband = Person.people[person.get("name")]
             husband.wife = wife
             wife.husband = husband
-    return list(Person.people.values())
+    return list_of_people
