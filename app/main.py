cf6bdd3d663cc76f0bdccbe618973631b454f5fe
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-02-13 14:39:35 +0200

    Refactor code

--- app/main.py
+++ app/main.py
@@ -4,22 +4,22 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.__class__.people[name] = self
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
+def create_person_list(people: list[dict]) -> list:
     person_list = [
-        Person(person["name"], person["age"])
+        Person(person.get("name"), person.get("age"))
         for person in people
     ]
     for i, person in enumerate(people):
-        if "wife" in person and person["wife"]:
+        if person.get("wife"):
             setattr(
                 person_list[i],
                 "wife",
                 Person.people.get(person["wife"])
             )
-        elif "husband" in person and person["husband"]:
+        elif person.get("husband"):
             setattr(
                 person_list[i],
                 "husband",
