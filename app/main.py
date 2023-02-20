e060530d2479812bef9fcf52492f566461287b2f
Yegor Turenko <ytferma@gmail.com>
2023-02-20 14:26:27 +0200

    typehints fixed, use get() method instead of indexation - Done

--- app/main.py
+++ app/main.py
@@ -1,22 +1,23 @@
 class Person:
-    people = {}
+    people: dict = {}
 
-    def __init__(self, name: str, age: str) -> any:
+    def __init__(self, name: str, age: str) -> None:
         self.name = name
         self.age = age
         Person.people[self.name] = self
 
 
-def create_person_list(people: list) -> list:
-    person_list = []
+def create_person_list(people: list) -> list[Person]:
+    person_list: list[Person] = []
     for person in people:
+        # next line initiates the creation of an instance of the class
         Person(person["name"], person["age"])
         person_list.append(Person.people[person["name"]])
     pers_dict = Person.people
     for person in people:
-        if "wife" in person and person["wife"] is not None:
+        if person.get("wife") is not None:
             pers_dict[person["name"]].wife = pers_dict[person["wife"]]
-        elif "husband" in person and person["husband"] is not None:
+        elif person.get("husband") is not None:
             pers_dict[person["name"]].husband = pers_dict[person["husband"]]
 
     return person_list
