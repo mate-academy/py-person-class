3b556649d5ced96f366d57c756a8aa79da487ddb
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-02-27 18:43:46 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 class Person:
-    # write your code here
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
     pass
 
-
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    def create_person_list(people: list) -> list:
+        person_list = []
+        for human in people:
+            human["name"] = Person(human["name"], human["age"])
+            person_list.append(human["name"])
+        for human in people:
+            if "wife" in human:
+                if human["wife"] is not None:
+                    human["name"].wife = Person.people[human["wife"]]
+            elif "husband" in human:
+                if human["husband"] is not None:
+                    human["name"].husband = Person.people[human["husband"]]
+        return [every["name"] for every in people]
+        pass
