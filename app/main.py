8b3b3348885d5507c7f2170e27e977e78c24d949
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-02-28 21:43:36 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,16 +6,16 @@ class Person:
         self.age = age
         Person.people[self.name] = self
 
-        
+
 def create_person_list(people: list) -> list:
     list_of_person = [Person(human["name"], human["age"]) for human in people]
     for human in people:
         if "wife" in human and human.get("wife") is not None:
             Person.people[human["name"]].wife = (
-                    Person.people[human["wife"]]
+                Person.people[human["wife"]]
             )
         elif "husband" in human and human.get("husband") is not None:
             Person.people[human["name"]].husband = (
-                    Person.people[human["husband"]]
+                Person.people[human["husband"]]
             )
     return list_of_person
