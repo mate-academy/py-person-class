81261b38d40821636b470c79c56607f7560eab14
Halyna Petrova <1208petrova@gmail.com>
2023-02-28 10:39:58 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -20,21 +20,4 @@ def create_person_list(people_list: list) -> list:
             husband = Person.people[person.get("husband")]
             Person.people[person.get("name")].husband = husband
 
-        return new_person_list
-
-
-#     # print(new_person_list[2].husband)
-#
-#     for i in new_person_list:
-#         print(i.__dict__)
-#
-#
-# people_list = [
-#     {"name": "Ross", "age": 30, "wife": "Rachel"},
-#     {"name": "Joey", "age": 29, "wife": None},
-#     {"name": "Rachel", "age": 28, "husband": "Ross"}
-# ]
-#
-# create_person_list(people_list)
-#
-
+    return new_person_list
