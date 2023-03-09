6583badefed100aca8e3ff74ec585e2d05d0131b
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-03-09 02:35:04 +0200

    create_person_list func

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        print(people)
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    print([Person(current["name"], current["age"]) for current in people])
+    return [Person(current["name"], current["age"]) for current in people]
+
+
+people = [
+    {"name": "Ross", "age": 30, "wife": "Rachel"},
+    {"name": "Joey", "age": 29, "wife": None},
+    {"name": "Rachel", "age": 28, "husband": "Ross"}
+]
+people = [
+    {"name": "Ross", "age": 30, "wife": "Rachel"},
+    {"name": "Joey", "age": 29, "wife": None},
+    {"name": "Rachel", "age": 28, "husband": "Ross"}
+]
+
+person_list = create_person_list(people)
