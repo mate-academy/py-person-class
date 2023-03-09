3fb086a038a56b541eafeaaaddff8d41d0834da9
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-03-09 07:29:44 +0200

    3f,10p

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
+# import time
+#
+#
+# def timer(func):
+#     print("timer")
+#
+#     def wrapper(*args, **kwargs):
+#         print("wrapper")
+#         start_ = time.time()
+#         func(people)
+#         end_ = time.time()
+#         print(end_ - start_)
+#
+#     return wrapper
+
+
 class Person:
     people = {}
 
@@ -7,16 +23,16 @@ class Person:
         print(people)
 
 
+# @timer
 def create_person_list(people: list) -> list:
+    # res = []  # [i for i in range(100_000_000)]
+    # for i in range(100_000_000):
+    #     res.append(i)
+    # print(len(res))
     print([Person(current["name"], current["age"]) for current in people])
     return [Person(current["name"], current["age"]) for current in people]
 
 
-people = [
-    {"name": "Ross", "age": 30, "wife": "Rachel"},
-    {"name": "Joey", "age": 29, "wife": None},
-    {"name": "Rachel", "age": 28, "husband": "Ross"}
-]
 people = [
     {"name": "Ross", "age": 30, "wife": "Rachel"},
     {"name": "Joey", "age": 29, "wife": None},
