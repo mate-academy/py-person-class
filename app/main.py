7d2134d80dc2d435e8d9fb776b8e1475f79fbacf
Vadym Rabotiuk <vadymrabotiuk@gmail.com>
2023-03-09 12:15:02 +0200

    simpled solution

--- app/main.py
+++ app/main.py
@@ -11,25 +11,18 @@ def create_person_list(people: list) -> list:
     final_list = []
 
     for human in people:
-        person_object = (
+
+        final_list.append(
             Person(human["name"], human["age"])
-            if human["name"] not in Person.people
-            else Person.people[human["name"]]
         )
-        final_list.append(person_object)
 
     for human in people:
-        husband_wife = "husband" if "husband" in human else "wife"
-        name_of_husband_wife = human[husband_wife]
 
-        if name_of_husband_wife:
-            wife_husband_object = None
+        if "wife" in human and human["wife"] is not None:
+            Person.people[human["name"]].wife = \
+                Person.people[human["wife"]]
 
-            if name_of_husband_wife in Person.people:
-                wife_husband_object = Person.people[name_of_husband_wife]
-
-            if husband_wife == "wife":
-                Person.people[human["name"]].wife = wife_husband_object
-            else:
-                Person.people[human["name"]].husband = wife_husband_object
+        if "husband" in human and human["husband"] is not None:
+            Person.people[human["name"]].husband = \
+                Person.people[human["husband"]]
     return final_list
