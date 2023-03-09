e244f5fe4b12c996117d74f93c43fe6b1a678e40
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-03-09 08:03:03 +0200

    lf wife

--- app/main.py
+++ app/main.py
@@ -16,21 +16,34 @@
 
 class Person:
     people = {}
+    wife_name = None
+    husband_name = None
 
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        print(people)
+
+        Person.people[name] = self
+
+        print(Person.people)
 
 
 # @timer
 def create_person_list(people: list) -> list:
+    to_iterate = people
+    print(to_iterate)
+    for i in people:
+        if "wife" in i.keys():
+            print("3d woman exist")
+            if i["wife"] is not None:
+                print("wife is not None")
+
+    res = [Person(current["name"], current["age"]) for current in to_iterate]
     # res = []  # [i for i in range(100_000_000)]
     # for i in range(100_000_000):
     #     res.append(i)
     # print(len(res))
-    print([Person(current["name"], current["age"]) for current in people])
-    return [Person(current["name"], current["age"]) for current in people]
+    return res
 
 
 people = [
@@ -40,3 +53,6 @@ people = [
 ]
 
 person_list = create_person_list(people)
+print(person_list)
+
+# print(person_list[0].husband)
