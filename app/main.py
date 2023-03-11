7eb28b3e6c263e47898f417265d7ad1d41981101
Mykola <nickkozlov90@gmail.com>
2023-03-11 12:12:01 +0200

    get() method aplied

--- app/main.py
+++ app/main.py
@@ -7,16 +7,16 @@ class Person:
         Person.people.update({self.name: self})
 
 
-def create_person_list(people: list) -> list:
+def create_person_list(people: list[dict]) -> list:
     result = []
     for person in people:
         Person(name=person["name"], age=person["age"])
     for person in people:
         if person.get("husband"):
-            Person.people[person["name"]].husband =\
-                Person.people[person["husband"]]
+            Person.people.get(person.get("name")).husband =\
+                Person.people.get(person.get("husband"))
         elif person.get("wife"):
-            Person.people[person["name"]].wife =\
-                Person.people[person["wife"]]
+            Person.people.get(person.get("name")).wife = \
+                Person.people.get(person.get("wife"))
         result.append(Person.people[person["name"]])
     return result
