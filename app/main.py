54b9873f0bdbcf3565d61028e34fcd6e950d6131
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-03-14 17:50:10 +0200

    Make all requested changes

--- app/main.py
+++ app/main.py
@@ -5,20 +5,23 @@ class Person:
         self.name = name
         self.age = age
 
-        self.__class__.people[self.name] = self
+        Person.people[self.name] = self
 
 
-def create_person_list(people_dicts: list) -> list:
+def create_person_list(
+        people_dicts: list[dict[str, str | int | None]]
+) -> list:
     people_instances = [
-        Person(person_dict["name"], person_dict["age"])
+        Person(person_dict.get("name"), person_dict.get("age"))
         for person_dict in people_dicts
     ]
 
     for person_dict, person_instance in zip(people_dicts, people_instances):
-        if "wife" in person_dict and person_dict["wife"] is not None:
-            person_instance.wife = Person.people[person_dict["wife"]]
+        if "wife" in person_dict and person_dict.get("wife") is not None:
+            person_instance.wife = Person.people.get(person_dict.get("wife"))
 
-        if "husband" in person_dict and person_dict["husband"] is not None:
-            person_instance.husband = Person.people[person_dict["husband"]]
+        if "husband" in person_dict and person_dict.get("husband") is not None:
+            person_instance.husband = \
+                Person.people.get(person_dict.get("husband"))
 
     return people_instances
