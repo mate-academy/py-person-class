db37f7e091779cadec0d82e41658015c20f6efbb
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-03-14 09:10:17 +0200

    Use zip()

--- app/main.py
+++ app/main.py
@@ -8,35 +8,19 @@ class Person:
         self.__class__.people[self.name] = self
 
 
-def create_person_list(people: list) -> list:
+def create_person_list(people_dicts: list) -> list:
     people_instances = []
 
-    for person_dict in people:
+    for person_dict in people_dicts:
         people_instances.append(
             Person(person_dict["name"], person_dict["age"])
         )
 
-    for person_dict in people:
-        if "wife" in person_dict \
-                and person_dict["wife"] is not None:
-            wife_name = person_dict["wife"]
+    for person_dict, person_instance in zip(people_dicts, people_instances):
+        if "wife" in person_dict and person_dict["wife"] is not None:
+            person_instance.wife = Person.people[person_dict["wife"]]
 
-            for person_instance in people_instances:
-                if person_instance.name == person_dict["name"]:
-                    for wife in people_instances:
-                        if wife.name == wife_name:
-                            person_instance.wife = wife
-                            break
+        if "husband" in person_dict and person_dict["husband"] is not None:
+            person_instance.husband = Person.people[person_dict["husband"]]
 
-        if "husband" in person_dict \
-                and person_dict["husband"] is not None:
-            husband_name = person_dict["husband"]
-
-            for person_instance in people_instances:
-                if person_instance.name == person_dict["name"]:
-                    for husband in people_instances:
-                        if husband.name == husband_name:
-                            person_instance.husband = husband
-                            break
-
-        return people_instances
+    return people_instances
