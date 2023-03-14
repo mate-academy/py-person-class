d0415b61975d825e85cf549128fe7908620d1720
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-03-14 11:30:28 +0200

    Use list comprehension

--- app/main.py
+++ app/main.py
@@ -9,12 +9,10 @@ class Person:
 
 
 def create_person_list(people_dicts: list) -> list:
-    people_instances = []
-
-    for person_dict in people_dicts:
-        people_instances.append(
-            Person(person_dict["name"], person_dict["age"])
-        )
+    people_instances = [
+        Person(person_dict["name"], person_dict["age"])
+        for person_dict in people_dicts
+    ]
 
     for person_dict, person_instance in zip(people_dicts, people_instances):
         if "wife" in person_dict and person_dict["wife"] is not None:
