bf2a9f24fbe8bdb90f92267e668f68e64617e20e
Maksym Bezhevets <begik4@gmail.com>
2023-03-16 21:41:15 +0200

    corrected

--- app/main.py
+++ app/main.py
@@ -9,14 +9,14 @@ class Person:
 
 def create_person_list(people: list) -> list:
     list_people = [
-        Person(person["name"], person["age"]) for person in people
+        Person(person.get("name"), person.get("age")) for person in people
     ]
     for person in people:
         if "wife" in person and person.get("wife") is not None:
-            Person.people[person["name"]].wife = Person.people[person["wife"]]
+            Person.people[person.get("name")].wife = Person.people[person.get("wife")]
 
         if "husband" in person and person.get("husband") is not None:
-            Person.people[person["name"]].husband = (
-                Person.people[person["husband"]]
+            Person.people[person.get("name")].husband = (
+                Person.people[person.get("husband")]
             )
     return list_people
