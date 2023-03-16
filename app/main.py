20d1c22669d0ba72cdc7771db4a868802b379d16
Petro Havrysh <petro.havrysh@gmail.com>
2023-03-16 17:59:05 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: str) -> None:
+        self.name = name
+        self.age = age
+        self.__class__.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+    # Створюємо всіх людей, зберігаючи їх у списку person_list
+    for person in people:
+        p = Person(person["name"], person["age"])
+        person_list.append(p)
+
+    # Установлюємо партнерів для кожної людини
+    for person in people:
+        p = Person.people[person["name"]]
+        if "wife" in person and person["wife"] is not None:
+            p.wife = Person.people[person["wife"]]
+        elif "husband" in person and person["husband"] is not None:
+            p.husband = Person.people[person["husband"]]
+
+    return person_list
