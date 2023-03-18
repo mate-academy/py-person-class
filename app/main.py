ae8deb79457c0f0ecc65d9e5d6e91ed3cb251c06
Dmytry Bondariev <bondarievmd@gmail.com>
2023-03-18 10:45:15 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -8,17 +8,18 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = [
-        Person(name=person["name"],
-               age=person["age"])
-        for person in people
-    ]
+    person_list = [Person(name=person["name"],
+                          age=person["age"])
+                   for person in people
+                   ]
     for person in people:
         if person.get("wife"):
-            Person.people[person["name"]].wife = \
-                Person.people[person["wife"]]
+            Person.people.get(person["name"]).wife = Person.people.get(
+                person.get("wife")
+            )
         if person.get("husband"):
-            Person.people[person["name"]].husband = \
-                Person.people[person["husband"]]
+            Person.people.get(person["name"]).husband = Person.people.get(
+                person.get("husband")
+            )
 
     return person_list
