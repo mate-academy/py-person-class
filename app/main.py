9f91e33c1b755fb8d17405fc4f95af065f45f2ea
Dmytro Shvets <dirolius@gmail.com>
2023-03-18 16:25:25 +0200

    code fixed

--- app/main.py
+++ app/main.py
@@ -9,12 +9,15 @@ class Person:
 
 def create_person_list(people: list[dict[str, str | int | None]]) -> list:
     person_list = []
-
-    for human in people:
+    [
         person_list.append(Person(human["name"], human["age"]))
-    for i, human in enumerate(people):
-        if "wife" in human and human["wife"] is not None:
-            person_list[i].wife = Person.people.get(human["wife"])
-        if "husband" in human and human["husband"] is not None:
-            person_list[i].husband = Person.people.get(human["husband"])
+        for human in people
+    ]
+    for index_person, person in enumerate(people):
+        if "wife" in person and person.get("wife") is not None:
+            person_list[index_person].wife\
+                = Person.people.get(person.get("wife"))
+        if "husband" in person and person.get("husband") is not None:
+            person_list[index_person].husband\
+                = Person.people.get(person.get("husband"))
     return person_list
