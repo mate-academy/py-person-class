ca9b3701cc30f1df927fd07a23084d86a7129cff
Bohdan Marko <goldenbohdanm@gmail.com>
2023-03-18 15:17:18 +0200

    Solution v3

--- app/main.py
+++ app/main.py
@@ -13,8 +13,8 @@ def create_person_list(people: list[dict]) -> list[Person]:
     for human in people:
         person_list.append(Person(human.get("name"), human.get("age")))
     for i, human in enumerate(people):
-        if "wife" in human and human.get("wife") is not None:
+        if human.get("wife"):
             person_list[i].wife = Person.people.get(human["wife"])
-        if "husband" in human and human.get("husband") is not None:
+        if human.get("husband"):
             person_list[i].husband = Person.people.get(human["husband"])
     return person_list
