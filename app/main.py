5086410359dface1bcd136b51c7fe7e4b4ca0472
Влад <vladearth7@gmail.com>
2023-03-21 05:33:25 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = [Person(peo["name"], peo["age"]) for peo in people]
+    for hus in people:
+        if "wife" in hus and hus.get("wife"):
+            Person.people.get(hus.get("name")).wife = \
+                Person.people[hus.get("wife")]
+        if "husband" in hus and hus.get("husband"):
+            Person.people.get(hus.get("name")).husband = \
+                Person.people[hus.get("husband")]
+    return person_list
--- tests/test_main.py
+++ tests/test_main.py
@@ -24,29 +24,19 @@ def created_person_list(people_data):
 
 
 def test_person_class_attribute_people_exists():
-    assert hasattr(Person, 'people'), (
-        "Class Person should have class attribute 'people'"
-    )
-    assert len(Person.people) == 0, (
-        "Initial length of 'Person.people' should equal to 0"
-    )
+    assert hasattr(
+        Person, "people"
+    ), "Class Person should have class attribute 'people'"
+    assert (
+        len(Person.people) == 0
+    ), "Initial length of 'Person.people' should equal to 0"
 
 
-@pytest.mark.parametrize(
-    'name,age',
-    [
-        ('Ross', 30),
-        ('Joey', 29)
-    ]
-)
+@pytest.mark.parametrize("name,age", [("Ross", 30), ("Joey", 29)])
 def test_person_class_name_age(name, age):
     person_inst = Person(name, age)
-    assert person_inst.name == name, (
-        "Person instance should have attribute 'name'"
-    )
-    assert person_inst.age == age, (
-        "Person instance should have attribute 'age'"
-    )
+    assert person_inst.name == name, "Person instance should have attribute 'name'"
+    assert person_inst.age == age, "Person instance should have attribute 'age'"
 
 
 def test_create_person_list_all_persons(people_data, created_person_list):
@@ -54,29 +44,31 @@ def test_create_person_list_all_persons(people_data, created_person_list):
         "All elements in result of 'create_person_list' should be instance "
         "of Person class"
     )
-    assert len(created_person_list) == len(people_data), (
-        "Length of initial list should equal to length of function result"
-    )
+    assert len(created_person_list) == len(
+        people_data
+    ), "Length of initial list should equal to length of function result"
 
 
 def test_create_person_list_order(people_data, created_person_list):
-    assert [person_dict['name'] for person_dict in people_data] == [
+    assert [person_dict["name"] for person_dict in people_data] == [
         person.name for person in created_person_list
-    ], (
-        "Order in function result should be the same"
-    )
+    ], "Order in function result should be the same"
 
 
 def test_create_person_list_has_wife(people_data, created_person_list):
-    assert hasattr(created_person_list[0], 'wife'), (
+    assert hasattr(created_person_list[0], "wife"), (
         f"Person with 'name' {created_person_list[0].name} should have "
         f"attribute 'wife' with name {people_data[0].wife.name}"
     )
 
 
-def test_create_person_list_has_wife_and_wife_have_husband(people_data, created_person_list):
-    assert hasattr(created_person_list[0], 'wife') and \
-           created_person_list[0].wife.husband == created_person_list[0], (
+def test_create_person_list_has_wife_and_wife_have_husband(
+    people_data, created_person_list
+):
+    assert (
+        hasattr(created_person_list[0], "wife")
+        and created_person_list[0].wife.husband == created_person_list[0]
+    ), (
         f"Person with 'name' {created_person_list[0].name} should have "
         f"attribute 'wife' with name {created_person_list[0].wife.name} and "
         f"Person.wife.husband should links to that Person"
@@ -84,26 +76,30 @@ def test_create_person_list_has_wife_and_wife_have_husband(people_data, created_
 
 
 def test_create_person_list_has_no_wife(people_data, created_person_list):
-    assert hasattr(created_person_list[1], 'wife') is False, (
+    assert hasattr(created_person_list[1], "wife") is False, (
         f"Person with 'name' {created_person_list[1].name} should not have "
         f"attribute wife"
     )
 
 
 def test_person_class_attribute_people(people_data, created_person_list):
-    assert all(isinstance(person, Person) for person in Person.people.values()), (
-        "All elements of Person class attribute 'people' should be Person instances"
-    )
-    assert len(Person.people) == len(people_data), (
-        "Length of Person class attribute people should be equal to length of initial list"
-    )
+    assert all(
+        isinstance(person, Person) for person in Person.people.values()
+    ), "All elements of Person class attribute 'people' should be Person instances"
+    assert len(Person.people) == len(
+        people_data
+    ), "Length of Person class attribute people should be equal to length of initial list"
 
 
-def test_create_person_list_returns_only_entering_people(people_data, created_person_list):
-    assert len(people_data) == len(created_person_list), (
-        "Length of passed list should equal to length of returned list"
-    )
-    assert [person['name'] for person in people_data] == [person.name for person in created_person_list], (
+def test_create_person_list_returns_only_entering_people(
+    people_data, created_person_list
+):
+    assert len(people_data) == len(
+        created_person_list
+    ), "Length of passed list should equal to length of returned list"
+    assert [person["name"] for person in people_data] == [
+        person.name for person in created_person_list
+    ], (
         "People, that are passed to the function should equal to people, "
         "that are returned"
     )
@@ -113,6 +109,12 @@ def test_person_instance_attribute_wife_and_husband_doesnt_exists():
     with open("app/main.py") as file:
         tree = ast.parse(file.read())
 
-    assert len(tree.__dict__["body"][0].__dict__["body"][1].__dict__["args"].__dict__["args"]) == 3, (
-        "'__init__' should takes only two arguments 'name' and 'age'!"
-    )
+    assert (
+        len(
+            tree.__dict__["body"][0]
+            .__dict__["body"][1]
+            .__dict__["args"]
+            .__dict__["args"]
+        )
+        == 3
+    ), "'__init__' should takes only two arguments 'name' and 'age'!"
