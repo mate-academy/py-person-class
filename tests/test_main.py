83b16d1a2d466ae9dbaed9eaebdca601cc7f8f6d
Влад <vladearth7@gmail.com>
2023-03-21 05:37:12 +0200

    Fix

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,16 @@
 import pytest
-
+import os
 from app.main import create_person_list
 from app.main import Person
 
 import ast
+
+
+def path_to_main():
+    base_path = os.path.join("app", "main.py")
+    return (
+        base_path if os.path.exists(base_path) else os.path.join(os.pardir, base_path)
+    )
 
 
 @pytest.fixture()
@@ -28,7 +35,7 @@ def test_person_class_attribute_people_exists():
         Person, "people"
     ), "Class Person should have class attribute 'people'"
     assert (
-        len(Person.people) == 0
+            len(Person.people) == 0
     ), "Initial length of 'Person.people' should equal to 0"
 
 
@@ -63,11 +70,11 @@ def test_create_person_list_has_wife(people_data, created_person_list):
 
 
 def test_create_person_list_has_wife_and_wife_have_husband(
-    people_data, created_person_list
+        people_data, created_person_list
 ):
     assert (
-        hasattr(created_person_list[0], "wife")
-        and created_person_list[0].wife.husband == created_person_list[0]
+            hasattr(created_person_list[0], "wife")
+            and created_person_list[0].wife.husband == created_person_list[0]
     ), (
         f"Person with 'name' {created_person_list[0].name} should have "
         f"attribute 'wife' with name {created_person_list[0].wife.name} and "
@@ -92,7 +99,7 @@ def test_person_class_attribute_people(people_data, created_person_list):
 
 
 def test_create_person_list_returns_only_entering_people(
-    people_data, created_person_list
+        people_data, created_person_list
 ):
     assert len(people_data) == len(
         created_person_list
@@ -110,11 +117,29 @@ def test_person_instance_attribute_wife_and_husband_doesnt_exists():
         tree = ast.parse(file.read())
 
     assert (
-        len(
-            tree.__dict__["body"][0]
-            .__dict__["body"][1]
-            .__dict__["args"]
-            .__dict__["args"]
-        )
-        == 3
+            len(
+                tree.__dict__["body"][0]
+                .__dict__["body"][1]
+                .__dict__["args"]
+                .__dict__["args"]
+            )
+            == 3
     ), "'__init__' should takes only two arguments 'name' and 'age'!"
+
+
+def test_removed_comment():
+    with open(path_to_main(), "r") as file:
+        main_content = file.read()
+
+        assert (
+                "# write your code here" not in main_content
+        ), "You have to remove the unnecessary comment '# write your code here'"
+
+
+def test_double_quotes_instead_of_single():
+    with open(path_to_main(), "r") as file:
+        main_content = file.read()
+
+        assert (
+                "'" not in main_content
+        ), "You have to use a double quotes \"\" instead of single ''"
