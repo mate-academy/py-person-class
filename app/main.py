b9dded6cb69eb3ae36fcf4b4c9e1c24de2fd7fb9
Natalia Stepanenko <natti8006@gmail.com>
2023-03-22 17:14:08 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -10,8 +10,8 @@ class Person:
 def create_person_list(people: list) -> list:
     result_list = [Person(person["name"], person["age"]) for person in people]
     for index, person in enumerate(people):
-        if "wife" in person.keys() and person["wife"] is not None:
+        if person.get("wife") and person["wife"] is not None:
             result_list[index].wife = Person.people[person["wife"]]
-        if "husband" in person.keys() and person["husband"] is not None:
+        if person.get("husband") and person["husband"] is not None:
             result_list[index].husband = Person.people[person["husband"]]
     return result_list
