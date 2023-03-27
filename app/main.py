ccc2317e6b116bab442f06cd05c17a9eef7a9fee
Andriy <an_mydel@gmail.com>
2023-03-27 17:07:32 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -8,7 +8,8 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    [Person(person["name"], person["age"]) for person in people]
+    for person in people:
+        Person(person["name"], person["age"])
     for person in people:
         if "wife" in person and person["wife"]:
             Person.people[person["name"]].wife = Person.people[person["wife"]]
