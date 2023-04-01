25ad9e36726c835dced6ce5a47ac7a943f9d31b2
Denys Belei <denysbelei96>
2023-04-01 17:01:10 +0100

    "solition"

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
+from ast import Str
+
+
 class Person:
-    # write your code here
-    pass
+    people = {}
 
+    def __init__(self, name, age) -> None:
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+        self.name = name
+        self.age = age
+        self.wife = None
+        self.husband = None
+        self.__class__.people[name] = self
+
+    def create_person_list(self, people) -> list:
+
+        person_list = []
+        for human in people:
+            person = Person(human['name'], human['age'])
+            if human['wife'] is not None:
+                person.wife = Person.people[human['wife']]
+                Person.people[human['wife']].husband = person
+            elif human['husband'] is not None:
+                person.husband = Person.people[human['husband']]
+                Person.people[human['husband']].wife = person
+            person_list.append(person)
+        return person_list
