89193230613fceeb81384ef50eb88b0f6777b02f
Denys Belei <denysbelei96>
2023-04-04 19:16:38 +0100

    "solition"

--- app/main.py
+++ app/main.py
@@ -1,27 +1,36 @@
-from ast import Str
-
 
 class Person:
     people = {}
 
-    def __init__(self, name, age) -> None:
-
+    def __init__(self, name, age):
         self.name = name
         self.age = age
         self.wife = None
         self.husband = None
         self.__class__.people[name] = self
 
-    def create_person_list(self, people) -> list:
 
-        person_list = []
-        for human in people:
-            person = Person(human['name'], human['age'])
-            if human['wife'] is not None:
-                person.wife = Person.people[human['wife']]
-                Person.people[human['wife']].husband = person
-            elif human['husband'] is not None:
-                person.husband = Person.people[human['husband']]
-                Person.people[human['husband']].wife = person
-            person_list.append(person)
-        return person_list
+def create_person_list(people: list) -> list:
+    person_list = []
+    for person in people:
+        name = person['name']
+        age = person['age']
+        human = Person(name, age)
+        if person.get('wife') is not None:
+            wife_name = person['wife']
+            wife = Person.people.get(wife_name)
+            if wife is not None:
+                human.wife = wife
+                wife.husband = human
+        elif person.get('husband') is not None:
+            husband_name = person['husband']
+            husband = Person.people.get(husband_name)
+            if husband is not None:
+                human.husband = husband
+                husband.wife = human
+        if human.wife is None:
+            del human.wife
+        if human.husband is None:
+            del human.husband
+        person_list.append(human)
+    return person_list
