1fca95261790db653fdfc98825c81cc79cbb7a6b
Dania Minyaylo <dendan202@gmail.com>
2023-04-04 18:58:17 +0300

    Solution Changed

--- app/main.py
+++ app/main.py
@@ -25,7 +25,6 @@ def create_person_list(people: list) -> list:
                            wife=wife)
             wife.husband = human
         else:
-
             human = Person(people_person["name"], people_person["age"])
         Person.people[people_person["name"]] = human
         list_of_people.append(human)
