536ff9fb3d3fb50be3ebf6b3d047696a7900bd04
Dania Minyaylo <dendan202@gmail.com>
2023-04-04 20:28:22 +0300

    Solution Changed_02

--- app/main.py
+++ app/main.py
@@ -4,30 +4,22 @@ class Person:
     def __init__(self, name: str, age: int, **kwargs) -> None:
         self.name = name
         self.age = age
-        if "husband" in kwargs.keys() and kwargs["husband"] is not None:
-            self.husband = kwargs["husband"]
-        elif "wife" in kwargs.keys() and kwargs["wife"] is not None:
-            self.wife = kwargs["wife"]
         Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    list_of_people = []
+    people_objs = [Person(person["name"], person["age"]) for person in people]
 
-    for people_person in people:
-        human = Person(people_person["name"], people_person["age"])
-        list_of_people.append(human)
+    for person in people:
+        human = Person.people[person["name"]]
 
-    for people_person in people:
-        human = Person.people[people_person["name"]]
-
-        if "husband" in people_person and people_person["husband"] is not None:
-            husband = Person.people[people_person["husband"]]
+        if person.get("husband") is not None:
+            husband = Person.people[person["husband"]]
             human.husband = husband
             husband.wife = human
-        elif "wife" in people_person and people_person["wife"] is not None:
-            wife = Person.people[people_person["wife"]]
+        elif person.get("wife") is not None:
+            wife = Person.people[person["wife"]]
             human.wife = wife
             wife.husband = human
 
-    return list_of_people
+    return people_objs
