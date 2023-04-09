065e6150c3acf1238936d56edc4d81b1572f1450
laskarj <koks.adekvat@gmail.com>
2023-04-09 22:40:06 +0200

    Solution of task "Person class"

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
+from typing import List
+
+
 class Person:
-    # write your code here
-    pass
+
+    people: dict = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list) -> List[Person]:
+    result = [Person(name=data.get("name"),
+                     age=data.get("age")) for data in people]
+
+    for person in people:
+        if person.get("wife") is not None:
+            Person.people[person["name"]].wife \
+                = Person.people[person["wife"]]
+
+        if person.get("husband") is not None:
+            Person.people[person["name"]].husband \
+                = Person.people[person["husband"]]
+
+    return result
