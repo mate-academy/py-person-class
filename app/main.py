7d6da85120af4f3a2fdd701e369c0fe0a5bc6d1c
laskarj <koks.adekvat@gmail.com>
2023-04-10 12:06:05 +0200

    Changed annotation List[Person] > list

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
-from typing import List
-
-
 class Person:
 
-    people: dict = {}
+    people = {}
 
     def __init__(self, name: str, age: int) -> None:
         self.name = name
@@ -11,17 +8,17 @@ class Person:
         Person.people[name] = self
 
 
-def create_person_list(people: list) -> List[Person]:
+def create_person_list(people: list) -> list:
     result = [Person(name=data.get("name"),
                      age=data.get("age")) for data in people]
 
     for person in people:
         if person.get("wife") is not None:
-            Person.people[person["name"]].wife \
-                = Person.people[person["wife"]]
+            Person.people[person.get("name")].wife = \
+                Person.people[person.get("wife")]
 
         if person.get("husband") is not None:
-            Person.people[person["name"]].husband \
-                = Person.people[person["husband"]]
+            Person.people[person.get("name")].husband = \
+                Person.people[person.get("husband")]
 
     return result
