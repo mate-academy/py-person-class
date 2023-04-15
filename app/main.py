36baedf8f9f3e33aa6397b4399bab5f3f87302ed
Denys Belei <denysbelei96>
2023-04-15 18:36:56 +0100

    "personSol"

--- app/main.py
+++ app/main.py
@@ -12,9 +12,11 @@ def create_person_list(people: list[dict[str, str | int | None]]) -> list:
     person_list = []
     for human in people:
         person_list.append(Person(human["name"], human["age"]))
-    for i, human in enumerate(people):
-        if "wife" in human and human["wife"] is not None:
-            person_list[i].wife = Person.people.get(human["wife"])
-        if "husband" in human and human["husband"] is not None:
-            person_list[i].husband = Person.people.get(human["husband"])
+    for human in people:
+        if human.get("wife") is not None:
+            a = Person.people.get(human["wife"])
+            Person.people[human["name"]].wife = a
+        if human.get("husband") is not None:
+            b = Person.people.get(human["husband"])
+            Person.people[human["name"]].husband = b
     return person_list
