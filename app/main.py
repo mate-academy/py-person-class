322fe92a95ce59559aae85c0956cab48873a3bda
Denys Belei <denysbelei96>
2023-04-15 19:41:14 +0100

    "personSolFlake8"

--- app/main.py
+++ app/main.py
@@ -14,9 +14,9 @@ def create_person_list(people: list[dict[str, str | int | None]]) -> list:
         person_list.append(Person(human["name"], human["age"]))
     for human in people:
         if human.get("wife") is not None:
-            a = Person.people.get(human["wife"])
-            Person.people[human["name"]].wife = a
+            A = Person.people.get(human["wife"])
+            Person.people[human["name"]].wife = A
         if human.get("husband") is not None:
-            b = Person.people.get(human["husband"])
-            Person.people[human["name"]].husband = b
+            B = Person.people.get(human["husband"])
+            Person.people[human["name"]].husband = B
     return person_list
