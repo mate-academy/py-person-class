0d9848e2c592060b97a96823968fbdaa54740d5d
Maks Blazhko <makssimm1@gmail.com>
2023-04-17 18:17:08 +0200

    add create_person_list

--- app/main.py
+++ app/main.py
@@ -1,8 +1,35 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name, age) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    result_list = []
+    for persons in people:
+        names = Person(persons.get("name"), persons.get("age"))
+        for relation in persons:
+            if relation == "wife":
+                names.wife = persons.get("husband")
+                # Person.people[persons.get("wife")]
+            if relation == "husband":
+                names.husband = persons.get("husband")
+        print(names.__dict__)
+        result_list.append(names)
+
+    return result_list
+
+
+people = [
+        {"name": "Ross", "age": 30, "wife": "Rachel"},
+        {"name": "Joey", "age": 29, "wife": None},
+        {"name": "Phoebe", "age": 31, "husband": None},
+        {"name": "Chandler", "age": 30, "wife": "Monica"},
+        {"name": "Monica", "age": 32, "husband": "Chandler"},
+        {"name": "Rachel", "age": 28, "husband": "Ross"},
+    ]
+
+create_person_list(people)
