51d599f6f56d74c09cc4488d8e88cb03edf82dd2
Oleksandr Yatsiuk <oleksandr.yatsiuk95@gmail.com>
2023-04-25 21:18:40 +0300

    Solution num2

--- app/main.py
+++ app/main.py
@@ -10,7 +10,10 @@ class Person:
 def create_person_list(people: list) -> list:
     person_list = [Person(p["name"], p["age"]) for p in people]
     for person in people:
-        if person.get("husband"):
+        if person.get("wife"):
+            Person.people[person["name"]].wife = Person.people[person["wife"]]
+    for person in people:
+        if person.get("husband") is not None:
             wife = Person.people[person["name"]]
             husband = Person.people[person["husband"]]
             wife.husband = husband
