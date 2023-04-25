673c149d2e2e3cbd4c75349de2c0594a9c085675
Viktoriia Krasnovyd <114735598+vkrasnovyd@users.noreply.github.com>
2023-04-25 19:04:37 +0200

    Replace 'elif' with 'if' in create_person_list function

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ def create_person_list(people: list) -> list:
     for person in people:
         if person.get("wife"):
             Person.people[person["name"]].wife = Person.people[person["wife"]]
-        elif person.get("husband"):
+        if person.get("husband"):
             Person.people[person["name"]].husband =\
                 Person.people[person["husband"]]
     return [person for person in Person.people.values()]
