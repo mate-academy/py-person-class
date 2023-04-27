1bffec09ad7b5e1ccb211a4bf54e39d84919caca
Vlad Tetruk <vladteteruk01@gmail.com>
2023-04-27 08:54:38 +0300

    'add better annotation'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 class Person:
-    # write your code here
-    pass
+    people = dict()  # name: Person
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.people[self.name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list[Person]:
+    all_people = [Person(human["name"], human["age"]) for human in people]
+    for person in people:
+        if person.get("wife"):
+            Person.people[person["name"]].wife = Person.people[person["wife"]]
+        elif person.get("husband"):
+            Person.people[person["name"]].husband = \
+                Person.people[person["husband"]]
+    return all_people
