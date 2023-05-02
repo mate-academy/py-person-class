af74f16bc3518ff2e243f257233664faf57cf5c6
Andrii Kuzmenkov <kuzmenkov.andr@gmail.com>
2023-05-02 19:46:33 +0300

    Refactored code

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.__class__.people[name] = self
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
@@ -13,9 +13,9 @@ def create_person_list(people: list) -> list:
         person_list.append(Person(person["name"], person["age"]))
 
     for person in people:
-        if "wife" in person.keys() and person["wife"] is not None:
+        if person.get("wife"):
             Person.people[person["name"]].wife = Person.people[person["wife"]]
-        elif "husband" in person.keys() and person["husband"] is not None:
+        elif person.get("husband"):
             Person.people[person["name"]].husband = \
                 Person.people[person["husband"]]
 
