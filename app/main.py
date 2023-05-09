cd281f1dc41c6a750b04aa2de3559e538220465a
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-05-09 16:01:09 +0300

    Change person to self

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people[name] = self
+        self.people[name] = self
 
 
 def create_person_list(people: list[dict]) -> list[Person]:
