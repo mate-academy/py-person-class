f60ab8b1282672d87303a6634dce858547fb4eaf
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-05-09 15:46:31 +0300

    Implement class person task

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list[Person]:
+    person_list = [
+        Person(person["name"], person["age"]) for person in people
+    ]
+
+    for person in people:
+        if person.get("wife"):
+            husband = Person.people[person["name"]]
+            wife = Person.people[person["wife"]]
+            husband.wife = wife
+            wife.husband = husband
+
+    return person_list
