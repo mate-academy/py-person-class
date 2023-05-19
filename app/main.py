98da7d0868e679f62ee9002d1a996b8a70082038
Arina Oltarzhevska <arinaoltarzhevska.ua@gmail.com>
2023-05-19 14:06:07 +0200

    Changes 1.2

--- app/main.py
+++ app/main.py
@@ -10,12 +10,11 @@ class Person:
 def create_person_list(people: list) -> list:
     list_of_people = [Person(person["name"], person["age"])
                       for person in people]
-    for one in people:
-        for key in one:
-            if key == "wife" and one["wife"] is not None:
-                Person.people[one["name"]].wife\
-                    = Person.people[one["wife"]]
-            elif key == "husband" and one["husband"] is not None:
-                Person.people[one["name"]].husband\
-                    = Person.people[one["husband"]]
+    for person in people:
+        wife = person.get("wife")
+        husband = person.get("husband")
+        if wife is not None:
+            Person.people[person["name"]].wife = Person.people[wife]
+        if husband is not None:
+            Person.people[person["name"]].husband = Person.people[husband]
     return list_of_people
