3ccfd1db5e2a9581371dd31479ebf6afa03d3f09
Oknametra <oknametra@gmail.com>
2023-05-20 19:50:15 +0200

    update

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.__class__.people[name] = self
+        self.people[name] = self
 
     @classmethod
     def get_person(cls, name: str) -> "Person":
