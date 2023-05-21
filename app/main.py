989f9dd6cfee800cd5a1d7faac24d405f6ba9f3c
Michael <manchester6913@gmail.com>
2023-05-21 17:27:56 -0400

    Solution_1

--- app/main.py
+++ app/main.py
@@ -8,22 +8,22 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    result = []
-    for person in people:
-        person_object = Person(person["name"], person["age"])\
-            if person["name"] not in Person.people \
-            else Person.people[person["name"]]
-        result.append(person_object)
+    result = [Person(person["name"], person["age"])
+              if person["name"] not in Person.people
+              else Person.people[person["name"]] for person in people]
 
     for person in people:
-        wife_husband = "wife" if "wife" in person else "husband"
+        if "wife" in person:
+            wife_husband = "wife"
+        elif "husband" in person:
+            wife_husband = "husband"
         name_of_wife_husband = person[wife_husband]
 
         if name_of_wife_husband:
             wife_husband_object = None
 
             if name_of_wife_husband not in Person.people:
-                wife_husband_object = Person.people(name_of_wife_husband, 0)
+                wife_husband_object = Person(name_of_wife_husband, 0)
             else:
                 wife_husband_object = Person.people[name_of_wife_husband]
 
