87dc1d628bf0b1f329873c7d11cc10a1107fa7c5
Oleksii Kiselov <crift34@gmail.com>
2023-05-24 11:27:37 -0400

    Implemented creating person instances and getting their list

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.__class__.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    created_people = []
+    for person_data in people:
+        person = Person(person_data["name"], person_data["age"])
+        created_people.append(person)
+
+    for person_data in people:
+        if "wife" in person_data and person_data["wife"] is not None:
+            name = person_data["name"]
+            wife = person_data["wife"]
+            Person.people[name].wife = Person.people[wife]
+            Person.people[wife].husband = Person.people[name]
+
+    return created_people
