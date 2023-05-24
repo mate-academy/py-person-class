fafc3484b5258025e1b3e602cc41144f8a443555
chipa <mezismezis@gmail.com>
2023-05-24 13:38:36 +0300

    =)

--- app/main.py
+++ app/main.py
@@ -11,14 +11,11 @@ def create_person_list(people: list) -> list:
     list_of_completed_people = [Person(i["name"], i["age"]) for i in people]
 
     for index, person in enumerate(people):
-        try:
+        if person.get("wife"):
             wife = Person.people[person["wife"]]
             list_of_completed_people[index].wife = wife
-        except KeyError:
-            try:
-                husband = Person.people[person["husband"]]
-                list_of_completed_people[index].husband = husband
-            except KeyError:
-                pass
+        if person.get("husband"):
+            husband = Person.people[person["husband"]]
+            list_of_completed_people[index].husband = husband
 
     return list_of_completed_people
