48ed4f4b39558341a1b806260158438a94d12d49
Glieb <gleblitvinenko3007@gmail.com>
2023-05-26 14:01:10 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -16,15 +16,16 @@ def create_person_list(people_data: list) -> list:
         name = person_dict["name"]
         age = person_dict["age"]
         person = Person(name=name, age=age)
+        person_list.append(person)
+
+    for person_dict in people_data:
         if "wife" in person_dict and person_dict["wife"] is not None:
             wife_name = person_dict["wife"]
-            if wife_name in Person.people:
-                person.wife = Person.people[wife_name]
-                Person.people[wife_name].husband = person
+            person = Person.people[person_dict["name"]]
+            wife = Person.people[wife_name]
+            person.wife = wife
         elif "husband" in person_dict and person_dict["husband"] is not None:
             husband_name = person_dict["husband"]
-            if husband_name in Person.people:
-                person.husband = Person.people[husband_name]
-                Person.people[husband_name].wife = person
-        person_list.append(person)
+            person = Person.people[person_dict["name"]]
+            person.husband = Person.people[husband_name]
     return person_list
