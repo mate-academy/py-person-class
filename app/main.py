dbe4c9c5179daf033db9ae04626c48efa6bbed84
Makeyev Yevhen <makeyev.ye@gmail.com>
2023-05-26 09:41:19 +0300

    Redundant person addition removed, temporary containers renamed

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class Person:
-    people: dict = {}
+    people = {}
 
     def __init__(self, name: str, age: int) -> None:
         self.name = name
@@ -7,22 +7,22 @@ class Person:
         Person.people[self.name] = self
 
 
-def create_person_list(people: list) -> list:
+def create_person_list(people: list) -> list[Person]:
     spouse_types: set[str] = {"wife", "husband"}
     for person in people:
         if person["name"] in Person.people:
             continue
-        current_person = Person(person["name"], person["age"])
-        Person.people[person["name"]] = current_person
+        current_person: Person = Person(person["name"], person["age"])
         spouse_type: set = (spouse_types & person.keys())
         if len(spouse_type) < 1 or person.get(next(iter(spouse_type))) is None:
             continue
         spouse_name: str = person.get(next(iter(spouse_type)))
         if spouse_name not in Person.people:
-            spouse_info = next((p for p in people
-                                if p["name"] == spouse_name), None)
-            Person.people[spouse_name] = Person(spouse_info["name"],
-                                                spouse_info["age"])
+            spouse_info = next((current_person
+                                for current_person in people
+                                if current_person["name"] == spouse_name),
+                               None)
+            Person(spouse_info["name"], spouse_info["age"])
         spouse_person: Person = Person.people[spouse_name]
         setattr(current_person, next(iter(spouse_type)),
                 spouse_person)
