825dc5e63f60a8cb8851983088dbbb46d5381140
Volodymyr Komarnytskyi <volodimirkomarnickij28@gmail.com>
2023-05-30 15:32:30 +0300

    Renamed item to people_info

--- app/main.py
+++ app/main.py
@@ -10,15 +10,15 @@ class Person:
 def create_person_list(people: list) -> list:
     person_list = []
 
-    for item in people:
-        person = Person(item["name"], item["age"])
+    for people_info in people:
+        person = Person(people_info["name"], people_info["age"])
 
-        wife_name = item.get("wife")
+        wife_name = people_info.get("wife")
         if wife_name in Person.people:
             person.wife = Person.people[wife_name]
             person.wife.husband = person
 
-        husband_name = item.get("husband")
+        husband_name = people_info.get("husband")
         if husband_name in Person.people:
             person.husband = Person.people[husband_name]
             person.husband.wife = person
