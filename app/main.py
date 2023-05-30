1444dcabccf9c26e27271b2c720c37cc50c66031
Volodymyr Komarnytskyi <volodimirkomarnickij28@gmail.com>
2023-05-30 14:26:54 +0300

    Used get() method to get wife/husband from dict and avoided using nested conditions

--- app/main.py
+++ app/main.py
@@ -12,15 +12,16 @@ def create_person_list(people: list) -> list:
 
     for item in people:
         person = Person(item["name"], item["age"])
-        if "wife" in item:
-            wife_name = item["wife"]
-            if wife_name in Person.people:
-                person.wife = Person.people[wife_name]
-                person.wife.husband = person
-        if "husband" in item:
-            husband_name = item["husband"]
-            if husband_name in Person.people:
-                person.husband = Person.people[husband_name]
-                person.husband.wife = person
+
+        wife_name = item.get("wife")
+        if wife_name in Person.people:
+            person.wife = Person.people[wife_name]
+            person.wife.husband = person
+
+        husband_name = item.get("husband")
+        if husband_name in Person.people:
+            person.husband = Person.people[husband_name]
+            person.husband.wife = person
         person_list.append(person)
+
     return person_list
