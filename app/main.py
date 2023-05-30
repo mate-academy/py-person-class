f16b0b712c72fa24b5ccafffc90b436377569c9f
Erik Agayan <erik.agayan1@gmail.com>
2023-05-30 13:49:03 +0300

    wife_name\husband_name vars

--- app/main.py
+++ app/main.py
@@ -13,19 +13,15 @@ def create_person_list(people: list) -> list:
     for pers in people:
         person = Person(pers["name"], pers["age"])
 
-        if "wife" in pers:
-            wife_name = pers["wife"]
+        wife_name = pers.get("wife")
+        if wife_name in Person.people:
+            person.wife = Person.people[wife_name]
+            person.wife.husband = person
 
-            if wife_name in Person.people:
-                person.wife = Person.people[wife_name]
-                person.wife.husband = person
-
-        if "husband" in pers:
-            husband_name = pers["husband"]
-
-            if husband_name in Person.people:
-                person.husband = Person.people[husband_name]
-                person.husband.wife = person
+        husband_name = pers.get("husband")
+        if husband_name in Person.people:
+            person.husband = Person.people[husband_name]
+            person.husband.wife = person
 
         person_list.append(person)
 
