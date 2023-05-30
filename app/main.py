9ce0e7a72b7370fc196b284d362d821e2c028d0d
Yevhenii Iovchu <persie.iovchu@gmail.com>
2023-05-30 14:27:51 +0300

    Got access to class atr thgrough class name, simplified code from 4 lines to 1 line, var 'wife_name' raised avobe if and changed condition for entering inside. Alse deleted additional connect between husband and wife that already existed

--- app/main.py
+++ app/main.py
@@ -4,26 +4,19 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.__class__.people[name] = self
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
     person_list = []
     for person_data in people:
-        name = person_data["name"]
-        age = person_data["age"]
-        person = Person(name, age)
-        person_list.append(person)
+        person_list.append(Person(person_data["name"], person_data["age"]))
 
     for person_data in people:
         person = Person.people[person_data["name"]]
-        if person_data.get("wife") is not None:
-            wife_name = person_data["wife"]
+        wife_name = person_data["wife"] if "wife" in person_data else None
+        if wife_name:
             person.wife = Person.people[wife_name]
             Person.people[wife_name].husband = person
-        if person_data.get("husband") is not None:
-            husband_name = person_data["husband"]
-            person.husband = Person.people[husband_name]
-            Person.people[husband_name].wife = person
 
     return person_list
