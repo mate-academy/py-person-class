cca802cde0b00bc0df1a45bb76ec84308d52c47f
Olha Tryhub <olha.tryhub@nure.ua>
2023-05-30 21:24:00 +0300

    Solution for Person Class

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    people_list_obj = [Person(pers["name"], pers["age"]) for pers in people]
+    for i in range(len(people)):
+        if people[i].get("wife"):
+            people_list_obj[i].wife = Person.people[people[i]["wife"]]
+        if people[i].get("husband"):
+            people_list_obj[i].husband\
+                = Person.people[people[i]["husband"]]
+
+    return people_list_obj
