53f1a07e2c086e58a461387cd5c5db703a68eadb
Volodymyr Holovko <m4rvel1341@gmail.com>
2023-05-30 14:10:04 +0200

    Creating a list of people

--- app/main.py
+++ app/main.py
@@ -1,8 +1,39 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int, *spouse) -> None:
+        self.name = name
+        self.age = age
+        self.spouse = spouse
+        self.__class__.people[name] = self
+
+    def __repr__(self) -> str:
+        return f"Person(name=\"{self.name}\", age={self.age})"
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+    for pers in people:
+        name = pers["name"]
+        age = pers["age"]
+        spouse_name = pers.get("wife") or pers.get("husband")
+
+        if spouse_name:
+            spouse = Person.people.get(spouse_name)
+            if spouse is None:
+                spouse = Person(spouse_name, None)
+
+            if "wife" in pers:
+                person = Person(name, age, spouse)
+                person.wife = spouse
+                spouse.husband = person
+            else:
+                person = Person(name, age, spouse)
+                person.husband = spouse
+                spouse.wife = person
+        else:
+            person = Person(name, age, None)
+
+        person_list.append(person)
+
+    return person_list
