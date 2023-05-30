000c4e704802c3a20807aae9a7267c4577e05a5c
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-05-30 18:00:56 +0300

    Implement .get() method

--- app/main.py
+++ app/main.py
@@ -15,11 +15,10 @@ def create_person_list(people: list) -> list:
         person_list.append(Person(name, age))
 
     for person in people:
-        if "wife" in person and person["wife"] in Person.people:
-            Person.people[person["name"]].wife = Person.people[person["wife"]]
-        if "husband" in person and person["husband"] in Person.people:
-            Person.people[person["name"]].husband = Person.people[
-                person["husband"]
-            ]
+        somebody = Person.people[person["name"]]
+        if person.get("wife"):
+            somebody.wife = Person.people[person["wife"]]
+        if person.get("husband"):
+            somebody.husband = Person.people[person["husband"]]
 
     return person_list
