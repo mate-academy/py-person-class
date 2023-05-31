9c9989fb1c86622648b1c71433034eb26f867c17
Vlad <vladislav.zhyla@gmail.com>
2023-05-31 20:49:27 +0300

    Conditions was simplified

--- app/main.py
+++ app/main.py
@@ -16,10 +16,10 @@ def create_person_list(people: list) -> list:
     for person in people:
         current_person = Person.people[person["name"]]
 
-        if "wife" in person.keys() and person["wife"]:
+        if person.get("wife"):
             current_person.wife = Person.people[person["wife"]]
 
-        if "husband" in person.keys() and person["husband"]:
+        if person.get("husband"):
             current_person.husband = Person.people[person["husband"]]
 
     return persons_ls
