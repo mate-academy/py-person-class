37f210b52ccefb8281184c1815a3769b2ada93bd
Mykhailo Korotia <mr.anderson528491@gmail.com>
2023-06-01 10:06:53 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -4,8 +4,6 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.wife = None
-        self.husband = None
         self.people[name] = self
 
 
@@ -21,28 +19,3 @@ def create_person_list(people: list[dict]) -> list[Person]:
         elif person_dict.get("husband") is not None:
             person.husband = Person.people[person_dict["husband"]]
     return person_list
-
-
-people = [
-    {"name": "Ross", "age": 30, "wife": "Rachel"},
-    {"name": "Joey", "age": 29, "wife": None},
-    {"name": "Rachel", "age": 28, "husband": "Ross"}
-]
-
-person_list = create_person_list(people)
-
-print(isinstance(person_list[0], Person))  # True
-print(person_list[0].name == "Ross")  # True
-print(person_list[0].wife is person_list[2])  # True
-print(person_list[0].wife.name == "Rachel")  # True
-
-print(person_list[1].name == "Joey")  # True
-print(person_list[1].wife is None)  # True
-
-print(isinstance(person_list[2], Person))  # True
-print(person_list[2].name == "Rachel")  # True
-print(person_list[2].husband is person_list[0])  # True
-print(person_list[2].husband.name == "Ross")  # True
-print(person_list[2].husband.wife is person_list[2])  # True
-
-print(Person.people)
