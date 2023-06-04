bff30937cf8a9a900b3392f10fc2c0a2ea94a3ff
Yuliia <verbikyullya@gmail.com>
2023-06-04 13:38:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
+# what method should it be? static or class?
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    result = []
+    for person in people:
+        Person(person.get("name"), person.get("age"))
+
+    for person in people:
+        if person.get("wife") is not None:
+            Person.people.get(person.get("name")).wife \
+                = Person.people.get(person.get("wife"))
+
+        if person.get("husband") is not None:
+            Person.people.get(person.get("name")).husband \
+                = Person.people.get(person.get("husband"))
+
+        result.append(Person.people.get(person.get("name")))
+
+    return result
