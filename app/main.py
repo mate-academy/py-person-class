5d744bb639044b4cf4caf0c3268273a28d588d9f
Anastasia <anastasiiya.p@gmail.com>
2023-06-06 18:52:32 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -11,28 +11,35 @@ def create_person_list(people: list) -> list:
 
     for human in people:
         new_person = Person(name=human["name"], age=human["age"])
-        if "wife" in human and human["wife"]:
-            new_person.wife = human["wife"]
-        if "husband" in human and human["husband"]:
-            new_person.husband = human["husband"]
 
         Person.people.update({human["name"]: new_person})
         person_list.append(new_person)
 
-    for _, person_instance in Person.people.items():
-        if hasattr(person_instance, "wife") \
-                and person_instance.wife in Person.people:
+    filt_wife = [
+        human for human in people if "wife" in human and human["wife"]
+    ]
+    filt_husband = [
+        human for human in people if "husband" in human and human["husband"]
+    ]
 
-            person_instance.wife = [
-                person_class for name, person_class in Person.people.items()
-                if person_instance.wife == name
-            ][0]
-        if hasattr(person_instance, "husband") \
-                and person_instance.husband in Person.people:
+    for wife in filt_wife:
+        for husband in filt_husband:
 
-            person_instance.husband = [
-                person_class for name, person_class in Person.people.items()
-                if person_instance.husband == name
-            ][0]
+            filt_wife_person = [
+                instance for instance in Person.people.values()
+                if instance.name == wife["wife"]
+            ]
+            filt_husband_person = [
+                instance for instance in Person.people.values()
+                if instance.name == husband["husband"]
+            ]
+
+            filt_husband_person[0].wife = filt_wife_person[0] \
+                if filt_wife_person[0] \
+                else wife["wife"]
+
+            filt_wife_person[0].husband = filt_husband_person[0] \
+                if filt_husband_person[0] \
+                else husband["husband"]
 
     return person_list
