7075f8e0a0e2d53b0301d7847479d0212a3a1ff6
Anastasia <anastasiiya.p@gmail.com>
2023-06-07 22:19:46 +0300

    Fixed2

--- app/main.py
+++ app/main.py
@@ -5,41 +5,27 @@ class Person:
         self.name = name
         self.age = age
 
+        self.people.update({self.name: self})
+
 
 def create_person_list(people: list) -> list:
-    person_list = []
+    person_list = [
+        Person(name=human["name"], age=human["age"]) for human in people
+    ]
 
     for human in people:
-        new_person = Person(name=human["name"], age=human["age"])
-
-        Person.people.update({human["name"]: new_person})
-        person_list.append(new_person)
-
-    filt_wife = [
-        human for human in people if "wife" in human and human["wife"]
-    ]
-    filt_husband = [
-        human for human in people if "husband" in human and human["husband"]
-    ]
-
-    for wife in filt_wife:
-        for husband in filt_husband:
-
-            filt_wife_person = [
-                instance for instance in Person.people.values()
-                if instance.name == wife["wife"]
-            ]
-            filt_husband_person = [
-                instance for instance in Person.people.values()
-                if instance.name == husband["husband"]
+        if "wife" in human and human["wife"]:
+            Person.people[
+                human["name"]
+            ].wife = Person.people[
+                human["wife"]
             ]
 
-            filt_husband_person[0].wife = filt_wife_person[0] \
-                if filt_wife_person[0] \
-                else wife["wife"]
-
-            filt_wife_person[0].husband = filt_husband_person[0] \
-                if filt_husband_person[0] \
-                else husband["husband"]
+        if "husband" in human and human["husband"]:
+            Person.people[
+                human["name"]
+            ].husband = Person.people[
+                human["husband"]
+            ]
 
     return person_list
