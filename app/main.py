82f5c8af264a02a06c17ba6a29533fd0d4bf9d9f
Bohdan Dovhan <dovganbv94@gmail.com>
2023-06-08 15:33:45 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people[name] = self
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
