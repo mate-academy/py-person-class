57d179e63185f58044c21cb530a698e5aa248fbe
Nazarii Schcur <nazae530@gmail.com>
2023-06-20 20:46:41 +0300

    corrected the comment

--- app/main.py
+++ app/main.py
@@ -6,18 +6,18 @@ class Person:
         self.age = age
         self.wife = None
         self.husband = None
-        self.__class__.people[name] = self
+        self.people[name] = self
 
 
 def create_person_list(people_data: list) -> list:
     people = []
     for person_dict in people_data:
-        name = person_dict["name"]
-        age = person_dict["age"]
+        name = person_dict.get("name")
+        age = person_dict.get("age")
         person = Person(name, age)
 
         if "wife" in person_dict and person_dict["wife"] is not None:
-            wife_name = person_dict["wife"]
+            wife_name = person_dict.get("wife")
             if wife_name in Person.people:
                 person.wife = Person.people[wife_name]
                 Person.people[wife_name].husband = person
@@ -26,7 +26,7 @@ def create_person_list(people_data: list) -> list:
             del person.wife
 
         if "husband" in person_dict and person_dict["husband"] is not None:
-            husband_name = person_dict["husband"]
+            husband_name = person_dict.get("husband")
             if husband_name in Person.people:
                 person.husband = Person.people[husband_name]
                 Person.people[husband_name].wife = person
