7c8624d31553b8197c72ee9bc6f393f000ff8bef
Nazarii Schcur <nazae530@gmail.com>
2023-06-24 21:23:17 +0300

    fix bag

--- app/main.py
+++ app/main.py
@@ -4,33 +4,34 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.wife = None
-        self.husband = None
         self.people[name] = self
 
 
 def create_person_list(people_data: list) -> list:
     people = []
+
     for person_dict in people_data:
         name = person_dict.get("name")
         age = person_dict.get("age")
-        person = Person(name, age)
 
-        if "wife" in person_dict and person_dict["wife"] is not None:
+        person = Person(name, age)
+        people.append(person)
+
+    for person_dict in people_data:
+        person = people[people_data.index(person_dict)]
+
+        if person_dict.get("wife") is not None:
             wife_name = person_dict.get("wife")
+
             if wife_name in Person.people:
                 person.wife = Person.people[wife_name]
                 Person.people[wife_name].husband = person
 
-        if hasattr(person, "wife") and person.wife is None:
-            del person.wife
-
-        if "husband" in person_dict and person_dict["husband"] is not None:
+        if person_dict.get("husband") is not None:
             husband_name = person_dict.get("husband")
+
             if husband_name in Person.people:
                 person.husband = Person.people[husband_name]
                 Person.people[husband_name].wife = person
-
-        people.append(person)
 
     return people
