85242f3a57502f1724585d082b21c5cff55b060c
Ivan Zhaivoronok <ivan.zhaivoronok@ukr.net>
2023-06-26 15:50:29 +0300

    Fixed bugs

--- app/main.py
+++ app/main.py
@@ -4,17 +4,22 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.__class__.people[name] = self
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
     person_list = list()
     for person in people:
-        _ = Person(person["name"], person["age"])
-    
+        person_list.append(Person(person["name"], person["age"]))
+
     for person in people:
         if person.get("wife"):
-            Person.people.get(person["name"]).wife = Person.people.get(person["wife"])
+            Person.people.get(
+                person["name"]
+            ).wife = Person.people.get(person["wife"])
         if person.get("husband"):
-            Person.people.get(person["name"]).husband = Person.people.get(person["husband"])
-    return list(Person.people.values())
+            Person.people.get(
+                person["name"]
+            ).husband = Person.people.get(person["husband"])
+
+    return person_list
