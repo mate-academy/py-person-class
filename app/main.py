82620d7691a9e7ae6bffe6a73791c6995372581e
Borys Kirichenko <boryskirichenko99@gmail.com>
2023-06-26 19:29:41 +0200

    all tests passed

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+    for person in people:
+        name = person.get("name")
+        age = person.get("age")
+        if name is not None and age is not None:
+            person_list.append(Person(name, age))
+    for person in people:
+        if "wife" in person and person["wife"] is not None:
+            wife = Person.people[person["wife"]]
+            Person.people[person["name"]].wife = wife
+        if "husband" in person and person["husband"] is not None:
+            husband = Person.people[person["husband"]]
+            Person.people[person["name"]].husband = husband
+    return person_list
