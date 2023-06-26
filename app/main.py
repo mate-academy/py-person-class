b4788bb40ec03436b8c4e25af77aa17b6dc3f3d1
andriy-py <andriy.omelya@gmail.com>
2023-06-26 23:15:06 +0300

    corrections have been made, the code has been changed

--- app/main.py
+++ app/main.py
@@ -4,16 +4,23 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.__class__.people[name] = self
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    persons_list = [Person(person["name"], person["age"]) for person in people]
+    # First pass: Create all Person instances
+    for person_dict in people:
+        Person(person_dict["name"], person_dict["age"])
 
-    for person, person_dict in zip(persons_list, people):
-        if "wife" in person_dict and person_dict["wife"] is not None:
-            person.wife = Person.people[person_dict["wife"]]
-        elif "husband" in person_dict and person_dict["husband"] is not None:
-            person.husband = Person.people[person_dict["husband"]]
+    # Second pass: Assign spouses
+    for person_dict in people:
+        person = Person.people[person_dict["name"]]
+        wife = person_dict.get("wife")
+        husband = person_dict.get("husband")
 
-    return persons_list
+        if wife is not None:
+            person.wife = Person.people.get(wife)
+        if husband is not None:
+            person.husband = Person.people.get(husband)
+
+    return list(Person.people.values())
