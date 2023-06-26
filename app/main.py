83e046c092fd073d195c3e656399f1473d9b5b0c
Nazarii Schcur <nazae530@gmail.com>
2023-06-26 17:02:44 +0300

    fix bag 26.06

--- app/main.py
+++ app/main.py
@@ -12,19 +12,19 @@ def create_person_list(people_data: list[dict]) -> list[Person]:
                      person_dict.get("age"))
               for person_dict in people_data]
 
-    for person_dict, person in zip(people_data, people):
+    for person_dict in people_data:
         if person_dict.get("wife") is not None:
             wife_name = person_dict.get("wife")
 
             if wife_name in Person.people:
-                person.wife = Person.people[wife_name]
-                Person.people[wife_name].husband = person
+                Person.people[person_dict["name"]].wife = \
+                    Person.people[wife_name]
 
         if person_dict.get("husband") is not None:
             husband_name = person_dict.get("husband")
 
             if husband_name in Person.people:
-                person.husband = Person.people[husband_name]
-                Person.people[husband_name].wife = person
+                Person.people[person_dict["name"]].husband = \
+                    Person.people[husband_name]
 
     return people
