c3b817ceeaf8dd8339f286e5f17fa3754f1229ef
Ivan Zhaivoronok <ivan.zhaivoronok@ukr.net>
2023-06-26 11:19:07 +0300

    Finish task

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 class Person:
     people = dict()
-    
+
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
@@ -8,4 +8,16 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    pass    
+    person_list = list()
+    for person in people:
+        temp_person = Person(person["name"], person["age"])
+        if person.get("wife"):
+            temp_person.wife = Person.people.get(person["wife"])
+            if temp_person.wife:
+                temp_person.wife.husband = temp_person
+        if person.get("husband"):
+            temp_person.husband = Person.people.get(person["husband"])
+            if temp_person.husband:
+                temp_person.husband.wife = temp_person
+        person_list.append(temp_person)
+    return person_list
