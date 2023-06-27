33395c7370caf3bd66514dd1dbad0ed0182cfd2f
Vladyslav Shepilov <shepsterya@gmail.com>
2023-06-27 23:27:36 +0300

    refactor

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people[name] = self
+        self.people[name] = self
 
 
 def create_person_list(list_people: list[dict]) -> list[Person]:
@@ -12,18 +12,14 @@ def create_person_list(list_people: list[dict]) -> list[Person]:
                           person_dict["age"]) for person_dict in list_people]
 
     for person_dict in list_people:
-        if "husband" in person_dict and person_dict["husband"] is not None:
-            curr_person_name: str = person_dict["name"]
-            husband_name: str = person_dict["husband"]
-            curr_person = Person.people[curr_person_name]
-            husband_person = Person.people[husband_name]
-            curr_person.husband = husband_person
+        if person_dict.get("husband") is not None:
+            curr_person = Person.people[person_dict["name"]]
+            husband = Person.people[person_dict["husband"]]
+            curr_person.husband = husband
 
-        elif "wife" in person_dict and person_dict["wife"] is not None:
-            curr_person_name: str = person_dict["name"]
-            wife_name: str = person_dict["wife"]
-            curr_person = Person.people[curr_person_name]
-            wife_person = Person.people[wife_name]
-            curr_person.wife = wife_person
+        elif person_dict.get("wife") is not None:
+            curr_person = Person.people[person_dict["name"]]
+            wife = Person.people[person_dict["wife"]]
+            curr_person.wife = wife
 
     return people_list
