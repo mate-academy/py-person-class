ba7af4867406dc8810d68440e16570e3aacc5fdd
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-06-28 00:54:32 +0300

    added changed function logic

--- app/main.py
+++ app/main.py
@@ -7,30 +7,28 @@ class Person:
         self.people[self.name] = self
 
 
-def add_names_links(people: list) -> list[Person]:
-    for item in people:
-        for compere_item in people:
-            compere_item_dict = compere_item.__dict__
-            if item.name != compere_item.name \
-                    and item.name in compere_item_dict.values():
-                attr = str([x for x in compere_item_dict.keys()
-                            if compere_item_dict[x] == item.name])
-                setattr(compere_item, attr[2:-2],
-                        Person.people.get(item.name))
-    return people
+def get_differ(variable_one: str, variable_two: str,
+               comparative: list | dict
+               ) -> str:
+    return variable_one if variable_one in comparative \
+        else variable_two if variable_two in comparative else ""
 
 
-def create_person_list(people: list[dict]) -> list:
+def create_person_list(people: list[dict]) -> list[Person]:
+    variable_one, variable_two = "wife", "husband"
     persons = []
     for person in people:
-        init_name, init_age = person["name"], person["age"]
-        appended_person = Person(
-            init_name, init_age)
-        rests = {key: value for key, value in person.items()
-                 if value not in [init_name, init_age]
-                 and value is not None}
-        for key, value in rests.items():
-            setattr(appended_person, key, value)
+        appended_person = Person(person["name"], person["age"])
+        differ = get_differ(variable_one, variable_two,
+                            list(person.keys()))
+        if differ and person[differ] is not None:
+            setattr(appended_person, differ, person[differ])
         persons.append(appended_person)
-    persons = add_names_links(people=persons)
+
+    for cls_person in persons:
+        cls_differ = get_differ(variable_one, variable_two,
+                                cls_person.__dict__)
+        if cls_differ:
+            setattr(cls_person, cls_differ,
+                    Person.people.get(cls_person.__dict__[cls_differ]))
     return persons
