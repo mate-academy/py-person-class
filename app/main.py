a40d3508fab3e13d3202cc183df7770546f4a0bf
Bohdan Liutko <luitko007@gmail.com>
2023-06-27 18:39:31 +0000

    Updated solution

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.people.update({name: self})
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
@@ -13,21 +13,15 @@ def create_person_list(people: list) -> list:
     person_list = []
     for person in people:
         person_list.append(Person(person.get("name"), person.get("age")))
-    for i in range(len(people)):
-        if people[i].get("wife") is not None:
-            person_list[i].wife = Person.people[people[i].get("wife")]
-        elif people[i].get("husband") is not None:
-            person_list[i].husband = Person.people[people[i].get("husband")]
+
+    for person in people:
+        if person.get("wife"):
+            Person.people.get(
+                person["name"]
+            ).wife = Person.people.get(person["wife"])
+        if person.get("husband"):
+            Person.people.get(
+                person["name"]
+            ).husband = Person.people.get(person["husband"])
 
     return person_list
-
-# people_list = []
-#     for person in people:
-#         people_list.append(Person(person["name"], person["age"]))
-#     for i in range(len(people)):
-#             if people[i]["wife"] is not None:
-#                 people_list[i].wife = Person.people[people[i]["wife"]]
-#             elif people[i]["husband"] is not None:
-#                 people_list[i].husband = Person.people[people[i]["husband"]]
-
-#     return people_list
