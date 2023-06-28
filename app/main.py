c6ea9a1fcd829909ebc5b1479405a6c50b89f841
Vitalii Safian <viisaf.prog.gt@gmail.com>
2023-06-28 22:06:19 +0300

    corrected code names

--- app/main.py
+++ app/main.py
@@ -4,23 +4,20 @@ class Person:
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
-        self.people[self.name] = self
+        self.people[name] = self
 
 
 def set_family_link(status: str, person: dict) -> None:
-    if status in person.keys() \
-            and person[status] in Person.people.keys():
-        setattr(Person.people[person["name"]],
-                status, Person.people[person[status]])
+    people_dict = Person.people
+    if status in person and person[status] in people_dict:
+        setattr(people_dict[person["name"]],
+                status, people_dict[person[status]])
 
 
 def create_person_list(people: list[dict]) -> list[Person]:
     wife, husband = "wife", "husband"
-    persons = []
-    for person_info in people:
-        appended_person = Person(person_info["name"], person_info["age"])
-        persons.append(appended_person)
-
+    persons = [Person(person["name"], person["age"])
+               for person in people]
     for person in people:
         set_family_link(wife, person)
         set_family_link(husband, person)
