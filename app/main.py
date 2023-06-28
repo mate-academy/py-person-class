38815203b3302f8a84308aac85d81efc4fdc585c
Dmytro Beschastnyi <d.beschastnyi@icloud.com>
2023-06-28 09:40:09 +0200

    wife/husbant is not None fixed

--- app/main.py
+++ app/main.py
@@ -13,9 +13,9 @@ def create_person_list(people: list) -> list:
     for person in people:
         result.append(Person(name=person["name"], age=person["age"]))
     for i in range(len(people)):
-        if people[i].get("wife"):
+        if people[i].get("wife") is not None:
             result[i].wife = Person.people[people[i].get("wife")]
-        if people[i].get("husband"):
+        if people[i].get("husband") is not None:
             result[i].husband = Person.people[people[i].get("husband")]
 
     return result
