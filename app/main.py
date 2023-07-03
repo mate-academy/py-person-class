f30208e9226d3a8ceae47e3b51f7a1fa71e7b570
lubart2517 <lubomirvihvatniuk@gmail.com>
2023-07-03 16:23:48 +0300

    implemented class and function

--- app/main.py
+++ app/main.py
@@ -1,8 +1,34 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(
+            self,
+            name: str,
+            age: str
+    ) -> None:
+        self.name = name
+        self.age = age
+        self.people[name] = self
+
+    def set_spouse(
+            self,
+            spouse_type: str,
+            spouse_name: str
+    ) -> None:
+        switcher = {"wife": "husband", "husband": "wife"}
+        if spouse_name and self.people.get(spouse_name):
+            setattr(self, spouse_type, self.people[spouse_name])
+            setattr(self.people[spouse_name], switcher[spouse_type], self)
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    result = []
+    for human in people:
+        person = Person(human["name"], human["age"])
+        if human.get("husband"):
+            person.set_spouse("husband", human["husband"])
+        if human.get("wife"):
+            person.set_spouse("wife", human["wife"])
+        result.append(person)
+    return result
