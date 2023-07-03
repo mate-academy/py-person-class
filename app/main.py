7736b2dd218b5619d2a43fb89507223e878730ee
Alla Kuksa <alla.kuksa92@gmail.com>
2023-07-03 20:54:14 +0300

    Changed mistakes

--- app/main.py
+++ app/main.py
@@ -9,16 +9,16 @@ class Person:
 
 def create_person_list(people: list) -> list:
     person_list = []
-    for one_person in people:
-        person = Person(one_person["name"], one_person["age"])
+    for person in people:
+        person = Person(person["name"], person["age"])
         person_list.append(person)
 
     for one_person in people:
         person = Person.people[one_person["name"]]
-        if "wife" in one_person and one_person["wife"] is not None:
+        if one_person.get("wife") is not None:
             person.wife = Person.people[one_person["wife"]]
             person.wife.husband = person
-        elif "husband" in one_person and one_person["husband"] is not None:
+        elif one_person.get("husband") is not None:
             person.husband = Person.people[one_person["husband"]]
             person.husband.wife = person
 
