ebe9fdcb6124cace5b72c15cdc1d9ef2012eaa5e
makukh <makukh@gmail.com>
2023-07-07 17:04:35 +0300

    save02

--- app/main.py
+++ app/main.py
@@ -1,16 +1,26 @@
 class Person:
+    people = {}
 
-    people = {Person.name: Person}
-
-    def __init__(self, name: str, age: int):
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
+        self.wife = None
+        self.husband = None
+        self.people[name] = self
 
     def create_person_list(people: list) -> list:
-        for individual in people:
-            Person(
-                name=individual["name"],
-                age=individual["age"],
-            )
-            if "wife" in individual:
-                Person["wife"] =
+        persons = []
+        for person in people:
+            name = person["name"]
+            age = person["age"]
+            new_person = Person(name, age)
+            spouse_name = person.get("wife") or person.get("husband")
+            if spouse_name:
+                if spouse_name in Person.people:
+                    spouse = Person.people[spouse_name]
+                    if "wife" in person:
+                        new_person.wife = spouse
+                    else:
+                        new_person.husband = spouse
+            persons.append(new_person)
+        return persons
