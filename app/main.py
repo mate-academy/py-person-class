ec7b88c7e91f727f340b6f1c13747a4428e690b9
Andrew <67094594+MishkaGAMII@users.noreply.github.com>
2023-07-09 10:03:42 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 class Person:
     people = {}
-    
-    def __init__(self, name : str, age : int) -> None:
+
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people[name] = self
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
