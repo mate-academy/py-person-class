482274de50accea0104d1a116a7fb26a7640df2a
makukh <makukh@gmail.com>
2023-07-10 21:17:06 +0300

    Solution, tests passed

--- app/main.py
+++ app/main.py
@@ -4,24 +4,27 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.wife = None
-        self.husband = None
         self.people[name] = self
 
-    @staticmethod
-    def create_person_list(people: list) -> list["Person"]:
-        persons = []
-        for person in people:
-            name = person["name"]
-            age = person["age"]
-            new_person = Person(name, age)
-            spouse_name = person.get("wife") or person.get("husband")
-            if spouse_name:
-                if spouse_name in Person.people:
-                    spouse = Person.people[spouse_name]
-                    if "wife" in person:
-                        new_person.wife = spouse
-                    else:
-                        new_person.husband = spouse
-            persons.append(new_person)
-        return persons
+
+def create_person_list(people: list) -> list:
+    persons = []
+
+    for person in people:
+
+        new_person = Person(person["name"], person["age"])
+        spouse_name = person.get("wife") or person.get("husband")
+
+        if spouse_name:
+            if spouse_name in Person.people:
+                spouse = Person.people[spouse_name]
+                if "wife" in person:
+                    new_person.wife = spouse
+                    spouse.husband = new_person
+                elif "husband" in person:
+                    new_person.husband = spouse
+                    spouse.wife = new_person
+
+        persons.append(new_person)
+
+    return persons
