d9a5f9697ce2a19225f36ee34ec062230716f15f
Andrew <67094594+MishkaGAMII@users.noreply.github.com>
2023-07-10 10:37:14 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,13 +9,15 @@ class Person:
 
 def create_person_list(people: list) -> list:
     result = []
-
+    
     for human in people:
         result.append(Person(human["name"], human["age"]))
     for person_index in range(len(people)):
         if people[person_index].get("wife") is not None:
-            result[person_index].wife = Person.people[people[person_index].get("wife")]
+            wife = people[person_index].get("wife")
+            result[person_index].wife = Person.people[wife]
         if people[person_index].get("husband") is not None:
-            result[person_index].husband = Person.people[people[person_index].get("husband")]
+            husband = people[person_index].get("husband")
+            result[person_index].husband = Person.people[husband]
 
     return result
