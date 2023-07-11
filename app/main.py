b3361baec0b0b93eefdb9f39cffa4fd3c191739e
WoyDiam <freedom.channel2003@gmail.com>
2023-07-11 22:03:46 +0300

    Used get method

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
+
         self.__class__.people[name] = self
 
     def __repr__(self) -> str:
@@ -12,19 +13,22 @@ class Person:
 
 def create_person_list(people: list) -> list:
     person_list = []
+    person_dict = {}
+
     for person_data in people:
         name = person_data["name"]
         age = person_data["age"]
         person = Person(name, age)
         person_list.append(person)
+        person_dict[name] = person
+
     for person_data in people:
-        person = Person.people[person_data["name"]]
-        if "wife" in person_data and person_data["wife"] is not None:
-            wife_name = person_data["wife"]
-            wife = Person.people[wife_name]
-            person.wife = wife
-        if "husband" in person_data and person_data["husband"] is not None:
-            husband_name = person_data["husband"]
-            husband = Person.people[husband_name]
-            person.husband = husband
+        person = person_dict[person_data["name"]]
+
+        if person_data.get("wife"):
+            person.wife = person_dict.get(person_data["wife"])
+
+        if person_data.get("husband"):
+            person.husband = person_dict.get(person_data["husband"])
+
     return person_list
