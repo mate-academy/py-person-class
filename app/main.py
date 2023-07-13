ccec1b3d4678d0fd1041524c4fdd0e7ea9c2878d
Andrii Ishchenko <iscenkoa@gmail.com>
2023-07-13 17:10:17 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -11,9 +11,9 @@ def create_person_list(people: list) -> list:
     person_list = [Person(human["name"], human["age"]) for human in people]
     for human in people:
         if human.get("wife"):
-            husband = Person.people[human["name"]]
-            wife = Person.people[human["wife"]]
-            husband.wife = wife
-            wife.husband = husband
+            Person.people[human["name"]].wife = Person.people[human["wife"]]
+        if human.get("husband"):
+            Person.people[human["name"]].husband \
+                = Person.people[human["husband"]]
 
     return person_list
