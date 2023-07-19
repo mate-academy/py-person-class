c488d18f169850d1b06f8a6cbf4a2a97fa81c487
Nikita Plotnytskyi <Nikita.plotnitckiy@gmail.com>
2023-07-19 03:52:27 +0300

    personality class implementation

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    list_person = []
+    for human in people:
+        new_person = Person(name=human["name"], age=human["age"])
+        list_person.append(new_person)
+    for human in people:
+        if human.get("wife"):
+            Person.people[human["name"]].wife = Person.people[human["wife"]]
+        if human.get("husband"):
+            Person.people[human["name"]].husband = \
+                Person.people[human["husband"]]
+    return list_person
