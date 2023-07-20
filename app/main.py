e1395ed9a081e6bb2fdc008b5acc3d546ded6059
Ostap <ostapbojko04@gmail.com>
2023-07-20 13:04:03 +0300

    py_person3

--- app/main.py
+++ app/main.py
@@ -9,28 +9,19 @@ class Person:
 
 def create_person_list(people: list[dict[str, str]]) -> list["Person"]:
     person_list = []
-    person_dict = {}
 
     # Create Person instances
+    person_list = [Person(person_data["name"],
+                          person_data["age"]) for person_data in people]
+
     for person_data in people:
         name = person_data["name"]
-        age = person_data["age"]
-        person = Person(name, age)
-        person_list.append(person)
-
-        person_dict[name] = person
-
-        # Add wife/husband attributes
-        for person_data in people:
-            name = person_data["name"]
-            spouse_name = (person_data.get("wife")
-                           or person_data.get("husband"))
-            if spouse_name:
-                spouse = person_dict.get(spouse_name)
-
-                if spouse:
-                    if "wife" in person_data:
-                        person_dict[name].wife = spouse
-                    spouse.husband = person_dict.get(name)
+        spouse_name = (person_data.get("wife") or person_data.get("husband"))
+        if spouse_name:
+            spouse = Person.people.get(spouse_name)
+            if spouse:
+                if "wife" in person_data:
+                    Person.people[name].wife = spouse
+                spouse.husband = Person.people.get(name)
 
     return person_list
