ce7f0d70725231ce17a3e2b301c14d1fcb84f61f
Юрий <you@exampleabc1234567892777@gmail.com>
2023-07-21 18:10:57 +0300

    'Solution1'

--- app/main.py
+++ app/main.py
@@ -1,66 +1,38 @@
 class Person:
-    people = {}  # Initialize as an empty dictionary
 
-    def __init__(self, name, age, wife=None, husband=None):
-        self.name = name
-        self.age = age
-        self.wife = wife
-        self.husband = husband
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name: str = name
+        self.age: int = age
+        self.wife = None
+        self.husband = None
         Person.people[name] = self
 
+    def get_wife(self) -> None:
 
-def create_person_list(people):
-    for p in people:
-        name = p["name"]
-        age = p["age"]
-        spouse_name = p.get("wife") or p.get("husband")
+        return self.wife
+
+    def get_husband(self) -> None:
+
+        return self.husband
+
+
+def create_person_list(people: [dict]) -> None:
+    person_list = []
+
+    for person_data in people:
+        name = person_data["name"]
+        age = person_data["age"]
         person = Person(name, age)
-        if spouse_name:
-            if spouse_name in Person.people:
-                spouse = Person.people[spouse_name]
-                if "wife" in p:
-                    person.wife = spouse
-                else:
-                    person.husband = spouse
-                if spouse.wife is None and "wife" in p:
-                    spouse.wife = person
-                elif spouse.husband is None and "husband" in p:
-                    spouse.husband = person
-            else:
-                # Handle spouse not found
-                if "wife" in p:
-                    person.wife = None
-                else:
-                    person.husband = None
+        person_list.append(person)
 
-    return list(Person.people.values())
+    for person in person_list:
+        for person_data in people:
+            if person.name == person_data["name"]:
+                if "wife" in person_data and person_data["wife"]:
+                    person.wife = Person.people[person_data["wife"]]
+                if "husband" in person_data and person_data["husband"]:
+                    person.husband = Person.people[person_data["husband"]]
 
-
-people = [
-    {"name": "Ross", "age": 30, "wife": "Rachel"},
-    {"name": "Joey", "age": 29, "wife": None},
-    {"name": "Rachel", "age": 28, "husband": "Ross"}
-]
-
-person_list = create_person_list(people)
-
-print(isinstance(person_list[0], Person))  # True
-print(person_list[0].name == "Ross")  # True
-if person_list[0].wife:
-    print(person_list[0].wife.name == "Rachel")  # True
-else:
-    print(person_list[0].wife)  # None
-
-print(person_list[1].name == "Joey")  # True
-if person_list[1].wife:
-    print(person_list[1].wife.name)  # None
-else:
-    print(person_list[1].wife)  # None
-
-print(isinstance(person_list[2], Person))  # True
-print(person_list[2].name == "Rachel")  # True
-print(person_list[2].husband is person_list[0])  # True
-print(person_list[2].husband.name == "Ross")  # True
-print(person_list[2].husband.wife is person_list[2])  # True
-
-print(Person.people)
+    return person_list
