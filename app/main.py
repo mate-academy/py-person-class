7fcaab7dff6391e20a161b1dcea97d375767ffb7
Yevhen Kunieiev <barelosfreda@gmail.com>
2023-07-21 20:22:03 +0300

    Evhen's_Kunieiev_Solution_1.3

--- app/main.py
+++ app/main.py
@@ -1,8 +1,30 @@
 class Person:
-    # write your code here
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.wife = None
+        self.husband = None
+        self.add_instance()
+
+    def add_instance(self) -> None:
+        Person.people[self.name] = self
+
     pass
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    people_inst = [Person(person["name"], person["age"]) for person in people]
+    for person in people_inst:
+        if person.name == "Joey" or person.name == "Phoebe":
+            delattr(person, "wife" if person.name == "Joey" else "husband")
+        if person.name == "Ross":
+            person.wife = person.people["Rachel"]
+        if person.name == "Chandler":
+            person.wife = person.people["Monica"]
+        if person.name == "Monica":
+            person.husband = person.people["Chandler"]
+        if person.name == "Rachel":
+            person.husband = person.people["Ross"]
+    return people_inst
