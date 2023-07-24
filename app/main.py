2ac05c79f642b574b83ef09e60dd10537719030e
Alex Pelevin <ya.pelevin1999@gmail.com>
2023-07-24 16:38:11 +0300

    fix after review

--- app/main.py
+++ app/main.py
@@ -1,17 +1,10 @@
-from __future__ import annotations
-
-
 class Person:
     people: dict = {}
 
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.people[self.name] = self
-
-    @classmethod
-    def find_by_name(cls, name: str) -> Person | None:
-        return cls.people.get(name)
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list[Person]:
@@ -20,10 +13,10 @@ def create_person_list(people: list) -> list[Person]:
 
     for person in people:
         if "wife" in person and person["wife"]:
-            person_obj = Person.find_by_name(person["name"])
-            person_obj.wife = Person.find_by_name(person["wife"])
+            person_obj = Person.people.get(person["name"])
+            person_obj.wife = Person.people.get(person["wife"])
         elif "husband" in person and person["husband"]:
-            person_obj = Person.find_by_name(person["name"])
-            person_obj.husband = Person.find_by_name(person["husband"])
+            person_obj = Person.people.get(person["name"])
+            person_obj.husband = Person.people.get(person["husband"])
 
     return list(Person.people.values())
