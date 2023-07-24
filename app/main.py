d568ff9550de4fd389882720dd67e273986abba0
Alex Pelevin <ya.pelevin1999@gmail.com>
2023-07-24 16:44:26 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -9,14 +9,14 @@ class Person:
 
 def create_person_list(people: list) -> list[Person]:
     for person in people:
-        Person(person["name"], person["age"])
+        Person(person.get("name"), person.get("age"))
 
     for person in people:
-        if "wife" in person and person["wife"]:
-            person_obj = Person.people.get(person["name"])
-            person_obj.wife = Person.people.get(person["wife"])
-        elif "husband" in person and person["husband"]:
-            person_obj = Person.people.get(person["name"])
-            person_obj.husband = Person.people.get(person["husband"])
+        if person.get("wife"):
+            person_obj = Person.people.get(person.get("name"))
+            person_obj.wife = Person.people.get(person.get("wife"))
+        elif person.get("husband"):
+            person_obj = Person.people.get(person.get("name"))
+            person_obj.husband = Person.people.get(person.get("husband"))
 
     return list(Person.people.values())
