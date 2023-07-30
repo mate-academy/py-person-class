140be4110246fe3308989607061af78fa8204e81
Ivan Proskurnytskyi <prsky.ivan@gmail.com>
2023-07-31 00:06:20 +0200

    function create_person_list takes list of people and returns list of Person instances

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    people_instances = []
+    for person in people:
+        people_instances.append(Person(person["name"], person["age"]))
+
+    for index, person in enumerate(people):
+        if "wife" in person and person["wife"] is not None:
+            wife_name = person["wife"]
+            if wife_name in Person.people:
+                people_instances[index].wife = Person.people[wife_name]
+        if "husband" in person and person["husband"] is not None:
+            husband_name = person["husband"]
+            if husband_name in Person.people:
+                people_instances[index].husband = Person.people[husband_name]
+    return people_instances
