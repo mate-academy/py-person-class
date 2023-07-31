c203bd5bb264b60251cd208aafa73713c8a129f4
Ivan Proskurnytskyi <prsky.ivan@gmail.com>
2023-07-31 14:29:11 +0200

    Refactored

--- app/main.py
+++ app/main.py
@@ -8,17 +8,13 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    people_instances = []
+    instances = []
     for person in people:
-        people_instances.append(Person(person["name"], person["age"]))
+        instances.append(Person(person["name"], person["age"]))
 
     for index, person in enumerate(people):
-        if "wife" in person and person["wife"] is not None:
-            wife_name = person["wife"]
-            if wife_name in Person.people:
-                people_instances[index].wife = Person.people[wife_name]
-        if "husband" in person and person["husband"] is not None:
-            husband_name = person["husband"]
-            if husband_name in Person.people:
-                people_instances[index].husband = Person.people[husband_name]
-    return people_instances
+        if person.get("wife"):
+            instances[index].wife = Person.people.get(person.get("wife"))
+        elif person.get("husband"):
+            instances[index].husband = Person.people.get(person.get("husband"))
+    return instances
