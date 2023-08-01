2c3fb25ce5c9cda5c302263940d3b7373c7fa2b0
Dmytro Prokhach <dprokhach@gmail.com>
2023-08-01 19:44:14 +0300

    Exersice done

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    relations = []
+    for human in people:
+        name = human["name"]
+        age = human["age"]
+        relations.append(Person(name, age))
+    for human in people:
+        if human.get("wife"):
+            wife = Person.people[human["wife"]]
+            Person.people[human["name"]].wife = wife
+        if human.get("husband"):
+            husband = Person.people[human["husband"]]
+            Person.people[human["name"]].husband = husband
+    return relations
