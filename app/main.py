a25bdaedbc18da7431097d71b7394d04140c0d6b
Roman Podolets <romanpyu@gmail.com>
2023-08-03 19:05:02 +0300

    Simplified family linking

--- app/main.py
+++ app/main.py
@@ -11,16 +11,11 @@ def create_person_list(people: list) -> list:
     people_list = []
     for person in people:
         person_temp = Person(person["name"], person["age"])
-        if "wife" in person.keys() and person["wife"] is not None:
+        if person.get("wife"):
             wife = Person.people.get(person["wife"])
             if wife:
                 person_temp.wife = wife
                 wife.husband = person_temp
-        elif "husband" in person.keys() and person["husband"] is not None:
-            husband = Person.people.get(person["husband"])
-            if husband:
-                person_temp.husband = husband
-                husband.wife = person_temp
 
         people_list.append(person_temp)
 
