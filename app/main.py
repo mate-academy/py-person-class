e5ab3f8c965c3a24d8fd5ae9daeeefd071e11c9f
Max Katkalov <makskatkalov@gmail.com>
2023-08-05 20:50:03 +0300

    class Person and func create_person_list implemented, tests passed

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int, **kwargs) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list:
+    res_person_instances = []
+    for person in people:
+        res_person_instances.append(Person(person["name"], person["age"]))
+
+    for person in people:
+        for key, value in person.items():
+            if key == "wife" and value is not None:
+                Person.people[person["name"]].wife = Person.people[value]
+            elif key == "husband" and value is not None:
+                Person.people[person["name"]].husband = Person.people[value]
+
+    return res_person_instances
