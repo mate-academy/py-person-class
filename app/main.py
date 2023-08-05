663f91b88dbcf36db125d0e84a90107a2c614954
Daniil Hinzburh <2ilgan1601@gmail.com>
2023-08-05 20:08:28 +0300

    task completed, but an error occurs in the test that is not related to the code, but the code is probably correct

--- app/main.py
+++ app/main.py
@@ -1,8 +1,33 @@
+from __future__ import annotations
+
+
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.add_object(self)
+
+    @classmethod
+    def add_object(cls, obj: Person) -> None:
+        cls.people[obj.name] = obj
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list[Person]:
+    result = []
+
+    for pers in people:
+        person = Person(name=pers["name"], age=pers["age"])
+        if "wife" in pers and pers["wife"] is not None:
+            for i in Person.people.values():
+                if i.name == pers["wife"]:
+                    person.wife = i
+                    i.husband = person
+        elif "husband" in pers and pers["husband"] is not None:
+            for i in Person.people.values():
+                if i.name == pers["husband"]:
+                    person.husband = i
+                    i.wife = person
+        result.append(person)
+    return result
