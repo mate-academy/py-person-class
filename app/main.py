43791edf24c477f4d39f53c30cb027254fb6cd21
Max Katkalov <makskatkalov@gmail.com>
2023-08-08 00:02:45 +0300

    line of iteration changed

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ def create_person_list(people: list[dict]) -> list:
     ]
 
     for person in people:
-        if person.get("wife") is not None:
+        if person.get("wife"):
             Person.people[person["name"]].wife = Person.people[person["wife"]]
         elif person.get("husband") is not None:
             Person.people[person["name"]].husband = \
