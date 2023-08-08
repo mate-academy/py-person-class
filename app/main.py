226b7df3ad97fd0a65e44342a6ebeaee1dbb35bb
Viktoriia Zaichenko <vikazaibuba@gmail.com>
2023-08-08 17:09:29 +0200

    solution "connection of persons"

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    persons = []
+    for individual in people:
+        person = Person(individual["name"], individual["age"])
+        if "wife" in individual and individual["wife"] is not None:
+            for partner in people:
+                if individual["wife"] == partner["name"]:
+                    person_wife = Person(partner["name"], partner["age"])
+                    person_wife.husband = person
+                    person.wife = person_wife
+        persons.append(person)
+    return persons
