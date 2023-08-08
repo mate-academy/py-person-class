d8f3676d28cb693c536ec4b1fc60a14c418adbcf
Naholiuk Yevhen <e.nagolyuk@gmail.com>
2023-08-08 18:54:53 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,41 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.__class__.people[name] = self
+
+    def set_wife(self, wife: str) -> None:
+        self.wife = wife
+
+    def set_husband(self, husband: str) -> None:
+        self.husband = husband
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people_list: list) -> list:
+    person_instances = {}
+    result = []
+
+    for person in people_list:
+        name = person["name"]
+        age = person["age"]
+        person_instance = Person(name, age)
+        person_instances[name] = person_instance
+        result.append(person_instance)
+
+    for person_data in people_list:
+        person_name = person_data["name"]
+        person_instance = person_instances[person_name]
+
+        wife_name = person_data.get("wife")
+        if wife_name:
+            wife_instance = person_instances[wife_name]
+            person_instance.set_wife(wife_instance)
+
+        husband_name = person_data.get("husband")
+        if husband_name:
+            husband_instance = person_instances[husband_name]
+            person_instance.set_husband(husband_instance)
+
+    return result
