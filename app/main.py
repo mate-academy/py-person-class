fe6266d52c6b6a8d8f4c214997bb4e0c4f9faedb
Yevhen Kunieiev <barelosfreda@gmail.com>
2023-08-08 21:14:18 +0300

    Solution 1.5.2

--- app/main.py
+++ app/main.py
@@ -4,22 +4,16 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.add_instance()
-        self.people[name] = self
-
-    def add_instance(self) -> None:
-        Person.people[self.name] = self
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
     persons_list = []
-    for friend in people:
-        name_instance = Person(friend["name"], friend["age"])
-        persons_list.append(name_instance)
+    name_instance = [persons_list.append(Person(friend["name"], friend["age"])) for friend in people]
+
     for character in people:
         if character.get("wife"):
             Person.people[character["name"]].wife = Person.people[character["wife"]]
         if character.get("husband"):
             Person.people[character["name"]].husband = Person.people[character["husband"]]
-
     return persons_list
