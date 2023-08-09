48b9afc8245f45ef96ca8dd597075f7e6c34162e
CaCtUs1k <denischernish19012004@gmail.com>
2023-08-09 18:53:26 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -8,14 +8,13 @@ class Person:
 
 
 def create_person_list(people_list: list) -> list:
-    person_instances = []
-    for person in people_list:
-        person_instances.append(Person(person["name"], person["age"]))
+    person_instances = [Person(person["name"], person["age"])
+                        for person in people_list]
 
     for person in people_list:
-        if "wife" in person and person["wife"]:
+        if person.get("wife"):
             Person.people[person["name"]].wife = Person.people[person["wife"]]
-        if "husband" in person and person["husband"]:
+        if person.get("husband"):
             Person.people[person["name"]].husband \
                 = Person.people[person["husband"]]
 
