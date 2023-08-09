4bf908f3f3486b1f53047834d1ee3bb3cae70bd1
Yevhen Kunieiev <barelosfreda@gmail.com>
2023-08-09 22:10:46 +0300

    Solution 1.5.4 (fixed flake8: len line 14 and 16)

--- app/main.py
+++ app/main.py
@@ -9,9 +9,9 @@ class Person:
 
 def create_person_list(people: list) -> list:
     persons_list = [Person(friend["name"], friend["age"]) for friend in people]
-    for character in people:
-        if character.get("wife"):
-            Person.people[character["name"]].wife = Person.people[character["wife"]]
-        if character.get("husband"):
-            Person.people[character["name"]].husband = Person.people[character["husband"]]
+    for frd in people:
+        if frd.get("wife"):
+            Person.people[frd["name"]].wife = Person.people[frd["wife"]]
+        if frd.get("husband"):
+            Person.people[frd["name"]].husband = Person.people[frd["husband"]]
     return persons_list
