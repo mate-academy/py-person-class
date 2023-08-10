03c3e8fb6fc620e873cc1aac9dc0f44484b08b01
Seniv Volodymyr <01156240@pw.edu.pl>
2023-08-10 14:23:46 +0200

    List comprehension added

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ class Person:
 
 
 def create_person_list(people: list[dict]) -> list[Person]:
-    person_list = [Person(person_data.get("name"), person_data.get("age")) for person_data in people]
+    person_list = [Person(person["name"], person["age"]) for person in people]
 
     for person_data in people:
         name = person_data["name"]
