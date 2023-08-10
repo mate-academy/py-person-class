623ed9450e9f8d486d005d85a344263fabdf2750
Denys <deibukdenyscloud@gmail.com>
2023-08-10 18:31:08 +0300

    Fix the indentation

--- app/main.py
+++ app/main.py
@@ -15,6 +15,9 @@ def create_person_list(people: list[dict]) -> list[Person]:
         person_obj: Person = Person.people[person["name"]]
         attr_name: str = "wife" if "wife" in person else "husband"
         if person.get(attr_name) is not None:
-            setattr(person_obj, attr_name,
-                    Person.people.get(person[attr_name]))
+            setattr(
+                person_obj,
+                attr_name,
+                Person.people.get(person[attr_name])
+            )
     return person_lst
