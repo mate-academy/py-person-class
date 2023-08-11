ec0cff9d4444f025257be6475d0767b0263e2fe8
Kateryna Zadorozhna <136272476+katryana@users.noreply.github.com>
2023-08-11 12:45:07 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 class Person:
     people = {}
-    # dictionary for saving links to class objects
 
     def __init__(self, name: str, age: int) -> None:
         self.name = name
@@ -9,21 +8,15 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    dict_info_by_name = {}
-    # dictionary for saving full people information: name, age, marriage
     for person in people:
-        dict_info_by_name[person["name"]] = person
-        Person(
-            person["name"],
-            person["age"]
-        )
+        Person(person["name"], person["age"])
 
-    for person_name in Person.people.keys():
-        if dict_info_by_name[person_name].get("wife") is not None:
-            Person.people[person_name].wife = \
-                Person.people[dict_info_by_name[person_name]["wife"]]
-        elif dict_info_by_name[person_name].get("husband") is not None:
-            Person.people[person_name].husband = \
-                Person.people[dict_info_by_name[person_name]["husband"]]
+    for person in people:
+        if person.get("wife"):
+            Person.people[person["name"]].wife = \
+                Person.people[person["wife"]]
+        elif person.get("husband"):
+            Person.people[person["name"]].husband = \
+                Person.people[person["husband"]]
 
     return list(Person.people.values())
