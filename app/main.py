619b3c0609d7b59ba50e357c620a44397a15f02c
Oleh Chernous <chernous.oleh@gmail.com>
2023-08-11 20:10:14 +0200

    deleted redundant check condition in for...in

--- app/main.py
+++ app/main.py
@@ -11,10 +11,10 @@ def create_person_list(people: list[dict]) -> list[Person]:
     person_list = [Person(man["name"], man["age"]) for man in people]
 
     for man in people:
-        if "wife" in man and man.get("wife") is not None:
+        if man.get("wife") is not None:
             Person.people[man["name"]].wife = Person.people[man["wife"]]
 
-        if "husband" in man and man.get("husband") is not None:
+        if man.get("husband") is not None:
             Person.people[man["name"]].husband = Person.people[man["husband"]]
 
     return person_list
