9ce9350c4f9e84e09f8bcac0b2680f1724b34820
Naholiuk Yevhen <e.nagolyuk@gmail.com>
2023-08-11 16:49:13 +0300

    Corrected code according to the remarks

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.__class__.people[name] = self
+        Person.people[name] = self
 
     def set_wife(self, wife: str) -> None:
         self.wife = wife
@@ -19,22 +19,23 @@ def create_person_list(people_list: list) -> list:
     for person_dict in people_list:
         name = person_dict["name"]
         age = person_dict["age"]
-        person_object = Person(name, age)
-        result.append(person_object)
+        result.append(Person(name, age))
 
-    for person_object in result:
-        for person_dict in people_list:
-            if person_dict["name"] == person_object.name:
-                wife_name = person_dict.get("wife")
-                if wife_name:
-                    for wife_instance in result:
-                        if wife_instance.name == wife_name:
-                            person_object.set_wife(wife_instance)
+    name_to_person = {person.name: person for person in result}
 
-                husband_name = person_dict.get("husband")
-                if husband_name:
-                    for husband_instance in result:
-                        if husband_instance.name == husband_name:
-                            person_object.set_husband(husband_instance)
+    for person in result:
+        person_dict = next(p for p in people_list if p["name"] == person.name)
+
+        wife_name = person_dict.get("wife")
+        if wife_name:
+            wife_instance = name_to_person.get(wife_name)
+            if wife_instance:
+                person.set_wife(wife_instance)
+
+        husband_name = person_dict.get("husband")
+        if husband_name:
+            husband_instance = name_to_person.get(husband_name)
+            if husband_instance:
+                person.set_husband(husband_instance)
 
     return result
