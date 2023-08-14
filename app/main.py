8355ef8429d5642274ae041e83dd226582963e5a
Illia <kononenko2109@gmail.com>
2023-08-14 15:31:38 +0300

    Implemented py-person-class Implemented this advise: instead the long condition use if person_dict.get("wife") Performed don't get this key two times changed lict to lict comprehension

--- app/main.py
+++ app/main.py
@@ -8,16 +8,15 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-    for person_dict in people:
-        name = person_dict["name"]
-        age = person_dict["age"]
-        person = Person(name, age)
-        person_list.append(person)
+    person_list = [
+        Person(person_dict["name"], person_dict["age"])
+        for person_dict in people
+    ]
 
     for person_dict in people:
         name = person_dict["name"]
-        if person_dict.get("wife"):
+        wife_name = person_dict.get("wife")
+        if wife_name:
             wife_name = person_dict["wife"]
             person = Person.people[name]
             wife = Person.people[wife_name]
