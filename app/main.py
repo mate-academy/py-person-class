2c785fa6f059c357774e43231e021da33d0d394c
Andrii Homeniuk <andreygomenuk@gmail.com>
2023-08-17 18:07:28 +0300

    1.Created class Person. 2.Implemented create_person_list function.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,31 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+
+    for person_data in people:
+        exemplar = Person(
+            person_data["name"],
+            person_data["age"]
+        )
+        wife = person_data.get("wife")
+        husband = person_data.get("husband")
+
+        if wife and wife in Person.people:
+            exemplar.wife = Person.people[wife]
+            Person.people[wife].husband = exemplar
+
+        elif husband and husband in Person.people:
+            exemplar.husband = Person.people[husband]
+            Person.people[husband].wife = exemplar
+
+        person_list.append(exemplar)
+
+    return person_list
