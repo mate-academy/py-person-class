5e98901cfb2971d0e1787c58dd3ccefe4dc87371
Oleh Bohatov <obohatov@gmail.com>
2023-08-20 21:15:06 +0200

    Solution without nested loops

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 class Person:
     people = {}
 
-    def __init__(self, name: str, age: int) -> None:
+    def __init__(self,
+                 name: str,
+                 age: int) -> None:
         self.name = name
         self.age = age
+        self.wife = None
+        self.husband = None
         Person.people[self.name] = self
 
 
@@ -11,11 +15,17 @@ def create_person_list(people: list) -> list:
     persons = []
     for citizen in people:
         person = Person(citizen["name"], citizen["age"])
-        if "wife" in citizen and citizen["wife"] is not None:
-            for human in people:
-                if citizen["wife"] == human["name"]:
-                    person_wife = Person(human["name"], human["age"])
-                    person_wife.husband = person
-                    person.wife = person_wife
         persons.append(person)
+
+    for citizen in people:
+        if "wife" in citizen and citizen["wife"] is not None:
+            person = Person.people[citizen["name"]]
+            spouse = Person.people[citizen["wife"]]
+            person.wife = spouse
+            spouse.husband = person
+
+    for person in persons:
+        if person.wife is None:
+            del person.wife
+
     return persons
