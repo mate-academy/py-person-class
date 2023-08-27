81c4be89f3975493d25dccadd5c856f3b64400e6
Depaton <you@example.com>
2023-08-28 01:31:21 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: str) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    update_list = [Person(person["name"], person["age"]) for person in people]
+    for person in range(len(people)):
+        for data in range(len(people)):
+
+            if "wife" in people[person] and people[person]["wife"] is not None:
+                if people[person]["wife"] == people[data]["name"]:
+                    update_list[person].wife = \
+                        Person.people[people[data]["name"]]
+
+            if "husband" in people[person] \
+                    and people[person]["husband"] is not None:
+
+                if people[person]["husband"] == people[data]["name"]:
+                    update_list[person].husband = \
+                        Person.people[people[data]["name"]]
+    return update_list
