e888f8aca3645147a0c36c5fb7f1db7d59c231aa
Yaroslav Yarovyi <yarovoyyaroslav21@gmail.com>
2023-08-27 20:50:03 +0300

    Create class Person and func create_person_list. Add empty line

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ class Person:
 
     people = {}
 
-    def __init__(self, name: str, age: int):
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
 
