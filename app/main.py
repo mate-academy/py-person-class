cede7ff0ab77b94835eaebc662c381bb1158aa09
Elena Komarova <elenkomarova4@gmail.com>
2023-08-30 14:39:16 +0300

    Solution_3

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ class Person:
         Person.people[name] = self
 
 
-def create_person_list(people_data: list) -> list:
+def create_person_list(people_data: list) -> list[Person]:
     person_instances = []
 
     for person_dict in people_data:
