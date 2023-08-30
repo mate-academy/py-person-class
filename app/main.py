10ffc1bdc5df727fdd4be0dac13bbe4bdcffa550
Elena Komarova <elenkomarova4@gmail.com>
2023-08-30 14:24:05 +0300

    'Solution_2'

--- app/main.py
+++ app/main.py
@@ -5,8 +5,6 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.wife = None
-        self.husband = None
         Person.people[name] = self
 
 
@@ -20,13 +18,11 @@ def create_person_list(people_data: list) -> list:
         person_instances.append(person_instance)
 
     for person_instance, person_dict in zip(person_instances, people_data):
-        if "wife" in person_dict and person_dict["wife"] in Person.people:
-            person_instance.wife = Person.people[person_dict["wife"]]
+        wife_name = person_dict.get("wife")
+        if wife_name and wife_name in Person.people:
+            person_instance.wife = Person.people[wife_name]
         husband_name = person_dict.get("husband")
         if husband_name in Person.people:
             person_instance.husband = Person.people[husband_name]
-
-        if person_dict.get("wife") is None:
-            delattr(person_instance, "wife")
 
     return person_instances
