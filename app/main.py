f37e7da73f60fd8cd20794b5c5945ffafeb43e8f
Depaton <you@example.com>
2023-08-31 16:37:21 +0300

    new new solution

--- app/main.py
+++ app/main.py
@@ -12,10 +12,10 @@ def create_person_list(people: list) -> list:
     for person in people:
         if "wife" in person and person["wife"] is not None:
             if person["wife"] in Person.people:
-                update_list[people.index(person)].wife = \
+                Person.people[person["name"]].wife = \
                     Person.people[person["wife"]]
         if "husband" in person and person["husband"] is not None:
             if person["husband"] in Person.people:
-                update_list[people.index(person)].husband = \
+                Person.people[person["name"]].husband = \
                     Person.people[person["husband"]]
     return update_list
