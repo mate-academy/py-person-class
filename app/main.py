cb275050fd33973ff6f29e96d41af648c9572e86
Artem <hartem472@gmail.com>
2023-08-31 18:10:17 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -11,21 +11,22 @@ class Person:
 def create_person_list(peoples: list) -> list:
     main_list = []
 
-    for one_people in peoples:
-        person = Person(one_people["name"], one_people["age"])
+    for people in peoples:
+        person = Person(people["name"], people["age"])
         main_list.append(person)
 
-    for one_people in peoples:
-        person = [p for p in main_list if p.name == one_people["name"]][0]
+    for people in peoples:
+        person = [p for p in main_list if p.name == people["name"]][0]
 
-        if "wife" in one_people:
+        if "wife" in people:
             for wife in main_list:
-                if one_people["wife"] == wife.name:
+                if people["wife"] == wife.name:
                     person.wife = wife
 
-        elif "husband" in one_people:
+        elif "husband" in people:
             for husband in main_list:
-                if one_people["husband"] == husband.name:
+                if people["husband"] == husband.name:
                     person.husband = husband
 
     return main_list
+
