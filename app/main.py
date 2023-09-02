4a4854cf3b5dcf0ae34524f07109d6d08c8c073e
Gulhaya Kulsahatowa <gulhayakulsahatowa@gmail.com>
2023-09-02 19:50:52 +0300

    solution1

--- app/main.py
+++ app/main.py
@@ -4,16 +4,14 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.__class__.people[name] = self
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
     result = []
 
     for person_data in people:
-        name = person_data["name"]
-        age = person_data["age"]
-        person = Person(name, age)
+        person = Person(person_data["name"], person_data["age"])
         result.append(person)
 
         if "wife" in person_data and person_data["wife"]:
