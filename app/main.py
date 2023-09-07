91a44a47123f962602c38cca6be4065410ffe72a
Evgeniy <Sherllaok@gmail.com>
2023-09-07 21:08:50 +0300

    fix code porting

--- app/main.py
+++ app/main.py
@@ -13,9 +13,11 @@ def create_person_list(people: list) -> list:
     ]
     for persons in people:
         if persons.get("wife"):
-            Person.people[persons.get("name")].wife = \
+            Person.people[persons.get("name")].wife = (
                 Person.people[persons.get("wife")]
+            )
         elif persons.get("husband"):
-            Person.people[persons.get("name")].husband = \
+            Person.people[persons.get("name")].husband = (
                 Person.people[persons.get("husband")]
+            )
     return person_list
