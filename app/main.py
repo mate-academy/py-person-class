e3f9efc7233a6e26f3a74fcbe6779a75b302b18e
Vadym Tiron <tironvadim1@gmail.com>
2023-09-11 16:44:08 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,26 +5,22 @@ class Person:
         self.name = name
         self.age = age
         Person.people[name] = self
-        self.wife = None
-        self.husband = None
 
 
 def create_person_list(people: list) -> list:
     person_list = []
     for person_data in people:
         name = person_data["name"]
-        age = person_data["age"]
+        age = person_data.get("age", None)
         person = Person(name, age)
 
-        if "wife" in person_data and person_data["wife"] is not None:
-            wife_name = person_data["wife"]
-            if wife_name in Person.people:
-                person.wife = Person.people[wife_name]
+        wife_name = person_data.get("wife", None)
+        if wife_name and wife_name in Person.people:
+            person.wife = Person.people[wife_name]
 
-        if "husband" in person_data and person_data["husband"] is not None:
-            husband_name = person_data["husband"]
-            if husband_name in Person.people:
-                person.husband = Person.people[husband_name]
+        husband_name = person_data.get("husband", None)
+        if husband_name and husband_name in Person.people:
+            person.husband = Person.people[husband_name]
 
         person_list.append(person)
 
