c3ed8848ba9a36aaeeeb267068ca91c3715cedc6
Alex <oleksa.shcherbakov@gmail.com>
2023-09-13 15:29:20 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,55 +1,16 @@
 class Person:
-    person_list = {}
+    dict_of_person_obj = {}
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
-        Person.person_list[self.name] = self
+        Person.dict_of_person_obj[self.name] = self
 
-
-def create_person_list(people: list) -> list:
-    new_arr = []
+def create_dict_of_person_obj(people: list) -> list:
+    list_person = []
     for person in people:
-        new_arr.append(Person(person.get("name"),person.get("age")))
-        if person.get('wife') != None:
-             person["wife"] = Person.person_list.get(person["name"])
-        if person.get('husband') != None:
-             person["husband"] = Person.person_list.get(person["name"])
-    print(new_arr)
-    return new_arr
-
-
-    # for person in people:
-    #     if person.get('wife') != None:
-    #         person["wife"] = Person.person_list.get(person["name"])
-    #     if person.get('husband') != None:
-    #         person["husband"] = Person.person_list.get(person["name"])
-    # print(people)
-
-    # print(Person.person_list)
-    # new_arr = []
-    # for person in people:
-    #      if person.get("name") in Person.person_list:
-    #          new_arr.append({person.get("name"): person})
-    #          # Person.person_list[person.get("name")] = person
-    #
-    # print(new_arr)
-
-    # for person in Person.person_list:
-    #     print(person)
-# if  person's key wife/husband is not None - create_person_list
-# should add attribute wife/husband respectively to its instance.
-# This attribute should be a link to a Person instance
-# with name the same as wife/husband key in person's dict.
-
-people = [
-    {"name": "Ross", "age": 30, "wife": "Rachel"},
-    {"name": "Joey", "age": 29, "wife": None},
-    {"name": "Rachel", "age": 28, "husband": "Ross"}
-]
-
-person_1 = Person("Rachel", 25)
-person_2 = Person("Joey", 35)
-person_3 = Person("Ross", 36)
-create_person_list(people)
-
-#print(Person.person_list)
+        list_person.append(Person(person.get("name"),person.get("age")))
+        if not isinstance(person.get('wife'), None):
+             person["wife"] = Person.dict_of_person_obj.get(person["name"])
+        if not isinstance(person.get('husband'), None):
+             person["husband"] = Person.dict_of_person_obj.get(person["name"])
+    return list_person
