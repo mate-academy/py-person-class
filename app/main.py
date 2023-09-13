022d922ea94bc6dd1daf24aa187fc30ef8ee1bd9
Alex <oleksa.shcherbakov@gmail.com>
2023-09-13 13:16:26 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,44 @@
 class Person:
-    # write your code here
-    pass
+    person_list = {}
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        Person.person_list[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    for person in people:
+        if person.get('wife') != None:
+            person["wife"] = Person.person_list.get(person["name"])
+        if person.get('husband') != None:
+            person["husband"] = Person.person_list.get(person["name"])
+    print(people)
+
+    # print(Person.person_list)
+    # new_arr = []
+    # for person in people:
+    #      if person.get("name") in Person.person_list:
+    #          new_arr.append({person.get("name"): person})
+    #          # Person.person_list[person.get("name")] = person
+    #
+    # print(new_arr)
+
+    # for person in Person.person_list:
+    #     print(person)
+# if  person's key wife/husband is not None - create_person_list
+# should add attribute wife/husband respectively to its instance.
+# This attribute should be a link to a Person instance
+# with name the same as wife/husband key in person's dict.
+
+people = [
+    {"name": "Ross", "age": 30, "wife": "Rachel"},
+    {"name": "Joey", "age": 29, "wife": None},
+    {"name": "Rachel", "age": 28, "husband": "Ross"}
+]
+
+person_1 = Person("Rachel", 25)
+person_2 = Person("Joey", 35)
+person_3 = Person("Ross", 36)
+create_person_list(people)
+
+#print(Person.person_list)
