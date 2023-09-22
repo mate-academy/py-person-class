c7fc3db0c9ca162001529ea2fc2b74fc4a9a2a3a
Olena Lisova <lisovausa@gmail.com>
2023-09-22 12:40:14 -0400

    Simplified homework py_person_class With the help of new methods setattr() and getattr()

--- app/main.py
+++ app/main.py
@@ -1,39 +1,28 @@
 class Person:
     people = {}
 
-    def __init__(self, name: str, age: str) -> None:
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    list_persons = [0 for _ in range(len(people))]
-    people_copy = people[:]
-    for i, humain in enumerate(people_copy):
+    list_persons = []
+    mari_list = ["wife", "husband"]
+    for humain in people:
         person = Person(humain["name"], humain["age"])
-        if "wife" in humain.keys():
-            if humain["wife"]:
-                person.wife = humain["wife"]
-                for index, humain_ch in enumerate(people_copy):
-                    if humain_ch["name"] == humain["wife"]:
-                        person_wife = Person(humain_ch["name"],
-                                             humain_ch["age"])
-                        person.wife = person_wife
-                        person_wife.husband = person
-                        list_persons[index] = person_wife
-                        people_copy.remove(humain_ch)
-        else:
-            if humain["husband"]:
-                person.husband = humain["husband"]
-                for index, humain_ch in enumerate(people_copy):
-                    if humain_ch["name"] == humain["husband"]:
-                        person_husband = Person(humain_ch["name"],
-                                                humain_ch["age"])
-                        person.husband = person_husband
-                        person_husband.wife = person
-                        list_persons[index] = person_husband
-                        people_copy.remove(humain_ch)
-        list_persons[i] = person
+        for mari in mari_list:
+            if humain.get(mari) and humain[mari]:
+                setattr(person, mari, humain[mari])
+        list_persons.append(person)
+
+    for person in list_persons:
+        for mari in mari_list:
+            if getattr(person, mari, None):
+                for person_2 in list_persons:
+                    if person_2.name == getattr(person, mari):
+                        setattr(person, mari, person_2)
+                        break
 
     return list_persons
