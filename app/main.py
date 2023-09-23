d52babf32860970a8de8f12d4fcbcf4c0d5f94ff
Andrey Britov <abritov82@gmail.com>
2023-09-23 12:45:46 +0300

    New commit

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.people[self.name] = self
+        Person.people[name] = self
 
 
-def create_person_list(people: list[dict]) -> list:
+def create_person_list(people: list[dict]) -> list[Person]:
     les = []
 
     for obj in people:
@@ -25,34 +25,3 @@ def create_person_list(people: list[dict]) -> list:
                 Person.people[obj["name"]].wife = Person.people[obj["wife"]]
 
     return les
-
-
-# people = [
-#     {'name': 'Ross', 'age': 30, 'wife': 'Rachel'},
-#     {'name': 'Joey', 'age': 29, 'wife': None},
-#     {'name': 'Rachel', 'age': 28, 'husband': 'Ross'}
-# ]
-
-# person_list = create_person_list(people)
-# print(person_list)
-# # print(isinstance(person_list[0], Person))  # True
-# # print(person_list[0].name)
-# # print(person_list[0].wife is person_list[2])  # True
-# # print(person_list[0].wife.name)  # 'Rachel'
-# # #
-# # print(person_list[1].name)  # == 'Joey'
-# print(person_list[1].wife)
-# AttributeError
-#
-# isinstance(person_list[2], Person) # True
-# person_list[2].name == 'Rachel'
-# person_list[2].husband is person_list[0] # True
-# # The same as person_list[0]
-# person_list[2].husband.name == 'Ross'
-# person_list[2].husband.wife is person_list[2]  # True
-
-# Person.people == {
-#     'Ross': <__main__.Person object at 0x10c20ca60>,
-#     'Joey': <__main__.Person object at 0x10c180a00>,
-#     'Rachel': <__main__.Person object at 0x10c1804f0>
-# }
