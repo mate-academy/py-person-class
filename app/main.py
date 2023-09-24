7a4e31896302c05371f94e346f3ee6e9b63f9a0d
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2023-09-24 20:25:25 +0300

    deleted_excess

--- app/main.py
+++ app/main.py
@@ -4,9 +4,7 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.wife = None
-        self.husband = None
-        self.__class__.people[name] = self
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
@@ -20,8 +18,7 @@ def create_person_list(people: list) -> list:
         if "wife" in person_dict and person_dict["wife"] is not None:
             wife_name = person_dict["wife"]
             Person.people[person_name].wife = Person.people[wife_name]
-        elif hasattr(Person.people[person_name], "wife"):
-            del Person.people[person_name].wife
+
         if "husband" in person_dict and person_dict["husband"] is not None:
             husband_name = person_dict["husband"]
             Person.people[person_name].husband = Person.people[husband_name]
