6d1fdc5fe39d366b155548b1ec5bec006ff753ef
Your_Name Your_Surname <your@email>
2023-09-25 20:01:14 +0300

    deleted unnecessary lines

--- app/main.py
+++ app/main.py
@@ -10,10 +10,7 @@ class Person:
 def create_person_list(people: list[dict]) -> list[Person]:
     res_list: list = [Person(pers["name"], pers["age"]) for pers in people]
     for elem in people:
-        if "wife" in elem.keys() and elem["wife"] is not None:
-            for key in Person.people:
-                if key == elem["wife"]:
-                    Person.people[elem["name"]].wife = Person.people[key]
-                    Person.people[key].husband = Person.people[elem["name"]]
-                    break
+        if elem.get("wife"):
+            Person.people[elem.get("wife")].husband = Person.people[elem["name"]]
+            Person.people[elem["name"]].wife = Person.people[elem.get("wife")]
     return res_list
