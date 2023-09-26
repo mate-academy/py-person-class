6727cd7f4dd2c8f856b2b0c55b5ad76ef736e25b
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-09-26 21:57:27 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -14,11 +14,9 @@ def create_person_list(people: list) -> list:
     result = [Person(human["name"], human["age"]) for human in people]
 
     for result_index, human in enumerate(people):
-    
         wife_name = human.get("wife")
         if wife_name is not None:
             result[result_index].wife = Person.people.get(wife_name)
-    
         husband_name = human.get("husband")
         if husband_name is not None:
             result[result_index].husband = Person.people.get(husband_name)
