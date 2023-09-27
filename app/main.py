13a11565a4223995baf156f14f5267d4e6c8e073
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2023-09-27 16:25:20 +0300

    fix: after review

--- app/main.py
+++ app/main.py
@@ -4,18 +4,18 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.__class__.people[name] = self
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    for item in people:
-        Person(item["name"], item["age"])
+    for person in people:
+        Person(person["name"], person["age"])
 
     person_list = []
-    for item in people:
-        new_person = Person.people.get(item["name"])
-        wife_name = item.get("wife")
-        husband_name = item.get("husband")
+    for person in people:
+        new_person = Person.people.get(person["name"])
+        wife_name = person.get("wife")
+        husband_name = person.get("husband")
         if wife_name:
             setattr(new_person, "wife", Person.people.get(wife_name))
 
