c234cabe3d62f68ddb3f4a28a40bb9a3a08b9741
Vishnevskyi Dmytro <vishneman@gmail.com>
2023-09-29 14:01:17 +0300

    solution for person_class

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int, *args) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people_list: list) -> list:
+
+    persons = [Person(per["name"], per["age"]) for per in people_list]
+
+    for people, value in zip(persons, people_list):
+        if "wife" in value and value["wife"]:
+            for count in Person.people:
+                if count == value["wife"]:
+                    people.wife = Person.people[count]
+        if "husband" in value and value["husband"]:
+            for count in Person.people:
+                if count == value["husband"]:
+                    people.husband = Person.people[count]
+    return persons
