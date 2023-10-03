0a414cec9375a293c68e4a422c918e11eeaf35b6
Mykola Kokoiev <veokoknik@gmail.com>
2023-10-03 15:21:27 +0200

    Use get

--- app/main.py
+++ app/main.py
@@ -12,11 +12,10 @@ def create_person_list(people: list) -> list:
         Person(name=person["name"], age=person["age"])
 
     for person in people:
-        if person[list(person.keys())[2]] is not None:
-            if list(person.keys())[2] == "wife":
-                Person.people[person["name"]].wife =\
-                    Person.people[person[list(person.keys())[2]]]
-            else:
-                Person.people[person["name"]].husband =\
-                    Person.people[person[list(person.keys())[2]]]
+        if person.get("wife") is not None:
+            Person.people[person["name"]].wife =\
+                Person.people[person[list(person.keys())[2]]]
+        if person.get("husband") is not None:
+            Person.people[person["name"]].husband =\
+                Person.people[person[list(person.keys())[2]]]
     return list(Person.people.values())
