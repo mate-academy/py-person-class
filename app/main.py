0c455c2eabb95e53b208129c52f50753cbfd7891
Vladyslav Pidborskyi <pidborskii17@gmail.com>
2023-10-03 20:29:34 +0300

    Removed redundant statements and unnecessary "for" loop -> list comprehension

--- app/main.py
+++ app/main.py
@@ -8,24 +8,19 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list: [Person] = []
-
-    for person in people:
-        person_list.append(
-            Person(
-                name=person["name"],
-                age=person["age"]
-            )
-        )
+    person_list: [Person] = [
+        Person(name=person["name"], age=person["age"])
+        for person in people
+    ]
 
     # Adding husband and wife for people
     for person in people:
-        if person.get("husband") is not None:
+        if isinstance(person.get("husband"), str):
             person_obj = Person.people[person["name"]]
             husband_obj = Person.people[person["husband"]]
             person_obj.husband = husband_obj
 
-        if person.get("wife") is not None:
+        if isinstance(person.get("wife"), str):
             person_obj = Person.people[person["name"]]
             wife_obj = Person.people[person["wife"]]
             person_obj.wife = wife_obj
