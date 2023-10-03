baea2974621906d5ef7241dc2d0a578a525e9b6d
Yuliia Hladyshkevych <yulia.gladyshkevych@mail.com>
2023-10-03 20:10:13 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -8,17 +8,16 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    for person in people:
-        Person(person["name"], person["age"])
 
-    person_list = []
-    for person in people:
-        new_person = Person.people.get(person["name"])
-        wife_name = person.get("wife")
-        husband_name = person.get("husband")
-        if wife_name:
-            setattr(new_person, "wife", Person.people.get(wife_name))
-        if husband_name:
-            setattr(new_person, "husband", Person.people.get(husband_name))
-        person_list.append(new_person)
+    person_list = [Person(person["name"], person["age"]) for person in people]
+
+    for index, person in enumerate(people):
+        if person.get("wife"):
+            wife_name = person["wife"]
+            person_list[index].wife = Person.people[wife_name]
+
+        if person.get("husband"):
+            husband_name = person["husband"]
+            person_list[index].husband = Person.people[husband_name]
+
     return person_list
