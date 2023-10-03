90b73667caa5b2b99a8f8c0c8912d42cb4a7943e
Mykhailo Slavsky <slavskij2017@gmail.com>
2023-10-04 00:51:33 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -11,6 +11,8 @@ def create_person_list(people: list) -> list:
     result_list = [Person(person["name"], person["age"]) for person in people]
     for pers in people:
         if pers.get("wife"):
-            Person.people[pers.get("wife")].husband = Person.people[pers["name"]]
-            Person.people[pers["name"]].wife = Person.people[pers.get("wife")]
+            Person.people[pers.get("wife")].husband\
+                = Person.people[pers["name"]]
+            Person.people[pers["name"]].wife\
+                = Person.people[pers.get("wife")]
     return result_list
