5fd15641784ee720a09e2c3ffdd680ce4b34379d
Dmytro Pyrih <dima.purig@gmail.com>
2023-10-03 09:06:55 +0300

    Cleaned Code

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ def create_person_list(people: list) -> list[Person]:
     for person in people:
         if "wife" in person and person["wife"] is not None:
             Person.people[person["name"]].wife = Person.people[person["wife"]]
-        elif "husband" in person and person["husband"] is not None:
+        elif "husband" in person and person["husband"]:
             Person.people[person["name"]].husband = (
                 Person.people[person["husband"]])
 
