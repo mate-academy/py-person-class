d62f5880dc315d9714a2f6af2ca3664d2eedafdb
Dima <dima.purig@gmail.com>
2023-10-04 20:03:38 +0300

    Fixed indentation

--- app/main.py
+++ app/main.py
@@ -8,16 +8,17 @@ class Person:
 
 
 def create_person_list(people: list) -> list[Person]:
-    person_list = [Person(person.get("name"),
-                          person.get("age"))
+    person_list = [Person(person.get("name"), person.get("age"))
                    for person in people]
 
     for person in people:
         if person.get("wife"):
-            Person.people[person.get("name")].wife = (
-                Person.people)[person.get("wife")]
+            Person.people[person.get("name")].wife = Person.people[
+                person.get("wife")
+            ]
         elif person.get("husband"):
-            Person.people[person.get("name")].husband = (
-                Person.people[person.get("husband")])
+            Person.people[person.get("name")].husband = Person.people[
+                person.get("husband")
+            ]
 
     return person_list
