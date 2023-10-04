3f750e6d350558e362cea5faf2f31b1a21cd967b
Vladyslav Shatrovskyi <shatrovskyi.vlad@gmail.com>
2023-10-04 20:24:24 +0300

    Improved Solution

--- app/main.py
+++ app/main.py
@@ -4,25 +4,19 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people[name] = self
+        self.people[name] = self
 
 
 def create_person_list(people_data: dict) -> list[Person]:
-    person_list = []
 
-    for person_item in people_data:
-        name = person_item["name"]
-        age = person_item["age"]
-        person = Person(name, age)
-
-        person_list.append(person)
+    person_list = [Person(person_item["name"], person_item["age"]) for person_item in people_data]
 
     for index, person in enumerate(people_data):
         wife = person.get("wife")
         husband = person.get("husband")
-        if wife is not None:
+        if wife:
             person_list[index].wife = Person.people.get(wife)
-        if husband is not None:
+        if husband:
             person_list[index].husband = Person.people.get(husband)
 
     return person_list
