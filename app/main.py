16fa047edb0ab1a56e878dc1d43fe95a31fafb3e
Denis Komandyr <sabsdaid@gmail.com>
2023-10-06 14:15:46 +0300

    simplified code

--- app/main.py
+++ app/main.py
@@ -8,20 +8,14 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_instances = []
-    for person in people:
-        current_person = Person(person["name"], person["age"])
+    persons_list = [Person(person["name"], person["age"]) for person in people]
+
+    for index, person in enumerate(people):
         if person.get("wife"):
             wife_name = person["wife"]
-            if wife_name in Person.people:
-                wife = Person.people[wife_name]
-                current_person.wife = wife
-                wife.husband = current_person
+            persons_list[index].wife = Person.people[wife_name]
         if person.get("husband"):
             husband_name = person["husband"]
-            if husband_name in Person.people:
-                husband = Person.people[husband_name]
-                current_person.husband = husband
-                husband.wife = current_person
-        person_instances.append(current_person)
-    return person_instances
+            persons_list[index].husband = Person.people[husband_name]
+
+    return persons_list
