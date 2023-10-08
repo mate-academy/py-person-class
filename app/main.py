c3b760770dd51eca5563fbe0846c1d322cce3166
Anton <antonrozegan1@gmail.com>
2023-10-08 23:04:14 +0300

    Solu

--- app/main.py
+++ app/main.py
@@ -2,26 +2,26 @@ class Person:
 
     people = {}
 
-    def __init__(self, name: int, age: str) -> None:
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         self.spouse = None
         Person.people[name] = self
 
-    def set_spouse(self, spouse_name: int) -> None:
+    def set_spouse(self, spouse_name: str) -> None:
         if spouse_name in Person.people:
-            self.spouse = Person[spouse_name]
+            self.spouse = Person.people[spouse_name]
             Person.people[spouse_name].spouse = self
 
-    def create_person_list(self, people: list) -> list:
-        person_instances = []
+def create_person_list(people: list) -> list:
+    person_instances = []
 
-        for person_info in people:
-            name = person_info["name"]
-            age = person_info["age"]
-            person = Person(name, age)
-            spouse_name = person_info.get("wife") or person_info.get("husband")
-            if spouse_name:
-                person.set_spouse(spouse_name)
-            person_instances.append(person)
-        return person_instances
+    for person_info in people:
+        name = person_info["name"]
+        age = person_info["age"]
+        person = Person(name, age)
+        spouse_name = person_info.get("wife") or person_info.get("husband")
+        if spouse_name:
+            person.set_spouse(spouse_name)
+        person_instances.append(person)
+    return person_instances
