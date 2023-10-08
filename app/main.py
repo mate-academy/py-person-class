42876c2b5eb42dece195fa5ea91d0d4e204a857d
Anton <antonrozegan1@gmail.com>
2023-10-08 23:28:48 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -8,20 +8,21 @@ class Person:
         self.spouse = None
         Person.people[name] = self
 
-    def set_spouse(self, spouse_name: str) -> None:
-        if spouse_name in Person.people:
-            self.spouse = Person.people[spouse_name]
-            Person.people[spouse_name].spouse = self
 
-def create_person_list(people: list) -> list:
+def create_person_list(people: list[dict]) -> list[Person]:
     person_instances = []
-
     for person_info in people:
         name = person_info["name"]
         age = person_info["age"]
         person = Person(name, age)
         spouse_name = person_info.get("wife") or person_info.get("husband")
         if spouse_name:
-            person.set_spouse(spouse_name)
+            if spouse_name in Person.people:
+                spouse = Person.people[spouse_name]
+                person.spouse = spouse
+                spouse.spouse = person
+            else:
+                pass
+
         person_instances.append(person)
     return person_instances
