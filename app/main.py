7a6460068cc487f51212469be4ba161d4111e2ba
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-10-08 14:19:41 +0300

    Fix error

--- app/main.py
+++ app/main.py
@@ -12,18 +12,9 @@ def create_person_list(people: list) -> list:
     for person in people:
         get_person = Person.people[person["name"]]
         if person.get("wife"):
-            person.wife = Person.people[person["wife"]]
+            get_person.wife = Person.people[person["wife"]]
         elif person.get("husband"):
             get_person.husband = Person.people[person["husband"]]
     return people_list
 
 
-people = [
-    {"name": "Ross", "age": 30, "wife": "Rachel"},
-    {"name": "Joey", "age": 29, "wife": None},
-    {"name": "Rachel", "age": 28, "husband": "Ross"}
-]
-
-person_list = create_person_list(people)
-ab = Person.people
-ac = (ab["name"])
