60095385e1478bae01839a3d20eb8af544b52194
Ryshtun Denys <rishtund@gmail.com>
2023-10-09 12:09:22 +0300

    alfa-centuri vesrion

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 class Person:
-    # write your code here
+
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = age
     pass
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    data = []
+    for person in people:
+        person["name"] = Person(person["name"], person["age"])
+        if bool(person.get("wife")):
+            person["name"].wife = person["wife"]
+        if bool(person.get("husband")):
+            person["name"].husband = person.get("husband")
+        data.append(person["name"])
+    return data
