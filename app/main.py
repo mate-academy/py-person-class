59f64db485187b68f2057f71ab5e1785a4986b79
Oleksandr Matviichuk <oleksandr.matviychuk@gmail.com>
2023-10-09 20:13:15 +0300

    Solution1

--- app/main.py
+++ app/main.py
@@ -8,10 +8,10 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    persons_list = [Person(one["name"], one["age"]) for one in people]
-    for key, value in enumerate(people):
-        if value.get("wife"):
-            persons_list[key].wife = Person.people[value.get("wife")]
-        elif value.get("husband"):
-            persons_list[key].husband = Person.people[value.get("husband")]
+    persons_list = [Person(person["name"], person["age"]) for person in people]
+    for index, man in enumerate(people):
+        if man.get("wife"):
+            persons_list[index].wife = Person.people[man.get("wife")]
+        elif man.get("husband"):
+            persons_list[index].husband = Person.people[man.get("husband")]
     return persons_list
