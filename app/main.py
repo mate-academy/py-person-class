9b03725385cf0144d532da0fceb1a1fb8ce01bc9
Alex <oleksa.shcherbakov@gmail.com>
2023-10-10 11:50:37 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,16 +1,28 @@
 class Person:
-    dict_of_person_obj = []
+    people = {}
+
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
-        Person.dict_of_person_obj[self.name] = self
+        Person.people[self.name] = self
 
-def create_dict_of_person_obj(people: list) -> list:
-    list_person = []
-    for person in people:
-        list_person.append(Person(person.get("name"),person.get("age")))
-        if not isinstance(person.get('wife'), None):
-             person["wife"] = Person.dict_of_person_obj.get(person["name"])
-        if not isinstance(person.get('husband'), None):
-             person["husband"] = Person.dict_of_person_obj.get(person["name"])
-    return list_person
+
+def create_person_list(people: list) -> list:
+    person_list = [Person(person["name"],
+                          person.get("age"))
+                   for person in people]
+
+    relationship = [(person["name"],
+                     person.get("wife"), person.get("husband"))
+                    for person in people]
+
+    for name, wife_name, husband_name in relationship:
+        person = Person.people[name]
+
+        if wife_name:
+            person.wife = Person.people.get(wife_name)
+
+        if husband_name:
+            person.husband = Person.people.get(husband_name)
+
+    return person_list
