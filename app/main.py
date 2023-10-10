9b8ce65baf0728c7cd43d1b6a014dd85c92af27f
Katherine-Greg <katiasimpson17@gmail.com>
2023-10-11 02:11:16 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -9,12 +9,12 @@ class Person:
 
 def create_person_list(people: list) -> list:
     list_of_people = [
-        Person(person_info["name"], person_info["age"]) for person_info in people
+        Person(person_info["name"], person_info["age"])
+        for person_info in people
     ]
-
-    for index, person_info in enumerate(people):
-        if person_info.get("wife"):
-            list_of_people[index].wife = Person.people.get(person_info["wife"])
-        if person_info.get("husband"):
-            list_of_people[index].husband = Person.people.get(person_info["husband"])
+    for index, info in enumerate(people):
+        if info.get("wife"):
+            list_of_people[index].wife = Person.people[info.get("wife")]
+        if info.get("husband"):
+            list_of_people[index].husband = Person.people[info.get("husband")]
     return list_of_people
