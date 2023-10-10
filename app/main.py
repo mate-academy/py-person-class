5f5c9da0915cfa3aaa2ecc17d623d133148d87f9
Anton <antonrozegan1@gmail.com>
2023-10-10 14:32:19 +0300

    nat

--- app/main.py
+++ app/main.py
@@ -10,17 +10,14 @@ class Person:
 
 def create_person_list(people: list[dict]) -> list[Person]:
     persons_ls = []
+    persons_ls = [Person(body["name"], body["age"]) for body in people]
 
-    for body in people:
-        person = Person(body["name"], body["age"])
-        persons_ls.append(person)
+    for char in range(len(people)):
 
-    for i in range(len(people)):
-
-        if "wife" in people[i] and people[i]["wife"] is not None:
-            persons_ls[i].wife = Person.people.get(people[i]["wife"])
+        if "wife" in people[char] and people[char]["wife"] is not None:
+            persons_ls[char].wife = Person.people.get(people[char]["wife"])
 
         if "husband" in people[i] and people[i]["husband"] is not None:
-            persons_ls[i].husband = Person.people.get(people[i]["husband"])
+            persons_ls[char].husband = Person.people.get(people[char]["husband"])
 
     return persons_ls
