6fc68ce8efcf21df9ef0c592538a125bf0938947
Max Muhanuk <ytmaksik425@gmail.com>
2023-10-12 12:57:02 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    pep = [Person(p["name"], p["age"]) for p in people]
+    for i, p in enumerate(people):
+        try:
+            if p["wife"] is not None:
+                pep[i].wife = Person.people[p["wife"]]
+        except KeyError:
+            if p["husband"] is not None:
+                pep[i].husband = Person.people[p["husband"]]
+
+    return pep
