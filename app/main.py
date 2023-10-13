1351b6746c997caaf92461369540651ca5f4ca73
Yuliia Dudych <juliadudych@gmail.com>
2023-10-13 21:19:31 +0300

    Add logic to populate Person.people dictionary

--- app/main.py
+++ app/main.py
@@ -5,3 +5,18 @@ class Person:
         self.name = name
         self.age = age
         Person.people[self.name] = self
+
+
+def create_person_list(people: list) -> list:
+    people_dict = Person.people
+    for person in people:
+        person_name = person["name"]
+        person_age = person["age"]
+        spouse = "wife" if "wife" in list(person.keys()) else "husband"
+        spouse_name = person[spouse]
+
+        if person_name not in people_dict:
+            Person(person_name, person_age)
+
+        if spouse_name:
+            setattr(people_dict[person_name], spouse, spouse_name)
