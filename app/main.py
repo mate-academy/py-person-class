74cbc293c519cc33fc22e0e78278c226da6d7d28
Ryshtun Denys <rishtund@gmail.com>
2023-10-15 11:55:32 +0300

    AlpDone

--- app/main.py
+++ app/main.py
@@ -5,17 +5,19 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people[name] = age
-    pass
+        self.people.update({self.name: self})
 
 
 def create_person_list(people: list) -> list:
     data = []
+
     for person in people:
-        person["name"] = Person(person["name"], person["age"])
-        if bool(person.get("wife")):
-            person["name"].wife = person["wife"]
-        if bool(person.get("husband")):
-            person["name"].husband = person.get("husband")
-        data.append(person["name"])
+        data.append(Person(person["name"], person["age"]))
+
+    for human in people:
+        if bool(human.get("wife")):
+            Person.people[human["name"]].wife = Person.people[human["wife"]]
+        if bool(human.get("husband")):
+            Person.people[human["name"]].husband = Person.people[human["husband"]]
+
     return data
