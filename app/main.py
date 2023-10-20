99c6478011abd01b59e04f4c3c9b32e6057df5ad
MykolaMazh <mykm3ua@gmail.com>
2023-10-20 12:20:01 +0200

    deleted redundant comment and get() method involved

--- app/main.py
+++ app/main.py
@@ -16,14 +16,13 @@ class Person:
 
 def create_person_list(people: list) -> list:
     for person_data in people:
-        # new_human = Person(human["name"], human["age"])
         new_person = Person(person_data["name"], person_data["age"])
-        if ("husband" in person_data
+        if (person_data.get("husband")
                 and person_data["husband"] in Person.people):
             husband_instance = Person.people[person_data["husband"]]
             new_person.husband = husband_instance
             husband_instance.wife = Person.people[person_data["name"]]
-        elif ("wife" in person_data
+        elif (person_data.get("wife")
               and person_data["wife"] in Person.people):
             wife_instance = Person.people[person_data["wife"]]
             new_person.wife = wife_instance
