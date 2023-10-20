7c7c12fced390847fbfd19ea14b378693d05f5ad
Olga Dolnyk <dolnik2104@gmail.com>
2023-10-20 13:59:36 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Optional, List
 
 
 class Person:
@@ -11,6 +11,8 @@ class Person:
         self.age = age
         self.spouse = spouse
         Person.people[name] = self
+        self.wife = None
+        self.husband = None
 
 
 def create_person_list(people_data: List[dict]) -> List[Person]:
@@ -19,18 +21,18 @@ def create_person_list(people_data: List[dict]) -> List[Person]:
     for person_data in people_data:
         name: str = person_data["name"]
         age: int = person_data["age"]
-        spouse_name: Optional[str] = \
-            person_data.get("wife") or person_data.get("husband")
+        spouse_name: Optional[str] = (person_data.get("wife")
+                                      or person_data.get("husband"))
 
-        spouse: Optional[Person] = Person.people.get(spouse_name) \
-            if spouse_name else None
+        spouse: Optional[Person] = (Person.people.get(spouse_name)
+                                    if spouse_name else None)
 
         person = Person(name, age, spouse)
         person_list.append(person)
 
         if spouse:
             if "wife" in person_data:
-                person.spouse = spouse
+                person.wife = spouse
             else:
                 person.husband = spouse
 
