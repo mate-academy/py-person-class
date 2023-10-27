46c66427e3113f499825be6703205149eaf295c5
Damodar <alaso.se@gmail.com>
2023-10-27 13:27:38 +0300

    Testing

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        self.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_instances = []
+    for human in people:
+        person_instances.append(Person.people[human["name"]])
+        for key in human:
+            if key == "wife" and human["wife"] is not None:
+                person_instances[-1].wife = Person.people[human["wife"]]
+            elif key == "husband" and human["husband"] is not None:
+                person_instances[-1].husband = Person.people[human["husband"]]
+    return person_instances
+
+
