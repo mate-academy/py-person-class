e2e222dc4044c5510e268f7998ebf1810241ec76
Marinel444 <380978907444m@gmail.com>
2023-10-28 15:58:12 +0300

    add parentheses

--- app/main.py
+++ app/main.py
@@ -11,9 +11,11 @@ def create_person_list(people: list) -> list:
     result = [Person(name=item["name"], age=item["age"]) for item in people]
     for person in people:
         if person.get("wife") is not None:
-            Person.people.get(person["name"]).wife = \
+            Person.people.get(person["name"]).wife = (
                 Person.people.get(person["wife"])
+            )
         if person.get("husband") is not None:
-            Person.people.get(person["name"]).husband = \
+            Person.people.get(person["name"]).husband = (
                 Person.people.get(person["husband"])
+            )
     return result
