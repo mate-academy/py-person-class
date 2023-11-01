9a2f102c73567a45b218923f738bf27a2857fa35
Oleksandr <gubar3113@gmail.com>
2023-11-01 23:02:27 +0200

    changed_solution_people

--- app/main.py
+++ app/main.py
@@ -9,18 +9,15 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
     for person_data in people:
         name = person_data["name"]
         age = person_data["age"]
         person = Person(name, age)
         spouse_name = person_data.get("wife") or person_data.get("husband")
-        if spouse_name:
-            spouse = Person.people.get(spouse_name)
-            if spouse:
-                person.spouse = spouse
-                if "wife" in person_data:
-                    person.wife = spouse
-                    spouse.husband = person
-        person_list.append(person)
-    return person_list
+        if spouse := Person.people.get(spouse_name):
+            person.spouse = spouse
+            if "wife" in person_data:
+                person.wife = spouse
+                spouse.husband = person
+
+    return list(Person.people.values())
