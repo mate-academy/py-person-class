95403ec81c70f1e264c180cf745191d147954194
Vlad <mr.darmstadtium@gmail.com>
2023-11-02 20:12:15 +0100

    Wrote all code

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people.update({self.name: self})
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list:
+
+    person_list = []
+
+    for person in people:
+        person = Person(person["name"], person["age"])
+        person_list.append(person)
+
+    for person in people:
+        name = person["name"]
+        spouse_name = person['wife' if 'wife' in person else 'husband']
+        spouse = Person.people.get(spouse_name) if spouse_name else None
+        Person.people[name].spouse = spouse
+
+    return person_list
