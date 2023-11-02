6997d261611070fbd7fdd60a5307cc4bee87b638
Volodymyr Ivanchuk <Volodymyr_Ivanchuk@jabil.com>
2023-11-02 14:15:35 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -6,10 +6,6 @@ class Person:
         self.age = age
         self.__class__.people[name] = self
 
-    @classmethod
-    def get_person(cls, name: str) -> str:
-        return cls.people.get(name)
-
 
 def create_person_list(people: list) -> list:
     person_list = []
@@ -21,8 +17,8 @@ def create_person_list(people: list) -> list:
 
     for person_data, person_inst in zip(people, person_list):
         if person_data.get("wife"):
-            person_inst.wife = Person.get_person(person_data["wife"])
+            person_inst.wife = Person.people.get(person_data["wife"])
         if person_data.get("husband"):
-            person_inst.husband = Person.get_person(person_data["husband"])
+            person_inst.husband = Person.people.get(person_data["husband"])
 
     return person_list
