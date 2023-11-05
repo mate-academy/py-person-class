e3943b6d87c2459d589b468338433eb2943cdbd0
Vitalij Chajka <chaika.vitalii@gmail.com>
2023-11-05 22:05:07 +0200

    fix class method calling

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.people[name] = self
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
@@ -12,14 +12,12 @@ def create_person_list(people: list) -> list:
     for person in people:
         person_list.append(Person(person["name"], person["age"]))
     for person in people:
-        if "wife" in person:
-            if person["wife"] is not None:
-                Person.people[person["name"]].wife = (
-                    Person.people[person["wife"]]
-                )
-        if "husband" in person:
-            if person["husband"] is not None:
-                Person.people[person["name"]].husband = (
-                    Person.people[person["husband"]]
-                )
+        if "wife" in person and person["wife"] is not None:
+            Person.people[person["name"]].wife = (
+                Person.people[person["wife"]]
+            )
+        if "husband" in person and person["husband"] is not None:
+            Person.people[person["name"]].husband = (
+                Person.people[person["husband"]]
+            )
     return person_list
