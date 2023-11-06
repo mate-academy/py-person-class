b76ec87730a80a64c4e501337956b855011ae85b
Vitalij Chajka <chaika.vitalii@gmail.com>
2023-11-06 14:35:54 +0200

    fix (use list comprehension and simply condition)

--- app/main.py
+++ app/main.py
@@ -8,15 +8,13 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
+    person_list = [Person(person["name"], person["age"]) for person in people]
     for person in people:
-        person_list.append(Person(person["name"], person["age"]))
-    for person in people:
-        if "wife" in person and person["wife"] is not None:
+        if person.get("wife") is not None:
             Person.people[person["name"]].wife = (
                 Person.people[person["wife"]]
             )
-        if "husband" in person and person["husband"] is not None:
+        if person.get("husband") is not None:
             Person.people[person["name"]].husband = (
                 Person.people[person["husband"]]
             )
