83a6e052c0ed6bba9edf6f2cf12a31707d62a52a
Oleksii Mileiko <alexexile1987@gmail.com>
2023-11-08 19:12:00 +0200

    changed name

--- app/main.py
+++ app/main.py
@@ -10,10 +10,12 @@ class Person:
 
 def create_person_list(people: list[dict]) -> list:
 
-    people_list = [Person(i["name"], i["age"]) for i in people]
-    for i in people:
-        if i.get("wife"):
-            Person.people[i["name"]].wife = Person.people[i["wife"]]
-        if i.get("husband"):
-            Person.people[i["name"]].husband = Person.people[i["husband"]]
+    people_list = [Person(person["name"], person["age"]) for person in people]
+    for preson in people:
+        if preson.get("wife"):
+            Person.people[preson["name"]].wife \
+                = Person.people[preson["wife"]]
+        if preson.get("husband"):
+            Person.people[preson["name"]].husband \
+                = Person.people[preson["husband"]]
     return people_list
