30adf27cd62727b17c6ad9eaf661cb6acd5a21a6
Volodymyr Panchenko <vpanchenko@spscommerce.com>
2023-11-10 15:31:12 +0200

    Changes after reviw

--- app/main.py
+++ app/main.py
@@ -10,10 +10,10 @@ class Person:
 def create_person_list(people: list) -> list:
     person_list = [Person(person["name"], person["age"]) for person in people]
     for index, person in enumerate(people):
-        if "wife" in person and person["wife"] is not None:
+        if person.get("wife"):
             person_list[index].wife = Person.people.get(person["wife"])
 
-        if "husband" in person and person["husband"] is not None:
+        if person.get("husband"):
             person_list[index].husband = Person.people.get(person["husband"])
 
     return person_list
