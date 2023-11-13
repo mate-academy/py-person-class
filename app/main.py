b9ed7a618df0e55ddd14f35f2d30349a9b606098
Mikola Rozhansky <bymikola@gmail.com>
2023-11-13 21:35:08 +0200

    init

--- app/main.py
+++ app/main.py
@@ -2,19 +2,44 @@ class Person:
     # write your code here
     people = {}
 
-    def __init__(self, name: str, age: int, wife: str, husband: str) -> None:
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.wife = wife
-        self.husband = husband
+        self.wife = None
+        self.husband = None
         self.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
+    out = []
+
     for person in people:
-        Person.name = person["name"]
-        Person.age = person["age"]
-        Person.wife = person["wife"]
+        # out.append(Person(person["name"], person["age"], person["wife"], person["husband"]))
+        print(person["name"])
+        new = Person(name=person["name"], age=person["age"])
+        out.append(new)
+        # new = Person(name=person["name"], age=person["age"], wife=person["wife"], husband=person["husband"])
+        # print(new.name)
+    for i in out:
+        print(i)
+        print(i.name, i.age, i.wife, i.husband)
+        for j in people:
+        #     # if i.name == person["wife"]:
+            if ("wife" in j) and (j["wife"] == i.name) and (j["wife"] is not None):
+                i.wife = i
+                print("!!!! ", i.name, j["wife"], i.wife.name)
+            if ("husband" in j) and (j["husband"] == i.name) and (j["husband"] is not None):
+                i.husband = i
+                print("!!!! ", i.name, j["husband"], i.husband.name)
+
+    # for person in people:
+    #     for data in out:
+    #         if person["wife"] == data.name:
+    #             data.wife = data
+    #         if person["husband"] == data.name:
+    #             data.husband = data
+    return out
+
 
 
 
