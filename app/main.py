8f682c6d8dc5b1e2a9421df84f99bbf009c17f3d
Serhii <serhii.domik@gmail.com>
2023-11-13 15:02:31 +0200

    Revert "'Solution'"
    This reverts commit e3c25e8b2edbcd91d04a30a1ac78d9f4cdae840e.

--- app/main.py
+++ app/main.py
@@ -1,18 +1,8 @@
 class Person:
-    people = {}
-
-    def __init__(self, name: str, age: int) -> None:
-        self.name = name
-        self.age = age
-        self.people.update({self.name: self})
+    # write your code here
+    pass
 
 
 def create_person_list(people: list) -> list:
-    result = [Person(person["name"], person["age"]) for person in people]
-    for partner in people:
-        name = partner["name"]
-        if "wife" in partner and partner["wife"] is not None:
-            Person.people[name].wife = Person.people[partner["wife"]]
-        elif "husband" in partner and partner["husband"] is not None:
-            Person.people[name].husband = Person.people[partner["husband"]]
-    return result
+    # write your code here
+    pass
