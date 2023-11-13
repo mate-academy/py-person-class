52c7a03d8bd12a8f134b72e8b63cc92bcd33210d
Mikola Rozhansky <bymikola@gmail.com>
2023-11-13 20:08:24 +0200

    init

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 class Person:
     # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int, wife: str, husband: str) -> None:
+        self.name = name
+        self.age = age
+        self.wife = wife
+        self.husband = husband
+        self.people[self.name] = self
+
+
+
+
 
 
 def create_person_list(people: list) -> list:
