7ef01d812e39ae1ae89587c9417575e9844c905a
Yura Push <youdeimos1@gmail.com>
2023-11-14 21:52:09 +0200

    used list comprehension instead if first for loop

--- app/main.py
+++ app/main.py
@@ -8,13 +8,13 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_instances = [Person(person["name"], person["age"]) for person in people]
+    person_inst = [Person(person["name"], person["age"]) for person in people]
 
-    for person_instance, person_data in zip(person_instances, people):
+    for person_instance, person_data in zip(person_inst, people):
         spouse_name = person_data.get("wife") or person_data.get("husband")
         if spouse_name:
             spouse_instance = Person.people.get(spouse_name)
             wife_or_husband = "wife" if "wife" in person_data else "husband"
             setattr(person_instance, wife_or_husband, spouse_instance)
 
-    return person_instances
+    return person_inst
