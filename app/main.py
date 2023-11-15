4330e9832bf975bd330cecaa3b86dd194d014dfb
Maksym Bondar <maks.bondar878@gmail.com>
2023-11-15 22:13:55 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> type:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+    for person_data in people:
+        person = Person(person_data["name"], person_data["age"])
+        person_list.append(person)
+
+    # Now that all instances are created, set the wife/husband relationships
+    for person_data in people:
+        person = Person.people[person_data["name"]]
+        spouse_name = person_data.get("wife") or person_data.get("husband")
+        if spouse_name is not None:
+            spouse = Person.people[spouse_name]
+            if "wife" in person_data:
+                person.wife = spouse
+                spouse.husband = person
+            else:
+                person.husband = spouse
+                spouse.wife = person
+
+    return person_list
