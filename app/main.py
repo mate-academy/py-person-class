f0785f67249f99e5c6d7ad9a4ca0a50681b3aee4
Dmytro Semenets <dmitriymine@gmail.com>
2023-11-17 12:34:45 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,39 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+    nameparts = []
+    for person in people:
+        person1 = list(person.values())
+        person2 = list(person.keys())
+        name = person1[0]
+        age = person1[1]
+
+        keylist = person2[2]
+        secondpart = person1[2]
+
+        nameparts.append(secondpart)
+
+        person_list.append(Person(name, age))
+
+    j = 0
+    for name in nameparts:
+        if name is None:
+            pass
+        else:
+            for i in range(len(person_list)):
+                if person_list[i].name == name:
+                    if keylist[i] == "wife":
+                        person_list[j].husband = person_list[i]
+                    else:
+                        person_list[j].wife = person_list[i]
+        j += 1
+
+    return person_list
