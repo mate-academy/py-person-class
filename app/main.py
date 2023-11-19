7ade498d4508162b98594a8167aecbc963ced550
Oksana Kozlova <brunhildaaaaa@gmail.com>
2023-11-19 18:43:37 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
-from __future__ import annotations
-
-
 class Person:
     people = {}
 
@@ -22,18 +19,17 @@ def find_partner(person: Person, spouse_name: str, is_wife: bool) -> None:
 
 
 def create_person_list(people: list) -> list:
-    created_person_list = \
-        [
-            Person(persona["name"], persona["age"])
-            for persona in people
-        ]
+    created_person_list = [
+        Person(persona["name"], persona["age"])
+        for persona in people
+    ]
 
     for persona in people:
         person = Person.people.get(persona["name"])
         if person:
-            if "wife" in persona and persona["wife"]:
-                find_partner(person, persona["wife"], is_wife=True)
-            if "husband" in persona and persona["husband"]:
-                find_partner(person, persona["husband"], is_wife=False)
+            if wife_name := persona.get("wife"): # noqa
+                find_partner(person, wife_name, is_wife=True)
+            if husband_name := persona.get("husband"): # noqa
+                find_partner(person, husband_name, is_wife=False)
 
     return created_person_list
