18582fe634a06a8c408430e7d3d30a47abef1ac3
Dmytro Gertsovenko <gertsovenko@gmail.com>
2023-11-20 18:46:29 +0200

    new version of husband/wife

--- app/main.py
+++ app/main.py
@@ -15,14 +15,12 @@ def create_person_list(people_list: list) -> list:
     for person in people_list:
         if "husband" in person and person["husband"]:
             for woman in new_people_list:
-                if woman.name == person["name"]:
-                    for man in new_people_list:
-                        if man.name == person["husband"]:
-                            woman.husband = man
-        if "wife" in person and person["wife"]:
+                for man in new_people_list:
+                    if woman.name == person["name"] and man.name == person["husband"]:
+                        woman.husband = man
+        elif "wife" in person and person["wife"]:
             for man in new_people_list:
-                if man.name == person["name"]:
-                    for woman in new_people_list:
-                        if woman.name == person["wife"]:
-                            man.wife = woman
+                for woman in new_people_list:
+                    if man.name == person["name"] and woman.name == person["wife"]:
+                        man.wife = woman
     return new_people_list
