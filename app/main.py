0e8aa129b6a2a7df82f58015c73d7de35748b8f3
Nataliia Povashevych <interchanal@ukr.net>
2023-11-22 22:04:35 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    inst_list = [Person(human["name"], human["age"]) for human in people]
+    for human in people:
+        name = human["name"]
+        if "wife" in human.keys():
+            if human["wife"] in Person.people:
+                Person.people[name].wife = Person.people[human["wife"]]
+        if "husband" in human.keys():
+            if human["husband"] in Person.people:
+                Person.people[name].husband = Person.people[human["husband"]]
+    return inst_list
