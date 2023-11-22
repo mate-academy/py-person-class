91208c012e3c1f01fd65bf61f281e915d3e17d1e
Vladyslav Moiseienko <moiseenkov651@gmail.com>
2023-11-22 14:11:19 +0200

    Corrections 2

--- app/main.py
+++ app/main.py
@@ -4,21 +4,21 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.people[name] = self
+        Person.people[name] = self
     pass
 
 
 def create_person_list(people: list) -> list[Person]:
+    Person.people = {}
     for person in people:
         Person(person["name"], person["age"])
     for person in people:
         current_person = Person.people[person["name"]]
         if person.get("wife") is not None:
-            wife = Person.people[person["wife"]]
-            current_person.wife = wife
+            current_person.wife = Person.people[person["wife"]]
         elif person.get("husband") is not None:
-            husband = Person.people[person["husband"]]
-            current_person.husband = husband
+            current_person.husband = Person.people[person["husband"]]
 
     return list(Person.people.values())
+
     pass
