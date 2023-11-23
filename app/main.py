d18f58fbb3604d72152bef378d97e151d221dd67
Vladyslav Moiseienko <147310033+Vlad1slavo@users.noreply.github.com>
2023-11-23 14:18:20 +0200

    Corrections 4

--- app/main.py
+++ app/main.py
@@ -10,8 +10,8 @@ class Person:
 def create_person_list(people: list) -> list[Person]:
     person_list = [Person(pers["name"], pers["age"]) for pers in people]
     for pers, person in zip(people, person_list):
-        if "wife" in pers and pers["wife"]:
+        if pers.get("wife"):
             person.wife = Person.people[pers["wife"]]
-        elif "husband" in pers and pers["husband"]:
+        elif pers.get("husband"):
             person.husband = Person.people[pers["husband"]]
     return person_list
