ce86022fbe0b53f0643fea922617ad3125319f93
Nataliia <natali4.kucher55@gmail.com>
2023-11-24 14:24:15 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.__class__.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    new_people_list = []
+    for dict_pers in people:
+        person = Person(dict_pers["name"], dict_pers["age"])
+        new_people_list.append(person)
+    for person in new_people_list:
+        if "wife" in dict_pers:
+            person.wife = Person.people[dict_pers["wife"]]
+            person.wife.husband = person
+        if "husband" in dict_pers:
+            person.husband = Person.people[dict_pers["husband"]]
+            person.husband.wife = person
+    return new_people_list
