21cd5617ce1bc7ee9e546543af273675b512a094
Svitlana Rozova <svitlana.rozova@gmail.com>
2023-11-30 13:18:52 +0200

    updated variables' names

--- app/main.py
+++ app/main.py
@@ -9,10 +9,10 @@ class Person:
 
 def create_person_list(people: list) -> list:
     person_list = [Person(person["name"], person["age"]) for person in people]
-    for i, person in enumerate(people):
+    for index, person in enumerate(people):
         if "wife" in person and person["wife"] in Person.people:
-            person_list[i].wife = Person.people[person["wife"]]
+            person_list[index].wife = Person.people[person["wife"]]
         if "husband" in person and person["husband"] in Person.people:
-            person_list[i].husband = Person.people[person["husband"]]
+            person_list[index].husband = Person.people[person["husband"]]
 
     return person_list
