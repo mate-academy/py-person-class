03bc6c0ea171af979260289e9a2eefe72d696681
angelzagniy <angelzagniy@gmail.com>
2023-12-05 13:20:36 +0200

    call ettribute people from Person, make list comprehension and use the get method and walrus operator

--- app/main.py
+++ app/main.py
@@ -4,18 +4,19 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.people[name] = self
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    list_of_people = []
-    for person in people:
-        list_of_people.append(Person(person["name"], person["age"]))
+    list_of_people = [
+        Person(person["name"], person["age"])
+        for person in people
+    ]
 
-    for i, person in enumerate(people):
-        if "wife" in person and person["wife"] is not None:
-            list_of_people[i].wife = Person.people[person["wife"]]
-        elif "husband" in person and person["husband"] is not None:
-            list_of_people[i].husband = Person.people[person["husband"]]
+    for index, person in enumerate(people):
+        if wife_name := person.get("wife"):
+            list_of_people[index].wife = Person.people[wife_name]
+        elif husband_name := person.get("husband"):
+            list_of_people[index].husband = Person.people[husband_name]
 
     return list_of_people
