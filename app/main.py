b3152d744c12c2255184cf98d3afdd13cc6e66a4
Max <bidun.max@gmail.com>
2023-12-07 21:51:31 +0200

    Solution_4

--- app/main.py
+++ app/main.py
@@ -11,11 +11,10 @@ def create_person_list(people: list) -> list:
     for person in people:
         Person(person["name"], person["age"])
     for person in people:
-        if "wife" in person and person.get("wife") in Person.people:
+        if Person.people.get(person.get("wife")):
             Person.people.get(person.get("name")).wife =\
                 Person.people.get(person.get("wife"))
-        elif ("husband" in person
-              and person.get("husband") in Person.people):
+        elif Person.people.get(person.get("husband")):
             Person.people.get(person.get("name")).husband = (
                 Person.people.get(person.get("husband")))
     return list(Person.people.values())
