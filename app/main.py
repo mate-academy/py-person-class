e32c4e564e5e8637def80189b4abce30ff6259ef
Vasyl Kalish <vasyl.documents@yahoo.it>
2023-12-10 15:36:03 +0100

    Close the improve task

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people.update({self.name: self})
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    result = []
+    for element in people:
+        result.append(Person(element["name"], element["age"]))
+
+    for num in range(len(people)):
+        k_val = list(people[num].keys())
+        if k_val[2] == "wife" and people[num]["wife"] is not None:
+            result[num].wife = Person.people[people[num]["wife"]]
+        else:
+            if k_val[2] == "husband" and people[num]["husband"] is not None:
+                result[num].husband = Person.people[people[num]["husband"]]
+    return result
