3fb86f07f560498d4ff909f26e8572baa4e84816
Vasyl Kalish <vasyl.documents@yahoo.it>
2023-12-11 22:02:29 +0100

    Fix the task

--- app/main.py
+++ app/main.py
@@ -11,13 +11,13 @@ def create_person_list(people: list) -> list:
     result_of_function = [Person(element["name"], element["age"])
                           for element in people
                           ]
-    for number in range(len(people)):
-        keys_of_people = list(people[number].keys())
-        if keys_of_people[2] == "wife" and people[number]["wife"] is not None:
-            result_of_function[number].wife \
-                = Person.people[people[number]["wife"]]
-        if (keys_of_people[2] == "husband"
-                and people[number]["husband"] is not None):
-            result_of_function[number].husband \
-                = Person.people[people[number]["husband"]]
+    ind_val = 0  # for iteration inside for loop
+    for values in people:
+        keys_of_people = list(values.keys())
+        if keys_of_people[2] == "wife" and values["wife"] is not None:
+            result_of_function[ind_val].wife = Person.people[values["wife"]]
+        if keys_of_people[2] == "husband" and values["husband"] is not None:
+            result_of_function[ind_val].husband \
+                = Person.people[values["husband"]]
+        ind_val += 1
     return result_of_function
