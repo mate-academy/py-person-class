d873d44fc8248cc02a579a0ed50bb07ce97ab757
Vasyl Kalish <vasyl.documents@yahoo.it>
2023-12-12 22:06:10 +0100

    Fix the task

--- app/main.py
+++ app/main.py
@@ -8,16 +8,13 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    result_of_function = [Person(element["name"], element["age"])
-                          for element in people
-                          ]
-    ind_val = 0  # for iteration inside for loop
-    for values in people:
-        keys_of_people = list(values.keys())
-        if keys_of_people[2] == "wife" and values["wife"] is not None:
-            result_of_function[ind_val].wife = Person.people[values["wife"]]
-        if keys_of_people[2] == "husband" and values["husband"] is not None:
-            result_of_function[ind_val].husband \
-                = Person.people[values["husband"]]
-        ind_val += 1
-    return result_of_function
+    people_instance = [Person(person["name"], person["age"])
+                       for person in people
+                       ]
+    for value, person in enumerate(people):
+        if "wife" in person and person.get("wife") is not None:
+            people_instance[value].wife = Person.people[person["wife"]]
+        if "husband" in person and person.get("husband") is not None:
+            people_instance[value].husband \
+                = Person.people[person["husband"]]
+    return people_instance
