d494b87dfcaaa8d652b5edcc9d7842944bc9a0c4
Maksym Symonovych <maksym.symonovych@gmail.com>
2023-12-15 16:21:36 +0200

    rewrite 'create_person_list' function

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ class Person:
     def __init__(self, name: str, age: int) -> Person:
         self.name = name
         self.age = age
+
         Person.people[name] = self
 
 
@@ -14,14 +15,11 @@ def create_person_list(people: list) -> list:
     person_list = [Person(person["name"], person["age"])
                    for person in people]
 
-    for i, person in enumerate(person_list):
-        if people[i].get("wife"):
-            person.wife = person.people[
-                people[i]["wife"]
-            ]
-        elif people[i].get("husband"):
-            person.husband = person.people[
-                people[i]["husband"]
-            ]
+    for person in people:
+        person_instance = Person.people[person["name"]]
+        if person.get("wife"):
+            person_instance.wife = Person.people[person["wife"]]
+        elif person.get("husband"):
+            person_instance.husband = Person.people[person["husband"]]
 
     return person_list
