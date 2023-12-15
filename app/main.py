0f96094c37e704bef56fb086f6c3d8bb2e4c02e6
Roman Sokolov <gnonasis@gmail.com>
2023-12-15 22:33:47 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(incoming_list: list) -> list:
+    """
+
+    :param incoming_list:
+    :return:
+    """
+    persons_list = []
+    for part_of_list in incoming_list:
+        name = part_of_list["name"]
+        age = part_of_list["age"]
+        person = Person(name, age)
+        persons_list.append(person)
+    for i, part_of_list in enumerate(incoming_list):
+        if part_of_list.get("wife"):
+            persons_list[i].wife = \
+                Person.people[part_of_list.get("wife")]
+        if part_of_list.get("husband"):
+            persons_list[i].husband = \
+                Person.people[part_of_list.get("husband")]
+    return persons_list
