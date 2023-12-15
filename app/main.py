8d4ddd6daa508477861a89c22f962a7d4e27c316
Maksym Symonovych <maksym.symonovych@gmail.com>
2023-12-15 16:37:54 +0200

    fixed the 'issue'

--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
-from __future__ import annotations
-
-
 class Person:
     people = {}
 
-    def __init__(self, name: str, age: int) -> Person:
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
 
