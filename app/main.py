c0e61bba237d54ed24f17732537f7b023e013a55
Roman Sokolov <gnonasis@gmail.com>
2023-12-16 16:21:43 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -8,22 +8,16 @@ class Person:
 
 
 def create_person_list(incoming_list: list) -> list:
-    """
+    persons_list = [Person(part_of_list["name"], part_of_list["age"])
+                    for part_of_list in incoming_list]
 
-    :param incoming_list:
-    :return:
-    """
-    persons_list = []
-    for part_of_list in incoming_list:
-        name = part_of_list["name"]
-        age = part_of_list["age"]
-        person = Person(name, age)
-        persons_list.append(person)
-    for i, part_of_list in enumerate(incoming_list):
+    for index, part_of_list in enumerate(incoming_list):
         if part_of_list.get("wife"):
-            persons_list[i].wife = \
-                Person.people[part_of_list.get("wife")]
+            persons_list[index].wife = Person.people[
+                part_of_list.get("wife")]
+
         if part_of_list.get("husband"):
-            persons_list[i].husband = \
-                Person.people[part_of_list.get("husband")]
+            persons_list[index].husband = Person.people[
+                part_of_list.get("husband")]
+
     return persons_list
