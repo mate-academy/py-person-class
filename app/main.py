cd0c29dc0a021db49f271a712d1a64f60a0fdcff
cyb1ove <cyb1ove.ua@gmail.com>
2023-12-16 21:26:18 +0200

    codestyle fixes

--- app/main.py
+++ app/main.py
@@ -6,11 +6,12 @@ class Person:
         self.age = age
         self.people[name] = self
 
+
 def create_person_list(people: list) -> list:
     person_list = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
-        for partner_type in ["wife", "husband"]: 
+        for partner_type in ["wife", "husband"]:
             if partner_type in person and person[partner_type] is not None:
                 person_object = Person.people[person["name"]]
                 partner_object = Person.people[person[partner_type]]
