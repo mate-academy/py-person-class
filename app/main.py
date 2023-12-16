371a31b605c3c2f29c2b4e0a1da6e33e90ebe68b
cyb1ove <cyb1ove.ua@gmail.com>
2023-12-16 21:22:16 +0200

    task completed

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
 
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.people[name] = self
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = [Person(person["name"], person["age"]) for person in people]
+    
+    for person in people:
+        for partner_type in ["wife", "husband"]:
+            if partner_type in person and person[partner_type] is not None:
+                person_object = Person.people[person["name"]]
+                partner_object = Person.people[person[partner_type]]
+                setattr(person_object, partner_type, partner_object)
+    
+    return person_list
