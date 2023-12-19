767d16d9b807e8d60195ad6fbb03188063062d45
Andrii <nadandrii9@gmail.com>
2023-12-19 17:48:31 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -6,25 +6,19 @@ class Person:
         self.age = age
         Person.people[name] = self
 
-    def wife_(self, wife_name: str) -> None:
-        if wife_name and wife_name in Person.people:
-            self.wife = Person.people[wife_name]
-
-    def husband_(self, husband_name: str) -> None:
-        if husband_name and husband_name in Person.people:
-            self.husband = Person.people[husband_name]
-
 
 def create_person_list(people_list: list) -> list:
-    for person_dict in people_list:
-        Person(person_dict["name"], person_dict["age"])
+    persons = [Person(person_dict["name"], person_dict["age"])
+               for person_dict in people_list]
 
     for person_dict in people_list:
         person = Person.people[person_dict["name"]]
         wife_name = person_dict.get("wife")
         husband_name = person_dict.get("husband")
-        if wife_name:
-            person.wife_(wife_name)
-        if husband_name:
-            person.husband_(husband_name)
-    return list(Person.people.values())
+
+        if wife_name and wife_name in Person.people:
+            person.wife = Person.people[wife_name]
+        if husband_name and husband_name in Person.people:
+            person.husband = Person.people[husband_name]
+
+    return persons
