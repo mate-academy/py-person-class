9c0fce008b03c73fc02f85cea2a880d13cec8f82
Bohdan Cherniavskyi <b.cherniavskyi@gmail.com>
2023-12-23 22:44:35 +0100

    fix long lines

--- app/main.py
+++ app/main.py
@@ -13,8 +13,12 @@ def create_person_list(people: list) -> list:
     for person in people:
         result_list.append(Person(name=person["name"], age=person["age"]))
     for person in people:
-        if person.get("wife") is not None:
-            Person.people[person["name"]].wife = Person.people[person["wife"]]
-        elif person.get("husband") is not None:
-            Person.people[person["name"]].husband = Person.people[person["husband"]]
+        if person.get("wife"):
+            Person.people[person["name"]].wife = (
+                Person.people[person["wife"]]
+            )
+        elif person.get("husband"):
+            Person.people[person["name"]].husband = (
+                Person.people[person["husband"]]
+            )
     return result_list
