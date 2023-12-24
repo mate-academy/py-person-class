643b702c25edfee1dd061481d585663c6fe8912d
Roman Sokolov <gnonasis@gmail.com>
2023-12-24 19:44:49 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -11,13 +11,13 @@ def create_person_list(people_data: list) -> list:
     people = [Person(person["name"], person["age"])
               for person in people_data]
 
-    for index, part_of_list in enumerate(people_data):
-        if part_of_list.get("wife"):
+    for index, person in enumerate(people_data):
+        if person.get("wife"):
             people[index].wife = Person.people[
-                part_of_list.get("wife")]
+                person.get("wife")]
 
-        if part_of_list.get("husband"):
+        if person.get("husband"):
             people[index].husband = Person.people[
-                part_of_list.get("husband")]
+                person.get("husband")]
 
     return people
