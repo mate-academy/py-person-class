2efa520726d9773240f8255ddd72f0f21b289c97
alex <tulyulyuk91@gmail.com>
2023-12-25 12:06:52 +0100

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people.update({name: self})
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    return [
+        Person(person["name"],
+               person["age"])
+               # person["wife"] if "wife" in person else person["husband"],
+               # person["spouse_name"])
+        for person in people
+    ]
