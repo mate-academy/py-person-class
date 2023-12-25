7ceb9c593d6acde797ad271704013fe60fe92b06
Maksym Nimych <maksimnimitch@gmail.com>
2023-12-26 00:33:29 +0200

    Delete blank lines

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 class Person:
-
     people = {}
 
     def __init__(self, name: str, age: int) -> None:
-
         self.name = name
         self.age = age
         self.people[name] = self
@@ -14,14 +12,11 @@ def create_person_list(people: list) -> list:
     result = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
-
         if person.get("wife"):
-
             Person.people[person["name"]].wife = Person.people[person["wife"]]
 
         if person.get("husband"):
-
-            Person.people[person["name"]].husband = \
-                Person.people[person["husband"]]
+            Person.people[person["name"]].husband = (Person.people
+                                                     [person["husband"]])
 
     return result
