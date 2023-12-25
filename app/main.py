10a9c33625d0fe41b3202b7cec28e0f32a6cde4d
Maksym Nimych <maksimnimitch@gmail.com>
2023-12-25 20:46:53 +0200

    Change var name & and statement

--- app/main.py
+++ app/main.py
@@ -11,17 +11,17 @@ class Person:
 
 def create_person_list(people: list) -> list:
 
-    result_list = [Person(person["name"], person["age"]) for person in people]
+    result = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
 
-        if ("wife" in person) and (not person["wife"] is None):
+        if person.get("wife"):
 
             Person.people[person["name"]].wife = Person.people[person["wife"]]
 
-        if ("husband" in person) and (not person["husband"] is None):
+        if person.get("husband"):
 
             Person.people[person["name"]].husband = \
                 Person.people[person["husband"]]
 
-    return result_list
+    return result
