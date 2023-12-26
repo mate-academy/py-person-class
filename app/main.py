a78b14d9be904e0bcce43e48205a4725a2798899
Evgeniy Vasin <evgeniy97v@gmail.com>
2023-12-26 18:59:56 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
 
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people):
+    person_instances = []
+    for person_info in people:
+        person = Person(person_info["name"], person_info["age"])
+        person_instances.append(person)
+    return person_instances
