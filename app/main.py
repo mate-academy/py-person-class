cfaabeb32fd688025319acc3d4c26fcd34d7f69a
ladmael <don.sosiska339@gmail.com>
2023-12-27 09:39:54 +0200

    fix flake8

--- app/main.py
+++ app/main.py
@@ -10,22 +10,25 @@ class Person:
     @classmethod
     def add_partner(cls, peoples: list) -> None:
         for human in peoples:
-            if human.get("wife") != -1 and human.get("wife") is not None:
+            if (human.get("wife") != -1
+                    and human.get("wife") is not None):
                 if human.get("wife") in cls.people:
-                    cls.people[human["name"]].wife = cls.people[human["wife"]]
-                    cls.people[human["wife"]].husband = cls.people[human["name"]]
-            elif human.get("husband") != -1 and human.get("husband") is not None:
+                    cls.people[human["name"]].wife = (
+                        cls.people)[human["wife"]]
+                    cls.people[human["wife"]].husband = (
+                        cls.people)[human["name"]]
+            elif (human.get("husband") != -1
+                  and human.get("husband") is not None):
                 if human.get("husband") in cls.people:
-                    cls.people[human["name"]].husband = cls.people[human["husband"]]
-                    cls.people[human["husband"]].wife = cls.people[human["name"]]
+                    cls.people[human["name"]].husband = (
+                        cls.people)[human["husband"]]
+                    cls.people[human["husband"]].wife = (
+                        cls.people)[human["name"]]
 
 
 def create_person_list(people_list: list) -> list:
-    result = [Person(human.get("name"), human.get("age")) for human in people_list]
+    result = [Person(human.get("name"), human.get("age"))
+              for human in people_list]
     Person.add_partner(people_list)
     print(Person.people)
     return result
-
-
-
-
