6f5d9ee0892f935efaa26065c4348cfe23c1c000
Roman Zaliznyi <romanzaliznyi.python@gmail.com>
2024-01-02 21:15:19 +0200

    Attempt4

--- app/main.py
+++ app/main.py
@@ -4,8 +4,6 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-#        self.wife = None
-#        self.husband = None
         Person.people[name] = self
 
 
@@ -17,20 +15,14 @@ def create_person_list(people: list) -> list:
         person = Person(name, age)
 
         if person_data.get("wife"):
-            if person_data["wife"] == None:
-                continue
-            else:    
-                wife_name = person_data["wife"]
-                if wife_name in Person.people:
-                    Person.wife = Person.people[wife_name]
+            wife_name = person_data["wife"]
+            if wife_name in Person.people:
+                Person.wife = Person.people[wife_name]
 
         if person_data.get("husband"):
             husband_name = person_data["husband"]
             if husband_name in Person.people:
                 Person.husband = Person.people[husband_name]
-        
-#        if person.wife is None:
-#            person_list.append(person = Person(name, age))
 
         person_list.append(person)
     return person_list
