13e75919782e04572670c8122f84b3eb2c0572db
Roman Zaliznyi <romanzaliznyi.python@gmail.com>
2024-01-02 19:37:51 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -18,14 +18,14 @@ def create_person_list(people: list) -> list:
 
         if "wife" in person_data and person_data["wife"] is not None:
             wife_name = person_data["wife"]
-            if wife_name in person.people:  # Використовуємо атрибут екземпляру
+            if wife_name in person.people:
                 person.wife = person.people[wife_name]
                 if person.wife.husband is None:
                     person.wife.husband = person
 
         elif "husband" in person_data and person_data["husband"] is not None:
             husband_name = person_data["husband"]
-            if husband_name in person.people:  # Використовуємо атрибут екземпляру
+            if husband_name in person.people:
                 person.husband = person.people[husband_name]
                 if person.husband.wife is None:
                     person.husband.wife = person
