748db16e5302cb342fea26f49e1d8fd3935aff99
D Petetskyi <4477617@gmail.com>
2024-01-03 19:39:59 +0200

    corrected var, implemented list compr"

--- app/main.py
+++ app/main.py
@@ -10,17 +10,16 @@ class Person:
 def create_person_list(people: list) -> list:
     result = list()
 
-    for person in people:
-        Person(person["name"], person["age"])
+    [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
-        tmp = Person.people[person["name"]]
+        instance_person = Person.people[person["name"]]
         if "wife" in person:
             if person["wife"]:
-                tmp.wife = Person.people[person["wife"]]
+                instance_person.wife = Person.people[person["wife"]]
         elif "husband" in person:
             if person["husband"]:
-                tmp.husband = Person.people[person["husband"]]
-        result.append(tmp)
+                instance_person.husband = Person.people[person["husband"]]
+        result.append(instance_person)
 
     return result
