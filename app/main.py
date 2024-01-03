ed54bc14e6d18c097c41304a101f86a175b8534e
AlexHCom <alexhcomgrit@gmail.com>
2024-01-03 16:50:58 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 class Person:
     people = {}
 
-    def __init__(self, name, age):
+
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         self.wife = None
