cc682920cc6937e26a7b79d8daa6701e57fa1375
AlexHCom <alexhcomgrit@gmail.com>
2024-01-04 12:09:47 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -8,21 +8,14 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-
-    # Create Person instances and populate the people dictionary
+    person_list = list()
     for person_data in people:
         person = Person(person_data["name"], person_data["age"])
-
-    # Assign wife/husband relationships
-    for person_data in people:
-        current_person = Person.people[person_data["name"]]
-
-        if "wife" in person_data and person_data["wife"]:
-            current_person.wife = Person.people[person_data["wife"]]
-        elif "husband" in person_data and person_data["husband"]:
-            current_person.husband = Person.people[person_data["husband"]]
-
-        person_list.append(current_person)
-
+        if "wife" in person:
+            if person_data["wife"]:
+                person = Person.people[person_data["wife"]]
+        elif "husband" in person:
+            if person_data["husband"]:
+                person.husband = Person.people[person_data["husband"]]
+        person_list.append(person)
     return person_list
