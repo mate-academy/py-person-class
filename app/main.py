c0f15dcaf99aed0aa7e2e75dacca7dee99de3b0c
Vlad Kleshko <vkoesko@gmail.com>
2024-01-08 15:08:00 +0200

    Used Person.people insted temp_people

--- app/main.py
+++ app/main.py
@@ -8,20 +8,19 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    temp_people = {}
     person_list = []
 
     for person_data in people:
         name = person_data["name"]
         age = person_data["age"]
-        temp_people[name] = Person(name, age)
+        Person.people[name] = Person(name, age)
 
     for person_data in people:
         name = person_data["name"]
-        person = temp_people[name]
+        person = Person.people[name]
         spouse_name = person_data.get("wife") or person_data.get("husband")
         if spouse_name is not None:
-            spouse = temp_people[spouse_name]
+            spouse = Person.people[spouse_name]
             setattr(
                 person, "wife" if "wife" in person_data else "husband", spouse
             )
