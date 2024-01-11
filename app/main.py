203de71e90ce4dc5ff92db7f88618f320d0ad14e
barabarinov <barabarinov@gmail.com>
2024-01-11 12:31:04 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -7,20 +7,28 @@ class Person:
         Person.people[name] = self
 
 
+def link_spouses(person_instance: Person, person_data: dict) -> None:
+    spouse_key = "wife" if "wife" in person_data else "husband"
+    spouse_name = person_data.get(spouse_key)
+
+    if spouse_name:
+        spouse_instance = Person.people.get(spouse_name)
+        if spouse_instance:
+            setattr(person_instance, spouse_key, spouse_instance)
+            setattr(
+                spouse_instance,
+                "husband" if spouse_key == "wife"
+                else "wife",
+                person_instance
+            )
+
+
 def create_person_list(people: list[dict]) -> list[Person]:
     person_list = []
 
-    for person in people:
-        instance = Person(person["name"], person["age"])
-
-        if "wife" in person and person["wife"]:
-            instance.wife = Person.people.get(person["wife"])
-            if instance.wife:
-                instance.wife.husband = instance
-        if "husband" in person and person["husband"]:
-            instance.husband = Person.people.get(person["husband"])
-            if instance.husband:
-                instance.husband.wife = instance
-        person_list.append(instance)
+    for person_data in people:
+        person_instance = Person(person_data["name"], person_data["age"])
+        link_spouses(person_instance, person_data)
+        person_list.append(person_instance)
 
     return person_list
