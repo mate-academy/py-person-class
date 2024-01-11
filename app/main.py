05e0bb9023c93d72b9fbacb3ed4d1dbba545edad
Ant <u123@ua.fm>
2024-01-11 18:27:29 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,32 @@
 class Person:
-    # write your code here
-    pass
+    people = {}     # { name -> Person }
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
+
+    def __repr__(self) -> str:
+        if hasattr(self, "wife"):
+            return f"Person| {self.name} /{self.age} wife= {self.wife.name}"
+        elif hasattr(self, "husband"):
+            return f"Person| {self.name} /{self.age} husb= {self.husband.name}"
+        else:
+            return f"Person| {self.name} /{self.age}"
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+    # filling list of <People> instances
+    for person in people:
+        human = Person(person["name"], person["age"])
+        person_list.append(human)
+    # check & create relations <wife/husband> for instances
+    for person in people:
+        name = person["name"]
+        if "wife" in person and person["wife"] is not None :
+            Person.people[name].wife = Person.people[person["wife"]]
+        elif "husband" in person and person["husband"] is not None :
+            Person.people[name].husband = Person.people[person["husband"]]
+
+    return person_list
