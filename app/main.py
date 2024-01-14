f981e077357fe957aae33ec957c78116ba9158cc
Serhii Dobrovinskii <serhiy.dobrovinskiy@gmail.com>
2024-01-14 15:43:31 +0200

    Sol

--- app/main.py
+++ app/main.py
@@ -1,8 +1,30 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.__class__.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people_list: list) -> list:
+    persons = []
+
+    for i in people_list:
+        name = i["name"]
+        age = i["age"]
+        person = Person(name, age)
+        persons.append(person)
+
+    for person in people_list:
+        if "wife" in person:
+            if person["wife"] is not None:
+
+                way_to_person = Person.people[person["name"]]
+                way_to_person.wife = Person.people[person["wife"]]
+        if "husband" in person:
+            if person["husband"] is not None:
+
+                way_to_person = Person.people[person["name"]]
+                way_to_person.husband = Person.people[person["husband"]]
+    return persons
