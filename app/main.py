7e3cbe7512d19268df2e3b8a4f14d12c6228af78
AntonSkyrda <mr.skyrda@gmail.com>
2024-01-15 21:04:30 +0200

    add comprehension, fix bracket`s position

--- app/main.py
+++ app/main.py
@@ -1,29 +1,23 @@
 class Person:
     people = {}
 
-    def __init__(self,
-                 name: str,
-                 age: int,
-                 ) -> None:
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-    for person in people:
-        human = Person(name=person["name"],
-                       age=person["age"],)
-        person_list.append(human)
+    person_list = [Person(name=person["name"], age=person["age"])
+                   for person in people]
 
     for person in people:
         human = Person.people[person["name"]]
         if "wife" in person:
             if person["wife"]:
-                human.wife = Person.people[person["wife"]]
+                human.wife = Person.people.get(person.get("wife"))
         elif "husband" in person:
             if person["husband"]:
-                human.husband = Person.people[person["husband"]]
+                human.husband = Person.people.get(person.get("husband"))
 
     return person_list
