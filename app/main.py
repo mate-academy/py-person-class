c09a7a9a0188b35e98ca088de4f5a72bc389f752
Ihor Bondarenko <igorb6766@gmail.com>
2024-01-15 12:45:15 +0200

    added comprehension and get()

--- app/main.py
+++ app/main.py
@@ -8,12 +8,15 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-    for new_person in people:
-        person_list.append(Person(new_person["name"], new_person["age"]))
-    for i in range(len(people)):
-        if "wife" in people[i] and people[i]["wife"]:
-            person_list[i].wife = Person.people[people[i]["wife"]]
-        elif "husband" in people[i] and people[i]["husband"]:
-            person_list[i].husband = Person.people[people[i]["husband"]]
+    person_list = [
+        (Person(new_person["name"], new_person["age"]))
+        for new_person in people
+    ]
+    for person in people:
+        if person.get("wife"):
+            person_list[people.index(person)].wife \
+                = Person.people[person["wife"]]
+        elif person.get("husband"):
+            person_list[people.index(person)].husband \
+                = Person.people[person["husband"]]
     return person_list
