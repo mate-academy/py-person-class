b39e5ad29c914c4499f6c1e2f8ce2989cb7185ab
Maksym Operchuk <operlast529@gmail.com>
2024-01-15 16:20:17 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -21,19 +21,3 @@ def create_person_list(people_list: list) -> list:
             human_in_people = Person.people[human["name"]]
             human_in_people.husband = Person.people[human["husband"]]
     return person_instances
-
-# people = [
-#     {"name": "Ross", "age": 30, "wife": "Rachel"},
-#     {"name": "Joey", "age": 29, "wife": None},
-#     {"name": "Rachel", "age": 28, "husband": "Ross"}
-# ]
-# #
-# person_list = create_person_list(people)
-# print(person_list)
-# print(Person.people)
-# print(Person.people["Rachel"].husband.name)
-# # for person_dict in people_list:
-# #     name = person_dict["name"]
-# #     age = person_dict["age"]
-# #     person_instance = Person(name, age)
-# #     person_instances.append(person_instance)
