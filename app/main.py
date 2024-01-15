e05daac43561b0dc700860e560902a0d16c0e021
mx <mnikulyak@gmail.com>
2024-01-15 13:57:35 +0200

    Amended as suggested

--- app/main.py
+++ app/main.py
@@ -9,34 +9,19 @@ class Person:
         self.name: str = name
         self.age: int = age
 
-        self.__class__.people[name] = self
+        self.people[name] = self
 
 
 def create_person_list(people_data: list) -> list:
-    for pers in people_data:
-        if "wife" in pers:
-            pers["spouse_name"] = pers["wife"]
-            pers["spouse_by_gender"] = "wife"
-        else:
-            pers["spouse_name"] = pers["husband"]
-            pers["spouse_by_gender"] = "husband"
-
     people_list = [Person(pers["name"], pers["age"]) for pers in people_data]
 
-    # using the fact that order is the same in `people_data` and `people_list`:
-    for idx_1 in range(len(people_data) - 1):
-        pers_1 = people_data[idx_1]  # a dict
-        person_1 = people_list[idx_1]  # a Person
-        for idx_2 in range(idx_1 + 1, len(people_data)):
-            pers_2 = people_data[idx_2]
-            person_2 = people_list[idx_2]
-
-            if pers_1["spouse_name"] == pers_2["name"]:
-                person_1.__dict__[pers_1["spouse_by_gender"]] = person_2
-                person_2.__dict__[pers_2["spouse_by_gender"]] = person_1
-
     for pers in people_data:
-        del pers["spouse_name"]
-        del pers["spouse_by_gender"]
+        if "wife" in pers and pers["wife"]:
+            Person.people[pers["name"]].wife = Person.people[pers["wife"]]
+            Person.people[pers["wife"]].husband = Person.people[pers["name"]]
+
+        elif "husband" in pers and pers["husband"]:
+            Person.people[pers["name"]].husband = Person.people[pers["husband"]]
+            Person.people[pers["husband"]].wife = Person.people[pers["name"]]
 
     return people_list
