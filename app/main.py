02839066e6c174480d0364e7a386840f2abdec61
Sofia Zholud <szholuds@gmail.com>
2024-01-15 15:10:46 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def create_person_list(people: list) -> list:
     person_list = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
-        if "wife" in person and person["wife"]:
+        if person.get("wife"):
             Person.people[person["name"]].wife = Person.people[person["wife"]]
         elif "husband" in person and person["husband"]:
             Person.people[person["name"]].husband = (
