ac4a4fb4296383edbd135dcb979f228b7fb3807a
Alex Grytsai <a.grytsai1987@gmail.com>
2024-01-16 06:54:17 +0200

    Changed lines 14 and 16 ()

--- app/main.py
+++ app/main.py
@@ -11,9 +11,9 @@ def create_person_list(people: list) -> list:
     for person in people:
         Person(person["name"], person["age"])
     for person in people:
-        if "wife" in person and person["wife"] is not None:
+        if person.get("wife"):
             Person.people[person["name"]].wife = Person.people[person["wife"]]
-        elif "husband" in person and person["husband"] is not None:
+        elif person.get("husband"):
             Person.people[person["name"]].husband = Person.people[
                 person["husband"]]
     return list(Person.people.values())
