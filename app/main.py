ac25851723f29e737d2fcf924a9d470d2c623f2b
Eugen Veklich <zond342@gmail.com>
2024-01-18 17:13:28 +0200

    used list comprehension and used .get()

--- app/main.py
+++ app/main.py
@@ -8,17 +8,13 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    new_people_list = []
-    for person_dict in people:
-        new_person = Person(person_dict["name"], person_dict["age"])
-        new_people_list.append(new_person)
+    new_people_list = [Person(person_dict["name"], person_dict["age"])
+                       for person_dict in people]
     for person in new_people_list:
         for person_dict in people:
             if person_dict["name"] == person.name:
-                if "wife" in person_dict:
-                    if person_dict["wife"] is not None:
-                        person.wife = Person.people[person_dict["wife"]]
-                if "husband" in person_dict:
-                    if person_dict["husband"] is not None:
-                        person.husband = Person.people[person_dict["husband"]]
+                if person_dict.get("wife"):
+                    person.wife = Person.people[person_dict["wife"]]
+                if person_dict.get("husband"):
+                    person.husband = Person.people[person_dict["husband"]]
     return new_people_list
