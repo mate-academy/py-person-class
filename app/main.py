417b0e9f21d9678829c7501db74bdc1b45c32312
volry <v.v.ryzhuk@gmail.com>
2024-01-19 20:16:23 +0200

    working code

--- app/main.py
+++ app/main.py
@@ -9,19 +9,19 @@ class Person:
 def create_person_list(people: list) -> list:
     persons =[]
     for person in people:
-        Person(person['name'], person['age'])
+        Person(person["name"], person["age"])
 
     for person in people:
-        person_obj = Person.people[person['name']]
+        person_obj = Person.people[person["name"]]
         # Add spouse only if the key exists
-        if 'wife' in person and person['wife']:
-            person_obj.wife = Person.people[person['wife']]
-        if 'husband' in person and person['husband']:
-            person_obj.husband = Person.people[person['husband']]
+        if "wife" in person and person["wife"]:
+            person_obj.wife = Person.people[person["wife"]]
+        if "husband" in person and person["husband"]:
+            person_obj.husband = Person.people[person["husband"]]
 
     # Build the list of Person instances without using a list comprehension
     person_list = []
     for person in people:
-        person_list.append(Person.people[person['name']])
+        person_list.append(Person.people[person["name"]])
 
     return person_list
