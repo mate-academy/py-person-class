d7fb737ef86013ce91d41943b85f9b21cebd4890
volry <v.v.ryzhuk@gmail.com>
2024-01-19 20:03:36 +0200

    working with classes

--- app/main.py
+++ app/main.py
@@ -1,13 +1,25 @@
 class Person:
+    people = {}
     def __init__(self, name, age):
         self.name = name
         self.age = age
         self.wife = None
         self.husband = None
 
+        Person.people[name] = self
 
 def create_person_list(people: list) -> list:
     persons =[]
     for person in people:
         persons.append(Person(person['name'], person["age"]))
+
+        for person_dict, person_obj in zip(people, persons):
+            spouse_name = person_dict.get('wife') or person_dict.get('husband')
+            if spouse_name:
+                if 'wife' in person_dict:
+                    person_obj.wife = Person.people[spouse_name]
+                else:
+                    person_obj.husband = Person.people[spouse_name]
+
+    return persons
 
