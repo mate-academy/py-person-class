ffee4d7af5a48bc310c81c7addd1cce4d39391bc
volry <v.v.ryzhuk@gmail.com>
2024-01-19 20:20:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 class Person:
     people = {}
-    def __init__(self, name, age):
+
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
 
         Person.people[name] = self
 
+
 def create_person_list(people: list) -> list:
-    persons =[]
+
     for person in people:
         Person(person["name"], person["age"])
 
