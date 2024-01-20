635f059d2220a1d4b54d66c18037c77fb2c1f0fb
Oleksii Yaroshenko <a.s.yaroshenko.zp@gmail.com>
2024-01-20 20:01:38 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,34 @@
 class Person:
-    # write your code here
-    pass
+    people = dict()
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    res_list = []
+
+    for dic in people:
+        if None in dic.values():
+            res_list.append(Person(dic["name"], dic["age"]))
+        elif "wife" in dic.keys():
+            person = Person(dic["name"], dic["age"])
+            person.wife = dic["wife"]
+            res_list.append(person)
+        elif "husband" in dic.keys():
+            person = Person(dic["name"], dic["age"])
+            person.husband = dic["husband"]
+            res_list.append(person)
+
+    for person_obj in res_list:
+        temp_dic = vars(person_obj)
+        if "wife" in temp_dic.keys():
+            name_wife = temp_dic["wife"]
+            person_obj.wife = Person.people[name_wife]
+        elif "husband" in temp_dic.keys():
+            name_husband = temp_dic["husband"]
+            person_obj.husband = Person.people[name_husband]
+
+    return res_list
