f0ff2fba384cab36d06806240c209d3810916095
Oleksii Yaroshenko <a.s.yaroshenko.zp@gmail.com>
2024-01-23 14:18:39 +0200

    optimized version solution

--- app/main.py
+++ app/main.py
@@ -11,24 +11,14 @@ def create_person_list(people: list) -> list:
     res_list = []
 
     for dic in people:
-        if None in dic.values():
-            res_list.append(Person(dic["name"], dic["age"]))
-        elif "wife" in dic.keys():
-            person = Person(dic["name"], dic["age"])
-            person.wife = dic["wife"]
-            res_list.append(person)
-        elif "husband" in dic.keys():
-            person = Person(dic["name"], dic["age"])
-            person.husband = dic["husband"]
-            res_list.append(person)
+        res_list.append(Person(dic["name"], dic["age"]))
 
-    for person_obj in res_list:
-        temp_dic = vars(person_obj)
-        if "wife" in temp_dic.keys():
-            name_wife = temp_dic["wife"]
-            person_obj.wife = Person.people[name_wife]
-        elif "husband" in temp_dic.keys():
-            name_husband = temp_dic["husband"]
-            person_obj.husband = Person.people[name_husband]
+    for dic in people:
+        if None in dic.values():
+            continue
+        elif "wife" in dic.keys():
+            Person.people[dic["name"]].wife = Person.people[dic["wife"]]
+        elif "husband" in dic.keys():
+            Person.people[dic["name"]].husband = Person.people[dic["husband"]]
 
     return res_list
