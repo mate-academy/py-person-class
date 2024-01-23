cf873b68c9a58b0151939814c60733b3e4ca7ac8
Maryna Barannik <sober2night@gmail.com>
2024-01-23 15:54:46 +0100

    'Fix'

--- app/main.py
+++ app/main.py
@@ -10,17 +10,24 @@ class Person:
 
 def create_person_list(people: list) -> list:
     person_instances = []
+
     for person_data in people:
         person_instance = Person(person_data["name"], person_data["age"])
-        if "wife" in person_data and person_data["wife"] is not None:
-            wife_instance = Person.people.get(person_data["wife"])
+
+        wife_name = person_data.get("wife")
+        husband_name = person_data.get("husband")
+
+        if wife_name is not None:
+            wife_instance = Person.people.get(wife_name)
             if wife_instance is not None:
                 person_instance.wife = wife_instance
                 wife_instance.husband = person_instance
-        elif "husband" in person_data and person_data["husband"] is not None:
-            husband_instance = Person.people.get(person_data["husband"])
+        elif husband_name is not None:
+            husband_instance = Person.people.get(husband_name)
             if husband_instance is not None:
                 person_instance.husband = husband_instance
                 husband_instance.wife = person_instance
+
         person_instances.append(person_instance)
+
     return person_instances
