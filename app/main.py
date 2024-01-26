5c1319e75163af6be7c2f58f46d1ddcd7811fd20
Vladyslav Supruniuk <Vladyslav.Supruniuk@contracted.pmi.com>
2024-01-26 14:34:34 +0200

    Fix

--- app/main.py
+++ app/main.py
@@ -5,26 +5,25 @@ class Person:
         self.name = name
         self.age = age
 
-        self.__class__.people[name] = self
+        Person.people[name] = self
 
 
 def create_person_list(people: list[dict]) -> list[Person]:
-    for person in people:
-        Person(person["name"], person["age"])
+    persons_list = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
-        if "wife" in person and person["wife"] in Person.people:
+        if person.get("wife") and Person.people.get(person["wife"]):
             wife = Person.people[person["wife"]]
             husband = Person.people[person["name"]]
 
             wife.husband = husband
             husband.wife = wife
 
-        if "husband" in person and person["husband"] in Person.people:
+        if person.get("husband") and Person.people.get(person["husband"]):
             wife = Person.people[person["name"]]
             husband = Person.people[person["husband"]]
 
             wife.husband = husband
             husband.wife = wife
 
-    return [person for person in Person.people.values()]
+    return persons_list
