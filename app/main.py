97d281fea50bc9db5d9131b8410a0f593ebc39c3
Vitalii <warcied1@gmail.com>
2024-01-27 14:23:33 +0200

    solution (added suggestions)

--- app/main.py
+++ app/main.py
@@ -6,30 +6,28 @@ class Person:
         self.name = name
         self.age = age
 
-        self.people[self.name] = self
+        Person.people[self.name] = self
 
 
 def find_partner(person_obj: Person,
                  person_to_find: str | None
                  ) -> Person | str | None:
-    if person_to_find and person_to_find in person_obj.people.keys():
+    if person_to_find in Person.people:
         partner = person_obj.people[person_to_find]
         if hasattr(partner, "wife"):
             partner.wife = person_obj
         elif hasattr(partner, "husband"):
             partner.husband = person_obj
-        return person_obj.people[person_to_find]
-    else:
-        return person_to_find
+        return Person.people[person_to_find]
 
 
 def create_person_list(people: [dict]) -> [Person]:
 
     for human_data in people:
         curr_person = Person(human_data["name"], human_data["age"])
-        if "wife" in human_data.keys() and human_data["wife"]:
+        if human_data.get("wife"):
             curr_person.wife = find_partner(curr_person, human_data["wife"])
-        elif "husband" in human_data.keys() and human_data["husband"]:
+        elif human_data.get("husband"):
             curr_person.husband = find_partner(curr_person,
                                                human_data["husband"])
 
