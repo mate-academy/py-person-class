c7e13d30c10a0ede25a3d517e9c636cf0dd2d4a7
Valeriy Semenik <valeriy.semenik@gmail.com>
2024-01-30 14:36:42 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,17 +4,15 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.people[name] = self
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    list_of_person = [Person(person["name"],
-                             person["age"]) for person in people]
-    for i, person in enumerate(people):
-        if person.get("husband"):
-            husband_name = person["husband"]
-            list_of_person[i].husband = Person.people[husband_name]
-        elif person.get("wife"):
-            wife_name = person["wife"]
-            list_of_person[i].wife = Person.people[wife_name]
+    list_of_person = [Person(person["name"], person["age"])
+                      for person in people]
+    for index, person in enumerate(people):
+        if husband_name := person.get("husband"):
+            list_of_person[index].husband = Person.people[husband_name]
+        elif wife_name := person.get("wife"):
+            list_of_person[index].wife = Person.people[wife_name]
     return list_of_person
