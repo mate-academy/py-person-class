28a59ad9b6362bf54836f660b803891324e7c5c6
KovbasinskyV <kovbasinskyvv@gmail.com>
2024-01-31 19:17:31 +0200

    solution3

--- app/main.py
+++ app/main.py
@@ -15,9 +15,11 @@ def create_person_list(people: list) -> list:
 
     for index, partner in enumerate(new_people_list):
         if partner.name == people[index]["name"]:
-            if "wife" in people[index] and people[index]["wife"] is not None:
+            if ("wife" in people[index]
+                    and people[index]["wife"] is not None):
                 partner.wife = Person.people[people[index]["wife"]]
-            if "husband" in people[index] and people[index]["husband"] is not None:
+            if ("husband" in people[index]
+                    and people[index]["husband"] is not None):
                 partner.husband = Person.people[people[index]["husband"]]
 
     return new_people_list
