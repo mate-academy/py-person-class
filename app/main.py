a1d6ac993ad6e1bddbae7994b85c12dd424e42f6
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-02-03 13:40:01 +0200

    Solution, flake8 fixes

--- app/main.py
+++ app/main.py
@@ -15,6 +15,7 @@ def create_person_list(people: list) -> list:
         if "wife" in person and person["wife"] is not None:
             Person.people[person["name"]].wife = Person.people[person["wife"]]
         elif "husband" in person and person["husband"] is not None:
-            Person.people[person["name"]].husband = Person.people[person["husband"]]
+            Person.people[person["name"]].husband\
+                = Person.people[person["husband"]]
 
     return list(Person.people.values())
