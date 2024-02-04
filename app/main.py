ce0ac517f5920d4b3aa0d27a8f3c77ddcde3c6eb
Misha <156539295+mgoryn@users.noreply.github.com>
2024-02-04 15:28:56 +0200

    Update Solution

--- app/main.py
+++ app/main.py
@@ -8,13 +8,7 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-
-    for person_info in people:
-        name = person_info["name"]
-        age = person_info["age"]
-        person = Person(name, age)
-        person_list.append(person)
+    person_list = [Person(person_info["name"], person_info["age"]) for person_info in people]
 
     for person_info in people:
         name = person_info["name"]
