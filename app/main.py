a722e616a2bf910efd34e5839afdaea165895bc4
Andrii Mogush <toltecos@ukr.net>
2024-02-04 16:28:53 +0200

    Fix Index is not good solution

--- app/main.py
+++ app/main.py
@@ -12,9 +12,10 @@ def create_person_list(people: list) -> list[Person]:
         Person(human.get("name"), human.get("age"))
         for human in people
     ]
-    for i, person in enumerate(people):
+    for person in people:
+        person_instances = Person.people.get(person.get("name"))
         if person.get("wife"):
-            people_instances[i].wife = Person.people[person.get("wife")]
+            person_instances.wife = Person.people.get(person.get("wife"))
         if person.get("husband"):
-            people_instances[i].husband = Person.people[person.get("husband")]
+            person_instances.husband = Person.people.get(person.get("husband"))
     return people_instances
