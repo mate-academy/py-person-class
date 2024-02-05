e6dd0f1239a2904a6f4dd42e9d7c39159b2a34b0
Ihor Dmytriv <dmytriwihor@gmail.com>
2024-02-05 22:01:15 +0100

    Refactored with use get()

--- app/main.py
+++ app/main.py
@@ -8,13 +8,16 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    people_list = []
+    people_list = [
+        Person(name=person.get("name"), age=person.get("age"))
+        for person in people
+    ]
     for person in people:
-        people_list.append(Person(name=person["name"], age=person["age"]))
-    for person in people:
-        name = person["name"]
-        if "wife" in person and person["wife"] in Person.people:
-            Person.people[name].wife = Person.people[person["wife"]]
-        elif "husband" in person and person["husband"] in Person.people:
-            Person.people[name].husband = Person.people[person["husband"]]
+        prs_name = person.get("name")
+        prs_wife = person.get("wife")
+        prs_husband = person.get("husband")
+        if prs_wife:
+            Person.people[prs_name].wife = Person.people.get(prs_wife)
+        elif prs_husband:
+            Person.people[prs_name].husband = Person.people.get(prs_husband)
     return people_list
