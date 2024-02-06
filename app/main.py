8d8a0202de76fb5472f3417b20b6d9d097d471c2
Maksym Hekalo <maximgekalo@gmail.com>
2024-02-06 22:11:02 +0200

    implement class and function

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
 class Person:
-    # write your code here
-    pass
+
+    people = dict()
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_instances = []
+    for person in people:
+        person_instance = Person(
+            name=person["name"],
+            age=person["age"]
+        )
+        person_instances.append(person_instance)
+
+    for person in people:
+        if not person.get("wife") is None is person.get("husband"):
+            instance_spouse_link = Person.people[list(person.values())[2]]
+            if list(person.keys())[2] != "wife":
+                Person.people[person["name"]].husband = instance_spouse_link
+            else:
+                Person.people[person["name"]].wife = instance_spouse_link
+    return person_instances
