e51a1fa2c69d1e660aa973e087ea3df493e51880
Serhii Dannyk <serhii.dannyk@gmail.com>
2024-02-07 09:55:34 +0200

    Changed creation of person_list using list comprehension

--- app/main.py
+++ app/main.py
@@ -26,13 +26,11 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
 
-    for person_data in people:
-        name = person_data["name"]
-        age = person_data["age"]
-        person = Person(name, age)
-        person_list.append(person)
+    person_list = [
+        Person(person_data["name"], person_data["age"])
+        for person_data in people
+    ]
 
     for person_data in people:
         person = Person.get_person(person_data["name"])
