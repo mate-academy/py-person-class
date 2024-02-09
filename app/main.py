a113d5636ee233685e7d4264a64bd13be893a685
Eugene Sherepa <evgenijserepa@gmail.com>
2024-02-09 13:47:55 +0200

    remake solution

--- app/main.py
+++ app/main.py
@@ -4,30 +4,21 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.people[self.name] = self
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    people_list = []
-
+    people_list = [
+        Person(
+            person["name"],
+            person["age"]
+        )
+        for person in people
+    ]
     for person in people:
-        name = person["name"]
-        age = person["age"]
-        new_person = Person(name, age)
-        people_list.append(new_person)
-
-        if "wife" in person:
-            wife_name = person["wife"]
-            if wife_name in Person.people:
-                wife_instance = Person.people[wife_name]
-                new_person.wife = wife_instance
-                wife_instance.husband = new_person
-
-        elif "husband" in person:
-            husband_name = person["husband"]
-            if husband_name in Person.people:
-                husband_instance = Person.people[husband_name]
-                new_person.husband = husband_instance
-                husband_instance.wife = new_person
+        if wife_name := person.get("wife"):
+            wife = Person.people[wife_name]
+            wife.husband = Person.people[person["name"]]
+            wife.husband.wife = wife
 
     return people_list
