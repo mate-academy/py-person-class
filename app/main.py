0da7bb296b49e5ed2ef6e5a2aaa21aa91f0d7d68
Hanna Holovach <ann.golovach@gmail.com>
2024-02-10 19:47:44 +0100

    added requested changes

--- app/main.py
+++ app/main.py
@@ -13,15 +13,15 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-    for d_person in people:
-        person_list.append(Person(d_person["name"], d_person["age"]))
+    person_list = [
+        Person(d_person["name"], d_person["age"]) for d_person in people
+    ]
 
     for d_person in people:
         person = Person.people[d_person["name"]]
-        if d_person.get("wife") is not None:
+        if d_person.get("wife"):
             person.wife = Person.people[d_person["wife"]]
-        if d_person.get("husband") is not None:
+        if d_person.get("husband"):
             person.husband = Person.people[d_person["husband"]]
 
     return person_list
