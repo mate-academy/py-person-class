8a854e7dbe6fe059dd145493eea82c2c22cd1c06
Maksym Shramko <shramkomax@gmail.com>
2024-02-13 11:57:36 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -12,20 +12,17 @@ def create_person_list(people: list) -> list:
     person_list = []
 
     for person_info in people:
-        name = person_info["name"]
-        age = person_info["age"]
-        person = Person(name, age)
-        person_list.append(person)
+        new_person = Person(person_info["name"], person_info["age"])
+        person_list.append(new_person)
 
         spouse_name = person_info.get("wife") or person_info.get("husband")
         if spouse_name:
             spouse_instance = Person.people.get(spouse_name)
             if spouse_instance:
                 if "wife" in person_info:
-                    person.wife = spouse_instance
-                    spouse_instance.husband = person
+                    new_person.wife = spouse_instance
+                    spouse_instance.husband = new_person
                 elif "husband" in person_info:
-                    person.husband = spouse_instance
-                    spouse_instance.wife = person
-
+                    new_person.husband = spouse_instance
+                    spouse_instance.wife = new_person
     return person_list
