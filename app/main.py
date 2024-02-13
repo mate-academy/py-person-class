8d53f3ea21a7f6884ab4add1ace3338e1f5256df
Alexander <sasacihun@gmail.com>
2024-02-13 13:24:39 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    res = []
+    for people_data in people:
+        person = Person(people_data["name"], people_data["age"])
+        if people_data.get("wife") in Person.people:
+            person.wife = Person.people[people_data["wife"]]
+            person.wife.husband = person
+        # it"s bad code, I know, wht cond.should I add to get rid of this code
+        # I don"t fully understand it, ChatGPT helped me to refactor my code
+        else:
+            pass
+        if people_data.get("husband") in Person.people:
+            person.husband = Person.people[people_data["husband"]]
+            person.husband.wife = person
+        else:
+            pass
+        res.append(person)
+    return res
