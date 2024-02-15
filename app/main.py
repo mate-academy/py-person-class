4cce1f169e4de55200e900e6b3f314cb3131c95b
donbas777 <renary34@gmail.com>
2024-02-15 21:11:01 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -12,9 +12,9 @@ def create_person_list(people: list) -> list:
                           age=person["age"])
                    for person in people]
     for person in people:
-        name = person["name"]
+        person_instance = person["name"]
         if wife_name := person.get("wife"):
-            Person.people[name].wife = Person.people.get(wife_name)
+            Person.people[person_instance].wife = Person.people.get(wife_name)
         if husband_name := person.get("husband"):
-            Person.people[name].husband = Person.people.get(husband_name)
+            Person.people[person_instance].husband = Person.people.get(husband_name)
     return people_list
