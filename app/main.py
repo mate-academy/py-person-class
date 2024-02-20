543cd70eb5ff031fa99775e05764a93f78e3be57
Maksim <sma4no.7@gmail.com>
2024-02-20 13:47:22 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -4,37 +4,19 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.wife = None
-        self.husband = None
         Person.people[name] = self
-
-    def remove_spouse_attribute(self) -> None:
-        if self.wife is None:
-            del self.wife
-        if self.husband is None:
-            del self.husband
 
 
 def create_person_list(people: list) -> list:
-    created_people = []
-    for person_data in people:
-        name = person_data["name"]
-        age = person_data["age"]
-        person = Person(name, age)
-        created_people.append(person)
-
-        if "wife" in person_data and person_data["wife"] in Person.people:
-            wife_name = person_data["wife"]
-            person.wife = Person.people.get(wife_name)
-            if person.wife:
-                person.wife.husband = person
-        elif ("husband" in person_data
-              and person_data["husband"] in Person.people):
-            husband_name = person_data["husband"]
-            person.husband = Person.people.get(husband_name)
-            if person.husband:
-                person.husband.wife = person
-
-        person.remove_spouse_attribute()
+    created_people = [
+        Person(person_data["name"], person_data["age"])
+        for person_data in people
+    ]
+    for person in people:
+        new_person = Person.people[person["name"]]
+        if person.get("wife"):
+            new_person.wife = Person.people[person["wife"]]
+        if person.get("husband"):
+            new_person.husband = Person.people[person["husband"]]
 
     return created_people
