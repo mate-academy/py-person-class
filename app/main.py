a07e4bd61e093a642f4f9e0a5a0b0151ddb5ff92
Alexander <oleksandr.tsvirko@gmail.com>
2024-02-25 11:38:34 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
 class Person:
-    # write your code here
-    pass
+
+    people: dict = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people.update({self.name: self})
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list:
+    """
+    this function takes list people and return
+    list with Person instances instead of dicts.
+    """
+
+    people_result = [
+        Person(name=person_dict["name"], age=person_dict["age"])
+        for person_dict in people
+    ]
+
+    for person_dict in people:
+        person_name = person_dict["name"]
+        if "wife" in person_dict and person_dict["wife"] is not None:
+            wife_name = person_dict["wife"]
+            Person.people[person_name].wife = Person.people[wife_name]
+            Person.people[wife_name].husband = Person.people[person_name]
+    return people_result
