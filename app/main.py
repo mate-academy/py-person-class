1dd84d665599b1e66afae74dacf63e4ca664a1f0
antonver <anton.verbovyi@gmail.com>
2024-02-25 23:16:52 +0100

    Class Person implemented

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(humans: list) -> list:
+    persons_list = [Person(man["name"], man["age"]) for man in humans]
+    for num in range(len(humans)):
+        if humans[num].get("husband"):
+            for man in persons_list:
+                if man.name == humans[num].get("husband"):
+                    persons_list[num].husband = man
+        elif humans[num].get("wife"):
+            for woman in persons_list:
+                if woman.name == humans[num].get("wife"):
+                    persons_list[num].wife = woman
+    return persons_list
