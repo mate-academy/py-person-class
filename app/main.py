95bc711c1618f8bf9157390e45f48884b2df0a7d
antonver <anton.verbovyi@gmail.com>
2024-02-26 17:10:04 +0100

    Class Person changed as required

--- app/main.py
+++ app/main.py
@@ -4,18 +4,16 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.people[name] = self
+        Person.people[name] = self
 
 
-def create_person_list(humans: list) -> list:
-    persons_list = [Person(man["name"], man["age"]) for man in humans]
-    for num in range(len(humans)):
-        if humans[num].get("husband"):
-            for man in persons_list:
-                if man.name == humans[num].get("husband"):
-                    persons_list[num].husband = man
-        elif humans[num].get("wife"):
-            for woman in persons_list:
-                if woman.name == humans[num].get("wife"):
-                    persons_list[num].wife = woman
+def create_person_list(people: list) -> list:
+    persons_list = [Person(person["name"], person["age"]) for person in people]
+    for number in range(len(people)):
+        if people[number].get("husband"):
+            persons_list[number].husband \
+                = persons_list[number].people[people[number]["husband"]]
+        elif people[number].get("wife"):
+            persons_list[number].wife \
+                = persons_list[number].people[people[number]["wife"]]
     return persons_list
