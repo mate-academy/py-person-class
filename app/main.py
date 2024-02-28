ae8e34f8d9ac5045eb043a3f86edcbd31b16b233
Kyrylo <kbuchinskiy1@gmail.com>
2024-02-28 15:59:20 +0200

    Extra fix

--- app/main.py
+++ app/main.py
@@ -12,16 +12,13 @@ class Person:
 
 
 def create_person_list(people: list[dict]) -> list[Person]:
-    Person.people = {person_info["name"]: Person(person_info["name"],
-                                                 person_info["age"])
-                     for person_info in people}
+    [Person(person_info["name"], person_info["age"])
+     for person_info in people]
     for person_info in people:
         person_instance = Person.people[person_info["name"]]
-        if "wife" in person_info and \
-                (wife_name := person_info["wife"]):
+        if wife_name := person_info.get("wife"):
             person_instance.wife = Person.people[wife_name]
-        if "husband" in person_info and \
-                (husband_name := person_info["husband"]):
+        if husband_name := person_info.get("husband"):
             person_instance.husband = Person.people[husband_name]
 
     return list(Person.people.values())
