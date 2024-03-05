184b79c60415f309f77aa3e2d6c4df13b1f7b3ad
Sribniak Taras <sribniak.terry@protonmail.com>
2024-03-05 16:37:03 +0200

    Initialization of Person class

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
+        self.wife = None
+        self.husband = None
+
+    def __repr__(self):
+        return f"{self.name}"
 
 
 def create_person_list(people: list) -> list:
