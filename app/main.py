5b9d80b4dc5ef0435ed230ee5d32ce6c7f61c867
Sribniak Taras <sribniak.terry@protonmail.com>
2024-03-05 22:35:51 +0200

    added create_person_list

--- app/main.py
+++ app/main.py
@@ -8,10 +8,22 @@ class Person:
         self.wife = None
         self.husband = None
 
-    def __repr__(self):
-        return f"{self.name}"
-
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+
+    for person_data in people:
+        person = Person(person_data["name"], person_data["age"])
+        person_list.append(person)
+
+    for ind, person_data in enumerate(people):
+        if "wife" in person_data and person_data["wife"] is not None:
+            person_list[ind].wife = [person for person in person_list
+                                     if person.name == person_data["wife"]][0]
+        elif "husband" in person_data and person_data["husband"] is not None:
+            person_list[ind].husband = [person for person
+                                        in person_list
+                                        if person.name
+                                        == person_data["husband"]][0]
+
+    return person_list
