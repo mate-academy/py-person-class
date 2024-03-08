b8646764d1dea9c35b6110a838ecc5e0a0e4075d
antonver <anton.verbovyi@gmail.com>
2024-03-08 21:39:07 +0100

    Class Person changed as required

--- app/main.py
+++ app/main.py
@@ -11,9 +11,12 @@ def create_person_list(people: list) -> list:
     persons_list = [Person(person["name"], person["age"]) for person in people]
     for number in range(len(people)):
         if people[number].get("husband"):
-            persons_list[number].husband \
-                = persons_list[number].people[people[number]["husband"]]
+            persons_list[number].husband = (
+                persons_list[number].people[people[number]["husband"]]
+            )
         elif people[number].get("wife"):
-            persons_list[number].wife \
-                = persons_list[number].people[people[number]["wife"]]
+            persons_list[number].wife = (
+                persons_list[number].people[people[number]["wife"]]
+            )
+
     return persons_list
