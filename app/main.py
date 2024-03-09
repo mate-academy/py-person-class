04732d79173198a44729e4d1a58be380d6da5e36
Ihor <159614451+Bujhm99@users.noreply.github.com>
2024-03-09 13:09:49 +0200

    Update main.py
    No comments

--- app/main.py
+++ app/main.py
@@ -26,25 +26,3 @@ def create_person_list(people: list) -> list:
                 setattr(first_hum, partner_type, human[partner_type])
     return [val for val in Person.people.values()]
 
-# people = [
-#     {"name": "Ross", "age": 30, "wife": "Rachel"},
-#     {"name": "Joey", "age": 29, "wife": None},
-#     {"name": "Rachel", "age": 28, "husband": "Ross"}
-# ]
-#
-# person_list = create_person_list(people)
-# isinstance(person_list[0], Person) # True
-# person_list[0].name == "Ross"
-# person_list[0].wife is person_list[2] # True
-# person_list[0].wife.name == "Rachel"
-#
-# person_list[1].name == "Joey"
-# # person_list[1].wife
-# # # AttributeError
-#
-# isinstance(person_list[2], Person) # True
-# person_list[2].name == "Rachel"
-# person_list[2].husband is person_list[0] # True
-# # The same as person_list[0]
-# person_list[2].husband.name == "Ross"
-# person_list[2].husband.wife is person_list[2]  # True
