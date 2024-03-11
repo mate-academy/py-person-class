5dfa96fa3276e63f3af52be3602efffabd47f066
Sribniak Taras <sribniak.terry@protonmail.com>
2024-03-11 13:03:20 +0200

    Optimization

--- app/main.py
+++ app/main.py
@@ -5,25 +5,15 @@ class Person:
         self.name = name
         self.age = age
         Person.people[name] = self
-        self.wife = None
-        self.husband = None
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-
-    for person_data in people:
-        person = Person(person_data["name"], person_data["age"])
-        person_list.append(person)
+    person_list = [Person(person_data["name"], person_data["age"]) for person_data in people]
 
     for ind, person_data in enumerate(people):
         if "wife" in person_data and person_data["wife"] is not None:
-            person_list[ind].wife = [person for person in person_list
-                                     if person.name == person_data["wife"]][0]
+            person_list[ind].wife = Person.people[people[ind]["wife"]]
         elif "husband" in person_data and person_data["husband"] is not None:
-            person_list[ind].husband = [person for person
-                                        in person_list
-                                        if person.name
-                                        == person_data["husband"]][0]
+            person_list[ind].husband = Person.people[people[ind]["husband"]]
 
     return person_list
