3d20b021ab99bbfc10d4919fa6470aa41ee63c4e
Bludger None <bludger404@gmail.com>
2024-03-11 13:18:42 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.__class__.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = [Person(person["name"], person["age"]) for person in people]
+
+    for person_data, person_instance in zip(people, person_list):
+        if "wife" in person_data:
+            if person_data["wife"] in Person.people:
+                person_instance.wife = Person.people[person_data["wife"]]
+        elif "husband" in person_data:
+            if person_data["husband"] in Person.people:
+                person_instance.husband = Person.people[person_data["husband"]]
+
+    return person_list
