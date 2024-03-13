88ec6fca7bb498f909105bc56f04423917edfe50
pochtalon <pochtalon1010@gmail.com>
2024-03-13 10:45:14 +0200

    Change whole logic

--- app/main.py
+++ app/main.py
@@ -1,39 +1,25 @@
 class Person:
     people = {}
 
-    def __init__(self, name: str, age: int, **kwargs):
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        if "wife" in kwargs:
-            self.add_satellite("wife", kwargs["wife"])
-        if "husband" in kwargs:
-            self.add_satellite("husband", kwargs["husband"])
         Person.people[name] = self
-
-    def add_satellite(self, satellite: str, satellite_name: str) -> None:
-        person = Person.people[satellite_name] \
-            if satellite_name in Person.people else None
-        setattr(self, satellite, person)
-        opposite = "husband" if satellite == "wife" else "wife"
-        if satellite_name in Person.people:
-            setattr(Person.people[satellite_name], opposite, self)
 
 
 def create_person_list(people: list) -> list:
     persons = []
     for human in people:
-        if "wife" in human:
-            if human["wife"] is None:
-                persons.append(Person(human["name"], human["age"]))
-            else:
-                persons.append(Person(human["name"],
-                                      human["age"],
-                                      wife=human["wife"]))
-        if "husband" in human:
-            if human["husband"] is None:
-                persons.append(Person(human["name"], human["age"]))
-            else:
-                persons.append(Person(human["name"],
-                                      human["age"],
-                                      husband=human["husband"]))
+        person = (Person(human["name"], human["age"]))
+        satellite = "wife" if "wife" in human else "husband"
+        if human[satellite] is not None:
+            add_satellites(person, satellite, human[satellite])
+        persons.append(person)
     return persons
+
+
+def add_satellites(person: "Person", satellite: str, satellite_name: str) -> None:
+    opposite = "husband" if satellite == "wife" else "wife"
+    if satellite_name in Person.people:
+        setattr(person, satellite, Person.people[satellite_name])
+        setattr(Person.people[satellite_name], opposite, person)
