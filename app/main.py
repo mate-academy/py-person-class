0c657e05ed781893ddea50f3c4fcca7a68895604
MRGL <Sonemongoo@gmail.com>
2024-03-15 01:14:25 +0200

    'Fix_Solution'

--- app/main.py
+++ app/main.py
@@ -8,22 +8,14 @@ class Person:
 
 
 def create_person_list(people: list) -> list[Person]:
-    p_list = []
-
-    for person_data in people:
-        person = Person(person_data["name"], person_data["age"])
-        p_list.append(person)
-
+    p_list = [Person(person["name"], person["age"]) for person in people]
     for person_data, person in zip(people, p_list):
-        if "wife" in person_data and person_data["wife"] is not None:
-            wife_name = person_data["wife"]
-            wife = next((p for p in p_list if p.name == wife_name), None)
+        if person_data.get("wife"):
+            wife = Person.people.get(person_data["wife"])
             if wife:
                 person.wife = wife
-        elif "husband" in person_data and person_data["husband"] is not None:
-            husband_name = person_data["husband"]
-            husband = next((p for p in p_list if p.name == husband_name), None)
+        elif person_data.get("husband"):
+            husband = Person.people.get(person_data["husband"])
             if husband:
                 person.husband = husband
-
     return p_list
