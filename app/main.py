e3900e7e1c67d02ceba15a8371d86048e2cd3051
Dmytro <labtech.kh@gmail.com>
2024-03-14 07:00:01 +0200

    Solution Person Class at March 14

--- app/main.py
+++ app/main.py
@@ -1,24 +1,30 @@
 class Person:
-
     people = {}
 
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        __class__.people[self.name] = self
+        Person.people[self.name] = self
 
 
 def create_person_list(peoples: list) -> list:
-    list_result = [Person(member["name"], member["age"]) for member in peoples]
+    list_result = []
+    new_person = None
+    [Person(member["name"], member["age"]) for member in peoples]
 
-    for index, member in enumerate(peoples):
-        if "wife" in member:
-            for pos in range(len(peoples)):
-                if list_result[pos].name == member["wife"]:
-                    list_result[index].wife = list_result[pos]
-        if "husband" in member:
-            for pos in range(len(peoples)):
-                if list_result[pos].name == member["husband"]:
-                    list_result[index].husband = list_result[pos]
+    for member in peoples:
+        if "wife" in member and member["wife"]:
+            new_person = Person.people.get(member["name"])
+            new_person.wife = Person.people.get(member["wife"])
+            list_result.append(new_person)
+
+        elif "husband" in member and member["husband"]:
+            new_person = Person.people.get(member["name"])
+            new_person.husband = Person.people.get(member["husband"])
+            list_result.append(new_person)
+
+        else:
+            new_person = Person.people.get(member["name"])
+            list_result.append(new_person)
 
     return list_result
