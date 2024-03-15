5c654c504d24102eff128851b3fa83bfd88b818c
Anton Kudriavtsev <bryanfinchnzt@gmail.com>
2024-03-15 04:51:11 -0700

    Simplified create_person_list

--- app/main.py
+++ app/main.py
@@ -9,28 +9,16 @@ class Person:
 
 def create_person_list(people: list) -> list:
     list_of_created_people = []
-    has_wife = []
-    has_husband = []
-    counter_for_pairs = 0
-    print(people)
     for person in people:
         inst_person = Person(person["name"], person["age"])
+        key = list({"wife", "husband"}.intersection(set(person.keys())))[0]
         list_of_created_people.append(inst_person)
-        if ("wife" in person.keys()):
-            if person["wife"]:
-                has_wife.append([inst_person, person["wife"]])
-                counter_for_pairs += 1
-        if ("husband" in person.keys()):
-            if person["husband"]:
-                has_husband.append([inst_person, person["husband"]])
-    while counter_for_pairs > 0:
-        print(has_wife)
-        print(has_husband)
-        husband = has_wife.pop(0)
-        for i in range(counter_for_pairs):
-            if husband[1] == has_husband[i][0].name:
-                wife = has_husband.pop(i)
-                husband[0].wife, wife[0].husband = wife[0], husband[0]
-                counter_for_pairs -= 1
-                break
+        if person[key] in Person.people.keys():
+            name = person[key]
+            if key == "wife":
+                inst_person.wife = Person.people[name]
+                Person.people[name].husband = inst_person
+            else:
+                inst_person.husband = Person.people[name]
+                Person.people[name].wife = inst_person
     return list_of_created_people
