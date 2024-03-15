e509367b178f0771bc467db807db26098bd777e8
tiway <stilon670@gmail.com>
2024-03-15 12:02:36 +0200

    with one error in the test

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    for person in people:
+        Person(person["name"], person["age"])
+    for j in people:
+        if "wife" in j:
+            if not j["wife"] is None:
+                Person.people[j["name"]].wife = Person.people[j["wife"]]
+            else:
+                Person.people[j["name"]].wife = None
+        if "husband" in j:
+            if not j["husband"] is None:
+                Person.people[j["name"]].husband = Person.people[j["husband"]]
+            else:
+                Person.people[j["name"]].husband = None
+    return list(Person.people.values())
+
+
