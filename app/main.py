4ea160018ec50ed307a69a6efd69fa0b11e1b0a0
NioviIki <charmniovi@tutanota.com>
2024-03-17 20:12:24 +0200

    added type annotation for Person.__init__

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 class Person:
     people = {}
 
-    def __init__(self, name, age, *args, **kwargs):
+    def __init__(self, name: str, age: str, *args, **kwargs) -> None:
         self.name = name
         self.age = age
         Person.people.update({self.name: self})
@@ -11,11 +11,7 @@ def create_person_list(people: list) -> list:
     person_instances = [Person(**person) for person in people]
     for person_data, person_instance in zip(people, person_instances):
         if person_data.get("wife"):
-            wife = Person.people.get(person_data["wife"])
-            if wife:
-                person_instance.wife = wife
+            person_instance.wife = Person.people.get(person_data["wife"])
         elif person_data.get("husband"):
-            husband = Person.people.get(person_data["husband"])
-            if husband:
-                person_instance.husband = husband
+            person_instance.husband = Person.people.get(person_data["husband"])
     return person_instances
