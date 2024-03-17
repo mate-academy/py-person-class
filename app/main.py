549c9ad8640839d183725673696f2bb8995f5d74
NioviIki <charmniovi@tutanota.com>
2024-03-17 19:51:36 +0200

    added create_person_list

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name, age, *args, **kwargs):
+        self.name = name
+        self.age = age
+        Person.people.update({self.name: self})
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_instances = [Person(**person) for person in people]
+    for person_data, person_instance in zip(people, person_instances):
+        if person_data.get("wife"):
+            wife = Person.people.get(person_data["wife"])
+            if wife:
+                person_instance.wife = wife
+        elif person_data.get("husband"):
+            husband = Person.people.get(person_data["husband"])
+            if husband:
+                person_instance.husband = husband
+    return person_instances
