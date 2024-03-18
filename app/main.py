addcb80429015a695a710dba7f4b03f3e60866e5
serhii-chaban <boetz1995@gmail.com>
2024-03-18 19:19:26 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
-from typing import List, Dict
-
-
 class Person:
     people = {}
 
@@ -10,11 +7,13 @@ class Person:
         self.people[self.name] = self
 
 
-def create_person_list(people: List[Dict]) -> List[Person]:
-    person_list = [Person(person["name"], person["age"]) for person in people]
+def create_person_list(people: list[dict]) -> list[Person]:
+    person_inst = [Person(person["name"], person["age"]) for person in people]
     for person in people:
-        if "wife" in person and person["wife"] is not None:
-            person.wife = Person.people[person["wife"]]
-        if "husband" in person and person["husband"] is not None:
-            person.husband = Person.people[person["husband"]]
-    return person_list
+        if person.get("wife") in Person.people:
+            wife_inst = Person.people[person["wife"]]
+            Person.people[person["name"]].wife = wife_inst
+        if person.get("husband") in Person.people:
+            husband_inst = Person.people[person["husband"]]
+            Person.people[person["name"]].husband = husband_inst
+    return person_inst
