f4b66fe527fd96e5c188dc0c86cceb71a675b5ba
serhii-chaban <boetz1995@gmail.com>
2024-03-18 19:17:17 +0200

    Solved

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
+from typing import List, Dict
+
+
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.people[self.name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: List[Dict]) -> List[Person]:
+    person_list = [Person(person["name"], person["age"]) for person in people]
+    for person in people:
+        if "wife" in person and person["wife"] is not None:
+            person.wife = Person.people[person["wife"]]
+        if "husband" in person and person["husband"] is not None:
+            person.husband = Person.people[person["husband"]]
+    return person_list
