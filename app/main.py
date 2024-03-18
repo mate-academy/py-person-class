699ffe0412d45b6434ed7a758afb7390bbb851b7
Oksana Dudok <oksanaklw@gmail.com>
2024-03-19 00:08:54 +0200

    after chenged

--- app/main.py
+++ app/main.py
@@ -12,9 +12,9 @@ def create_person_list(people: list) -> list:
     for person in people:
         person_list.append(Person(person["name"], person["age"]))
     for index, person in enumerate(people):
-        if person.get("wife") and person["wife"]:
+        if person.get("wife"):
             person_list[index].wife = Person.people[person["wife"]]
-        elif person.get("husband") and person["husband"]:
+        elif person.get("husband"):
             person_list[index].husband = Person.people[person["husband"]]
 
     return person_list
