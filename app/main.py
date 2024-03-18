66170d7d621b02e6ffb55aaf15b0f5543b9ac146
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-03-18 14:01:24 +0100

    Removed unused lines and changed (if)

--- app/main.py
+++ app/main.py
@@ -11,12 +11,10 @@ def create_person_list(people: list) -> list:
     for person in people:
         Person(person["name"], person["age"])
     for person in people:
-        if "wife" in person and person["wife"] is not None:
-            if person["wife"] in Person.people:
-                Person.people[person["name"]].wife \
-                    = Person.people[person["wife"]]
-        if "husband" in person and person["husband"] is not None:
-            if person["husband"] in Person.people:
-                Person.people[person["name"]].husband \
-                    = Person.people[person["husband"]]
+        if person.get("wife"):
+            Person.people[person["name"]].wife \
+                = Person.people[person["wife"]]
+        if person.get("husband"):
+            Person.people[person["name"]].husband \
+                = Person.people[person["husband"]]
     return list(Person.people.values())
