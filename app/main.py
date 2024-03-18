c606ea7a11ed69805593bfdafeb7384851b33d93
Serhii Marynin <serhii.marynin@gmail.com>
2024-03-18 18:24:27 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
 class Person:
-    # write your code here
-    pass
+    people = dict()
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    def get_instance(name: str) -> Person:
+        for person in person_instances:
+            if person.name == name:
+                return person
+
+    person_instances = [
+        Person(person["name"], person["age"])
+        for person in people
+    ]
+
+    for index, person in enumerate(people):
+        if person.get("wife", None) is not None:
+            person_instances[index].wife = get_instance(person["wife"])
+        if person.get("husband", None) is not None:
+            person_instances[index].husband = get_instance(person["husband"])
+
+    return person_instances
