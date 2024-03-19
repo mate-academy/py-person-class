3836af4b390fd322208bbdd358c661f6d7916dfd
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-03-19 08:37:43 +0100

    Returned to the previous version and added list comprehension

--- app/main.py
+++ app/main.py
@@ -8,12 +8,10 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    for person in people:
-        Person(person["name"], person["age"])
+    [Person(person["name"], person["age"]) for person in people]
     for person in people:
         if person.get("wife"):
-            Person.people[person["name"]].wife \
-                = Person.people[person["wife"]]
+            Person.people[person["name"]].wife = Person.people[person["wife"]]
         if person.get("husband"):
             Person.people[person["name"]].husband \
                 = Person.people[person["husband"]]
