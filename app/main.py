dab0aaa539e9932f6072c2a0098c29ce78a23fab
semermaks <semermaks@mail.com>
2024-03-20 10:15:38 +0200

    Remove unnecessary variables

--- app/main.py
+++ app/main.py
@@ -10,9 +10,7 @@ class Person:
 def create_person_list(people: list) -> list:
     person_list = []
     for person_info in people:
-        name = person_info["name"]
-        age = person_info["age"]
-        person = Person(name, age)
+        person = Person(person_info["name"], person_info["age"])
         if "wife" in person_info and person_info["wife"] is not None:
             wife_person = Person.people.get(person_info["wife"])
             if wife_person:
@@ -20,8 +18,7 @@ def create_person_list(people: list) -> list:
                 wife_person.husband = person
 
         elif "husband" in person_info and person_info["husband"] is not None:
-            husband_name = person_info["husband"]
-            husband_person = Person.people.get(husband_name)
+            husband_person = Person.people.get(person_info["husband"])
             if husband_person:
                 person.husband = husband_person
                 husband_person.wife = person
