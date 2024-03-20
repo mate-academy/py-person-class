85f75dd56cc2f84ab0d442ad19a63237720acbec
Oleksandr Moroz <molecxander@gmail.com>
2024-03-20 09:52:46 +0200

    Code optimized

--- app/main.py
+++ app/main.py
@@ -12,8 +12,7 @@ def create_person_list(people: list) -> list:
     for person_data in people:
         person = Person(person_data["name"], person_data["age"])
         if "wife" in person_data and person_data["wife"]:
-            wife_name = person_data["wife"]
-            person.wife = Person.people.get(wife_name)
+            person.wife = Person.people.get(person_data["wife"])
         elif "husband" in person_data and person_data["husband"]:
             person.husband = Person.people.get(person_data["husband"])
             if person.husband:
