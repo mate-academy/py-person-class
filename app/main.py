e5a29d1a984817928ababfbe6d3824345d6a8e68
Oleksandr Moroz <molecxander@gmail.com>
2024-03-20 09:49:34 +0200

    Code optimized

--- app/main.py
+++ app/main.py
@@ -10,20 +10,13 @@ class Person:
 def create_person_list(people: list) -> list:
     person_list = []
     for person_data in people:
-        name = person_data["name"]
-        age = person_data["age"]
-        person = Person(name, age)
-        if "wife" in person_data and person_data["wife"] is not None:
+        person = Person(person_data["name"], person_data["age"])
+        if "wife" in person_data and person_data["wife"]:
             wife_name = person_data["wife"]
-            wife = Person.people.get(wife_name)
-            person.wife = wife
-            if wife is not None:
-                wife.husband = person
-        elif "husband" in person_data and person_data["husband"] is not None:
-            husband_name = person_data["husband"]
-            husband = Person.people.get(husband_name)
-            person.husband = husband
-            if husband is not None:
-                husband.wife = person
+            person.wife = Person.people.get(wife_name)
+        elif "husband" in person_data and person_data["husband"]:
+            person.husband = Person.people.get(person_data["husband"])
+            if person.husband:
+                person.husband.wife = person
         person_list.append(person)
     return person_list
