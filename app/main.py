29724725b6b9768c24759517a4d3e7f7afb214dc
bwaah1 <chykivan@gmail.com>
2024-03-20 14:55:37 +0200

    Fix error in __init__ constructor

--- app/main.py
+++ app/main.py
@@ -4,30 +4,25 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.wife = None
-        self.husband = None
         Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    result = []
+    people_names = [
+        Person(person["name"], person["age"])
+        for person in people
+    ]
 
-    for person_data in people:
-        person_name = person_data["name"]
-        new_person = Person(name=person_name, age=person_data["age"])
-        result.append(new_person)
+    for person in people:
+        if person.get("wife"):
+            Person.people[person["name"]].wife = Person.people[
+                person["wife"]
+            ]
+            Person.people[person["wife"]].husband = Person.people[person["name"]]
+        if person.get("husband"):
+            Person.people[person["name"]].wife = Person.people[
+                person["husband"]
+            ]
+            Person.people[person["husband"]].husband = Person.people[person["name"]]
 
-    for person_data in people:
-        person_name = person_data["name"]
-        person_instance = Person.people[person_name]
-        if person_data.get("wife") is not None:
-            person_instance.wife = Person.people[person_data["wife"]]
-            person_instance.wife.husband = person_instance
-        elif person_data.get("husband") is not None:
-            person_instance.husband = Person.people[person_data["husband"]]
-            person_instance.husband.wife = person_instance
-        else:
-            person_instance.wife = None
-            person_instance.husband = None
-
-    return result
+    return people_names
