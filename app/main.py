9c7a0942068705daea01c15f017f27739197cf89
Serhii Marynin <serhii.marynin@gmail.com>
2024-03-20 17:35:30 +0200

    Update logics

--- app/main.py
+++ app/main.py
@@ -8,20 +8,14 @@ class Person:
 
 
 def create_person_list(people: list) -> list[Person]:
-    def get_instance(name: str) -> Person:
-        for person in person_instances:
-            if person.name == name:
-                return person
+    person_instances = [Person(person["name"], person["age"])
+                        for person in people]
 
-    person_instances = [
-        Person(person["name"], person["age"])
-        for person in people
-    ]
-
-    for index, person in enumerate(people):
-        if person.get("wife", None) is not None:
-            person_instances[index].wife = get_instance(person["wife"])
-        if person.get("husband", None) is not None:
-            person_instances[index].husband = get_instance(person["husband"])
+    for person in people:
+        if person.get("wife"):
+            Person.people[person["name"]].wife = Person.people[person["wife"]]
+        if person.get("husband"):
+            Person.people[person["name"]
+                          ].husband = Person.people[person["husband"]]
 
     return person_instances
