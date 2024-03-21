16d7855f968fd4821b96723c4b6f11e20bfc4ee2
Vladyslav Prymas <prymas.vladyslav@gmail.com>
2024-03-21 17:36:06 +0200

    Made changes

--- app/main.py
+++ app/main.py
@@ -4,20 +4,21 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.__class__.people[self.name] = self
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
     person_list = []
+    lover_list = ["husband", "wife"]
     for data in people:
         person = Person(data["name"], data["age"])
-        if "wife" in data and data["wife"] is not None:
-            if data["wife"] in person.__class__.people:
-                person.wife = person.__class__.people[data["wife"]]
-                person.wife.husband = person
-        elif "husband" in data and data["husband"] is not None:
-            if data["husband"] in person.__class__.people:
-                person.husband = person.__class__.people[data["husband"]]
-                person.husband.wife = person
+        for elem in lover_list:
+            if elem in data and data[elem] is not None:
+                if elem == "wife" and data[elem] in Person.people:
+                    person.wife = Person.people[data[elem]]
+                    Person.people[data[elem]].husband = person
+                elif elem == "husband" and data[elem] in Person.people:
+                    person.husband = Person.people[data[elem]]
+                    Person.people[data[elem]].wife = person
         person_list.append(person)
     return person_list
