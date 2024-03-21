31fa75ef6f298f04ee550ba1ed486dcd09afeae5
Sofiia <sonagm9@gmail.com>
2024-03-21 22:34:22 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -6,27 +6,25 @@ class Person:
         self.age = age
         self.__class__.people[name] = self
 
-    @classmethod
-    def add_spouse(cls, person: dict, spouse_name: str) -> None:
-        spouse = cls.people.get(spouse_name)
+    def add_spouse(self, spouse_name: str) -> None:
+        spouse = self.__class__.people.get(spouse_name)
         if spouse:
-            if hasattr(person, "wife"):
-                person.wife = spouse
-                spouse.husband = person
+            if hasattr(self, "wife"):
+                self.wife = spouse
+                spouse.husband = self
             else:
-                person.husband = spouse
-                spouse.wife = person
+                self.husband = spouse
+                spouse.wife = self
 
 
 def create_person_list(people: list) -> list:
     person_list = []
     for person_data in people:
-        name = person_data["name"]
-        age = person_data["age"]
-        person = Person(name, age)
+        person = Person(person_data["name"], person_data["age"])
         person_list.append(person)
+
         spouse_key = "wife" if "wife" in person_data else "husband"
-        spouse_name = person_data[spouse_key]
-        if spouse_name:
-            Person.add_spouse(person, spouse_name)
+        if spouse_key in person_data:
+            person.add_spouse(person_data[spouse_key])
+
     return person_list
