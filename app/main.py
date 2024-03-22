aa5996cb4c978cab5aec48d1b3bc6596eb47f310
Olena Poplias <poplyasitasony@gmail.com>
2024-03-21 21:05:22 -0400

    "i to index, list comprehension instead of loop"

--- app/main.py
+++ app/main.py
@@ -8,12 +8,10 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-    for person in people:
-        person_list.append(Person(person["name"], person["age"]))
-    for i, person in enumerate(people):
+    person_list = [Person(person["name"], person["age"]) for person in people]
+    for index, person in enumerate(people):
         if person.get("wife"):
-            person_list[i].wife = Person.people[person["wife"]]
+            person_list[index].wife = Person.people[person["wife"]]
         elif person.get("husband"):
-            person_list[i].husband = Person.people[person["husband"]]
+            person_list[index].husband = Person.people[person["husband"]]
     return person_list
