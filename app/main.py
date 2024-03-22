37345c2fd481a2057c67ef529448699913929bf5
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-03-22 21:27:54 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -4,14 +4,16 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.people[self.name] = self
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list[dict]) -> list:
     persons = [Person(person["name"], person["age"]) for person in people]
     for person in people:
         if "wife" in person and person["wife"]:
-            Person.people[person["name"]].wife = Person.people[person["wife"]]
-        elif "husband" in person and person["husband"]:
-            Person.people[person["name"]].husband = Person.people[person["husband"]]
+            wife = Person.people[person["wife"]]
+            Person.people[person["name"]].wife = wife
+        if "husband" in person and person["husband"]:
+            husband = Person.people[person["husband"]]
+            Person.people[person["name"]].husband = husband
     return persons
