f2575d47e10cb2f526fdae68fa2163b7c40aea9d
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-03-22 13:56:08 +0200

    Changes have been made

--- app/main.py
+++ app/main.py
@@ -8,15 +8,14 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    result_list = []
-    for person_data in people:
-        Person.people[person_data["name"]]\
-            = Person(person_data["name"], person_data["age"])
+    result_list = [
+        Person(person_data["name"], person_data["age"])
+        for person_data in people
+    ]
     for person_data in people:
         person = Person.people[person_data["name"]]
-        if "wife" in person_data and person_data["wife"]:
-            person.wife = Person.people.get(person_data["wife"])
-        if "husband" in person_data and person_data["husband"]:
-            person.husband = Person.people.get(person_data["husband"])
-        result_list.append(person)
+        if wife_name := person_data.get("wife"):
+            person.wife = Person.people.get(wife_name)
+        if husband_name := person_data.get("husband"):
+            person.husband = Person.people.get(husband_name)
     return result_list
