3541a6e53375f4c9c11e6ffbe2594173d731de8f
Viktor Zhdanovych <cryingtm27@gmail.com>
2024-03-24 16:32:27 +0200

    fix codestile

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class Person:
-    people = {}
+    people = dict()
 
     def __init__(self, name: str, age: int) -> None:
         self.name = name
@@ -11,10 +11,10 @@ def create_person_list(people: list) -> list:
     people_list = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
-        if person.get("wife"):
-            Person.people[person["name"]].wife = Person.people[person["wife"]]
-        if person.get("husband"):
-            Person.people[person["name"]].husband = Person.people[
-                person["husband"]]
+        person_instance = Person.people[person["name"]]
+        if wife_name := person.get("wife"):
+            person_instance.wife = Person.people[wife_name]
+        if husband_name := person.get("husband"):
+            person_instance.husband = Person.people[husband_name]
 
     return people_list
