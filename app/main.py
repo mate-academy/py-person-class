5d82a508e2e9f1f69cdad6565d93bfbb47ace8ca
haldaniko <haldaniko@gmail.com>
2024-03-27 14:43:46 +0100

    Solution 2

--- app/main.py
+++ app/main.py
@@ -8,12 +8,12 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    my_list = []
+    persons = []
     for man in people:
-        my_list.append(Person(man["name"], man["age"]))
+        persons.append(Person(man["name"], man["age"]))
     for man in people:
         if man.get("wife"):
-            my_list[people.index(man)].wife = Person.people[man["wife"]]
+            persons[people.index(man)].wife = Person.people[man["wife"]]
         elif man.get("husband"):
-            my_list[people.index(man)].husband = Person.people[man["husband"]]
-    return my_list
+            persons[people.index(man)].husband = Person.people[man["husband"]]
+    return persons
