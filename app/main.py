94230880a0a15d36887713a65ea377db21177adf
Liliia <furman06228@gmail.com>
2024-04-01 01:30:22 +0200

    Fixed

--- app/main.py
+++ app/main.py
@@ -4,30 +4,27 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.wife = None
-        self.husband = None
-        self.__class__.people[name] = self
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
     person_list = []
 
     for person_data in people:
-        person = Person(person_data["name"], person_data["age"])
-        person_list.append(person)
+        person_list = [Person(person_data["name"], person_data["age"]) for person_data in people]
 
     for person_data in people:
         person = Person.people[person_data["name"]]
         if "wife" in person_data and person_data["wife"] is not None:
             person.wife = Person.people[person_data["wife"]]
             person.wife.husband = person
+        elif "wife" in person_data is None:
+            person_data["wife"] = None
 
         elif "husband" in person_data and person_data["husband"] is not None:
             person.husband = Person.people[person_data["husband"]]
             person.husband.wife = person
-
         else:
-            person.wife = None
-            person.husband = None
+            person_data["husband"] = None
 
     return person_list
