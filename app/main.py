7f6283a800f1ed095c238a075b2437ac9d7d0e33
Liliia <furman06228@gmail.com>
2024-04-06 14:00:41 +0200

    Fixed 4.0.

--- app/main.py
+++ app/main.py
@@ -19,14 +19,12 @@ def create_person_list(people: list) -> list:
             if wife_name in Person.people:
                 person.wife = Person.people[wife_name]
                 person.wife.husband = person
-            person_data["wife"] = None
 
         if "husband" in person_data:
             husband_name = person_data["husband"]
             if husband_name in Person.people:
                 person.husband = Person.people[husband_name]
                 person.husband.wife = person
-            person_data["husband"] = None
 
         person_list.append(person)
 
