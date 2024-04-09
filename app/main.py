41edba33edd576c6ccb355a4880428101aa67178
hhorhe <horhehorda@gmail.com>
2024-04-09 22:14:52 +0200

    'Solution_2'

--- app/main.py
+++ app/main.py
@@ -12,30 +12,13 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-    for person_data in people:
-        person_list.append(Person(person_data["name"], person_data["age"]))
-    for person in people:
-        person_instance = Person.people.get(person["name"])
-        if person.get("wife"):
-            person_instance.wife = Person.people[person["wife"]]
-        if person.get("husband"):
-            person_instance.husband = Person.people[person["husband"]]
-    return person_list
-
-
-if __name__ == "__main__":
-    people = [
-        {"name": "Ross", "age": 30, "wife": "Rachel"},
-        {"name": "Joey", "age": 29, "wife": None},
-        {"name": "Rachel", "age": 28, "husband": "Ross"},
+    person_list = [
+        Person(pers_data["name"], pers_data["age"]) for pers_data in people
     ]
-
-    person_list = create_person_list(people)
-    print(isinstance(person_list[0], Person))  # True
-    print(person_list[0].name == "Ross")
-    print(person_list[0].wife is person_list[2])  # True
-    print(person_list[0].wife.name)  # == "Rachel"
-
-    for person_instance in create_person_list(people):
-        print(person_instance)
+    for pers in people:
+        person_instance = Person.people.get(pers["name"])
+        if pers.get("wife"):
+            person_instance.wife = Person.people[pers["wife"]]
+        if pers.get("husband"):
+            person_instance.husband = Person.people[pers["husband"]]
+    return person_list
