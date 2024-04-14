da4c24e38a3986b21a23ee8005d07bd04d5e66ab
RuslanLevchuk <rusik.levchuk.m@gmail.com>
2024-04-14 11:39:47 +0300

    made loops refactoring

--- app/main.py
+++ app/main.py
@@ -11,20 +11,15 @@ def create_person_list(people_lst: list) -> list:
 
     friends = [Person(i["name"], i["age"]) for i in people_lst]
     # filter by woman and husband and his existing
-    for person in [
-        woman for woman in people_lst if "husband" in woman.keys()
-                                         and woman["husband"]
-    ]:
-        if person["husband"]:
+
+    for person in people_lst:
+        if person.get("husband"):
             Person.people[person["name"]].husband = (
                 Person.people.get(person["husband"])
             )
-    # filter by man and wife and her existing
-    for person in [
-        man for man in people_lst if "wife" in man.keys()
-    ]:
-        if person["wife"]:
+        if person.get("wife"):
             Person.people[person["name"]].wife = (
                 Person.people.get(person["wife"])
             )
+
     return friends
