899583eb4ebafa66a2a0219bd35170a4b10ae5cd
Tkachov Mykhailo <mtkachov435@gmail.com>
2024-04-16 13:12:34 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,35 +1,29 @@
 class Person:
     people = {}
 
-    def __init__(self, name : str, age : int) -> None:
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.people[name] = Person
+        self.people[name] = self
 
 
-def create_person_list(people: list) -> list:
+def create_person_list(people: list) -> list[Person]:
     humans = []
-    wife = []
-    husband = []
-    for index, person in enumerate(people):
-        key = person.keys()
+    for person in people:
         human = Person(person["name"], person["age"])
         humans.append(human)
-        Person.people[person["name"]] = human
-        if "wife" in key and person["wife"]:
+        if person.get("wife"):
             human.wife = person["wife"]
-            wife.append(index)
-        elif "husband" in key and person["husband"]:
+        elif person.get("husband"):
             human.husband = person["husband"]
-            husband.append(index)
     for index, human in enumerate(humans):
-        if index in wife:
+        if hasattr(human, "wife"):
             for person in humans[index:]:
                 if human.wife == person.name:
                     human.wife = person
                     person.husband = human
                     break
-        elif index in husband:
+        elif hasattr(human, "husband"):
             for person in humans[index:]:
                 if human.husband == person.name:
                     human.husband = person
