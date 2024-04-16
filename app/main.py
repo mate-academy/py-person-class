82896b3bd87c5f73cca419b606c5ec6f7da16abb
Vlad <begavlad068@gmail.com>
2024-04-16 21:29:14 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -8,13 +8,18 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_instances = [Person(person_dict["name"], person_dict["age"]) for person_dict in people]
-    [setattr(person_instances[[person_dict["name"] for person_dict in people].index(person_dict["name"])], "husband",
+    person_instances = [Person(person_dict["name"],
+                               person_dict["age"]) for person_dict in people]
+    [setattr(person_instances[[person_dict["name"] for person_dict in
+                               people].index(person_dict["name"])], "husband",
              person_instances[
-                 [person_dict["name"] for person_dict in people].index(person_dict.get("husband"))]) if person_dict.get(
+                 [person_dict["name"] for person_dict in people].index
+                 (person_dict.get("husband"))]) if person_dict.get(
         "husband") else None for person_dict in people]
-    [setattr(person_instances[[person_dict["name"] for person_dict in people].index(person_dict["name"])], "wife",
+    [setattr(person_instances[[person_dict["name"] for person_dict
+                               in people].index(person_dict["name"])], "wife",
              person_instances[
-                 [person_dict["name"] for person_dict in people].index(person_dict.get("wife"))]) if person_dict.get(
+                 [person_dict["name"] for person_dict in people].index
+                 (person_dict.get("wife"))]) if person_dict.get(
         "wife") else None for person_dict in people]
     return person_instances
