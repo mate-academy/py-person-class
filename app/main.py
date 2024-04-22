dd279c491a47a1f398bcc5d5dd42182776261627
Triukhan <tryukhan.d@gmail.com>
2024-04-22 22:59:32 +0300

    triukhan solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,39 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int | None) -> None:
+        self.name = name
+        self.age = age
+        self.people[self.name] = self
+
+    @classmethod
+    def create_or_get(cls, name: str, age: int = None) -> "Person":
+        if name in cls.people:
+            person_instance = cls.people[name]
+            person_instance.age = age
+        else:
+            person_instance = cls(name, age)
+        return person_instance
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people_data: list) -> list:
+    person_list = []
+    for person_data in people_data:
+        person_instance = Person.create_or_get(
+            person_data["name"],
+            person_data.get("age")
+        )
+
+        if person_data.get("wife"):
+            person_instance.wife = Person.create_or_get(
+                person_data["wife"]
+            )
+
+        if person_data.get("husband"):
+            person_instance.husband = Person.create_or_get(
+                person_data["husband"]
+            )
+
+        person_list.append(person_instance)
+
+    return person_list
