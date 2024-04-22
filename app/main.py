74dd55a5c28013575055de29bdff3ebfd1b3aa2f
Romanskyy Andrey <andrey.romanskyy@icloud.com>
2024-04-22 18:35:31 +0300

    Solution
    Signed-off-by: Romanskyy Andrey <andrey.romanskyy@icloud.com>

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list["Person"]:  # type: ignore
+    persons = [
+        Person(person_dict["name"], person_dict["age"])
+        for person_dict in people
+    ]
+    for person_dict in people:
+
+        person = Person.people[person_dict["name"]]
+        spouse_key = (
+            "wife"
+            if "wife" in person_dict
+            else "husband" if "husband" in person_dict else None
+        )
+        if spouse_key and person_dict[spouse_key]:
+            spouse_name = person_dict[spouse_key]
+            setattr(person, spouse_key, Person.people[spouse_name])
+
+    return persons
