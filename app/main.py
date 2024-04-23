09dafd544bc3cc8a5fa32734c7066a757d76ff14
Bohdan Bastiuk <bbastyuk@gmail.com>
2024-04-23 23:51:15 +0200

    use .get

--- app/main.py
+++ app/main.py
@@ -13,11 +13,11 @@ def create_person_list(people: list) -> list:
 
     for person_dict in people:
         current_person = Person.people.get(person_dict["name"])
-        if "wife" in person_dict and person_dict["wife"]:
+        if person_dict.get("wife"):
             current_person.wife = Person.people.get(person_dict["wife"])
             if current_person.wife:
                 current_person.wife.husband = current_person
-        if "husband" in person_dict and person_dict["husband"]:
+        if person_dict.get("husband"):
             current_person.husband = Person.people.get(person_dict["husband"])
             if current_person.husband:
                 current_person.husband.wife = current_person
