8ac9d0a5d4e205f22788f627a59768c00180d1b5
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-04-27 20:24:55 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -8,11 +8,9 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    # First, create all Person instances
     for person in people:
         Person(person["name"], person["age"])
 
-    # Second, assign wife or husband if applicable
     for person in people:
         if "wife" in person and person["wife"] is not None:
             Person.people[person["name"]].wife = Person.people[person["wife"]]
