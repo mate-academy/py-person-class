e39a42b0bdc96fef153d8b68a1d19849cbc36f06
Vova <v.kampanchenko@gmail.com>
2024-04-27 14:56:10 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ class Person:
         self.name = name
         self.age = age
         Person.people[self.name] = self
-        # Add spouse attribute
         for key, value in married.items():
             if value is not None:
                 setattr(self, key, value)
@@ -18,11 +17,9 @@ def create_person_list(people: list) -> list:
                 k: v for k, v in person.items() if k in ["wife", "husband"]
             }
         )
-
     for person in Person.people.values():
         if hasattr(person, "wife") and person.wife:
             person.wife = Person.people.get(person.wife)
         elif hasattr(person, "husband") and person.husband:
             person.husband = Person.people.get(person.husband)
-
     return list(Person.people.values())
