0b003114ed6102797df33000bbe0a14e2babfccc
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-04-28 19:06:18 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -13,10 +13,10 @@ def create_person_list(people: list) -> list:
         new_person = Person(person["name"], person["age"])
         created_people.append(new_person)
     for person in people:
-        if "wife" in person and person["wife"] is not None:
-            Person.people[person["name"]].wife = Person.people[person["wife"]]
-        elif "husband" in person and person["husband"] is not None:
-            Person.people[person["name"]].husband\
-                = Person.people[person["husband"]]
+        person_instance = Person.people[person["name"]]
+        if wife_name := person.get("wife"):
+            person_instance.wife = Person.people[wife_name]
+        elif husband_name := person.get("husband"):
+            person_instance.husband = Person.people[husband_name]
 
     return created_people
