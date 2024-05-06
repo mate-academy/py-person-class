6ac1d5140a5f0bb82e3bcb97cfcd89173aa56e15
necobra <necobrane105@gmail.com>
2024-05-06 13:54:51 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
+from __future__ import annotations
+
+
 class Person:
-    # write your code here
-    pass
+    people: dict[str, Person] = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people.update({name: self})
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list:
+    ans_list: list = []
+    for person in people:
+        person_inst = Person(
+            name=person["name"],
+            age=person["age"]
+        )
+        ans_list.append(person_inst)
+
+    for index, person in enumerate(people):
+        if person.get("wife"):
+            ans_list[index].wife = Person.people[person["wife"]]
+        elif person.get("husband"):
+            ans_list[index].husband = Person.people[person["husband"]]
+
+    return ans_list
