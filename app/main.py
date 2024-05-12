6cbafcbbccfd046094a09a9a96a483494c9462a2
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-05-12 13:56:23 +0300

    update if check

--- app/main.py
+++ app/main.py
@@ -10,9 +10,9 @@ class Person:
 def create_person_list(people: list) -> list:
     list_person = [Person(person["name"], person["age"]) for person in people]
     for person in people:
-        if "wife" in person and person["wife"] is not None:
+        if person.get("wife"):
             Person.people[person["name"]].wife = Person.people[person["wife"]]
-        if "husband" in person and person["husband"] is not None:
+        if person.get("husband"):
             Person.people[person["name"]].husband = (
                 Person.people[person["husband"]]
             )
