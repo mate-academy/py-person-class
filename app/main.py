fe419958a65eccc6886faa45ce39d1fce0fa61e4
Vova <v.kampanchenko@gmail.com>
2024-05-13 18:08:17 +0300

    Solution 2

--- app/main.py
+++ app/main.py
@@ -1,25 +1,23 @@
 class Person:
     people = {}
 
-    def __init__(self, name: str, age: int, **married) -> None:
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[self.name] = self
-        for key, value in married.items():
-            if value is not None:
-                setattr(self, key, value)
 
 
 def create_person_list(people: list) -> list:
     for person in people:
         Person(
-            person["name"], person["age"], **{
-                k: v for k, v in person.items() if k in ["wife", "husband"]
-            }
+            person["name"], person["age"]
         )
-    for person in Person.people.values():
-        if hasattr(person, "wife") and person.wife:
-            person.wife = Person.people.get(person.wife)
-        elif hasattr(person, "husband") and person.husband:
-            person.husband = Person.people.get(person.husband)
+    for person in people:
+        prsn = Person.people[person["name"]]
+        if person.get("wife"):
+            wife = Person.people[person["wife"]]
+            prsn.wife = wife
+        elif person.get("husband"):
+            husband = Person.people[person["husband"]]
+            prsn.husband = husband
     return list(Person.people.values())
