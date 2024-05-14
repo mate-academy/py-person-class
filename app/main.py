1d26c932ae88a40f52efe678151bd919b5fd09bd
necobra <necobrane105@gmail.com>
2024-05-14 19:14:10 +0300

    Solution v3

--- app/main.py
+++ app/main.py
@@ -4,16 +4,16 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people.update({name: self})
+        Person.people[name] = self
 
 
 def create_person_list(people: list[dict]) -> list:
-    ans_list = [Person(person["name"], person["age"]) for person in people]
+    person_insts = [Person(person["name"], person["age"]) for person in people]
 
     for index, person in enumerate(people):
         if person.get("wife"):
-            ans_list[index].wife = Person.people[person["wife"]]
+            person_insts[index].wife = Person.people[person["wife"]]
         elif person.get("husband"):
-            ans_list[index].husband = Person.people[person["husband"]]
+            person_insts[index].husband = Person.people[person["husband"]]
 
-    return ans_list
+    return person_insts
