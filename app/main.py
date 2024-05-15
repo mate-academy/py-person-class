702c41e9e54bfba8327051b06f90d22535503038
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2024-05-15 16:50:15 +0300

    solution to person class

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+    for person_dict in people:
+        new_person = Person(person_dict.get("name"), person_dict.get("age"))
+        if "wife" in person_dict and person_dict.get("wife"):
+            new_person.wife = person_dict.get("wife")
+        elif "husband" in person_dict and person_dict.get("husband"):
+            new_person.husband = person_dict.get("husband")
+        person_list.append(new_person)
+    for person in person_list:
+        if hasattr(person, "wife"):
+            person.wife = Person.people.get(person.wife)
+        elif hasattr(person, "husband"):
+            person.husband = Person.people.get(person.husband)
+    return person_list
