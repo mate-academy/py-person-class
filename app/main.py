1759681bf0ca977602e8522e1b9f557718160d6d
lilarin <80650911+lilarin@users.noreply.github.com>
2024-05-21 16:29:08 +0300

    Test retry

--- app/main.py
+++ app/main.py
@@ -9,11 +9,9 @@ class Person:
 
 def create_person_list(people: list) -> list:
     people_objects = [Person(person["name"], person["age"]) for person in people]
-
     for person in people:
         if person.get("wife"):
             Person.people[person["name"]].wife = Person.people[person["wife"]]
         elif person.get("husband"):
             Person.people[person["name"]].husband = Person.people[person["husband"]]
-
     return people_objects
