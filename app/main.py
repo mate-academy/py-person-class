765085f9f330497aed69154c3b2c385e685b6074
lilarin <80650911+lilarin@users.noreply.github.com>
2024-05-21 16:36:22 +0300

    Solution update (flake8 error with max length error fix)

--- app/main.py
+++ app/main.py
@@ -8,10 +8,15 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    people_objects = [Person(person["name"], person["age"]) for person in people]
+    people_objects = [
+        Person(person["name"], person["age"])
+        for person in people
+    ]
     for person in people:
         if person.get("wife"):
-            Person.people[person["name"]].wife = Person.people[person["wife"]]
+            husband_obj = Person.people[person["name"]]
+            husband_obj.wife = Person.people[person["wife"]]
         elif person.get("husband"):
-            Person.people[person["name"]].husband = Person.people[person["husband"]]
+            wife_obj = Person.people[person["name"]]
+            wife_obj.husband = Person.people[person["husband"]]
     return people_objects
