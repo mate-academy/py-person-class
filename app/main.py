bf7f906d69570835f1895f9c09a339a5b72b6dde
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-05-21 17:50:12 +0300

    add solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str = None, age: int = None, **kwargs) -> None:
+        self.name = name
+        self.age = age
+        if "wife" in kwargs and kwargs["wife"] is not None:
+            self.wife = kwargs["wife"]
+        elif "husband" in kwargs and kwargs["husband"] is not None:
+            self.husband = kwargs["husband"]
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = [
+        Person(**person) for person in people
+    ]
+    print(person_list)
+    for person in person_list:
+        if hasattr(person, "wife"):
+            person.wife = Person.people.get(person.wife)
+        elif hasattr(person, "husband"):
+            person.husband = Person.people.get(person.husband)
+
+    return person_list
