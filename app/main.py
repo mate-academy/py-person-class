d030593109fb75d987dfb1dc4a8bdd632096dfd2
Yurii Yurchenko <fuceqwertyt@gmail.com>
2024-05-22 14:34:23 +0300

    oneliner + if fix

--- app/main.py
+++ app/main.py
@@ -8,15 +8,14 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-    for person_info in people:
-        person = Person(person_info["name"], person_info["age"])
-        person_list.append(person)
-
+    person_list = [
+        Person(person_info["name"], person_info["age"])
+        for person_info in people
+    ]
     for person_info in people:
         person = Person.people[person_info["name"]]
-        if "wife" in person_info and person_info["wife"]:
+        if person_info.get("wife"):
             person.wife = Person.people[person_info["wife"]]
-        elif "husband" in person_info and person_info["husband"]:
+        elif person_info.get("husband"):
             person.husband = Person.people[person_info["husband"]]
     return person_list
