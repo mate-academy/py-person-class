7382a21fff17f20f47227f68cfd442e51e46579c
unlie9 <unlie.devops9@gmail.com>
2024-05-22 18:39:19 +0300

    solution1

--- app/main.py
+++ app/main.py
@@ -1,8 +1,33 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list) -> None:
+    person_list = []
+
+    for person_dict in people:
+        name = person_dict["name"]
+        age = person_dict["age"]
+
+        person = Person(name, age)
+        person_list.append(person)
+
+    for person_dict in people:
+        name = person_dict["name"]
+        if "wife" in person_dict and person_dict["wife"] is not None:
+            # Установка атрибута wife
+            wife_name = person_dict["wife"]
+            Person.people[name].wife = Person.people[wife_name]
+        elif "husband" in person_dict and person_dict["husband"] is not None:
+            # Установка атрибута husband
+            husband_name = person_dict["husband"]
+            Person.people[name].husband = Person.people[husband_name]
+
+    return person_list
