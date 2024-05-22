9de0af984bb722bd876ec911bf0060335e0f2217
Maksym Rozdoba <rozdobamax@gmail.com>
2024-05-22 15:42:22 +0300

    Add solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people.update({self.name: self})
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    instances = []
+    for person in people:
+        instances.append(Person(person["name"], person["age"]))
+    for index_1, person in enumerate(people):
+        for index_2, check in enumerate(people):
+            if person.get("wife") == check["name"]:
+                instances[index_1].wife = instances[index_2]
+            if person.get("husband") == check["name"]:
+                instances[index_1].husband = instances[index_2]
+    return instances
