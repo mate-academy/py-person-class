79f132704ae5b883d3477bceb69a3c2bcd4d4ce8
MaglorFeanorson <157366510+MaglorFeanorson@users.noreply.github.com>
2024-05-22 17:59:43 +0300

    "Solution"

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int, *args) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people_list: list) -> list[Person]:
+    persons = [Person(temp_person["name"], temp_person["age"])
+               for temp_person in people_list
+               ]
+
+    for person, value in zip(persons, people_list):
+        if value.get("wife"):
+            wife = Person.people.get(value["wife"])
+            if wife:
+                person.wife = wife
+        if value.get("husband"):
+            husband = Person.people.get(value["husband"])
+            if husband:
+                person.husband = husband
+    return persons
