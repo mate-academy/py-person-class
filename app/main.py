28ae22aa749c5b27df74ff8015ff7fd5c06be1f1
Yurii Yurchenko <fuceqwertyt@gmail.com>
2024-05-22 14:19:29 +0300

    atributes fix

--- app/main.py
+++ app/main.py
@@ -4,8 +4,6 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.wife = None
-        self.husband = None
         Person.people[name] = self
 
 
@@ -21,7 +19,4 @@ def create_person_list(people: list) -> list:
             person.wife = Person.people[person_info["wife"]]
         elif "husband" in person_info and person_info["husband"]:
             person.husband = Person.people[person_info["husband"]]
-        else:
-            del person.wife
-            del person.husband
     return person_list
