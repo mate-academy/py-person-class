2c8f6ed4c81caf2b31c82e26ea6db6c0b9359745
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-05-22 16:15:52 +0300

    change solution

--- app/main.py
+++ app/main.py
@@ -2,25 +2,27 @@ class Person:
 
     people = {}
 
-    def __init__(self, name: str = None, age: int = None, **kwargs) -> None:
+    def __init__(self, name: str = None, age: int = None) -> None:
         self.name = name
         self.age = age
-        if "wife" in kwargs and kwargs["wife"] is not None:
-            self.wife = kwargs["wife"]
-        elif "husband" in kwargs and kwargs["husband"] is not None:
-            self.husband = kwargs["husband"]
         Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
     person_list = [
-        Person(**person) for person in people
+        Person(
+            name=person["name"],
+            age=person["age"]
+        ) for person in people
     ]
-    print(person_list)
-    for person in person_list:
-        if hasattr(person, "wife"):
-            person.wife = Person.people.get(person.wife)
-        elif hasattr(person, "husband"):
-            person.husband = Person.people.get(person.husband)
+    for person in people:
+        if person.get("wife"):
+            Person.people.get(person["name"]).wife = (
+                Person.people.get(person["wife"])
+            )
+        elif person.get("husband"):
+            Person.people.get(person["name"]).husband = (
+                Person.people.get(person["husband"])
+            )
 
     return person_list
