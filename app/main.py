792377e7e6375d332f90dc7ba379f575aa776105
Oleh Deshko <kochaminny@gmail.com>
2024-05-23 11:28:03 +0300

    'Solution1'

--- app/main.py
+++ app/main.py
@@ -12,8 +12,8 @@ def create_person_list(people: list) -> list:
                         for person in people]
     for person in people:
         person_instance = Person.people[person["name"]]
-        if "wife" in person and person["wife"]:
+        if person.get("wife"):
             person_instance.wife = Person.people[person["wife"]]
-        if "husband" in person and person["husband"]:
+        if person.get("husband"):
             person_instance.husband = Person.people[person["husband"]]
     return person_instances
