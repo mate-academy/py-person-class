594bd6a9336d46c486d8a836d592bfdea66f6351
unlie9 <unlie.devops9@gmail.com>
2024-05-23 14:14:50 +0300

    sol2

--- app/main.py
+++ app/main.py
@@ -12,22 +12,18 @@ class Person:
 def create_person_list(people: list) -> None:
     person_list = []
 
-    for person_dict in people:
-        name = person_dict["name"]
-        age = person_dict["age"]
-
-        person = Person(name, age)
-        person_list.append(person)
+    person_list.extend([Person
+                        (person_dict["name"], person_dict["age"])
+                        for person_dict in people])
 
     for person_dict in people:
         name = person_dict["name"]
-        if "wife" in person_dict and person_dict["wife"] is not None:
-            # Установка атрибута wife
-            wife_name = person_dict["wife"]
+        wife_name = person_dict.get("wife")
+        husband_name = person_dict.get("husband")
+
+        if wife_name is not None:
             Person.people[name].wife = Person.people[wife_name]
-        elif "husband" in person_dict and person_dict["husband"] is not None:
-            # Установка атрибута husband
-            husband_name = person_dict["husband"]
+        elif husband_name is not None:
             Person.people[name].husband = Person.people[husband_name]
 
     return person_list
