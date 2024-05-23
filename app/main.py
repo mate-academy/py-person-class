210cf96b166ac38d5bd765b2b3dc5e33904ee06d
Dima <dima2000yan@gmail.com>
2024-05-23 19:07:23 +0300

    person-class solution fix

--- app/main.py
+++ app/main.py
@@ -10,12 +10,11 @@ class Person:
 def create_person_list(people: list) -> list:
     person_list = [Person(person["name"], person["age"]) for person in people]
 
-    for person in people:
-        if person.get("wife"):
-            man = Person.people[person["name"]]
-            man.wife = Person.people[person["wife"]]
-        elif person.get("husband"):
-            woman = Person.people[person["name"]]
-            woman.husband = Person.people[person["husband"]]
+    for person_dict in people:
+        person = Person.people[person_dict["name"]]
+        if person_dict.get("wife"):
+            person.wife = Person.people[person_dict["wife"]]
+        elif person_dict.get("husband"):
+            person.husband = Person.people[person_dict["husband"]]
 
     return person_list
