cd35a2be8bd125503c1313cc643567bb97689572
MaglorFeanorson <daniilrog17@gmail.com>
2024-05-24 23:13:37 +0300

    "SolutionV4"

--- app/main.py
+++ app/main.py
@@ -8,16 +8,21 @@ class Person:
 
 
 def create_person_list(people_list: list) -> list[Person]:
-    persons = [Person(temp_person["name"], temp_person["age"])
-               for temp_person in people_list]
+    persons = [
+        Person(temp_person["name"], temp_person["age"])
+        for temp_person in people_list
+    ]
 
-    for person, value in zip(persons, people_list):
-        if value.get("wife"):
-            wife = Person.people.get(value.get("wife"))
-            if wife:
-                person.wife = wife
-        if value.get("husband"):
-            husband = Person.people.get(value.get("husband"))
-            if husband:
-                person.husband = husband
+    for i in range(len(persons)):
+        person = persons[i]
+        value = people_list[i]
+
+        wife_name = value.get("wife")
+        if wife_name:
+            person.wife = Person.people.get(wife_name)
+
+        husband_name = value.get("husband")
+        if husband_name:
+            person.husband = Person.people.get(husband_name)
+
     return persons
