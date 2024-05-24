2e81292f798d1ae6fb3fe20d501e7b360bf82b2a
Aspafr77 <tfmetal77@gmail.com>
2024-05-24 18:35:51 +0300

    corrected_2

--- app/main.py
+++ app/main.py
@@ -17,11 +17,11 @@ def create_person_list(people: list) -> list:
     for person in people:
         name = person["name"]
         person_instance = Person.people[name]
-        if "wife" in person and person["wife"]:
+        if person.get("wife"):
             wife_instance = Person.people[person["wife"]]
             person_instance.wife = wife_instance
             wife_instance.husband = person_instance
-        elif "husband" in person and person["husband"]:
+        elif person.get("husband"):
             husband_instance = Person.people[person["husband"]]
             person_instance.husband = husband_instance
             husband_instance.wife = person_instance
