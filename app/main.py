95aad5678f9dcd7233a81395c29fb1f3579cf22f
Sergii Semenets <serhey.semenets@gmail.com>
2024-05-24 11:37:08 +0300

    change only .get()

--- app/main.py
+++ app/main.py
@@ -10,8 +10,8 @@ class Person:
 def create_person_list(people: list) -> list:
     person_list = [Person(person["name"], person["age"]) for person in people]
     for index, person in enumerate(people):
-        if person.get("wife") and person["wife"]:
+        if person.get("wife"):
             person_list[index].wife = Person.people[person["wife"]]
-        elif person.get("husband") and person["husband"]:
+        elif person.get("husband"):
             person_list[index].husband = Person.people[person["husband"]]
     return person_list
