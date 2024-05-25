24e867a4abd07820b7303ea2c7e0b331c59c7ac4
NikitaNikolaitsev <159700503+NikitaNikolaitsev@users.noreply.github.com>
2024-05-25 09:11:03 +0300

    Update main.py
    add list comprehension

--- app/main.py
+++ app/main.py
@@ -8,13 +8,11 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-    for person in people:
-        person_list.append(Person(person["name"], person["age"]))
+    person_list = [Person(person["name"], person["age"]) for person in people]
     for person in people:
         person_instance = Person.people[person["name"]]
-        if "wife" in person and person["wife"]:
+        if person.get("wife"):
             person_instance.wife = Person.people[person["wife"]]
-        if "husband" in person and person["husband"]:
+        if person.get("husband"):
             person_instance.husband = Person.people[person["husband"]]
     return person_list
