ddd663d3c5870639627c8cdfe879ed438d9aec8d
Vlad <vlad@example.com>
2024-05-28 20:08:21 +0300

    fix code

--- app/main.py
+++ app/main.py
@@ -10,11 +10,8 @@ class Person:
 def create_person_list(people: list) -> list:
     list_person = [Person(person["name"], person["age"]) for person in people]
     for i in range(len(people)):
-        if people[i].get("husband", people[i].get("wife")):
-            if people[i].get("husband"):
-                list_person[i].husband = Person.people[
-                    people[i].get("husband")]
-            else:
-                list_person[i].wife = Person.people[
-                    people[i].get("wife")]
+        if people[i].get("husband", 0):
+            list_person[i].husband = Person.people[people[i].get("husband")]
+        if people[i].get("wife", 0):
+            list_person[i].wife = Person.people[people[i].get("wife")]
     return list_person
