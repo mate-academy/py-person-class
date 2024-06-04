995d52286ddc41716c9035eed2a13d3103b4de19
timur <stukantimur811@gmail.com>
2024-06-04 15:40:31 +0300

    changed function create_person_list

--- app/main.py
+++ app/main.py
@@ -8,29 +8,16 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
+    person_list = [Person(person["name"], person["age"]) for person in people]
+    print(Person.people)
     for person in people:
-
-        if list(person.values())[2]:
-            if list(person.keys())[2] == "wife":
-                person_list.append(Person(person["name"], person["age"]))
-                if person["wife"] in person_list[-1].people:
-                    person_list[-1].wife \
-                        = person_list[-1].people[person["wife"]]
-                    person_list[-1].people[person["wife"]].husband \
-                        = person_list[-1]
-                else:
-                    person_list[-1].wife = None
-            else:
-                person_list.append(Person(person["name"], person["age"]))
-                if person["husband"] in person_list[-1].people:
-                    person_list[-1].husband \
-                        = person_list[-1].people[person["husband"]]
-                    person_list[-1].people[person["husband"]].wife \
-                        = person_list[-1]
-                else:
-                    person_list[-1].husband = None
-        else:
-            person_list.append(Person(person["name"], person["age"]))
+        if "wife" in person and person["wife"]:
+            if person["wife"] in Person.people:
+                Person.people[person["name"]].wife \
+                    = Person.people[person["wife"]]
+        if "husband" in person and person["husband"]:
+            if person["husband"] in Person.people:
+                Person.people[person["name"]].husband \
+                    = Person.people[person["husband"]]
 
     return person_list
