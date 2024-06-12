8c2ba33135f26b562d9ed55f82fc73bf37db7a95
Ivan <ivan.seeker6@gmail.com>
2024-06-12 09:49:24 +0300

    sol2

--- app/main.py
+++ app/main.py
@@ -1,25 +1,26 @@
+
 class Person:
     people = {}
 
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.__class__.people[name] = self
+        Person.people[name] = self
 
 
-def create_person_list(people: dict) -> list:
+def create_person_list(people: list) -> list:
     for single_person in people:
         Person(single_person["name"], single_person["age"])
 
     for single_person in people:
         person = Person.people[single_person["name"]]
-        spouse_name = single_person.get("wife") or single_person.get("husband")
-        if spouse_name:
-            spouse = Person.people[spouse_name]
-            if "wife" in single_person:
-                person.wife = spouse
-            else:
-                person.husband = spouse
-            person.spouse = spouse
+
+        wife_name = single_person.get("wife")
+        if wife_name:
+            person.wife = Person.people[wife_name]
+
+        husband_name = single_person.get("husband")
+        if husband_name:
+            person.husband = Person.people[husband_name]
 
     return list(Person.people.values())
