e0525be2ed51e18a6889adf9d80895ca3cb3b87a
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-06-14 13:12:48 +0300

    changed people_dict -> people_list

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ class Person:
 
 
 def create_person_list(people: list[dict[str, str]]) -> list[Person]:
-    people_dict = [
+    people_list = [
         Person(person["name"], int(person["age"])) for person in people
     ]
     for person_data in people:
@@ -20,4 +20,4 @@ def create_person_list(people: list[dict[str, str]]) -> list[Person]:
             Person.people[person_data.get("name")].wife = Person.people[person_data.get("wife")]
         if person_data.get("husband"):
             Person.people[person_data.get("name")].husband = Person.people[person_data.get("husband")]
-    return people_dict
+    return people_list
