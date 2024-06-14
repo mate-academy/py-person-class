37b877fd4d3722ee3b2a760a8ace6a443224419d
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-06-14 15:00:43 +0300

    іSimplyfied if statements in person_date loop

--- app/main.py
+++ app/main.py
@@ -13,13 +13,13 @@ class Person:
 
 def create_person_list(people: list[dict[str, str]]) -> list[Person]:
     people_list = [
-        Person(person["name"], int(person["age"])) for person in people
+        Person(person["name"], person["age"]) for person in people
     ]
     for person_data in people:
         if person_data.get("wife"):
-            Person.people[person_data.get("name")].wife\
-                = Person.people[person_data.get("wife")]
+            Person.people[person_data["name"]].wife\
+                = Person.people[person_data["wife"]]
         if person_data.get("husband"):
-            Person.people[person_data.get("name")].husband\
-                = Person.people[person_data.get("husband")]
+            Person.people[person_data["name"]].husband\
+                = Person.people[person_data["husband"]]
     return people_list
