43a55a5f5b20f3c356064a2b1a6f81ecdd862013
Ppllupold <kotnazar2005@gmail.com>
2024-06-14 21:41:31 +0300

    reformated

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 class Person:
-
     people = {}
 
     def __init__(self, name: str, age: int) -> None:
@@ -10,26 +9,10 @@ class Person:
 
 
 def create_person_list(people: list[dict]) -> list[Person]:
-    lst = []
-    with_wifes = []
-    with_husbands = []
-
+    person_list = [Person(dict["name"], dict["age"]) for dict in people]
     for dic in people:
         if "wife" in dic and dic["wife"]:
-            man = Person(dic["name"], dic["age"])
-            man.wife = dic["wife"]
-            with_wifes.append(man)
-            lst.append(man)
-        elif "husband" in dic and dic["husband"]:
-            man = Person(dic["name"], dic["age"])
-            man.wife = dic["husband"]
-            with_husbands.append(man)
-            lst.append(man)
-        else:
-            lst.append(Person(dic["name"], dic["age"]))
-    for man in with_wifes:
-        for woman in with_husbands:
-            if man.wife == woman.name:
-                man.wife = woman
-                woman.husband = man
-    return lst
+            Person.people[dic["name"]].wife = Person.people[dic["wife"]]
+        if "husband" in dic and dic["husband"]:
+            Person.people[dic["name"]].husband = Person.people[dic["husband"]]
+    return person_list
