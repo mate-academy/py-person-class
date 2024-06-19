10fda2d1849654705e4c016b85703211001ce0da
Obusker <mr.kudryachenko@gmail.com>
2024-06-19 16:14:53 +0300

    Commit

--- app/main.py
+++ app/main.py
@@ -11,9 +11,11 @@ def create_person_list(people: list) -> list[Person]:
     people_list = [Person(person["name"], person["age"]) for person in people]
     for checked_person in people:
         if checked_person.get("wife"):
-            Person.people[checked_person["name"]].wife\
-                = Person.people[checked_person["wife"]]
+            Person.people[checked_person["name"]].wife = (
+                Person.people[checked_person["wife"]]
+            )
         if checked_person.get("husband"):
-            Person.people[checked_person["name"]].husband\
-                = Person.people[checked_person["husband"]]
+            Person.people[checked_person["name"]].husband = (
+                Person.people[checked_person["husband"]]
+            )
     return people_list
