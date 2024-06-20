0dcc36c9d8f0741fb8772d9203dc7636f6c126f0
maksym_korniev <maksymkorniev88@gmail.com>
2024-06-20 23:17:15 +0300

    implemented realisation of person class and "create_person_list" method which returns list of persons and added persons to Person.people dict

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list[Person]:
+    add_persons_to_people(people)
+    return add_husband_and_wife(people)
+
+
+def add_persons_to_people(people: list[dict]) -> None:
+    for person in people:
+        Person.people[person["name"]] = Person(person["name"], person["age"])
+
+
+def add_husband_and_wife(people: list[dict]) -> list[Person]:
+    result = []
+    for person in people:
+        created_person = Person(person["name"], person["age"])
+        if person.get("wife") is not None:
+            created_person.wife = Person.people[person["wife"]]
+            Person.people[person["wife"]].husband = created_person
+
+        result.append(created_person)
+
+    return result
