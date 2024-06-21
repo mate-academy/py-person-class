236d5403cbe6d6cbd8236f29ce7a42c9b85d96ab
Bielichenko <ibelich12@gmail.com>
2024-06-21 18:15:21 +0300

    solution without flake8

--- app/main.py
+++ app/main.py
@@ -1,8 +1,35 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.people[name] = self
+
+    def set_person_partner(self, person: dict):
+        person_partner_type = self.get_person_partner_type(person)
+        person_partner_name = person[person_partner_type]
+
+        if person_partner_name is not None:
+            setattr(self, person_partner_type, self.people[person_partner_name])
+
+    @staticmethod
+    def get_person_partner_type(person: dict) -> str:
+        for key in person.keys():
+            if key == "wife" or key == "husband":
+                return key
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    people_inst_list = []
+
+    for person in people:
+        person_inst = Person(person["name"], person["age"])
+        people_inst_list.append(person_inst)
+
+    for i in range(len(people_inst_list)):
+        person_inst = people_inst_list[i]
+        person_inst.set_person_partner(people[i])
+
+    return people_inst_list
+
