d4ea5c134d691d3f424376a1c3e9fdfa67d5f244
NazarSerdiuk1 <nazarserdiuk260@gmail.com>
2024-06-28 14:27:10 +0300

    Solution5

--- app/main.py
+++ app/main.py
@@ -9,19 +9,18 @@ class Person:
 
 def create_person_list(people_list: list) -> list:
     [
-        Person(
-            person_dict["name"], person_dict["age"]
-        )
+        Person(person_dict["name"], person_dict["age"])
         for person_dict in people_list
     ]
 
+    # Assign spouses
     for person_dict in people_list:
         person = Person.people[person_dict["name"]]
-        if "wife" in person_dict and person_dict["wife"]:
-            person.wife = Person.people[person_dict["wife"]]
-            person.wife.husband = person
-        elif "husband" in person_dict and person_dict["husband"]:
-            person.husband = Person.people[person_dict["husband"]]
-            person.husband.wife = person
+        if spouse_name := person_dict.get("wife"):
+            person.wife = spouse = Person.people[spouse_name]
+            spouse.husband = person
+        elif spouse_name := person_dict.get("husband"):
+            person.husband = spouse = Person.people[spouse_name]
+            spouse.wife = person
 
     return list(Person.people.values())
