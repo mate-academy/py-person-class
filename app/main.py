191c25a5e3dc64560f6f08791fe5548f7f9eb6ab
NazarSerdiuk1 <nazarserdiuk260@gmail.com>
2024-06-28 14:43:19 +0300

    Solution7

--- app/main.py
+++ app/main.py
@@ -13,7 +13,6 @@ def create_person_list(people_list: list) -> list:
         for person_dict in people_list
     ]
 
-
     for person_dict in people_list:
         person = Person.people[person_dict["name"]]
         if spouse_name := person_dict.get("wife"):
