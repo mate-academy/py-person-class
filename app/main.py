7457b2f3e47f59edc75b9517b129deb5a17ed0c5
Bielichenko <ibelich12@gmail.com>
2024-07-03 20:04:07 +0300

    added fixes

--- app/main.py
+++ app/main.py
@@ -4,35 +4,17 @@ class Person:
     def __init__(self, name: str, age: str) -> None:
         self.name = name
         self.age = age
-        self.people[name] = self
-
-    def set_person_partner(self, person: dict) -> None:
-        person_partner_type = self.get_person_partner_type(person)
-        person_partner_name = person[person_partner_type]
-
-        if person_partner_name is not None:
-            setattr(
-                self,
-                person_partner_type,
-                self.people[person_partner_name]
-            )
-
-    @staticmethod
-    def get_person_partner_type(person: dict) -> str:
-        for key in person.keys():
-            if key == "wife" or key == "husband":
-                return key
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    people_inst_list = []
+    people_insts = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
-        person_inst = Person(person["name"], person["age"])
-        people_inst_list.append(person_inst)
+        person_inst = Person.people[person["name"]]
+        if person.get("wife"):
+            person_inst.wife = Person.people[person["wife"]]
+        if person.get("husband"):
+            person_inst.husband = Person.people[person["husband"]]
 
-    for i in range(len(people_inst_list)):
-        person_inst = people_inst_list[i]
-        person_inst.set_person_partner(people[i])
-
-    return people_inst_list
+    return people_insts
