2fcf404a1a481f240e6ac23696bf6dfd808427b8
Vitalii <dumavb777@gmail.com>
2024-07-09 13:19:34 +0300

    fix comment

--- app/main.py
+++ app/main.py
@@ -4,15 +4,7 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.wife = {}
-        self.husband = {}
         self.people[self.name] = self
-
-
-def remove_atr(instance: Person, key: str) -> None:
-    if (hasattr(instance, key)
-            and not isinstance(getattr(instance, key), Person)):
-        delattr(instance, key)
 
 
 def set_atr(key: str, item: dict, keys: list) -> None:
@@ -32,8 +24,5 @@ def create_person_list(people: list) -> list:
         keys = item.keys()
         set_atr("wife", item, keys)
         set_atr("husband", item, keys)
-        for instance in people_instances:
-            remove_atr(instance, "wife")
-            remove_atr(instance, "husband")
 
     return people_instances
