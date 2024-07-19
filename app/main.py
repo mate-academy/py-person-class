6a24453c153abd4c30f7cff82eca652d0b62caba
Roman Potykevych <rutkoroman@gmail.com>
2024-07-19 17:01:17 +0300

    fix comparison to None

--- app/main.py
+++ app/main.py
@@ -16,10 +16,10 @@ def create_person_list(people: list) -> list:
         person_list.append(created_person)
     for person in people:
         person_index = people.index(person)
-        if "husband" in person.keys() and person["husband"] != None:
+        if "husband" in person.keys() and person["husband"] is not None:
             husband_name = person["husband"]
             person_list[person_index].husband = Person.people[husband_name]
-        if "wife" in person.keys() and person["wife"] != None:
+        if "wife" in person.keys() and person["wife"] is not None:
             wife_name = person["wife"]
             person_list[person_index].wife = Person.people[wife_name]
     return person_list
