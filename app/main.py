674f8e6312233ca824de6a820fdf155d4c5e4d75
Roman Potykevych <rutkoroman@gmail.com>
2024-07-19 18:09:37 +0300

    add list comprehension creat_person_list func

--- app/main.py
+++ app/main.py
@@ -9,11 +9,9 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-    for person in people:
-        created_person = Person(person["name"],
-                                person["age"])
-        person_list.append(created_person)
+    person_list = [Person(person["name"], person["age"])
+                   for person in people
+                   ]
     for person in people:
         person_index = people.index(person)
         if person.get("husband"):
