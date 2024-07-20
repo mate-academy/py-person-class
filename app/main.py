f34a043e14ac68ac7912de1eb63ea7a810e8f79d
Ihor Utkin <igorutkin2002@gmail.com>
2024-07-21 01:24:49 +0200

    Resolve

--- app/main.py
+++ app/main.py
@@ -1,8 +1,40 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(
+            self,
+            name: str,
+            age: int
+    ) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    for person in people:
+        Person(person["name"], person["age"])
+
+    print(Person.people)
+
+    for person in people:
+        instance = Person.people[person["name"]]
+
+        if "wife" in person and person["wife"]:
+            instance.wife = Person.people[person["wife"]]
+        elif "husband" in person and person["husband"]:
+            instance.husband = Person.people[person["husband"]]
+
+    result = []
+
+    for person in people:
+        result.append(Person.people[person["name"]])
+
+    return result
+
+
+people = [
+    {"name": "Ross", "age": 30, "wife": "Rachel"},
+    {"name": "Joey", "age": 29, "wife": None},
+    {"name": "Rachel", "age": 28, "husband": "Ross"}
+]
