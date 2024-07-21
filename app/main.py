8d328e91b2a63912483017fd46a8b95d6588fb14
Yaroslav Havryliuk <mordegear90@gmail.com>
2024-07-21 12:06:07 +0300

    Everything works fine but now I hate this series =/

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    for pers in people:
+        if pers["name"] not in Person.people:
+            Person(pers["name"], pers["age"])
+
+    for pers in people:
+        person = Person.people[pers["name"]]
+        if "wife" in pers and pers["wife"] is not None:
+            person.wife = Person.people[pers["wife"]]
+        if "husband" in pers and pers["husband"] is not None:
+            person.husband = Person.people[pers["husband"]]
+
+    return [Person.people[pers["name"]] for pers in people]
