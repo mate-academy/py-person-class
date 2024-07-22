1e2c75500cbe4d823411533ef2c9e7d90a3658e3
Maksym Morozov <maxmorozov1991@gmail.com>
2024-07-22 23:12:02 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -11,7 +11,10 @@ def create_person_list(people: list) -> list:
     person_list = []
 
     for person_data in people:
-        person_list.append(Person(name=person_data["name"], age=person_data["age"]))
+        person_list.append(Person
+                           (name=person_data["name"],
+                            age=person_data["age"])
+                           )
 
     for person_data, person in zip(people, person_list):
         if "wife" in person_data and person_data["wife"] is not None:
