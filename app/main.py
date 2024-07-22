cb58c3c2c31b15da3f3df929cf75f2ea816baa25
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-07-22 17:25:00 +0300

    fix create_person_list

--- app/main.py
+++ app/main.py
@@ -13,8 +13,10 @@ def create_person_list(people: list) -> list:
 
     for person in people:
         if "wife" in person and person["wife"]:
-            Person.people[person["name"]].wife = Person.people[person["wife"]]
+            Person.people[person["name"]].wife = \
+                Person.people[person["wife"]]
         elif "husband" in person and person["husband"]:
-            Person.people[person["name"]].husband = Person.people[person["husband"]]
+            Person.people[person["name"]].husband = \
+                Person.people[person["husband"]]
 
     return [person for person in Person.people.values()]
