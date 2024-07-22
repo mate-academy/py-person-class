506b6979c9a9eab8ffb0667342e7422e1891830c
oleksandr-saulko <aleksandrsaulko@gmail.com>
2024-07-22 22:35:21 +0300

    develop

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    persons = []
+    if not people:
+        return persons
+
+    for i, person in enumerate(people):
+        persons.append(Person(person["name"], person["age"]))
+        if person.get("wife") and not person["wife"].startswith("None"):
+            persons[i].wife = person["wife"]
+        elif person.get("husband") and not person["husband"].startswith("None"):
+            persons[i].husband = person["husband"]
+
+    return persons
