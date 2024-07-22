6fe9b50c59e13287db6860d783482412114aeeeb
Ihor Utkin <igorutkin2002@gmail.com>
2024-07-22 13:24:34 +0200

    Resolve .2

--- app/main.py
+++ app/main.py
@@ -12,29 +12,17 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
+    result = []
     for person in people:
         Person(person["name"], person["age"])
-
-    print(Person.people)
+        result.append(Person.people[person["name"]])
 
     for person in people:
         instance = Person.people[person["name"]]
 
-        if "wife" in person and person["wife"]:
+        if person.get("wife"):
             instance.wife = Person.people[person["wife"]]
-        elif "husband" in person and person["husband"]:
+        if person.get("husband"):
             instance.husband = Person.people[person["husband"]]
 
-    result = []
-
-    for person in people:
-        result.append(Person.people[person["name"]])
-
     return result
-
-
-people = [
-    {"name": "Ross", "age": 30, "wife": "Rachel"},
-    {"name": "Joey", "age": 29, "wife": None},
-    {"name": "Rachel", "age": 28, "husband": "Ross"}
-]
