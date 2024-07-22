9f1c3217c6c0ed5cf6cfac88cf582fd0457695d7
Halyna Rohatska <rohatskahr@gmail.com>
2024-07-22 17:42:08 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-
 class Person:
 
     people = {}
@@ -8,19 +7,20 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people[self.name] = self
+
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-
     for person in people:
         Person(person["name"], person["age"])
 
+
     for person in people:
-        person_example = Person.people[person["name"]]
+        new_person = Person.people[person["name"]]
         if person.get("wife"):
-            person_example.wife = Person.people[person["wife"]]
+            new_person.wife = Person.people[person["wife"]]
         elif person.get("husband"):
-            person_example.husband = Person.people[person["husband"]]
+            new_person.husband = Person.people[person["husband"]]
 
     return list(Person.people.values())
