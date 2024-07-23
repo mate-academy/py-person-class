44f04fa487172f3c4323763125c5c0801ca6e86c
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-07-23 19:08:06 +0300

    fixed if, long string and added get

--- app/main.py
+++ app/main.py
@@ -10,17 +10,16 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
+    person_list = [
+        Person(name=one_person["name"], age=one_person["age"])
+        for one_person in people
+    ]
+
     for one_person in people:
-        person_list.append(
-            Person(name=one_person["name"], age=one_person["age"])
-        )
-    for one_person in people:
-        if "wife" in one_person and one_person["wife"] is not None:
-            Person.people[one_person["name"]].wife = \
-                Person.people[one_person["wife"]]
-        if "husband" in one_person and one_person["husband"] is not None:
-            Person.people[one_person["name"]].husband = \
-                Person.people[one_person["husband"]]
+        person_name = Person.people[one_person.get("name")]
+        if one_person.get("wife"):
+            person_name.wife = Person.people[one_person.get("wife")]
+        if one_person.get("husband"):
+            person_name.husband = Person.people[one_person.get("husband")]
 
     return person_list
