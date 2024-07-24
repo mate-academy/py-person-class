a662df3b71c7af3c2987896a925ae54452a18cb0
Denys Piuro <denys.pyuro@gmail.com>
2024-07-24 18:53:07 +0300

    Fixed 2 your comments

--- app/main.py
+++ app/main.py
@@ -4,25 +4,20 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-
         Person.people[name] = self
 
     def set_relationships(self, wife: str = None, husband: str = None) -> None:
-        if wife and wife in Person.people:
+        if wife:
             self.wife = Person.people[wife]
-        if husband and husband in Person.people:
+        if husband:
             self.husband = Person.people[husband]
 
 
 def create_person_list(people: list) -> list:
-    person_objects = []
-
-    for person_dict in people:
-        name = person_dict["name"]
-        age = person_dict["age"]
-
-        person = Person(name, age)
-        person_objects.append(person)
+    person_objects = [
+        Person(person_dict["name"], person_dict["age"])
+        for person_dict in people
+    ]
 
     for person_dict in people:
         name = person_dict["name"]
