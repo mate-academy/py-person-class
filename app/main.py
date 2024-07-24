5ac91544228b9dbdddf9b1a7aa2de5420a820380
Andrii <andrii.khamaza0@gmail.com>
2024-07-24 16:17:17 +0300

    fix mistake

--- app/main.py
+++ app/main.py
@@ -11,10 +11,14 @@ class Person:
 def create_person_list(people: list) -> list:
     res_arr = [Person(person["name"], person["age"]) for person in people]
 
-    for i, person in enumerate(people):
+    for person in people:
         if person.get("wife"):
-            res_arr[i].wife = Person.people[person["wife"]]
+            Person.people[person["name"]].wife = Person.people[
+                person["wife"]
+            ]
         if person.get("husband"):
-            res_arr[i].husband = Person.people[person["husband"]]
+            Person.people[person["name"]].husband = Person.people[
+                person["husband"]
+            ]
 
     return res_arr
