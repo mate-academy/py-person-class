048d9dcdb3fe48e41ccf09998baf8e732cf11945
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-07-25 00:07:45 +0300

    create class person and method create_person_list and return list with Person instances instead of dicts

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people_list):
+    person_instances = []
+    for person_dict in people_list:
+        person = Person(person_dict["name"], person_dict["age"])
+        person_instances.append(person)
+
+    for person_dict in people_list:
+        person = Person.people[person_dict["name"]]
+        if "wife" in person_dict and person_dict["wife"]:
+            spouse = Person.people[person_dict["wife"]]
+            person.wife = spouse
+            spouse.husband = person
+        elif "husband" in person_dict and person_dict["husband"]:
+            spouse = Person.people[person_dict["husband"]]
+            person.husband = spouse
+            spouse.wife = person
+
+    return person_instances
