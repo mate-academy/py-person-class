5b0aa2ba3cf1faa3e9ba2b894bb5a0b777b11b80
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-07-25 15:42:13 +0300

    Task 6 fix

--- app/main.py
+++ app/main.py
@@ -8,21 +8,20 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
 
-    for person_dict in people:
-        name = person_dict["name"]
-        age = person_dict["age"]
-        person = Person(name, age)
-        person_list.append(person)
+    person_list = [
+        Person(person_dict["name"],
+               person_dict["age"])
+        for person_dict in people
+    ]
 
     for person_dict in people:
         person = Person.people[person_dict["name"]]
 
-        if "wife" in person_dict and person_dict["wife"]:
-            person.wife = Person.people[person_dict["wife"]]
+        if person_dict.get("wife"):
+            person.wife = Person.people.get(person_dict["wife"])
 
-        if "husband" in person_dict and person_dict["husband"]:
-            person.husband = Person.people[person_dict["husband"]]
+        if person_dict.get("husband"):
+            person.husband = Person.people.get(person_dict["husband"])
 
     return person_list
