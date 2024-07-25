e3c16f0e805d1ab7f7fbe794c56b41428e16b542
Pasha <pavik.varganov@gmail.com>
2024-07-25 14:57:44 +0300

    Solution 2 delete wife and husband attr from __init__

--- app/main.py
+++ app/main.py
@@ -4,25 +4,22 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.wife = None
-        self.husband = None
         Person.people[name] = self
 
 
 def create_person_list(people_data: list) -> list:
-    class_list = []
+    class_list = [
+        Person(person_data["name"], person_data["age"])
+        for person_data in people_data
+    ]
+
     for person_data in people_data:
-        class_list.append(Person(person_data["name"], person_data["age"]))
+        if person_data.get("wife"):
+            Person.people[person_data["name"]].wife = \
+                Person.people[person_data["wife"]]
 
-    for i, person_data in enumerate(people_data):
-        if person_data.get("wife") is not None:
-            class_list[i].wife = Person.people[person_data["wife"]]
-        else:
-            delattr(class_list[i], "wife")
-
-        if person_data.get("husband") is not None:
-            class_list[i].husband = Person.people[person_data["husband"]]
-        else:
-            delattr(class_list[i], "husband")
+        if person_data.get("husband"):
+            Person.people[person_data["name"]].husband = \
+                Person.people[person_data["husband"]]
 
     return class_list
