ed09946b5538ee8007a184e46e4f9f20880d8ed7
Bogdan <bogdan.rylov422@gmail.com>
2024-07-27 18:31:37 +0300

    Reformat code according to mentor's requirements

--- app/main.py
+++ app/main.py
@@ -8,19 +8,20 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    people_list = [
-        Person(person["name"], person["age"])
-        for person in people
-    ]
+    people_list = []
 
     for person in people:
-        spouse = person.get("wife") or person.get("husband")
-        if spouse:
-            person_object = Person.people[person["name"]]
-            spouse_object = Person.people[spouse]
+        person_object = Person(person["name"], person["age"])
+        people_list.append(person_object)
+        spouse_name = person.get("wife") or person.get("husband")
+
+        if spouse_name and spouse_name in Person.people:
+            spouse_object = Person.people[spouse_name]
             if "wife" in person:
                 person_object.wife = spouse_object
+                spouse_object.husband = person_object
             else:
                 person_object.husband = spouse_object
+                spouse_object.wife = person_object
 
     return people_list
