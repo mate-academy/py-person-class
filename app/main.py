5b7feedd9b92d301de2938011c20fbdff14fdfdf
Vlada <vlada.belayy@mail.com>
2024-07-27 22:53:41 +0300

    last commit

--- app/main.py
+++ app/main.py
@@ -8,18 +8,20 @@ class Person:
 
 
 def create_person_list(people_list: list) -> list:
-    person_instances = [
-        Person(person["name"], person["age"])
-        for person in people_list
-    ]
+    person_instances = []
+
     for person_dict in people_list:
-        person_instance = Person.people[person_dict["name"]]
+        person_instance = Person(person_dict["name"], person_dict["age"])
+        person_instances.append(person_instance)
+
         wife_name = person_dict.get("wife")
         husband_name = person_dict.get("husband")
 
-        if wife_name:
+        if wife_name and wife_name in Person.people:
             person_instance.wife = Person.people[wife_name]
-        if husband_name:
+            Person.people[wife_name].husband = person_instance
+        if husband_name and husband_name in Person.people:
             person_instance.husband = Person.people[husband_name]
+            Person.people[husband_name].wife = person_instance
 
     return person_instances
