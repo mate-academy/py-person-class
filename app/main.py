02b47c90a364c1d395ac27ee05585d0fa8db9fb5
anastasiaaq <ndvojlenko@gmail.com>
2024-07-28 17:27:29 +0300

    Deleted print() and .keys()

--- app/main.py
+++ app/main.py
@@ -12,21 +12,19 @@ def create_person_list(people: list) -> list:
     result = []
     for person in people:
         Person(person["name"], person["age"])
-        print(person)
 
     for person in people:
         instance = Person.people[person["name"]]
 
-        if "wife" in person.keys() and person["wife"] is not None:
+        if "wife" in person and person["wife"] is not None:
             wife = Person.people[person["wife"]]
             instance.wife = wife
             wife.husband = instance
 
-        if "husband" in person.keys() and person["husband"] is not None:
+        if "husband" in person and person["husband"] is not None:
             husband = Person.people[person["husband"]]
             instance.husband = husband
             husband.wife = instance
 
-        print(instance.name)
         result.append(instance)
     return result
