925e3bba49a97cb0779df0ee9b8573506f01be5b
Illia Losiev <elijahlosiev7@gmail.com>
2024-07-29 00:06:44 +0300

    'fixed-solution'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    # First pass: Create Person objects
+    for data in people:
+        Person(data["name"], data["age"])
+
+    # Second pass: Set spouse information
+    for data in people:
+        person = Person.people[data["name"]]
+        if "wife" in data and data["wife"]:
+            wife_name = data["wife"]
+            if wife_name in Person.people:
+                person.wife = Person.people[wife_name]
+        if "husband" in data and data["husband"]:
+            husband_name = data["husband"]
+            if husband_name in Person.people:
+                person.husband = Person.people[husband_name]
+
+    return list(Person.people.values())
