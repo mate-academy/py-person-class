9b7fb1bb33f3e69fb0ce7ba57cbff30239a28387
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-07-30 07:51:06 +0300

    create class person and method create_person_list and return list with Person instances instead of dicts(add list comprehension and "get" for dict

--- app/main.py
+++ app/main.py
@@ -8,23 +8,20 @@ class Person:
 
 
 def create_person_list(people_list: list) -> list:
-    person_instances = [
-        Person(
-            person_dict["name"], person_dict["age"]
-        )
-        for person_dict in people_list
-    ]
+    person_instances = []
 
     for person_dict in people_list:
-        person = Person.people[person_dict["name"]]
-        spouse_name = person_dict.get("wife") or person_dict.get("husband")
-        if spouse_name:
-            spouse = Person.people[spouse_name]
-            if person_dict.get("wife"):
-                person.wife = spouse
-                spouse.husband = person
-            elif person_dict.get("husband"):
-                person.husband = spouse
-                spouse.wife = person
+        person_instance = Person(person_dict["name"], person_dict["age"])
+        person_instances.append(person_instance)
+
+        wife_name = person_dict.get("wife")
+        husband_name = person_dict.get("husband")
+
+        if wife_name and Person.people.get(wife_name):
+            person_instance.wife = Person.people[wife_name]
+            Person.people[wife_name].husband = person_instance
+        if husband_name and Person.people.get(husband_name):
+            person_instance.husband = Person.people[husband_name]
+            Person.people[husband_name].wife = person_instance
 
     return person_instances
