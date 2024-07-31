226201985d7d60ac2255bc1dd81208813668dc52
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-07-31 15:29:47 +0300

    use get method

--- app/main.py
+++ app/main.py
@@ -13,12 +13,12 @@ def create_person_list(people: list) -> list:
         for person in people
     ]
     for num, person in enumerate(people_list):
-        if "wife" in people[num] and people[num]["wife"]:
+        if people[num].get("wife"):
             person.wife = people[num]["wife"]
             for person_wife in people_list:
                 if person_wife.name == person.wife:
                     person.wife = person_wife
-        if "husband" in people[num] and people[num]["husband"]:
+        if people[num].get("husband"):
             person.husband = people[num]["husband"]
             for person_husband in people_list:
                 if person_husband.name == person.husband:
