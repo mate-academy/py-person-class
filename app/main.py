fef6ff3631ceb71da818b0a71135a86616039b37
Ann <grotesski@gmail.com>
2024-08-03 16:52:04 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -7,20 +7,21 @@ class Person:
         Person.people[name] = self
 
 
-people = []
-
-
-def create_person_list(people: list) -> list:
+def create_person_list(people_data: list) -> list:
     people_instances = [Person(person_data.get("name"), person_data.get("age"))
-                        for person_data in people]
+                        for person_data in people_data]
 
-    for person_data in people:
+    for person_data in people_data:
         person = Person.people.get(person_data.get("name"))
 
         if person_data.get("wife"):
-            person.wife = Person.people.get(person_data.get("wife"))
+            wife = Person.people.get(person_data.get("wife"))
+            if wife:
+                person.wife = wife
 
         if person_data.get("husband"):
-            person.husband = Person.people.get(person_data.get("husband"))
+            husband = Person.people.get(person_data.get("husband"))
+            if husband:
+                person.husband = husband
 
     return people_instances
