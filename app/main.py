91c354670fd9e415652e1edb3e8f6b9157ffd572
Serhii Chubur <serhichubur15@gmail.com>
2024-08-05 16:27:38 +0300

    idk how to solve (AttributeError: "str" object has no attribute "husband")

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    new_people_list = []
+    for person in people:
+        new_person = Person(person["name"], person["age"])
+
+        if "wife" in person and person["wife"] is not None:
+            new_person.wife = person["wife"]
+
+        if "husband" in person and person["husband"] is not None:
+            new_person.husband = person["husband"]
+
+        new_people_list.append(new_person)
+
+    return new_people_list
