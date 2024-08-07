64136d21636e5a351cd77c32fca2bee1a899de6f
Heorhii Nezbrytskyi <heorhii.nezbrytskyi@nasdaq.com>
2024-08-07 12:52:30 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,21 +1,19 @@
 class Person:
     people = {}
 
-    def __init__(self, name: str, age: int) -> "Person":
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.people[name] = self
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
+    person_list = [Person(person["name"], person["age"]) for person in people]
     for person in people:
-        Person(person["name"], person["age"])
-    for person in people:
-        if "wife" in person and person["wife"] is not None:
-            Person.people[person["name"]].wife = (
-                Person.people)[person["wife"]]
+        persons = Person.people
+        if person.get("wife") is not None:
+            persons[person["name"]].wife = persons[person["wife"]]
             continue
-        if "husband" in person and person["husband"] is not None:
-            Person.people[person["name"]].husband = (
-                Person.people)[person["husband"]]
-    return list(Person.people.values())
+        if person.get("husband") is not None:
+            persons[person["name"]].husband = persons[person["husband"]]
+    return person_list
