5a87dcc333f051fa3c799f37ee2d7177f32ee86f
Ekaterina <sudarkova.ekaterina@gmail.com>
2024-08-13 02:44:07 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,8 +4,6 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.wife = None
-        self.husband = None
         Person.people[name] = self
 
 
@@ -13,16 +11,10 @@ def create_person_list(people: list) -> list:
     for person_info in people:
         Person(person_info["name"], person_info["age"])
     for person_info in people:
-        person = Person.people[person_info["name"]]
-        if person_info.get("wife") in (None, "None"):
-            if hasattr(person, "wife"):
-                delattr(person, "wife")
-        elif person_info.get("wife") not in (None, "None"):
-            person.wife = Person.people[person_info["wife"]]
+        person_instance = Person.people[person_info["name"]]
+        if "wife" in person_info and person_info["wife"]:
+            person_instance.wife = Person.people[person_info["wife"]]
+        if "husband" in person_info and person_info["husband"]:
+            person_instance.husband = Person.people[person_info["husband"]]
 
-        if person_info.get("husband") in (None, "None"):
-            if hasattr(person, "husband"):
-                delattr(person, "husband")
-        elif person_info.get("husband") not in (None, "None"):
-            person.husband = Person.people[person_info["husband"]]
     return list(Person.people.values())
