894c4a20ebb13f4b16567b04ad8b7f7c26dd9028
Arthur <avers.target@gmail.com>
2024-08-14 16:28:14 +0200

    Revert "Solution v1"
    This reverts commit 0119d77cf9dbaadaca16e510bc0378edadb91093.

--- app/main.py
+++ app/main.py
@@ -1,25 +1,8 @@
 class Person:
-    people = {}
-
-    def __init__(self, name: str, age: int) -> None:
-        self.name = name
-        self.age = age
-        Person.people[name] = self
+    # write your code here
+    pass
 
 
 def create_person_list(people: list) -> list:
-    person_instances = []
-
-    for person_dict in people:
-        person = Person(name=person_dict["name"], age=person_dict["age"])
-        person_instances.append(person)
-
-    for person_dict in people:
-        person = Person.people[person_dict["name"]]
-        if "husband" in person_dict and person_dict["husband"]:
-            person.husband = Person.people[person_dict["husband"]]
-
-        if "wife" in person_dict and person_dict["wife"]:
-            person.wife = Person.people[person_dict["wife"]]
-
-    return person_instances
+    # write your code here
+    pass
