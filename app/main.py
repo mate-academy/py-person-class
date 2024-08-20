d1ce0300fd6486e2248239bbac517aa2e0124fb2
Nikita Ponomarov <wertyciti29@gmail.com>
2024-08-20 16:47:09 +0300

    Changing the iteration approach and assignment of ‘wife’ and ‘husband’

--- app/main.py
+++ app/main.py
@@ -9,17 +9,20 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    i = 0
 
     person_list = [Person(person["name"], person["age"]) for person in people]
 
     for person in person_list:
-        wife = people[i].get("wife")
-        husband = people[i].get("husband")
-        i += 1
-        if wife:
-            person.wife = Person.people[wife]
-        elif husband:
-            person.husband = Person.people[husband]
+
+        for human in people:
+
+            if person.name == human["name"]:
+                wife = human.get("wife")
+                husband = human.get("husband")
+
+                if wife:
+                    person.wife = Person.people[wife]
+                elif husband:
+                    person.husband = Person.people[husband]
 
     return person_list
