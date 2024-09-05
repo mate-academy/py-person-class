335b4d86f9b473c170567fda6d0c4e797baca1fa
Nazar Slavych <slavuch232355@gmail.com>
2024-09-05 08:00:26 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: str) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+
+    # First, create all Person instances without wife/husband attributes
+    for person_dict in people:
+        name = person_dict["name"]
+        age = person_dict["age"]
+        person_instance = Person(name, age)
+        person_list.append(person_instance)
+
+    for person_dict in people:
+        person_instance = Person.people[person_dict["name"]]
+        if "wife" in person_dict and person_dict["wife"] is not None:
+            person_instance.wife = Person.people[person_dict["wife"]]
+        if "husband" in person_dict and person_dict["husband"] is not None:
+            person_instance.husband = Person.people[person_dict["husband"]]
+
+    return person_list
