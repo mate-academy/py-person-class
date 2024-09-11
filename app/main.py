c10c8049dadbc850c7440bfdce8be2f6d261e4f0
Maksym Lupekha <2627number2@gmail,com>
2024-09-11 13:18:16 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
 class Person:
     people: dict = {}
 
-    def __init__(
-            self,
-            name: str,
-            age: int
-    ) -> None:
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[self.name] = self
