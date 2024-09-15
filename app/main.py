eaf35fa3ccf48cf51f706ef65e3fa8138672a560
kostya.harashchuk <garashchukke@gmail.com>
2024-09-15 08:52:34 +0200

    fix flake errors

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 class Person:
     people = {}
+
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         self.people[name] = self
+
+
 def create_person_list(people: list) -> list:
     new_people_list = []
     wife = "wife"
