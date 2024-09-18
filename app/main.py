857a826bf74cb1b0099e26425b193a976359d516
Volodymyr Pankiv <ivovan4ik@gmail.com>
2024-09-18 14:50:43 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,21 +1,15 @@
-from typing import List, Dict, Union, Optional
-
-
 class Person:
-    people: Dict[str, 'Person'] = {}
 
-    def __init__(self, name: str, age: str) -> None:
-        self.name: str = name
-        self.age: str = age
-        self.wife: Optional['Person'] = None
-        self.husband: Optional['Person'] = None
+    people = {}
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
         Person.people[name] = self
 
 
-def create_person_list(
-    people: List[Dict[str, Union[str, Optional[str]]]]
-) -> List[Person]:
+def create_person_list(people):
     for person_dict in people:
         name = person_dict["name"]
         age = person_dict["age"]
