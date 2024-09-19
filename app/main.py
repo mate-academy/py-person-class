4cd8339fc0b02895db5ab63968142d7f06a1e043
Mike Shmygel <m.shmygel@gmail.com>
2024-09-19 11:21:16 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,13 +4,11 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people[self.name] = self
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-
-    for person_data in people:
-        Person(person_data["name"], person_data["age"])
+    [Person(person_data["name"], person_data["age"]) for person_data in people]
 
     for person_data in people:
         person_instance = Person.people[person_data["name"]]
