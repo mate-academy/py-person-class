541cb5b563d830aeb1a74ea7d9c4d3fb9e04eea6
Insarov Kuzma <kuzma.insarov813@gmail.com>
2024-09-22 16:37:02 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,30 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list) -> list[Person]:
+    # Creating people without relationship
+    person_list = []
+    for person_dict in people:
+        name = person_dict["name"]
+        age = person_dict["age"]
+        person_list.append(Person(name, age))
+
+    # Adding references to wives/husbands
+    for person_dict in people:
+        name = person_dict["name"]
+        current_person = Person.people[name]
+
+        if "wife" in person_dict and person_dict["wife"] is not None:
+            wife_name = person_dict["wife"]
+            current_person.wife = Person.people[wife_name]
+        elif "husband" in person_dict and person_dict["husband"] is not None:
+            husband_name = person_dict["husband"]
+            current_person.husband = Person.people[husband_name]
+
+    return person_list
