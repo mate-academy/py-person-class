72d11f7e6f0ce7b6b45bd646f68786af04978f37
Yaroslav <104159378+y-havryliv@users.noreply.github.com>
2024-09-24 16:33:35 +0300

    created class Person, and function create_person_list

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(peopl: list) -> list:
+    person_list = [Person(person["name"], person["age"]) for person in peopl]
+    for person in person_list:
+        for pers in peopl:
+            if pers.get("husband") and pers["name"] == person.name:
+                person.husband = Person.people[pers["husband"]]
+            if pers.get("wife") and pers["name"] == person.name:
+                person.wife = Person.people[pers["wife"]]
+    return person_list
