b1770b5b98d3370211708328bb942a8e32af5070
Anastasiia Mylnikova <87014295+amylnikova@users.noreply.github.com>
2024-09-24 23:29:29 +0300

    person class task solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    list_of_people = []
+    for person in people:
+        new_person = Person(person["name"], person["age"])
+        list_of_people.append(new_person)
+    for person in people:
+        current_person = Person.people[person["name"]]
+        if "wife" in person and person["wife"] is not None:
+            current_person.wife = Person.people.get(person["wife"])
+        if "husband" in person and person["husband"] is not None:
+            current_person.husband = Person.people.get(person["husband"])
+    return list_of_people
