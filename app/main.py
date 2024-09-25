631bebe08e05879fda99e7f6e27d7d9aaaf2fa1b
roman <skyfoxwork@gmail.com>
2024-09-25 15:36:02 +0300

    simplified function create_person_list()

--- app/main.py
+++ app/main.py
@@ -6,16 +6,30 @@ class Person:
         self.age = age
         Person.people[self.name] = self
 
+
 def create_person_list(people: list[dict]) -> list[Person]:
     person_list = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
         if person.get("wife"):
-            (Person.people[person.get("name")].__dict__
-             .update({"wife": Person.people[person.get("wife")]}))
+            Person.people[person.get("name")].wife \
+                = Person.people[person.get("wife")]
 
         if person.get("husband"):
-            (Person.people[person.get("name")].__dict__
-             .update({"husband": Person.people[person.get("husband")]}))
+            Person.people[person.get("name")].husband \
+                = Person.people[person.get("husband")]
 
     return person_list
+
+
+# people = [
+#     {"name": "Ross", "age": 30, "wife": "Rachel"},
+#     {"name": "Joey", "age": 29, "wife": None},
+#     {"name": "Rachel", "age": 28, "husband": "Ross"}
+# ]
+#
+# person_list = create_person_list(people)
+#
+# print(person_list)
+# for person in person_list:
+#     print(person.__dict__)
