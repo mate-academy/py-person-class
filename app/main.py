1bfdb409598b6d2be38ce73fc824b3c814dfed96
Max <mxbely98@gmail.com>
2024-09-25 13:05:52 +0300

    changed solution for one loop and changed return type

--- app/main.py
+++ app/main.py
@@ -7,12 +7,13 @@ class Person:
         Person.people[name] = self
 
 
-def create_person_list(peopl: list) -> list:
+def create_person_list(peopl: list) -> list[Person]:
     person_list = [Person(person["name"], person["age"]) for person in peopl]
-    for person in person_list:
-        for pers in peopl:
-            if pers.get("husband") and pers["name"] == person.name:
-                person.husband = Person.people[pers["husband"]]
-            if pers.get("wife") and pers["name"] == person.name:
-                person.wife = Person.people[pers["wife"]]
+    for person in peopl:
+        if person.get("husband"):
+            Person.people[person.get("name")].husband \
+                = Person.people[person.get("husband")]
+        if person.get("wife"):
+            Person.people[person.get("name")].wife \
+                = Person.people[person.get("wife")]
     return person_list
