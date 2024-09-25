9f4688e2952f83ad3c3d6bac4438c79538049f20
roman <skyfoxwork@gmail.com>
2024-09-25 13:39:59 +0300

    update __init__

--- app/main.py
+++ app/main.py
@@ -4,8 +4,7 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people.update({self.name: self})
-
+        Person.people[self.name] = self
 
 def create_person_list(people: list[dict]) -> list[Person]:
     person_list = [Person(person["name"], person["age"]) for person in people]
