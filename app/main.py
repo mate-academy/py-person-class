634ec0ca0142246282090a83046a46143e271b67
Dima Havrylenko <94nj111@gmail.com>
2024-09-27 15:17:52 +0300

    implement Person

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+    for person in people:
+        new_person = Person(person.get("name"), person.get("age"))
+        if (person.get("wife")
+                and (p_wife := Person.people.get(person.get("wife")))):
+            weddings(new_person, p_wife)
+        if (person.get("husband")
+                and (p_husband := Person.people.get(person.get("husband")))):
+            weddings(p_husband, new_person)
+        person_list.append(new_person)
+    return person_list
+
+
+def weddings(person1: Person, person2: Person) -> None:
+    person1.wife = person2
+    person2.husband = person1
