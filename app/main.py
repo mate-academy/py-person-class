baa21d1164201192637c5ed099882f7d782912fb
Yurii <yurazosia@gmail.com>
2024-09-27 17:56:08 +0300

    Use list comprehension and add people from init

--- app/main.py
+++ app/main.py
@@ -4,14 +4,11 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-
-    for person in people:
-        person_list.append(Person(person["name"], person["age"]))
-        Person.people[person["name"]] = person_list[-1]
+    person_list = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
         if person.get("wife"):
