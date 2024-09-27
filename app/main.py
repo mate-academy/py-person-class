44a79437a1f97e557458753b221c9f613755e76d
Illia <ilyachernets@gmail.com>
2024-09-27 23:00:15 +0300

    use walrus operator

--- app/main.py
+++ app/main.py
@@ -12,11 +12,10 @@ def create_person_list(people: list) -> list:
     people_list = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
-        if person.get("wife"):
+        if wife := person.get("wife"):
             Person.people[person["name"]].wife = Person.people[person["wife"]]
-        if person.get("husband"):
-            Person.people[person["name"]].husband = (
-                Person.people[person["husband"]]
-            )
+
+        if husband := person.get("husband"):
+            Person.people[person["name"]].husband = Person.people[person["husband"]]
 
     return people_list
