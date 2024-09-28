a305dd5f2939e8410533bb217c96973f9c995130
Ярослав Шевченко <yaroslav_0205@MacBook-Pro-Aroslav.local>
2024-09-28 23:07:41 +0300

    person_class

--- app/main.py
+++ app/main.py
@@ -1,8 +1,46 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self,
+                 name: str,
+                 age: int,
+                 *,
+                 partner: str = None,
+                 partner_type: str = None
+                 ) -> None:
+        self.name = name
+        self.age = age
+        self.partner_name = partner
+        self.partner_type = partner_type
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list[Person]:
+    person_list = []
+    for person in people:
+        if "wife" in person and person["wife"]:
+            partner_name = person["wife"]
+            partner_type = "wife"
+        elif "husband" in person and person["husband"]:
+            partner_name = person["husband"]
+            partner_type = "husband"
+        else:
+            partner_name = None
+            partner_type = None
+        person_instance = Person(
+            person["name"],
+            person["age"],
+            partner=partner_name,
+            partner_type=partner_type
+        )
+        person_list.append(person_instance)
+    for person in person_list:
+        if person.partner_name:
+            partner = Person.people[person.partner_name]
+            if person.partner_type == "wife":
+                person.wife = partner
+                partner.husband = person
+            elif person.partner_type == "husband":
+                person.husband = partner
+                partner.wife = person
+    return person_list
