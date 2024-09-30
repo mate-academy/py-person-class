434a816a1e0672a3a2d2dca22599ac5c0af78d0c
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-09-30 17:36:38 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    human_lst = [Person(human["name"], human["age"]) for human in people]
+
+    for human in people:
+        current_name = Person.people[human["name"]]
+        husband = human.get("husband")
+        wife = human.get("wife")
+
+        if husband:
+            current_name.husband = Person.people[husband]
+        if wife:
+            current_name.wife = Person.people[wife]
+
+    return human_lst
