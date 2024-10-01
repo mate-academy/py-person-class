9e91c2953071735e327c18385c9fa5a66cb6a0d8
Dima Havrylenko <94nj111@gmail.com>
2024-10-01 11:14:04 +0300

    implement Person v2

--- app/main.py
+++ app/main.py
@@ -7,21 +7,15 @@ class Person:
         self.age = age
         Person.people[self.name] = self
 
+    def weddings(self, other) -> None:
+        self.wife = other
+        other.husband = self
+
 
 def create_person_list(people: list) -> list:
-    person_list = []
+    [Person(person.get("name"), person.get("age")) for person in people]
     for person in people:
-        new_person = Person(person.get("name"), person.get("age"))
-        if (person.get("wife")
-                and (p_wife := Person.people.get(person.get("wife")))):
-            weddings(new_person, p_wife)
-        if (person.get("husband")
-                and (p_husband := Person.people.get(person.get("husband")))):
-            weddings(p_husband, new_person)
-        person_list.append(new_person)
-    return person_list
-
-
-def weddings(person1: Person, person2: Person) -> None:
-    person1.wife = person2
-    person2.husband = person1
+        if wife := person.get("wife"):
+            (Person.people.get(person.get("name"))
+             .weddings(Person.people.get(wife)))
+    return list(Person.people.values())
