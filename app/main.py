820115efd5d80da1623964e61cc7f1e1a1de9ff9
Evgeniya <didovichevgeniya@gmail.com>
2024-10-01 14:36:57 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -16,8 +16,8 @@ def create_person_list(people: list[dict]) -> list:
 
     for person_dict in people:
         person = Person.people[person_dict["name"]]
-        if "wife" in person_dict and person_dict["wife"]:
+        if person_dict.get("wife"):
             person.wife = Person.people.get(person_dict["wife"], None)
-        if "husband" in person_dict and person_dict["husband"]:
-            person.husband = Person.people.get(person_dict["husband"], None)
+        if person_dict.get("husband"):
+            person.husband = Person.people.get(person_dict["husband"])
     return person_instances
