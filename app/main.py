c6e37a0ce368f773638301c1ce76bebc6b9d1194
Anton <1r0nt0ny1970@gmail.com>
2024-10-01 21:06:13 +0300

    'Used_list_comprehension'

--- app/main.py
+++ app/main.py
@@ -8,18 +8,17 @@ class Person:
 
 
 def create_person_list(people: list[dict]) -> list[Person]:
-    person_instances = []
-
-    for person_data in people:
-        person = Person(person_data["name"], person_data["age"])
-        person_instances.append(person)
+    person_instances = [
+        Person(person_data["name"], person_data["age"])
+        for person_data in people
+    ]
 
     for person_data in people:
         if "wife" in person_data:
             partner_name = person_data["wife"]
             if (
-                    partner_name is not None
-                    and partner_name in Person.people
+                partner_name is not None
+                and partner_name in Person.people
             ):
                 Person.people[person_data["name"]].wife = (
                     Person.people[partner_name]
@@ -28,10 +27,11 @@ def create_person_list(people: list[dict]) -> list[Person]:
         if "husband" in person_data:
             partner_name = person_data["husband"]
             if (
-                    partner_name is not None
-                    and partner_name in Person.people
+                partner_name is not None
+                and partner_name in Person.people
             ):
                 Person.people[person_data["name"]].husband = (
                     Person.people[partner_name]
                 )
+
     return person_instances
