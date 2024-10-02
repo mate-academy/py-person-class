1f1d31f071021f1c605b4dba648e2ac309b541dc
Ihor Chaikovskyy <Altairerko@gmail.com>
2024-10-02 15:50:03 +0300

    using get()

--- app/main.py
+++ app/main.py
@@ -8,12 +8,17 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = [Person(person_dict["name"],
-                          person_dict["age"]) for person_dict in people]
-    for person_dict in people:
-        person = Person.people[person_dict["name"]]
-        if "wife" in person_dict and person_dict["wife"] is not None:
-            person.wife = Person.people[person_dict["wife"]]
-        elif "husband" in person_dict and person_dict["husband"] is not None:
-            person.husband = Person.people[person_dict["husband"]]
-    return person_list
+    person_dict = {person["name"]: Person(person["name"],
+                                          person["age"]) for person in people}
+
+    for person in people:
+        person_obj = person_dict[person["name"]]
+        wife_name = person.get("wife")
+        husband_name = person.get("husband")
+
+        if wife_name:
+            person_obj.wife = person_dict.get(wife_name)
+        if husband_name:
+            person_obj.husband = person_dict.get(husband_name)
+
+    return list(person_dict.values())
