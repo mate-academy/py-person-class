8158bd4908658ca5256e3811d8dcb7fbe8cb2dcd
Bondzik-S <bondziks@gmail.com>
2024-10-03 17:58:27 +0300

    Fixed solution

--- app/main.py
+++ app/main.py
@@ -14,10 +14,20 @@ def create_person_list(people: list[dict]) -> list[Person]:
     ]
 
     for person_data in people:
-        if "wife" in person_data and person_data["wife"] in Person.people:
-            Person.people[person_data["name"]].wife = Person.people[person_data["wife"]]
+        if (
+            "wife" in person_data
+            and person_data["wife"] in Person.people
+        ):
+            Person.people[person_data["name"]].wife = (
+                Person.people[person_data["wife"]]
+            )
 
-        if "husband" in person_data and person_data["husband"] in Person.people:
-            Person.people[person_data["name"]].husband = Person.people[person_data["husband"]]
+        if (
+            "husband" in person_data
+            and person_data["husband"] in Person.people
+        ):
+            Person.people[person_data["name"]].husband = (
+                Person.people[person_data["husband"]]
+            )
 
     return person_instances
