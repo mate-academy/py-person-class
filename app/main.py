ef3ae53094e02c3085cf833445c4c49869c348ad
Lora Evseenko <loraevso22@gmail.com>
2024-10-03 19:48:44 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -7,32 +7,25 @@ class Person:
         self.age = age
         Person.people[name] = self
 
-    def set_partner(self, partner: "Person") -> None:
-        if "husband" in partner.__dict__:
-            self.husband = partner
-            partner.wife = self
-        else:
-            self.wife = partner
-            partner.husband = self
-
 
 def create_person_list(people: list[dict]) -> list["Person"]:
-    person_instances = {}
 
-    for person_data in people:
-        name = person_data["name"]
-        age = person_data["age"]
-
-        person_instance = Person(name, age)
-        person_instances[name] = person_instance
+    person_instances = {
+        person_data["name"]: Person(person_data["name"], person_data["age"])
+        for person_data in people
+    }
 
     for person_data in people:
         name = person_data["name"]
         partner_key = "wife" if "wife" in person_data else "husband"
         partner_name = person_data.get(partner_key)
-
         if partner_name:
             partner_instance = person_instances[partner_name]
-            person_instances[name].set_partner(partner_instance)
+            setattr(person_instances[name], partner_key, partner_instance)
+            setattr(
+                partner_instance,
+                "husband" if partner_key == "wife" else "wife",
+                person_instances[name]
+            )
 
     return list(person_instances.values())
