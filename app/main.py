e4d3148403919478b7c458d3f7105e8a176aad5b
Matvii Safonov <matveysafonov2@gmail.com>
2024-10-03 18:42:00 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -13,16 +13,12 @@ def create_person_list(people_list: list) -> list:
     person_instances = [Person(person["name"],
                                person["age"]) for person in people_list]
 
+    people_dict = {p["name"]: p for p in people_list}
+
     for person in person_instances:
-        for original_person in people_list:
-            if original_person["name"] == person.name:
-                if spouse_name := (
-                        original_person.get("wife" if "wife"
-                                                      in original_person
-                                            else "husband")):
-                    if "wife" in original_person:
-                        person.wife = Person.people[spouse_name]
-                    else:
-                        person.husband = Person.people[spouse_name]
+        if original_person := people_dict.get(person.name):
+            spouse_key = "wife" if "wife" in original_person else "husband"
+            if spouse_name := original_person.get(spouse_key):
+                setattr(person, spouse_key, Person.people[spouse_name])
 
     return person_instances
