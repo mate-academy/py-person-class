a5200868b0b04581ee52767c0bf01e1ce50ba23f
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-10-03 17:07:32 +0300

    Fixed mentor's notes

--- app/main.py
+++ app/main.py
@@ -9,16 +9,16 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    human_lst = [Person(human["name"], human["age"]) for human in people]
+    persons = [Person(person["name"], person["age"]) for person in people]
 
-    for human in people:
-        current_name = Person.people[human["name"]]
-        husband = human.get("husband")
-        wife = human.get("wife")
+    for name in people:
+        current_name = Person.people[name["name"]]
+        husband = name.get("husband")
+        wife = name.get("wife")
 
         if husband:
             current_name.husband = Person.people[husband]
         if wife:
             current_name.wife = Person.people[wife]
 
-    return human_lst
+    return persons
