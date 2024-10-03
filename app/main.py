bd15d2a92e417e496df5269fa1bbe0c7f0593072
Oleksii Ivanov <oleks4335@gmail.com>
2024-10-03 23:18:30 +0200

    Fix class person solution

--- app/main.py
+++ app/main.py
@@ -9,16 +9,16 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    list_of_person = []
-    for person_data in people:
-        person_object = Person(person_data["name"], person_data["age"])
-        list_of_person.append(person_object)
+    list_of_person = [
+        Person(person_data["name"], person_data["age"])
+        for person_data in people
+    ]
 
     for person_data in people:
         person = Person.people[person_data["name"]]
-        if "wife" in person_data and person_data["wife"]:
+        if person_data.get("wife"):
             person.wife = Person.people[person_data["wife"]]
-        elif "husband" in person_data and person_data["husband"]:
+        elif person_data.get("husband"):
             person.husband = Person.people[person_data["husband"]]
 
     return list_of_person
