c135368e17726a96989ec373e46e2221846edd7e
Oleksii Ivanov <oleks4335@gmail.com>
2024-10-03 21:32:31 +0200

    Add class person solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    list_of_person = []
+    for person_data in people:
+        person_object = Person(person_data["name"], person_data["age"])
+        list_of_person.append(person_object)
+
+    for person_data in people:
+        person = Person.people[person_data["name"]]
+        if "wife" in person_data and person_data["wife"]:
+            person.wife = Person.people[person_data["wife"]]
+        elif "husband" in person_data and person_data["husband"]:
+            person.husband = Person.people[person_data["husband"]]
+
+    return list_of_person
