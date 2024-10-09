9820e6b30f56c5d088226030a4450bd5c6ae8e10
Yulia <yulia250603@gmail.com>
2024-10-09 19:34:11 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -7,25 +7,25 @@ class Person:
         Person.people[self.name] = self
 
 
-def create_person_list(people: list) -> list:
-    Person.people.clear()
-
-    person_list = []
-
-    for person_dict in people:
-        person = Person(name=person_dict["name"], age=person_dict["age"])
-        person_list.append(person)
+def create_person_list(people: list[dict[str, str | int]]) -> list[Person]:
+    person_list: list[Person] = [
+        Person(
+            name=person_dict["name"],
+            age=person_dict["age"]
+        )
+        for person_dict in people
+    ]
 
     for person_dict in people:
         person = Person.people[person_dict["name"]]
         if "wife" in person_dict and person_dict["wife"] is not None:
             person.wife = Person.people[person_dict["wife"]]
-        elif hasattr(person, "wife"):
-            delattr(person, "wife")
+        elif hasattr(person, "wife"):  # Перевіряємо атрибут wife
+            delattr(person, "wife")  # Видаляємо атрибут, якщо його немає
 
         if "husband" in person_dict and person_dict["husband"] is not None:
             person.husband = Person.people[person_dict["husband"]]
-        elif hasattr(person, "husband"):
-            delattr(person, "husband")
+        elif hasattr(person, "husband"):  # Перевіряємо атрибут husband
+            delattr(person, "husband")  # Видаляємо атрибут, якщо його немає
 
     return person_list
