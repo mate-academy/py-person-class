d708ec9af8287d29a0a5fc27c2f10470c648d314
Dmytro Kornitskyi <d.kornitskyi@ukr.net>
2024-10-10 17:53:08 +0300

    Changes

--- app/main.py
+++ app/main.py
@@ -1,37 +1,24 @@
 class Person:
     people = {}
 
-    def __init__(self, name, age) -> None:
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.wife = None
-        self.husband = None
-
         Person.people[name] = self
 
 
-def create_person_list(people_list: list) -> list:
-    for person_data in people_list:
-        name = person_data["name"]
-        age = person_data["age"]
-        Person(name=name, age=age)
+def create_person_list(people: list[dict]) -> list[Person]:
+    person_list = [
+        Person(name=person_data["name"], age=person_data["age"])
+        for person_data in people
+    ]
 
-    for person_data in people_list:
-        name = person_data["name"]
-        person = Person.people[name]
+    for person_data in people:
+        person = Person.people[person_data["name"]]
 
-        if "wife" in person_data and person_data["wife"] is not None:
-            wife_name = person_data["wife"]
-            person.wife = Person.people[wife_name]
-        else:
-            if hasattr(person, "wife"):
-                del person.wife
+        if person_data.get("wife"):
+            person.wife = Person.people[person_data["wife"]]
+        if person_data.get("husband"):
+            person.husband = Person.people[person_data["husband"]]
 
-        if "husband" in person_data and person_data["husband"] is not None:
-            husband_name = person_data["husband"]
-            person.husband = Person.people[husband_name]
-        else:
-            if hasattr(person, "husband"):
-                del person.husband
-
-    return list(Person.people.values())
+    return person_list
