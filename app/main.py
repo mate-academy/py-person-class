1087df057ebf6c8f52172b27281255d2be346012
srx <srx92@protonmail.com>
2024-10-11 17:42:54 +0300

    Solution ver. 1.1 - code fix

--- app/main.py
+++ app/main.py
@@ -8,20 +8,20 @@ class Person:
     ) -> None:
         self.name = name
         self.age = age
-        self.people[self.name] = self
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    persons = []
-    for person in people:
-        persons.append(Person(person["name"], person["age"]))
-
+    persons = [
+        Person(person["name"], person["age"])
+        for person in people
+    ]
     for person in people:
         if "wife" in person and person["wife"]:
-            wife = person["wife"]
-            Person.people[person["name"]].wife = Person.people[wife]
-        if "husband" in person and person["husband"]:
-            husband = person["husband"]
-            Person.people[person["name"]].husband = Person.people[husband]
+            wife = Person.people[person["wife"]]
+            Person.people[person["name"]].wife = wife
+        elif "husband" in person and person["husband"]:
+            husband = Person.people[person["husband"]]
+            Person.people[person["name"]].husband = husband
 
     return persons
