b188bc8ffd3dfe1f24c3a2042e59364f2cee7a20
Bohdan Mykyichuk <bogdannew@i.ua>
2024-10-13 21:59:10 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,43 +1,35 @@
-from typing import Optional, Dict
-
-
 class Person:
 
-    people: Dict[str, "Person"] = {}
+    people: dict = {}
 
-    def __init__(self, name: str,
-                 age: int,
-                 spouse_name: Optional[str] = None) -> None:
-        self.name: str = name
-        self.age: int = age
-
-        if spouse_name:
-            self.wife: Optional[Person] = None
-            self.husband: Optional[Person] = None
-            self.set_spouse(spouse_name)
-
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.spouse = None
         Person.people[name] = self
 
     def set_spouse(self, spouse_name: str) -> None:
         spouse = Person.people.get(spouse_name)
         if spouse:
-            if self.name in Person.people and hasattr(spouse, "husband"):
-                self.wife = spouse
-                spouse.husband = self
-            elif self.name in Person.people and hasattr(spouse, "wife"):
-                self.husband = spouse
-                spouse.wife = self
+            self.spouse = spouse
+            spouse.spouse = self
+            if "wife" in vars(self).keys():
+                setattr(self, "husband", spouse)
+                setattr(spouse, "wife", self)
+            else:
+                setattr(self, "wife", spouse)
+                setattr(spouse, "husband", self)
 
 
 def create_person_list(people: list) -> list:
+
     person_list = []
-    for person in people:
-        name = person["name"]
-        age = person["age"]
-        spouse_key = "wife" if "wife" in person else "husband"
-        spouse_name = person.get(spouse_key)
-
-        person_instance = Person(name, age, spouse_name)
-        person_list.append(person_instance)
-
+    for person_data in people:
+        name: str = person_data["name"]
+        age: int = person_data["age"]
+        person = Person(name, age)
+        spouse_name = person_data.get("wife") or person_data.get("husband")
+        if spouse_name:
+            person.set_spouse(spouse_name)
+        person_list.append(person)
     return person_list
