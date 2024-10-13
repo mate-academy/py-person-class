29d688bb7f3e5a72c2903175950b29eb53f2a1b4
Dmytro Hrabovskyi <dima.grabovskiy2000@gmail.com>
2024-10-13 19:21:33 +0300

    correction on lines 16 and 20

--- app/main.py
+++ app/main.py
@@ -12,11 +12,12 @@ def create_person_list(people: list) -> list:
     person_list = [Person(name=person["name"], age=person["age"])
                    for person in people]
     for person in people:
-        main_person = Person.people[person["name"]]
         if person.get("husband"):
+            main_person = Person.people[person["name"]]
             husband_person = Person.people[person["husband"]]
             main_person.husband = husband_person
         elif person.get("wife"):
+            main_person = Person.people[person["name"]]
             wife_person = Person.people[person["wife"]]
             main_person.wife = wife_person
     return person_list
