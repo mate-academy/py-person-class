3e6e3c5407b6eea5220529cc9cc7ff02537db68d
Viacheslav <4zik88@gmail.com>
2024-10-14 20:47:16 +0300

    Refactoring code after review

--- app/main.py
+++ app/main.py
@@ -14,16 +14,15 @@ def create_person_list(people: list) -> list:
         age = person_data["age"]
         person = Person(name, age)
         person_instances.append(person)
-
     for person_data in people:
         name = person_data["name"]
         person_instance = Person.people[name]
 
-        if "wife" in person_data and person_data["wife"] is not None:
-            wife_name = person_data["wife"]
-            person_instance.wife = Person.people[wife_name]
-        elif "husband" in person_data and person_data["husband"] is not None:
-            husband_name = person_data["husband"]
-            person_instance.husband = Person.people[husband_name]
+        spouse = person_data.get("wife") or person_data.get("husband")
+        if spouse:
+            if "wife" in person_data:
+                person_instance.wife = Person.people[spouse]
+            elif "husband" in person_data:
+                person_instance.husband = Person.people[spouse]
 
     return person_instances
