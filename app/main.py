9b007d4757cafc6a7b635c4dbe11d79ac210440f
Viacheslav <4zik88@gmail.com>
2024-10-14 21:28:08 +0300

    Refactoring code after review Part 3

--- app/main.py
+++ app/main.py
@@ -4,7 +4,14 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        Person.people[name] = self
+
+    @classmethod
+    def add_person(cls, person_instance: "Person") -> None:
+        cls.people[person_instance.name] = person_instance
+
+    @classmethod
+    def get_person(cls, name: str) -> "Person":
+        return cls.people.get(name)
 
 
 def create_people(people: list) -> list:
@@ -13,6 +20,7 @@ def create_people(people: list) -> list:
         name = person_data["name"]
         age = person_data["age"]
         person = Person(name, age)
+        Person.add_person(person)
         person_instances.append(person)
     return person_instances
 
@@ -20,13 +28,14 @@ def create_people(people: list) -> list:
 def set_spouses(people: list) -> None:
     for person_data in people:
         name = person_data["name"]
-        person_instance = Person.people[name]
+        person_instance = Person.get_person(name)
         spouse = person_data.get("wife") or person_data.get("husband")
         if spouse:
+            spouse_instance = Person.get_person(spouse)
             if "wife" in person_data:
-                person_instance.wife = Person.people[spouse]
+                person_instance.wife = spouse_instance
             elif "husband" in person_data:
-                person_instance.husband = Person.people[spouse]
+                person_instance.husband = spouse_instance
 
 
 def create_person_list(people: list) -> list:
