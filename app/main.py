e2e012a8c3a4d7852ab0e7aa6ae37fe138ffd4ab
Bohdan Mykyichuk <bogdannew@i.ua>
2024-10-17 18:54:03 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,36 +1,21 @@
 class Person:
-
-    people: dict = {}
+    people = {}
 
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.spouse = None
         Person.people[name] = self
-
-    def set_spouse(self, spouse_name: str) -> None:
-
-        spouse = Person.people.get(spouse_name)
-        if spouse:
-            self.spouse = spouse
-            spouse.spouse = self
-            if "wife" in vars(self).keys():
-                setattr(self, "husband", spouse)
-                setattr(spouse, "wife", self)
-            else:
-                setattr(self, "wife", spouse)
-                setattr(spouse, "husband", self)
 
 
 def create_person_list(people: list) -> list:
+    person_list = [Person(
+        person["name"], person["age"]) for person in people]
 
-    person_list = []
-    for person_data in people:
-        name: str = person_data["name"]
-        age: int = person_data["age"]
-        person = Person(name, age)
-        spouse_name = person_data.get("wife") or person_data.get("husband")
-        if spouse_name:
-            person.set_spouse(spouse_name)
-        person_list.append(person)
+    for person in people:
+        if person.get("wife"):
+            Person.people[person["name"]].wife = Person.people[person["wife"]]
+        if person.get("husband"):
+            Person.people[person["name"]].husband = (
+                Person.people[person["husband"]])
+
     return person_list
