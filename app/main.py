aa6b9b633b19a595bbf7c648205c851daad8a1e6
sviatoslavtataryn <www.t.svyatoslav55@gmail.com>
2024-10-23 19:47:01 +0200

    implement - class Person - function create_person_list()

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name, age) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people_list) -> list:
+    person_instances = []
+
+    for person in people_list:
+        person_instance = Person(person["name"], person["age"])
+        person_instances.append(person_instance)
+
+    for person in people_list:
+        if "wife" in person and person["wife"]:
+            Person.people[person["name"]].wife = Person.people[person["wife"]]
+        elif "husband" in person and person["husband"]:
+            Person.people[person["name"]].husband = Person.people[person["husband"]]
+
+    return person_instances
