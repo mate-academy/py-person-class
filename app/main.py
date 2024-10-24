bbacf4384d01602c0d868dff1cea0cbbbd68d4f8
Maksym Kryvsha <KryivshaMaxim@gmail.com>
2024-10-24 12:56:12 +0300

    everything works, but I don't know how

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.people[name] = self
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
@@ -12,8 +12,11 @@ def create_person_list(people: list) -> list:
     for person_dict in people:
         person = Person(person_dict["name"], person_dict["age"])
         person_list.append(person)
+    for person_dict in people:
         if person_dict.get("wife") is not None:
-            person.wife = person_dict["wife"]
+            Person.people[person_dict["name"]].wife = (
+                Person.people)[person_dict["wife"]]
         if person_dict.get("husband") is not None:
-            person.husband = person_dict["husband"]
+            Person.people[person_dict["name"]].husband = (
+                Person.people)[person_dict["husband"]]
     return person_list
