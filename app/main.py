9f9c56c8fdcaa486408fbec3a0d9df6e085aa033
kkkkkkkkatya <vasyabarilyuk@gmail.com>
2024-10-24 22:38:22 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,27 +1,30 @@
 class Person:
-    people = {}
+    people = {}  # Class attribute to store all instances by name
 
-    def __init__(self, name: str, age: int):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-        Person.people[name] = self
+        self.wife = None  # Initialize to None, can be set later
+        self.husband = None  # Initialize to None, can be set later
+        Person.people[name] = self  # Store instance in the class attribute
 
 
-def create_person_list(people: list) -> list:
-    person_list = []
+def create_person_list(people):
+    # Step 1: Create Person instances for each person in the input list
+    person_list = [Person(person["name"], person["age"]) for person in people]
 
-    # Create Person instances
-    for person_info in people:
-        person = Person(person_info["name"], person_info["age"])
-        person_list.append(person)
-
-    # Set spouse relationships
-    for person_info in people:
-        person = Person.people[person_info["name"]]
-        if "wife" in person_info and person_info["wife"] is not None:
-            person.wife = Person.people[person_info["wife"]]
-        elif "husband" in person_info and person_info["husband"] is not None:
-            person.husband = Person.people[person_info["husband"]]
+    # Step 2: Set wife or husband attributes where applicable
+    for person_dict in people:
+        person_instance = Person.people[person_dict["name"]]
+        if "wife" in person_dict:
+            if person_dict["wife"] is not None:
+                person_instance.wife = Person.people[person_dict["wife"]]
+            else:
+                del person_instance.wife
+        elif "husband" in person_dict:
+            if person_dict["husband"] is not None:
+                person_instance.husband = Person.people[person_dict["husband"]]
+            else:
+                del person_instance.husband
 
     return person_list
-
