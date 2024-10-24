6ef754490385e6f6547c64069d55ee4864efcb6e
Maksym Kryvsha <KryivshaMaxim@gmail.com>
2024-10-24 12:24:01 +0300

    something is wrong, but I don't know what

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        self.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+    for person_dict in people:
+        person = Person(person_dict["name"], person_dict["age"])
+        person_list.append(person)
+        if person_dict.get("wife") is not None:
+            person.wife = person_dict["wife"]
+        if person_dict.get("husband") is not None:
+            person.husband = person_dict["husband"]
+    return person_list
